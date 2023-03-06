585819f945700c1dda8bd8b3205ee5ddf3d406f4
Igor Šarčević <igor@renderedtext.com>
2023-03-06 11:05:57 +0000

    Add test dependencies for running BDD tests, cabbage, wallaby and test watching

--- mix.exs
+++ mix.exs
@@ -50,7 +50,12 @@ defmodule Operately.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+
+      # testing
+      {:cabbage, "~> 0.3.0", only: :test},
+      {:wallaby, "~> 0.30.0", runtime: false, only: :test},
+      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false}
     ]
   end
 
