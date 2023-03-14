9fd967889da30b15d2a284e0e8c9edffc0f3abc6
Igor Šarčević <igor@renderedtext.com>
2023-03-14 12:54:07 +0000

    Set up uberauth with google

--- config/config.exs
+++ config/config.exs
@@ -60,6 +60,15 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+config :ueberauth, Ueberauth,
+  providers: [
+    google: {Ueberauth.Strategy.Google, []}
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Google.OAuth,
+  client_id: {System, :get_env, ["GOOGLE_LOGIN_CLIENT_ID"]},
+  client_secret: {System, :get_env, ["GOOGLE_LOGIN_CLIENT_SECRET"]}
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
