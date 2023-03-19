6681089b95aea7dbbb8baf76d318bed6f1f6e4bc
Igor Šarčević <igor@renderedtext.com>
2023-03-19 11:09:53 +0000

    Render objective tree

--- lib/operately_web/components/objective_tree.ex
+++ lib/operately_web/components/objective_tree.ex
@@ -18,49 +18,64 @@ defmodule OperatelyWeb.ObjectiveTree do
     alias OperatelyWeb.ObjectiveTree.Tree
 
     tree = Tree.build_tree(assigns.objective, assigns.alignments)
-    objectives = Tree.to_list_preorder(tree)
 
-    assigns = %{objectives: objectives}
+    objective_children(%{nodes: tree, indent: "ml0"})
+  end
 
+  defp objective_children(%{nodes: [], indent: indent} = assigns) do
+    ""
+  end
+
+  defp objective_children(%{nodes: nodes, indent: indent} = assigns) do
     ~H"""
-      <%= for {objective, depth} <- @objectives do %>
-        <%= objective_tree_node(%{objective: objective, depth: depth}) %>
-      <% end %>
+      <div class={"relative " <> @indent}>
+        <div class="absolute -mt-4 border-l border-gray-200 top-0 bottom-8"></div>
+        <div class="absolute w-2 h-2 rounded-full bg-gray-200 top-0" style="margin-top: -7px; margin-left: -3px"></div>
+
+        <%= for node <- @nodes do %>
+          <%= objective_tree_node(%{node: node}) %>
+        <% end %>
+      </div>
     """
   end
 
-  defp objective_tree_node(%{objective: objective, depth: depth} = assigns) do
-    style = "margin-left: #{depth * 20}px"
-
+  defp objective_tree_node(%{node: node} = assigns) do
     ~H"""
-      <div style={style} class="flex items-center relative">
-        <div class="w-4">
-          <div class="border-l border-gray-300 absolute top-0 bottom-0"></div>
+    <div>
+      <%= objective_tree_objective(%{objective: @node.objective}) %>
+      <%= objective_children(%{nodes: @node.children, indent: "ml-10"}) %>
+    </div>
+    """
+  end
 
-          <div class="flex items-center -ml-1">
+  defp objective_tree_objective(%{objective: objective} = assigns) do
+    ~H"""
+      <div class="flex items-center relative z-10">
+        <div class="w-4">
+          <div class="flex items-center" style="margin-left: -1px">
             <div class="w-1 h-1 bg-gray-200 rounded-full"></div>
-            <div class="w-4 border border-gray-200"></div>
+            <div class="w-4 border-t border-gray-200"></div>
           </div>
         </div>
 
-        <div class="flex-1 objective my-1 shadow-md rounded-lg shadow bg-white">
+        <div class="flex-1 my-1 shadow-md rounded-lg shadow bg-white">
           <div class="px-2 py-1">
             <div class="flex items-center">
               <div class="w-10 mr-2">
                 <div class="flex items-center">
                   <div>
                     <img
-                        alt="Man"
-                        src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                        class="h-10 w-10 rounded object-cover"
-                        />
+                      alt="Man"
+                      src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                      class="h-10 w-10 rounded object-cover"
+                    />
                   </div>
 
                 </div>
               </div>
 
               <div class="flex-1">
-                <span><%= objective.name %></span>
+                <span><%= @objective.name %></span>
 
                 <div>
                   <p class="text-sm"><span class="text-sky-500">John Doe</span> &middot; <span class="text-sky-500">Head of Customer Success</span></p>
