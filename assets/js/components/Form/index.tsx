e963144a1a22226f6fe0f6b60226b06c8b74b3aa
Igor Šarčević <igor@renderedtext.com>
2023-04-09 10:19:29 +0200

    Fixup the form to pass the gherkin test

--- assets/js/components/Form/index.tsx
+++ assets/js/components/Form/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function SubmitButton({onClick}) {
   return (
-    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit" onClick={onClick}>Submit</button>
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit" onClick={onClick}>Save</button>
   )
 }
 
