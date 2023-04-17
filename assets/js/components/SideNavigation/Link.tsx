fbe3e5d8691efc6fbb799f78edabaf95f4524e00
Igor Šarčević <igor@renderedtext.com>
2023-04-17 12:14:25 +0000

    Add icons to the sidebar

--- assets/js/components/SideNavigation/Link.tsx
+++ assets/js/components/SideNavigation/Link.tsx
@@ -2,19 +2,28 @@ import React from 'react';
 
 import { NavLink } from 'react-router-dom';
 
-export default function Link({to, title}: {to: string, title: string}) {
-  const baseClass = "block rounded m-2 p-2 py-1 text-[18px] font-medium ";
+interface LinkProps {
+  to: string;
+  title: string | null;
+  icon: JSX.Element | null;
+}
+
+export default function Link({to, title, icon}: LinkProps) : JSX.Element {
+  const baseClass = "block rounded mx-6 p-2 py-1 text-[18px] font-medium flex gap-2";
 
   const classNameHandler = ({isActive, isPending}) => {
-    if(isActive) return baseClass + 'text-dark-base bg-brandPrimaryLight-2';
-    if(isPending) return baseClass + 'text-dark-2 bg-brandPrimaryLight-2';
+    if(isActive) return baseClass + ' text-dark-base bg-brandPrimaryLight-2';
+    if(isPending) return baseClass + ' text-dark-2 bg-brandPrimaryLight-2';
 
-    return baseClass + 'text-dark-2';
+    return baseClass + ' text-dark-2';
   };
 
   return (
     <li>
-      <NavLink to={to} className={classNameHandler}>{title}</NavLink>
+      <NavLink to={to} className={classNameHandler}>
+        <div>{icon}</div>
+        <div>{title}</div>
+      </NavLink>
     </li>
   );
 }
--- assets/js/components/SideNavigation/index.tsx
+++ assets/js/components/SideNavigation/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Link from './Link';
 import { useTranslation } from "react-i18next";
 
+import Icon from '../Icon';
+
 interface SideNavigationProps {
   isCollapsed: boolean;
   onCollapse: () => void;
@@ -13,12 +15,17 @@ function SideNavigation({ isCollapsed }: SideNavigationProps) {
   return (
     <aside className="h-screen sticky flex w-72">
       <div className="m-2 rounded bg-light-base items-strech w-full">
+        <div className="flex items-center gap-1 mb-12 mx-2 mt-4">
+          <Icon name="logo" />
+          <span className="text-lg font-bold block">Operately</span>
+        </div>
+
         <ul className={`list-none m-0 p-0 ${isCollapsed ? 'hidden' : 'block'}`}>
-          <Link to="/objectives" title={t("Objectives")} />
+          <Link to="/objectives" title={t("Objectives")} icon={<Icon name="goal" />} />
           <Link to="/tenets" title={t("Tenets")} />
           <Link to="/projects" title={t("Projects")} />
           <Link to="/kpis" title={t("KPIs")} />
-          <Link to="/groups" title={t("Groups")} />
+          <Link to="/groups" title={t("Groups")} icon={<Icon name="group" />} />
           <Link to="/people" title={t("People")} />
         </ul>
       </div>
