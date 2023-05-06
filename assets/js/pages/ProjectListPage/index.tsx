fce26761c805f77a2a954eb228d507f981940a67
Igor Šarčević <igor@renderedtext.com>
2023-05-06 20:37:33 +0000

    Generate dates

--- assets/js/pages/ProjectListPage/index.tsx
+++ assets/js/pages/ProjectListPage/index.tsx
@@ -28,6 +28,17 @@ function ListOfProjects({ projects }) {
   );
 }
 
+function daysIntoYear() {
+  return (
+    (Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) -
+      Date.UTC(date.getFullYear(), 0, 0)) /
+    24 /
+    60 /
+    60 /
+    1000
+  );
+}
+
 export function ProjectListPage() {
   const { t } = useTranslation();
   const { loading, error, data } = useProjects({});
@@ -57,57 +68,71 @@ export function ProjectListPage() {
   //   </div>
   // );
 
+  let labels: { label: string; position: number }[] = [];
+  let now = new Date();
+
+  let position = 50;
+  let date = new Date(now.getFullYear(), now.getMonth(), 1);
+  while (position > -100) {
+    labels.push({
+      label:
+        date.getMonth() > 0
+          ? date.toLocaleString("default", { month: "short" })
+          : date.getFullYear().toString(),
+      position: position,
+    });
+    date = new Date(date.getFullYear(), date.getMonth() - 1, 1);
+    position -= 100 / 12;
+  }
+
+  position = 50;
+  date = new Date(now.getFullYear(), now.getMonth() + 1, 1);
+  while (position > 200) {
+    labels.push({
+      label:
+        date.getMonth() > 0
+          ? date.toLocaleString("default", { month: "short" })
+          : date.getFullYear().toString(),
+      position: position,
+    });
+    date = new Date(date.getFullYear(), date.getMonth() + 1, 1);
+    position += 100 / 12;
+  }
+
+  // const offset = -50 - 100 * (daysIntoYear(data) / 365);
+
   return (
     <div className="bottom-0 left-0 right-0 top-20 absolute flex items-center justify-center">
-      <div className="absolute bottom-4 left-0 right-0 top-0 text-center flex items-center gap-32 justify-center">
-        <div className="flex flex-col h-full items-center justify-center gap-2">
-          <div className="border-l border-gray-700 h-full"></div>
-          <div className="">feb</div>
-        </div>
-        <div className="flex flex-col h-full items-center justify-center gap-2">
-          <div className="border-l border-gray-700 h-full"></div>
-          <div className="">mar</div>
-        </div>
-        <div className="flex flex-col h-full items-center justify-center gap-2">
-          <div className="border-l border-gray-700 h-full"></div>
-          <div className="">apr</div>
-        </div>
-        <div className="flex flex-col h-full items-center justify-center gap-2">
-          <div className="border-l border-gray-700 h-full"></div>
-          <div className="">may</div>
-        </div>
-        <div className="flex flex-col h-full items-center justify-center gap-2">
-          <div className="border-l border-gray-700 h-full"></div>
+      <div className="absolute bottom-4 left-0 right-0 top-0 text-center">
+        {labels.map((label) => (
+          <div
+            className="flex flex-col h-full items-center justify-center gap-2 w-[1px]"
+            style={{
+              position: "absolute",
+              left: `${label.position}%`,
+              top: "0px",
+            }}
+          >
+            <div className="border-l border-gray-700 h-full"></div>
+            <div className="">{label.label}</div>
+          </div>
+        ))}
+        <div className="flex flex-col h-full items-center justify-center gap-2 relative z-1">
           <div className="bg-brand-base text-new-dark-1 rounded px-2 font-semibold">
             TODAY
           </div>
-        </div>
-        <div className="flex flex-col h-full items-center justify-center gap-2">
-          <div className="border-l border-gray-700 h-full"></div>
-          <div className="">jun</div>
-        </div>
-        <div className="flex flex-col h-full items-center justify-center gap-2">
-          <div className="border-l border-gray-700 h-full"></div>
-          <div className="">jul</div>
-        </div>
-        <div className="flex flex-col h-full items-center justify-center gap-2">
-          <div className="border-l border-gray-700 h-full"></div>
-          <div className="">aug</div>
-        </div>
-        <div className="flex flex-col h-full items-center justify-center gap-2">
-          <div className="border-l border-gray-700 h-full"></div>
-          <div className="">sep</div>
+          <div className="border-l border-brand-base h-full mb-8"></div>
         </div>
 
-        <div className="bg-new-dark-2 backdrop-blur-sm border border-gray-700 py-2 px-4 rounded-lg text-left top-[250px] left-40 w-64 absolute">
+        <div className="bg-new-dark-2 backdrop-blur-sm border border-gray-700 py-1 px-4 rounded-lg text-left top-[250px] left-40 w-64 absolute">
           Zendesk Integration
         </div>
 
-        <div className="bg-new-dark-2 backdrop-blur-sm border border-gray-700 py-2 px-4 rounded-lg text-left top-[300px] left-40 w-64 absolute">
+        <div className="bg-new-dark-2 backdrop-blur-sm border border-gray-700 py-1 px-4 rounded-lg text-left top-[290px] left-40 w-64 absolute">
           Okta Support
         </div>
 
-        <div className="bg-new-dark-2 backdrop-blur-sm border border-gray-700 py-2 px-4 rounded-lg text-left top-[350px] left-32 w-[800px] absolute">
+        <div className="bg-new-dark-2 backdrop-blur-sm border border-gray-700 py-1 px-4 rounded-lg text-left top-[330px] left-32 w-[800px] absolute">
           Hire new engineers
         </div>
       </div>
