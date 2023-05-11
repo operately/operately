aa761ac5d5106b31a979e3e5849120cb86c003df
Igor Šarčević <igor@renderedtext.com>
2023-05-11 12:35:55 +0000

    Use description from the backend

--- assets/js/pages/ProjectPage/index.tsx
+++ assets/js/pages/ProjectPage/index.tsx
@@ -31,14 +31,18 @@ function About({ data }) {
   return (
     <div className="fadeIn">
       <div className="mt-12">
-        <div className="border-t border-b border-gray-700 flex items-center justify-between">
-          <div className="mt-4 flex gap-2 items-center mb-4">
-            <Avatar person={data.project.owner} />
-            <div>
-              <div className="font-bold">{data.project.owner.fullName}</div>
-              <div className="text-sm">{data.project.owner.title}</div>
+        <div className="border-t border-b border-gray-700 flex items-center justify-between py-4">
+          {data.project.owner ? (
+            <div className="flex gap-2 items-center">
+              <Avatar person={data.project.owner} />
+              <div>
+                <div className="font-bold">{data.project.owner.fullName}</div>
+                <div className="text-sm">{data.project.owner.title}</div>
+              </div>
             </div>
-          </div>
+          ) : (
+            <div></div>
+          )}
 
           <div className="text-right">
             <div>Next update scheduled for May 20st, 2023.</div>
@@ -49,30 +53,10 @@ function About({ data }) {
         </div>
       </div>
 
-      <div className="text-xl mt-8">
-        <p>
-          Software development teams are constantly striving to improve their
-          processes to deliver high-quality software faster and more
-          efficiently. The DevOps Research and Assessment (DORA) metrics are
-          widely recognized as a standard for measuring software development
-          performance. However, collecting and analyzing DORA metrics can be a
-          challenging and time-consuming task. To address this need, we propose
-          "Superpace," a software tool designed to help teams and organizations
-          improve their software development processes using the DORA metrics.
-        </p>
-
-        <p className="mt-4">
-          The primary objective of "Superpace" is to provide a comprehensive
-          view of an organization's software development processes, enabling
-          teams to identify areas for improvement and optimize their workflows.
-          The tool focuses on the four key areas of DORA metrics, which are
-          deployment frequency, lead time for changes, mean time to restore
-          (MTTR), and change failure rate. By collecting and analyzing data from
-          various sources, including code repositories, continuous
-          integration/continuous delivery (CI/CD) systems, and issue tracking
-          systems, "Superpace" will provide teams with valuable insights to make
-          informed decisions.
-        </p>
+      <div className="text-xl mt-8 flex flex-col gap-4">
+        {data.project.description.split("\n\n").map((p, i) => (
+          <p key={i}>{p}</p>
+        ))}
       </div>
 
       <div className="mt-16">
