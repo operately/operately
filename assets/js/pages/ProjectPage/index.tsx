d3716979d52bc43c82c166e05d01467e4cf6fcc5
Igor Šarčević <igor@renderedtext.com>
2023-05-11 15:41:14 +0000

    Make milestones checkable and hoverable

--- assets/js/pages/ProjectPage/index.tsx
+++ assets/js/pages/ProjectPage/index.tsx
@@ -14,11 +14,11 @@ function Milestone({ milestone }) {
     <div className="border-t border-gray-700 flex items-center justify-between">
       <div className="mt-4 flex gap-2 items-center mb-4">
         {milestone.status === "done" ? (
-          <div className="border-2 border-brand-base h-7 w-7 rounded-full flex items-center justify-center">
+          <div className="border-2 border-brand-base h-7 w-7 rounded-full flex items-center justify-center cursor-pointer">
             <Icon name="checkmark" color="brand" size="small" />{" "}
           </div>
         ) : (
-          <div className="border-2 border-gray-700 h-7 w-7 rounded-full flex items-center justify-center"></div>
+          <div className="border-2 border-gray-700 h-7 w-7 rounded-full flex items-center justify-center hover:border-brand-base cursor-pointer"></div>
         )}
         {milestone.title}
       </div>
