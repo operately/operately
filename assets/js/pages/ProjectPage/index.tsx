0e10e400218155f5d0eda2fccd21be199b47316a
Igor Šarčević <igor@renderedtext.com>
2023-06-04 19:20:38 +0000

    Colors for resources

--- assets/js/pages/ProjectPage/index.tsx
+++ assets/js/pages/ProjectPage/index.tsx
@@ -208,14 +208,16 @@ function Champion({ person }): JSX.Element {
 function KeyResources(): JSX.Element {
   return (
     <div className="grid grid-cols-3 gap-2">
-      <div className="bg-shade-1 px-4 py-3 flex items-center gap-2 rounded-lg">
-        <Icons.BrandGithub size={20} />
+      <div className="bg-shade-1 px-4 py-3 flex items-center gap-2 rounded-lg cursor-pointer">
+        <Icons.BrandGithub size={20} className="text-pink-400" />
         GitHub Repository
       </div>
 
-      <div className="bg-shade-1 px-4 py-3 flex items-center gap-2 rounded-lg">
-        <Icons.BrandSlack size={20} />
-        Slack Channel
+      <div className="bg-shade-1 px-4 py-3 flex items-center gap-2 rounded-lg cursor-pointer">
+        <div className="shrink-0">
+          <Icons.File size={20} className="text-blue-400" />
+        </div>
+        CFO Presentation
       </div>
 
       <div className="border border-shade-3 border-dashed px-4 py-3 flex items-center gap-2 rounded-lg">
