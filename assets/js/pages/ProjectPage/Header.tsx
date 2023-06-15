dcbf28ad4243ecbb3f9883647a45ee8399137473
Igor Šarčević <igor@renderedtext.com>
2023-06-15 07:32:17 +0000

    Refactor project header into small components

--- assets/js/pages/ProjectPage/Header.tsx
+++ assets/js/pages/ProjectPage/Header.tsx
@@ -11,30 +11,58 @@ interface HeaderProps {
 }
 
 export default function Header({ project }: HeaderProps): JSX.Element {
+  return (
+    <div className="pt-12 pb-8 relative">
+      <ProjectName project={project} />
+      <Contributors project={project} />
+    </div>
+  );
+}
+
+function ProjectName({ project }) {
+  return (
+    <div className="text-center text-5xl font-bold max-w-2xl mx-auto">
+      {project.name}
+    </div>
+  );
+}
+
+function Contributors({ project }) {
   const contributorsPath = `/projects/${project.id}/contributors`;
 
   return (
-    <div className="pt-12 pb-8 relative">
-      <div className="text-center text-5xl font-bold max-w-2xl mx-auto">
-        {project.name}
+    <Link to={contributorsPath}>
+      <div className="mt-4 flex items-center justify-center gap-2 cursor-pointer">
+        <ChampionAvatar champion={project.owner} />
+        <ContributorList project={project} />
+        <AddContributor />
       </div>
+    </Link>
+  );
+}
 
-      <Link
-        to={contributorsPath}
-        className="mt-4 flex items-center justify-center gap-2 cursor-pointer"
-      >
-        <div className="relative border-2 rounded-full border-yellow-400 p-0.5">
-          <Avatar person={project.owner} size={AvatarSize.Small} />
-        </div>
+function ChampionAvatar({ champion }) {
+  return (
+    <div className="relative border-2 rounded-full border-yellow-400 p-0.5">
+      <Avatar person={champion} size={AvatarSize.Small} />
+    </div>
+  );
+}
 
-        {project.contributors.map((c) => (
-          <Avatar key={c.person.id} person={c.person} size={AvatarSize.Small} />
-        ))}
+function ContributorList({ project }: { project: Project }) {
+  return (
+    <>
+      {project.contributors.map((c) => (
+        <Avatar key={c.person.id} person={c.person} size={AvatarSize.Small} />
+      ))}
+    </>
+  );
+}
 
-        <div className="border border-white-3 border-dashed rounded-full p-1 text-white-3">
-          <Icons.IconPlus />
-        </div>
-      </Link>
+function AddContributor() {
+  return (
+    <div className="border border-white-3 border-dashed rounded-full p-1 text-white-3">
+      <Icons.IconPlus />
     </div>
   );
 }
