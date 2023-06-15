89504d3f4500750a13c62d00a5c28bfe6af4dde1
Igor Šarčević <igor@renderedtext.com>
2023-06-15 07:26:14 +0000

    Bootstrap contributors page

--- assets/js/pages/ProjectPage/Header.tsx
+++ assets/js/pages/ProjectPage/Header.tsx
@@ -1,31 +1,40 @@
 import React from "react";
-import * as Icons from "tabler-icons-react";
+import * as Icons from "@tabler/icons-react";
 
-import Flare from "./Flare";
 import Avatar, { AvatarSize } from "@/components/Avatar";
 
 import { Link } from "react-router-dom";
+import { Project } from "@/graphql/Projects";
 
-export default function Header({ project }): JSX.Element {
+interface HeaderProps {
+  project: Project;
+}
+
+export default function Header({ project }: HeaderProps): JSX.Element {
+  const contributorsPath = `/projects/${project.id}/contributors`;
+
   return (
     <div className="pt-12 pb-8 relative">
       <div className="text-center text-5xl font-bold max-w-2xl mx-auto">
         {project.name}
       </div>
 
-      <div className="mt-4 flex items-center justify-center gap-2 cursor-pointer">
+      <Link
+        to={contributorsPath}
+        className="mt-4 flex items-center justify-center gap-2 cursor-pointer"
+      >
         <div className="relative border-2 rounded-full border-yellow-400 p-0.5">
           <Avatar person={project.owner} size={AvatarSize.Small} />
         </div>
 
-        {project.contributors.map((c, index: number) => (
-          <Avatar person={c.person} size={AvatarSize.Small} />
+        {project.contributors.map((c) => (
+          <Avatar key={c.person.id} person={c.person} size={AvatarSize.Small} />
         ))}
 
         <div className="border border-white-3 border-dashed rounded-full p-1 text-white-3">
-          <Icons.Plus />
+          <Icons.IconPlus />
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
