af6d8492a99455d6dfdb3c6ac4654021253241de
Igor Šarčević <igor@renderedtext.com>
2023-06-26 10:04:32 +0000

    Add favicon to browser

--- lib/operately_web.ex
+++ lib/operately_web.ex
@@ -17,7 +17,7 @@ defmodule OperatelyWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts images favicon.ico favicon.svg robots.txt)
 
   def router do
     quote do
