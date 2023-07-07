80aa9405c0b26137dd6aa71e24348616283f3959
Igor Šarčević <igor@renderedtext.com>
2023-07-07 11:39:48 +0000

    Pin other things to the home page

--- assets/js/pages/HomePage/index.tsx
+++ assets/js/pages/HomePage/index.tsx
@@ -20,9 +20,17 @@ export function HomePage() {
   const company = companyData.data.company;
 
   return (
-    <div className="max-w-5xl mx-auto mt-20">
+    <div className="max-w-5xl mx-auto mt-20 flex flex-col gap-8">
+      <div className="flex items-center justify-center">
+        <input
+          type="text"
+          className="w-1/2 rounded-lg px-4 py-2 bg-dark-3 block border border-shade-2 placeholder:text-white-2"
+          placeholder="Search projects, people, goals..."
+        />
+      </div>
+
       <div className="grid grid-cols-3 gap-4">
-        <Card colSpan={1}>
+        <Card colSpan={1} linkTo="/account">
           <div className="flex flex-col items-center justify-center h-full">
             <Avatar size="xxlarge" person={me} />
 
@@ -81,6 +89,17 @@ export function HomePage() {
             </div>
           </div>
         </Card>
+      </div>
+
+      <div className="mb-8 flex items-center justify-center text-sm gap-2">
+        <div className="font-medium flex items-center gap-2 border border-shade-3 rounded-[20px] px-3 py-1.5 cursor-pointer">
+          <Icons.IconGridPattern size={16} />
+          Edit Home Page
+        </div>
+
+        <div className="font-medium flex items-center gap-2 border border-shade-3 rounded-[20px] px-3 py-1.5 cursor-pointer">
+          <Icons.IconArrowUp size={16} /> Back to Top
+        </div>
       </div>
     </div>
   );
