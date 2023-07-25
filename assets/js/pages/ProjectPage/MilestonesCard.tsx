f2c1d207155bb26a62b838a736f05acb91796728
Igor Šarčević <igor@renderedtext.com>
2023-07-25 14:33:41 +0000

    Show milestones card with complete on the project page

--- assets/js/pages/ProjectPage/MilestonesCard.tsx
+++ assets/js/pages/ProjectPage/MilestonesCard.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import classnames from "classnames";
+
 import * as Icons from "@tabler/icons-react";
 import * as Cards from "@/components/Cards";
 import * as Milestones from "@/graphql/Projects/milestones";
@@ -45,10 +47,22 @@ function EmptyState() {
 
 function MilestonesList({ project }) {
   const milestones = Milestones.sortByDeadline(project.milestones);
+  const [pending, done] = [
+    milestones.filter((milestone) => milestone.status === "pending"),
+    milestones.filter((milestone) => milestone.status === "done"),
+  ];
+
+  const showSeparator = pending.length > 0 && done.length > 0;
 
   return (
     <div className="flex flex-col gap-2 py-2">
-      {milestones.map((milestone) => (
+      {pending.map((milestone) => (
+        <Item key={milestone.id} milestone={milestone} />
+      ))}
+
+      {showSeparator && <div className="font-extrabold pt-2 text-xs">Completed</div>}
+
+      {done.map((milestone) => (
         <Item key={milestone.id} milestone={milestone} />
       ))}
     </div>
@@ -59,8 +73,15 @@ function Item({ milestone }) {
   return (
     <div className="flex flex-col gap-0.5 text-xs">
       <div className="font-semibold line-clamp-1">{milestone.title}</div>
-      <div className="text-yellow-400/80 font-medium">
+
+      <div
+        className={classnames("flex items-center gap-1", {
+          "text-green-400/80": milestone.status === "done",
+          "text-yellow-400/80": milestone.status === "pending",
+        })}
+      >
         <FormattedTime time={milestone.deadlineAt} format="long-date" />
+        {milestone.status === "done" && <Icons.IconCheck size={14} />}
       </div>
     </div>
   );
