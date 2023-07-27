9e6a3701244d4d1a6ec9ec89660561b2a8358b68
Igor Šarčević <igor@renderedtext.com>
2023-07-27 14:13:34 +0000

    Only the champion can edit the phase, timeline, or the project health

--- assets/js/pages/ProjectPage/Timeline.tsx
+++ assets/js/pages/ProjectPage/Timeline.tsx
@@ -14,42 +14,62 @@ import * as Icons from "@tabler/icons-react";
 
 import * as ProjectIcons from "@/components/ProjectIcons";
 
+interface ContextDescriptor {
+  project: Projects.Project;
+  refetch: () => void;
+  editable: boolean;
+}
+
+const Context = React.createContext<ContextDescriptor | null>(null);
+
 export default function Timeline({ me, project, refetch }) {
+  const editable = project.champion.id === me.id;
+
   return (
-    <div className="flex items-start gap-4 mb-4">
-      <Phase me={me} project={project} />
-      <Dates me={me} project={project} refetch={refetch} />
-      <Health me={me} project={project} />
+    <Context.Provider value={{ project, refetch, editable }}>
+      <div className="flex items-start gap-4 mb-4">
+        <Phase />
+        <Dates />
+        <Health />
+      </div>
+    </Context.Provider>
+  );
+}
+
+function Label({ title }) {
+  return <div className="font-bold text-sm ml-1">{title}</div>;
+}
+
+function Health() {
+  return (
+    <div className="flex flex-col">
+      <Label title="Health" />
+      <HealthPopover />
     </div>
   );
 }
 
-function Health({ me, project }) {
+function Phase() {
   return (
     <div className="flex flex-col">
-      <div className="font-bold text-sm ml-1">Health</div>
-      <HealthPopover editable={project.champion.id === me.id} project={project} />
+      <Label title="Phase" />
+      <PhasePopover />
     </div>
   );
 }
 
-function Phase({ me, project }) {
-  return (
-    <div className="flex flex-col">
-      <div className="font-bold text-sm ml-1">Phase</div>
-      <PhasePopover editable={project.champion.id === me.id} project={project} />
-    </div>
-  );
+function ArrowRight() {
+  return <span className="mt-0.5">-&gt;</span>;
 }
 
-function Dates({ me, project, refetch }) {
+function Dates() {
   return (
     <div className="flex flex-col">
-      <div className="font-bold text-sm ml-1">Timeline</div>
+      <Label title="Timeline" />
       <div className="flex items-center">
-        <StartDate me={me} project={project} refetch={refetch} />
-        <span className="mt-0.5">-&gt;</span>
-        <DueDate me={me} project={project} refetch={refetch} />
+        <StartDate />
+        <ArrowRight />
+        <DueDate />
       </div>
     </div>
   );
@@ -59,7 +79,8 @@ function SelectBox({ children, editable }) {
   return (
     <div
       className={classnames({
-        "flex items-center gap-2 cursor-pointer rounded px-1.5 py-0.5 mt-1": true,
+        "flex items-center gap-2 rounded px-1.5 py-0.5 mt-1": true,
+        "cursor-pointer": editable,
         "hover:shadow hover:bg-white-1/[3%]": editable,
       })}
     >
@@ -83,7 +104,9 @@ function HealthTitle({ health }) {
   }
 }
 
-function HealthPopover({ project, editable }) {
+function HealthPopover() {
+  const { project, editable } = React.useContext(Context);
+
   const label = (
     <SelectBox editable={editable}>
       <div className="flex items-center gap-1 -ml-1">
@@ -92,6 +115,10 @@ function HealthPopover({ project, editable }) {
       </div>
     </SelectBox>
   );
+
+  if (!editable) {
+    return label;
+  }
 
   return (
     <Popover.Root>
@@ -118,13 +145,19 @@ function HealthPopover({ project, editable }) {
   );
 }
 
-function PhasePopover({ project, editable }) {
+function PhasePopover() {
+  const { project, editable } = React.useContext(Context);
+
   const label = (
     <SelectBox editable={editable}>
       <ProjectIcons.IconForPhase phase={project.phase} />
       <span className="capitalize font-medium text-white-1/80">{project.phase}</span>
     </SelectBox>
   );
+
+  if (!editable) {
+    return label;
+  }
 
   return (
     <Popover.Root>
@@ -156,6 +189,19 @@ function PhasePopover({ project, editable }) {
   );
 }
 
+function SelectBoxOption({ children, onClick, active }) {
+  return (
+    <div
+      className="flex justify-between items-center rounded px-1.5 py-0.5 mt-1 hover:bg-white-1/[3%] cursor-pointer"
+      onClick={onClick}
+    >
+      <div className="flex items-center gap-3">{children}</div>
+
+      {active && <Icons.IconCheck size={16} className="text-white-1" />}
+    </div>
+  );
+}
+
 function PhasePopoverOption({ phase, project }) {
   const navigate = useNavigate();
   const active = project.phase === phase;
@@ -167,16 +213,10 @@ function PhasePopoverOption({ phase, project }) {
   };
 
   return (
-    <div
-      className="flex items-center justify-between gap-8 hover:bg-shade-1 cursor-pointer px-2 py-1 rounded text-sm font-medium"
-      onClick={onClick}
-    >
-      <div className="flex items-center gap-3">
-        <ProjectIcons.IconForPhase phase={phase} />
-        <span className="capitalize">{phase}</span>
-      </div>
-      {active && <Icons.IconCheck size={16} className="text-white-1" />}
-    </div>
+    <SelectBoxOption onClick={onClick} active={active}>
+      <ProjectIcons.IconForPhase phase={phase} />
+      <span className="capitalize">{phase}</span>
+    </SelectBoxOption>
   );
 }
 
@@ -191,21 +231,15 @@ function HealthPopoverOption({ health, title, project }) {
   };
 
   return (
-    <div
-      className="flex items-center justify-between gap-8 hover:bg-shade-1 cursor-pointer px-2 py-1 rounded text-sm font-medium"
-      onClick={onClick}
-    >
-      <div className="flex items-center gap-3">
-        <ProjectIcons.IconForHealth health={health} />
-        <span className="capitalize">{title}</span>
-      </div>
-      {active && <Icons.IconCheck size={16} className="text-white-1" />}
-    </div>
+    <SelectBoxOption onClick={onClick} active={active}>
+      <ProjectIcons.IconForHealth health={health} />
+      <span className="capitalize">{title}</span>
+    </SelectBoxOption>
   );
 }
 
-function StartDate({ me, project, refetch }) {
-  const startDate = project.startedAt ? Time.parseDateWithoutTime(project.startedAt) : null;
+function StartDate() {
+  const { project, refetch, editable } = React.useContext(Context) as ContextDescriptor;
 
   const [update] = Projects.useSetProjectStartDateMutation({ onCompleted: refetch });
 
@@ -221,8 +255,8 @@ function StartDate({ me, project, refetch }) {
   return (
     <div className="flex flex-col">
       <DatePickerWithClear
-        editable={project.champion.id === me.id}
-        selected={startDate}
+        editable={editable}
+        selected={project.startedAt}
         onChange={change}
         placeholder="Start Date"
       />
@@ -230,8 +264,8 @@ function StartDate({ me, project, refetch }) {
   );
 }
 
-function DueDate({ me, project, refetch }) {
-  const dueDate = project.deadline ? Time.parseDateWithoutTime(project.deadline) : null;
+function DueDate() {
+  const { project, refetch, editable } = React.useContext(Context) as ContextDescriptor;
 
   const [update] = Projects.useSetProjectDueDateMutation({ onCompleted: refetch });
 
@@ -246,18 +280,14 @@ function DueDate({ me, project, refetch }) {
 
   return (
     <div className="flex flex-col">
-      <DatePickerWithClear
-        editable={project.champion.id === me.id}
-        selected={dueDate}
-        onChange={change}
-        placeholder="Due Date"
-      />
+      <DatePickerWithClear editable={editable} selected={project.deadline} onChange={change} placeholder="Due Date" />
     </div>
   );
 }
 
 function DatePickerWithClear({ selected, onChange, editable = true, placeholder }) {
   const [open, setOpen] = React.useState(false);
+  const selectedDate = Time.parse(selected);
 
   const handleChange = (date: Date | null) => {
     if (!editable) return;
@@ -268,13 +298,17 @@ function DatePickerWithClear({ selected, onChange, editable = true, placeholder 
 
   let value = (
     <SelectBox editable={editable}>
-      {selected ? (
-        <FormattedTime time={selected} format="short-date" />
+      {selectedDate ? (
+        <FormattedTime time={selectedDate} format="short-date" />
       ) : (
         <span className="text-white-1/60">{placeholder}</span>
       )}
     </SelectBox>
   );
+
+  if (!editable) {
+    return value;
+  }
 
   return (
     <Popover.Root open={open} onOpenChange={(state) => setOpen(state)}>
@@ -285,7 +319,7 @@ function DatePickerWithClear({ selected, onChange, editable = true, placeholder 
           <div className="p-1 bg-dark-3 rounded-lg shadow-lg border border-dark-5 mt-2">
             <DatePicker
               inline
-              selected={selected}
+              selected={selectedDate}
               onChange={handleChange}
               onInputClick={() => setOpen(true)}
               className="border-none"
