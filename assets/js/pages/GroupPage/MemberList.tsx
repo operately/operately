eeb063d8bec2956a087779b54eb9ecc487d458c6
Igor Šarčević <igor@renderedtext.com>
2023-08-13 16:37:05 +0000

    Only the avatars are clickable on the group page / member list

--- assets/js/pages/GroupPage/MemberList.tsx
+++ assets/js/pages/GroupPage/MemberList.tsx
@@ -11,17 +11,19 @@ export default function MemberList({ group }: { group: Groups.Group }) {
   const gotoGroupMembersPage = () => navigate(`/groups/${group.id}/members`);
 
   return (
-    <div
-      className="flex gap-2 items-center mb-4 cursor-pointer"
-      onClick={gotoGroupMembersPage}
-      data-test-id="group-members"
-    >
-      {group.members.map((m) => (
-        <Avatar key={m.id} person={m} />
-      ))}
+    <div>
+      <div
+        className="inline-flex gap-2 items-center mb-4 cursor-pointer"
+        onClick={gotoGroupMembersPage}
+        data-test-id="group-members"
+      >
+        {group.members.map((m) => (
+          <Avatar key={m.id} person={m} />
+        ))}
 
-      <div className="shrink-0 relative w-8 h-8 border-dashed border border-white-3 rounded-full p-0.5 text-white-3 flex items-center justify-center">
-        <Icons.IconPlus size={16} />
+        <div className="shrink-0 relative w-8 h-8 border-dashed border border-white-3 rounded-full p-0.5 text-white-3 flex items-center justify-center">
+          <Icons.IconPlus size={16} />
+        </div>
       </div>
     </div>
   );
