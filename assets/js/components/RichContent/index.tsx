2c68fdcfeef4582d170a2168207d5b12ba1023dc
Igor Šarčević <igor@renderedtext.com>
2023-08-19 11:17:53 +0200

    Include all common elements into the rich editor context (#127)

--- assets/js/components/RichContent/index.tsx
+++ assets/js/components/RichContent/index.tsx
@@ -1,15 +1,8 @@
 import React from "react";
 
-import Bold from "@tiptap/extension-bold";
-import Document from "@tiptap/extension-document";
-import Paragraph from "@tiptap/extension-paragraph";
-import Text from "@tiptap/extension-text";
 import Mention from "@tiptap/extension-mention";
-import ListItem from "@tiptap/extension-list-item";
-import BulletList from "@tiptap/extension-bullet-list";
-import OrderedList from "@tiptap/extension-ordered-list";
-import HardBreak from "@tiptap/extension-hard-break";
-import Heading from "@tiptap/extension-heading";
+import StarterKit from "@tiptap/starter-kit";
+
 import { generateHTML } from "@tiptap/html";
 
 /*
@@ -33,19 +26,7 @@ export default function RichContent({ jsonContent, className }: RichContentProps
     }
 
     const json = JSON.parse(jsonContent);
-
-    const html = generateHTML(json, [
-      Document,
-      Paragraph,
-      Text,
-      Bold,
-      Mention,
-      ListItem,
-      BulletList,
-      OrderedList,
-      HardBreak,
-      Heading,
-    ]);
+    const html = generateHTML(json, [Mention, StarterKit]);
 
     return <div className={"ProseMirror " + className} dangerouslySetInnerHTML={{ __html: html }} />;
   } catch (e) {
