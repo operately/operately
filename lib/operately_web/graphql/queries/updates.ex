22f78110d40e892258043b0b6fe6419aa3a9f9de
Igor Šarčević <igor@renderedtext.com>
2023-08-21 12:42:57 +0000

    Use updates only as the source for the feed

--- lib/operately_web/graphql/queries/updates.ex
+++ lib/operately_web/graphql/queries/updates.ex
@@ -1,7 +1,21 @@
 defmodule OperatelyWeb.GraphQL.Queries.Updates do
   use Absinthe.Schema.Notation
 
+  input_object :updates_filter do
+    field :project_id, :id
+  end
+
   object :update_queries do
+    field :updates, non_null(list_of(:update)) do
+      arg :filter, non_null(:updates_filter)
+
+      resolve fn args, _ ->
+        update = Operately.Updates.list_updates(args.filter.project_id, "project")
+
+        {:ok, update}
+      end
+    end
+
     field :update, non_null(:update) do
       arg :id, non_null(:id)
 
