d1c73e6f3256e14145a9c9448efe219d2e0ef5dc
Igor Šarčević <igor@renderedtext.com>
2023-09-03 17:58:41 +0000

    Generate graphql definition

--- lib/mix/tasks/operately.gen.update.type.ex
+++ lib/mix/tasks/operately.gen.update.type.ex
@@ -1,7 +1,13 @@
 defmodule Mix.Tasks.Operately.Gen.Update.Type do
+  #
+  # Usage example:
+  # mix operately.gen.update.type ProjectStartTimeChanged old_start_time:utc_datetime new_start_time:utc_datetime
+  #
   def run([module_name | fields]) do
     fields = parse_fields(fields)
+
     gen_type_module(module_name, fields)
+    gen_graphql_object_schema(module_name, fields)
   end
 
   def gen_type_module(module_name, fields) do
@@ -16,7 +22,7 @@ defmodule Mix.Tasks.Operately.Gen.Update.Type do
 
       @primary_key false
       embedded_schema do
-        #{Enum.map(fields, &field/1) |> Enum.join("\n    ")}
+        #{Enum.map(fields, &schema_field/1) |> Enum.join("\n    ")}
       end
 
       def changeset(attrs) do
@@ -31,7 +37,7 @@ defmodule Mix.Tasks.Operately.Gen.Update.Type do
     IO.puts(content)
   end
 
-  def field({field_name, field_type}) do
+  def schema_field({field_name, field_type}) do
     "field :#{field_name}, :#{field_type}"
   end
 
@@ -41,4 +47,35 @@ defmodule Mix.Tasks.Operately.Gen.Update.Type do
       {String.to_atom(Enum.at(field, 0)), String.to_atom(Enum.at(field, 1))}
     end)
   end
+  
+  def gen_graphql_object_schema(module_name, fields) do
+    snake_case_module_name = Macro.underscore(module_name)
+    full_object_name = "update_content_#{snake_case_module_name}"
+
+    content = """
+      object :#{full_object_name} do
+        #{Enum.map(fields, &graphql_field/1) |> indent(4)}
+      end
+    """
+
+    IO.puts(content)
+  end
+
+  def graphql_field({field_name, field_type}) do
+    """
+    field :#{field_name}, :non_null(#{field_type}) do
+      resolve fn update, _, _ ->
+        {:ok, update.content["#{field_name}"]}
+      end
+    end
+    """
+  end
+
+  def indent(string, indent) do
+    string
+    |> String.split("\n")
+    |> Enum.map(fn(line) -> String.duplicate(" ", indent) <> line end)
+    |> Enum.join("\n")
+  end
+
 end
