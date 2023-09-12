3565e8213395a5f28035d2329b7719543dbe80aa
Igor Šarčević <igor@renderedtext.com>
2023-09-12 08:07:22 +0000

    Use oban worker for update email

--- lib/operately_email/update_email.ex
+++ lib/operately_email/update_email.ex
@@ -1,4 +1,6 @@
 defmodule OperatelyEmail.UpdateEmail do
+  use Oban.Worker
+
   def perform(%{update_id: update_id}) do
     update = Operately.Updates.get_update!(update_id)
     recipients = recipients(update)
