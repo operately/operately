024a1d400a7c5ab637bb61e88043d6004ffb26b5
Igor Šarčević <igor@renderedtext.com>
2023-09-12 10:07:30 +0000

    Don't send completed milestones in the daily assignments email

---
+++ test/operately_email/assignments/loader_test.exs
@@ -0,0 +1,73 @@
+defmodule OperatelyEmail.Assignments.LoaderTest do
+  use Operately.DataCase
+
+  import Operately.ProjectsFixtures
+  import Operately.PeopleFixtures
+  import Operately.CompaniesFixtures
+
+  setup do
+    company = company_fixture()
+    champion = person_fixture(%{company_id: company.id})
+
+    project = project_fixture(%{
+      company_id: company.id,
+      creator_id: champion.id,
+    })
+
+    {:ok, %{company: company, champion: champion, project: project}}
+  end
+
+  describe "project milestones" do
+    setup ctx do
+      milestone(ctx, "Buy domain", :done, days_ago(10))
+      milestone(ctx, "Publish book", :pending, days_ago(2))
+      milestone(ctx, "Update website", :pending, days_ago(0))
+      milestone(ctx, "Gain 1000 followers", :pending, days_from_now(10))
+
+      assignments = OperatelyEmail.Assignments.Loader.load(ctx.champion)
+
+      {:ok, %{names: assignment_names(assignments)}}
+    end
+
+    test "it sends pending overdue milestones", ctx do
+      assert Enum.member?(ctx.names, "Publish book")
+    end
+
+    test "it sends pending milestones due today", ctx do
+      assert Enum.member?(ctx.names, "Update website")
+    end
+
+    test "it doesn't send already completed milestones", ctx do
+      refute Enum.member?(ctx.names, "Buy domain")
+    end
+
+    test "it doesn't send milestones due in the future", ctx do
+      refute Enum.member?(ctx.names, "Buy domain")
+    end
+  end
+
+  #
+  # Utility functions
+  #
+
+  defp milestone(ctx, title, status, deadline) do
+    milestone_fixture(ctx.champion, %{
+      title: title,
+      project_id: ctx.project.id,
+      deadline_at: deadline,
+      status: status
+    })
+  end
+
+  defp assignment_names(assignment_groups) do
+    Enum.flat_map(assignment_groups, &(&1.assignments)) |> Enum.map(&(&1.name))
+  end
+
+  defp days_ago(days) do
+    DateTime.add(DateTime.utc_now(), -days, :day)
+  end
+
+  defp days_from_now(days) do
+    DateTime.add(DateTime.utc_now(), days, :day)
+  end
+end
