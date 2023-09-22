859b276452bec70732f54a43ce24156a1e7bba39
Igor Šarčević <igor@renderedtext.com>
2023-09-22 18:18:39 +0000

    Fix all milestone comment tests

--- test/operately/comments_test.exs
+++ test/operately/comments_test.exs
@@ -6,54 +6,61 @@ defmodule Operately.CommentsTest do
   describe "milestone_comments" do
     alias Operately.Comments.MilestoneComment
 
-    import Operately.CommentsFixtures
+    import Operately.CompaniesFixtures
+    import Operately.PeopleFixtures
+    import Operately.ProjectsFixtures
 
-    @invalid_attrs %{action: nil}
+    setup do
+      company = company_fixture()
+      person = person_fixture(company_id: company.id)
+      project = project_fixture(company_id: company.id, creator_id: person.id)
+      milestone = milestone_fixture(person, %{project_id: project.id, title: "Create a milestone"})
 
-    test "list_milestone_comments/0 returns all milestone_comments" do
-      milestone_comment = milestone_comment_fixture()
-      assert Comments.list_milestone_comments() == [milestone_comment]
+      comment_attrs = %{
+        author_id: person.id,
+        content: %{
+          "message" => Operately.UpdatesFixtures.rich_text_fixture("hello world")
+        }
+      }
+
+      {:ok, milestone_comment} = Operately.Comments.create_milestone_comment(
+        person,
+        milestone,
+        "none",
+        comment_attrs
+      )
+
+      {:ok, milestone_comment: milestone_comment}
     end
 
-    test "get_milestone_comment!/1 returns the milestone_comment with given id" do
-      milestone_comment = milestone_comment_fixture()
-      assert Comments.get_milestone_comment!(milestone_comment.id) == milestone_comment
+    test "list_milestone_comments/0 returns all milestone_comments", ctx do
+      assert Comments.list_milestone_comments() == [ctx.milestone_comment]
     end
 
-    test "create_milestone_comment/1 with valid data creates a milestone_comment" do
-      valid_attrs = %{action: :none}
-
-      assert {:ok, %MilestoneComment{} = milestone_comment} = Comments.create_milestone_comment(valid_attrs)
-      assert milestone_comment.action == :none
+    test "get_milestone_comment!/1 returns the milestone_comment with given id", ctx do
+      assert Comments.get_milestone_comment!(ctx.milestone_comment.id) == ctx.milestone_comment
     end
 
-    test "create_milestone_comment/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Comments.create_milestone_comment(@invalid_attrs)
-    end
-
-    test "update_milestone_comment/2 with valid data updates the milestone_comment" do
-      milestone_comment = milestone_comment_fixture()
+    test "update_milestone_comment/2 with valid data updates the milestone_comment", ctx do
       update_attrs = %{action: :complete}
-
-      assert {:ok, %MilestoneComment{} = milestone_comment} = Comments.update_milestone_comment(milestone_comment, update_attrs)
+      assert {:ok, %MilestoneComment{} = milestone_comment} = Comments.update_milestone_comment(ctx.milestone_comment, update_attrs)
       assert milestone_comment.action == :complete
     end
 
-    test "update_milestone_comment/2 with invalid data returns error changeset" do
-      milestone_comment = milestone_comment_fixture()
-      assert {:error, %Ecto.Changeset{}} = Comments.update_milestone_comment(milestone_comment, @invalid_attrs)
-      assert milestone_comment == Comments.get_milestone_comment!(milestone_comment.id)
+    test "update_milestone_comment/2 with invalid data returns error changeset", ctx do
+      update_attrs = %{action: nil}
+
+      assert {:error, %Ecto.Changeset{}} = Comments.update_milestone_comment(ctx.milestone_comment, update_attrs)
+      assert ctx.milestone_comment == Comments.get_milestone_comment!(ctx.milestone_comment.id)
     end
 
-    test "delete_milestone_comment/1 deletes the milestone_comment" do
-      milestone_comment = milestone_comment_fixture()
-      assert {:ok, %MilestoneComment{}} = Comments.delete_milestone_comment(milestone_comment)
-      assert_raise Ecto.NoResultsError, fn -> Comments.get_milestone_comment!(milestone_comment.id) end
+    test "delete_milestone_comment/1 deletes the milestone_comment", ctx do
+      assert {:ok, %MilestoneComment{}} = Comments.delete_milestone_comment(ctx.milestone_comment)
+      assert_raise Ecto.NoResultsError, fn -> Comments.get_milestone_comment!(ctx.milestone_comment.id) end
     end
 
-    test "change_milestone_comment/1 returns a milestone_comment changeset" do
-      milestone_comment = milestone_comment_fixture()
-      assert %Ecto.Changeset{} = Comments.change_milestone_comment(milestone_comment)
+    test "change_milestone_comment/1 returns a milestone_comment changeset", ctx do
+      assert %Ecto.Changeset{} = Comments.change_milestone_comment(ctx.milestone_comment)
     end
   end
 end
