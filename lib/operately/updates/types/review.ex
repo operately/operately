36f9cce3d4a2e7f1a937ca7b44cfa354c990db6a
Igor Šarčević <igor@renderedtext.com>
2023-10-06 09:45:18 +0000

    Submit review that was requested

--- lib/operately/updates/types/review.ex
+++ lib/operately/updates/types/review.ex
@@ -8,6 +8,7 @@ defmodule Operately.Updates.Types.Review do
     field :review_reason, Ecto.Enum, values: [:phase_change]
     field :previous_phase, :string
     field :new_phase, :string
+    field :review_request_id, :id
   end
 
   def changeset(attrs) do
@@ -16,12 +17,21 @@ defmodule Operately.Updates.Types.Review do
     |> validate_required([:survey, :previous_phase, :new_phase])
   end
 
-  def build(survey, previous_phase, new_phase) do
+  def build(survey, previous_phase, new_phase, review_request_id) do
     %{
       :survey => survey,
-      :review_reason => :phase_change,
+      :review_reason => review_reason(review_request_id),
       :previous_phase => previous_phase,
       :new_phase => new_phase,
+      :review_request_id => review_request_id
     }
+  end
+
+  defp review_reason(review_request_id) do
+    if review_request_id do
+      :phase_change
+    else
+      :review_request
+    end
   end
 end
