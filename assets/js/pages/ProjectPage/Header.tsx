d1d1afe3a0dd8ccfc5dd7d5a0f6edb5738f23f69
Igor Šarčević <igor@renderedtext.com>
2023-10-10 11:25:04 +0000

    Remove health info from the top of the project, as it is was a placeholder

--- assets/js/pages/ProjectPage/Header.tsx
+++ assets/js/pages/ProjectPage/Header.tsx
@@ -11,12 +11,8 @@ import { Link } from "react-router-dom";
 import { Project } from "@/graphql/Projects";
 import * as Contributors from "@/graphql/Projects/contributors";
 import * as Icons from "@tabler/icons-react";
-import * as ProjectIcons from "@/components/ProjectIcons";
 
 import { TextTooltip } from "@/components/Tooltip";
-import * as Popover from "@/components/Popover";
-import FormattedTime from "@/components/FormattedTime";
-import * as Time from "@/utils/time";
 
 interface HeaderProps {
   project: Project;
@@ -31,84 +27,6 @@ export default function Header({ project }: HeaderProps): JSX.Element {
   );
 }
 
-function HealthInfo({ project }) {
-  switch (project.health) {
-    case "on_track":
-      return (
-        <Popover.Root>
-          <Popover.Trigger asChild>
-            <div className="flex items-center gap-1 bg-dark-3 border border-shade-2 text-white-1/80 rounded font-medium cursor-pointer">
-              <div className="flex items-center">
-                <div className="px-2 py-1 border-r border-shade-2">
-                  <ProjectIcons.IconOnTrack />
-                </div>
-                <div className="px-2 py-1 border-r border-shade-2">On-Track</div>
-                <div className="px-2 py-1">
-                  <span className="capitalize">{project.phase} Phase</span>
-                </div>
-              </div>
-            </div>
-          </Popover.Trigger>
-
-          <Popover.Portal>
-            <Popover.Content
-              side="bottom"
-              className="w-96 outline-none bg-dark-2 rounded shadow-xl border border-dark-8 mt-4 p-4"
-            >
-              <p className="font-bold text-white-1">Health: On-Track</p>
-              <p>
-                Based on the last{" "}
-                <a href="#" className="text-blue-400 underline">
-                  status update
-                </a>{" "}
-                from Sep 19th, the project is <span className="font-bold text-white-1">on-track</span> and due to finish
-                on <FormattedTime time={project.deadline} format="short-date" />.
-              </p>
-
-              <p className="font-bold text-white-1 mt-4">Phase: Planning</p>
-              <p>
-                The project is in the {project.phase} phase for 19 days. The next phase is {project.nextPhase},
-                scheduled to start on <FormattedTime time={Time.today()} format="short-date" />.
-              </p>
-
-              <p className="font-bold text-white-1 mt-4">Next Status Update</p>
-              <p>
-                The next status update is due on <FormattedTime time={Time.today()} format="short-date" />.
-              </p>
-            </Popover.Content>
-          </Popover.Portal>
-        </Popover.Root>
-      );
-
-    case "at_risk":
-      return (
-        <TextTooltip text="The project is At Risk, based on the latest status update.">
-          <div className="flex items-center gap-1 bg-dark-3 border border-shade-2 text-white-2 px-1.5 py-0.5 cursor-default rounded">
-            Status <ProjectIcons.IconAtRisk /> <span className="font-semibold text-sm">At-Risk</span>
-          </div>
-        </TextTooltip>
-      );
-    case "off_track":
-      return (
-        <TextTooltip text="The project is Off-Track, based on the latest status update.">
-          <div className="flex items-center gap-1 bg-dark-3 border border-shade-2 text-white-2 px-1.5 py-0.5 cursor-default rounded">
-            Status <ProjectIcons.IconOffTrack /> <span className="font-semibold text-sm">Off-Track</span>
-          </div>
-        </TextTooltip>
-      );
-    case "unknown":
-      return (
-        <TextTooltip text="The project's health is unknown. Write a status update to set the health.">
-          <div className="flex items-center gap-1 bg-dark-3 border border-shade-2 text-white-2 px-1.5 py-0.5 cursor-default rounded">
-            Status <ProjectIcons.IconUnknownHealth /> <span className="font-semibold text-sm">Unknown</span>
-          </div>
-        </TextTooltip>
-      );
-    default:
-      throw new Error(`Unknown health: ${project.health}`);
-  }
-}
-
 function ProjectName({ project }) {
   return (
     <div className="flex items-end justify-between">
@@ -116,10 +34,6 @@ function ProjectName({ project }) {
         {project.name}
 
         <PrivateIndicator project={project} />
-      </div>
-
-      <div className="flex items-center gap-2">
-        <HealthInfo project={project} />
       </div>
     </div>
   );
