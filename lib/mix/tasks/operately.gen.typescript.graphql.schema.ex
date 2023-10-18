4a4f857537dac05fec9b0783cfdaad5c478e1cd2
Igor Šarčević <igor@renderedtext.com>
2023-10-18 09:41:49 +0000

    Allow using graphql-codegen

--- lib/mix/tasks/operately.gen.typescript.graphql.schema.ex
+++ lib/mix/tasks/operately.gen.typescript.graphql.schema.ex
@@ -1,6 +1,6 @@
 defmodule Mix.Tasks.Operately.Gen.Typescript.Graphql.Schema do
   def run(_args) do
-    Mix.Tasks.Absinthe.Schema.Sdl.run(["--schema", "OperatelyWeb.Schema"])
+    Mix.Tasks.Absinthe.Schema.Sdl.run(["--schema", "OperatelyWeb.Graphql.Schema"])
     File.mkdir_p!("tmp")
     File.rename("schema.graphql", "tmp/schema.graphql")
     System.cmd("npm", ["run", "codegen"], cd: "assets")
