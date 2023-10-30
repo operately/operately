420ce1ca3692fa5f9ed346181009926230af2aa3
Igor Šarčević <igor@renderedtext.com>
2023-10-30 13:48:23 +0000

    Remove esbuild

--- mix.exs
+++ mix.exs
@@ -43,7 +43,6 @@ defmodule Operately.MixProject do
       {:heroicons, "~> 0.5"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.7.2"},
-      {:esbuild, "~> 0.5", runtime: Mix.env() == :dev},
       {:tailwind, "~> 0.1.8", runtime: Mix.env() == :dev},
       {:swoosh, "~> 1.3"},
       {:finch, "~> 0.13"},
