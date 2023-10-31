d3ddaf8c947c15d5a4e2dd4f94264b9cb91388c4
Igor Šarčević <igor@renderedtext.com>
2023-10-31 16:41:08 +0100

    In case a blob is present in the rich text content, link to it (#229)

--- lib/operately_email/views/ui_components.ex
+++ lib/operately_email/views/ui_components.ex
@@ -2,4 +2,10 @@ defmodule OperatelyEmail.Views.UIComponents do
   def cta_button(url, text) do
     "<a href='#{url}' style='display: inline-block; cursor: pointer; font-family: sans-serif; font-weight: 600; padding: 10px 20px; background: #16a34a; color: white; border-radius: 6px; text-decoration: none;'>#{text}</a>"
   end
+
+  def rich_text(content) do
+    opts = [domain: OperatelyWeb.Endpoint.url()]
+
+    Prosemirror2Html.convert(content, opts)
+  end
 end
