4af58ad7500c2e27bb089c5137a80cc9eacfc220
Igor Šarčević <igor@renderedtext.com>
2023-11-06 16:45:22 +0000

    Make acknowledge button larger

--- assets/js/pages/ProjectStatusUpdatePage/AckCTA.tsx
+++ assets/js/pages/ProjectStatusUpdatePage/AckCTA.tsx
@@ -6,14 +6,12 @@ import { GhostButton } from "@/components/Button";
 import { useLoadedData, usePageRefetch } from "./loader";
 
 export function AckCTA() {
-  const { project, update, me } = useLoadedData();
+  const { project, update } = useLoadedData();
   const refetch = usePageRefetch();
-
-  const [ack, { loading }] = Updates.useAckUpdate();
+  const [ack] = Updates.useAckUpdate();
 
   if (update.acknowledged) return null;
-  if (!project.reviewer) return null;
-  if (project.reviewer.id !== me.id) return null;
+  if (!project.permissions.canAcknowledgeCheckIn) return null;
 
   const handleAck = async () => {
     await ack({
@@ -27,7 +25,7 @@ export function AckCTA() {
 
   return (
     <div className="flex flex-row items-center justify-center mt-4 mb-4">
-      <GhostButton size="lg" testId="acknowledge-update" loading={loading} onClick={handleAck}>
+      <GhostButton size="lg" testId="acknowledge-update" onClick={handleAck}>
         Acknowledge this Check-In
       </GhostButton>
     </div>
--- lib/operately/projects/permissions.ex
+++ lib/operately/projects/permissions.ex
@@ -11,7 +11,8 @@ defmodule Operately.Projects.Permissions do
       can_delete_milestone: is_contributor?(project, user),
       can_edit_milestone: is_contributor?(project, user),
 
-      can_check_in: is_champion?(project, user)
+      can_check_in: is_champion?(project, user),
+      can_acknowledge_check_in: is_reviewer?(project, user)
     }
   end
 
@@ -23,6 +24,10 @@ defmodule Operately.Projects.Permissions do
 
   defp is_champion?(project, user) do
     Enum.any?(project.contributors, fn c -> c.person_id == user.id && c.role == :champion end)
+  end
+
+  defp is_reviewer?(project, user) do
+    Enum.any?(project.contributors, fn c -> c.person_id == user.id && c.role == :reviewer end)
   end
 
   defp is_public?(project) do
