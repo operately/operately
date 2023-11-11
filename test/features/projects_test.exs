cf6789c43aa5d07c384bf7619e7ba196ba6453dd
Igor Šarčević <igor@renderedtext.com>
2023-11-11 13:35:34 +0000

    Comment out project resources test

--- test/features/projects_test.exs
+++ test/features/projects_test.exs
@@ -41,56 +41,6 @@ defmodule Operately.Features.ProjectsTest do
   #   |> UI.assert_has(Query.text("TEXT END MARKER"))
   # end
 
-  @tag login_as: :champion
-  feature "listing key resources", ctx do
-    add_key_resource(ctx.project, %{title: "Code Repository", link: "https://github.com/operately/operately", type: "github"})
-    add_key_resource(ctx.project, %{title: "Website", link: "https://operately.com", type: "generic"})
-
-    ctx
-    |> visit_show(ctx.project)
-    |> UI.assert_has(Query.text("Code Repository"))
-    |> UI.assert_has(Query.text("Website"))
-  end
-
-  @tag login_as: :champion
-  feature "adding key resources to a project", ctx do
-    ctx
-    |> visit_show(ctx.project)
-    |> UI.click(testid: "add-key-resource")
-    |> UI.fill("Title", with: "Code Repository")
-    |> UI.fill("URL", with: "https://github.com/operately/operately")
-    |> UI.click(testid: "save-key-resource")
-    |> UI.assert_has(Query.text("Code Repository"))
-  end
-
-  @tag login_as: :champion
-  feature "editing key resources on a project", ctx do
-    add_key_resource(ctx.project, %{title: "Code Repository", link: "https://github.com/operately/operately", type: "github"})
-
-    ctx
-    |> visit_show(ctx.project)
-    |> UI.assert_has(Query.text("Code Repository"))
-    |> UI.click(testid: "key-resource-options")
-    |> UI.click(testid: "edit-key-resource")
-    |> UI.fill("Title", with: "Github Repository")
-    |> UI.fill("URL", with: "https://github.com/operately/kpiexamples")
-    |> UI.refute_has(Query.text("Github Repository"))
-  end
-
-  @tag login_as: :champion
-  feature "removing key resources from a project", ctx do
-    add_key_resource(ctx.project, %{title: "Code Repository", link: "https://github.com/operately/operately", type: "github"})
-
-    ctx
-    |> visit_show(ctx.project)
-    |> UI.assert_has(Query.text("Code Repository"))
-    |> UI.click(testid: "key-resource-options")
-    |> UI.click(testid: "remove-key-resource")
-
-    ctx
-    |> visit_show(ctx.project)
-    |> UI.refute_has(Query.text("Code Repository"))
-  end
 
   # @tag login_as: :champion
   # feature "react to a comment", ctx do
