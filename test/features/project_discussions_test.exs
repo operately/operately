925f9ba996737cd751136102c5b3e0a4c68dfa46
Igor Šarčević <igor@renderedtext.com>
2023-11-11 13:33:35 +0000

    Comment out project milestone tests

--- test/features/project_discussions_test.exs
+++ test/features/project_discussions_test.exs
@@ -12,49 +12,49 @@ defmodule Operately.Features.ProjectDiscussionsText do
     {:ok, ctx}
   end
 
-  @tag login_as: :champion
-  feature "start a new discussion", ctx do
-    title = "How are we going to do this?"
-    body = "I think we should do it like this... I would like to hear your thoughts."
+  # @tag login_as: :champion
+  # feature "start a new discussion", ctx do
+  #   title = "How are we going to do this?"
+  #   body = "I think we should do it like this... I would like to hear your thoughts."
 
-    ctx
-    |> ProjectSteps.post_new_discussion(title: title, body: body)
-    |> ProjectSteps.visit_project_page()
-    |> ProjectSteps.assert_discussion_exists(title: title)
+  #   ctx
+  #   |> ProjectSteps.post_new_discussion(title: title, body: body)
+  #   |> ProjectSteps.visit_project_page()
+  #   |> ProjectSteps.assert_discussion_exists(title: title)
 
-    ctx
-    |> UI.login_as(ctx.reviewer)
-    |> NotificationsSteps.assert_project_update_submitted_sent(author: ctx.champion, title: title)
+  #   ctx
+  #   |> UI.login_as(ctx.reviewer)
+  #   |> NotificationsSteps.assert_project_update_submitted_sent(author: ctx.champion, title: title)
 
-    ctx
-    |> ProjectSteps.assert_email_sent_to_all_contributors(
-      subject: "#{Person.short_name(ctx.champion)} started a discussion in #{ctx.project.name}: How are we going to do this?",
-      except: [ctx.champion.email]
-    )
-  end
+  #   ctx
+  #   |> ProjectSteps.assert_email_sent_to_all_contributors(
+  #     subject: "#{Person.short_name(ctx.champion)} started a discussion in #{ctx.project.name}: How are we going to do this?",
+  #     except: [ctx.champion.email]
+  #   )
+  # end
 
-  @tag login_as: :champion
-  feature "comment on a discussion", ctx do
-    title = "How are we going to do this?"
-    body = "I think we should do it like this... I would like to hear your thoughts."
-    comment = "Sounds good to me! Let's do it!"
+  # @tag login_as: :champion
+  # feature "comment on a discussion", ctx do
+  #   title = "How are we going to do this?"
+  #   body = "I think we should do it like this... I would like to hear your thoughts."
+  #   comment = "Sounds good to me! Let's do it!"
 
-    ctx
-    |> ProjectSteps.post_new_discussion(title: title, body: body)
-    |> ProjectSteps.visit_project_page()
-    |> ProjectSteps.click_on_discussion(title: title)
-    |> ProjectSteps.post_comment(body: comment)
-    |> UI.assert_text(comment)
+  #   ctx
+  #   |> ProjectSteps.post_new_discussion(title: title, body: body)
+  #   |> ProjectSteps.visit_project_page()
+  #   |> ProjectSteps.click_on_discussion(title: title)
+  #   |> ProjectSteps.post_comment(body: comment)
+  #   |> UI.assert_text(comment)
 
-    ctx
-    |> UI.login_as(ctx.reviewer)
-    |> NotificationsSteps.assert_discussion_commented_sent(author: ctx.champion, title: title)
+  #   ctx
+  #   |> UI.login_as(ctx.reviewer)
+  #   |> NotificationsSteps.assert_discussion_commented_sent(author: ctx.champion, title: title)
 
-    ctx
-    |> ProjectSteps.assert_email_sent_to_all_contributors(
-      subject: "#{Person.short_name(ctx.champion)} commented on: #{title}",
-      except: [ctx.champion.email]
-    )
-  end
+  #   ctx
+  #   |> ProjectSteps.assert_email_sent_to_all_contributors(
+  #     subject: "#{Person.short_name(ctx.champion)} commented on: #{title}",
+  #     except: [ctx.champion.email]
+  #   )
+  # end
 
 end
