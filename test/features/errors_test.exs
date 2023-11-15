77c9c5c5c937903590dc7f340e7a3059d28eab86
Igor Šarčević <igor@renderedtext.com>
2023-11-15 13:53:32 +0000

    Page Not Found page

---
+++ test/features/errors_test.exs
@@ -0,0 +1,25 @@
+defmodule Operately.Features.ErrorsTest do
+  use Operately.FeatureCase
+  alias Operately.Support.Features.UI
+
+  import Operately.CompaniesFixtures
+  import Operately.PeopleFixtures
+
+  setup ctx do
+    company = company_fixture(%{name: "Test Org"})
+    person = person_fixture_with_account(%{company_id: company.id, full_name: "John Johnson"})
+
+    ctx = UI.init_ctx(ctx, %{company: company, person: person})
+    ctx = UI.login_as(ctx, ctx.person)
+
+    ctx
+  end
+
+  feature "visiting a non existing page", ctx do
+    ctx
+    |> UI.visit("/non-existing-page")
+    |> UI.assert_text("Page Not Found")
+    |> UI.click(testid: "back-to-lobby")
+    |> UI.assert_text("Lobby")
+  end
+end
