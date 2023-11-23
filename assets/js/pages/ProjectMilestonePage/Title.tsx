38371c723c6fdc4e15913033a0e5db465c2afdb6
Igor Šarčević <igor@renderedtext.com>
2023-11-23 18:09:13 +0000

    Edit milestone

--- assets/js/pages/ProjectMilestonePage/Title.tsx
+++ assets/js/pages/ProjectMilestonePage/Title.tsx
@@ -1,11 +1,17 @@
 import * as React from "react";
 
-export function Title({ milestone }) {
+import { GhostButton } from "@/components/Button";
+
+export function Title({ milestone, form }) {
   return (
-    <div className="flex items-center gap-2 mb-8">
+    <div className="flex justify-between items-center mb-4">
       <div className="text-2xl font-extrabold text-content-accent hover:bg-shade-1 flex-1 -m-1.5 p-1.5">
         {milestone.title}
       </div>
+
+      <GhostButton size="xs" onClick={form.completeMilestone} data-test-id="edit-milestone" type="secondary">
+        Edit
+      </GhostButton>
     </div>
   );
 }
