df2dadeb574d821379564cfd49c8089c95242865
Igor Šarčević <igor@renderedtext.com>
2023-12-12 15:29:33 +0000

    Implement a way to fill the targets

---
+++ assets/js/pages/GoalAddPage/Target.tsx
@@ -0,0 +1,109 @@
+import React from "react";
+
+import * as Icons from "@tabler/icons-react";
+
+function Row({ icon, name, from, to, unit }) {
+  return (
+    <div className="flex items-start gap-2 first:border-t border-b border-surface-outline py-2 group/row">
+      <div className="flex-1 flex items-center gap-2">
+        <div className="w-8 pl-1">{icon}</div>
+
+        <div className="flex-1 flex items-center gap-2">
+          <div className="flex-1 flex items-center">{name}</div>
+          <div className="w-12 flex items-center">{from}</div>
+          <div className="w-12 flex items-center">{to}</div>
+          <div className="w-24 flex items-center">{unit}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function TargetHeader() {
+  return (
+    <Row
+      icon={<Icons.IconHash className="text-content-base ml-1.5" size={12} />}
+      name={<div className="text-xs font-bold">NAME</div>}
+      from={<div className="text-xs font-bold">FROM</div>}
+      to={<div className="text-xs font-bold">TO</div>}
+      unit={<div className="text-xs font-bold">UNIT</div>}
+    />
+  );
+}
+
+interface TargetProps {
+  form: any;
+  index: number;
+  placeholders?: string[];
+}
+
+export function Target({ form, placeholders = [], index }: TargetProps) {
+  const [active, setActive] = React.useState(false);
+
+  const icon = (
+    <div className="rounded-full bg-accent-1 w-5 h-5 flex items-center justify-center ml-0.5">
+      <div className="text-xs font-bold text-white-1">{index}</div>
+    </div>
+  );
+
+  const nameInput = <TextInput active={active} setActive={setActive} placeholder={placeholders[0]} />;
+  const fromInput = <NumberInput active={active} setActive={setActive} placeholder={placeholders[1]} />;
+  const toInput = <NumberInput active={active} setActive={setActive} placeholder={placeholders[2]} />;
+  const unitInput = <TextInput active={active} setActive={setActive} placeholder={placeholders[3]} />;
+
+  return <Row icon={icon} name={nameInput} from={fromInput} to={toInput} unit={unitInput} />;
+}
+
+function TextInput({ autoFocus = false, placeholder, active, setActive }) {
+  const [value, setValue] = React.useState("");
+
+  return (
+    <GenericInput
+      autoFocus={autoFocus}
+      placeholder={placeholder}
+      active={active}
+      setActive={setActive}
+      value={value}
+      setValue={setValue}
+    />
+  );
+}
+
+function NumberInput({ autoFocus = false, placeholder, active, setActive }) {
+  const [value, setValue] = React.useState("");
+  const re = /^[0-9\b\.]+$/;
+
+  const onChange = (str: string) => {
+    if (str === "" || re.test(str)) {
+      setValue(str);
+    }
+  };
+
+  return (
+    <GenericInput
+      autoFocus={autoFocus}
+      placeholder={placeholder}
+      active={active}
+      setActive={setActive}
+      value={value}
+      setValue={onChange}
+    />
+  );
+}
+
+function GenericInput({ autoFocus = false, placeholder, active, setActive, value, setValue }) {
+  return (
+    <input
+      className={
+        "placeholder:text-content-subtle px-0 py-1 w-full group-hover/row:bg-surface-highlight " +
+        (active ? "bg-surface-highlight" : "bg-transparent")
+      }
+      placeholder={placeholder}
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+      autoFocus={autoFocus}
+      onFocus={() => setActive(true)}
+      onBlur={() => setActive(false)}
+    />
+  );
+}
--- assets/js/pages/GoalAddPage/page.tsx
+++ assets/js/pages/GoalAddPage/page.tsx
@@ -12,6 +12,7 @@ import * as Icons from "@tabler/icons-react";
 import { useLoadedData } from "./loader";
 import { useForm } from "./useForm";
 import Button from "@/components/Button";
+import { Target, TargetHeader } from "./Target";
 
 export function Page() {
   const { company, me } = useLoadedData();
@@ -40,106 +41,12 @@ function Form({ form }) {
         </div>
         <span className="font-bold text-lg">Targets</span>
         <div className="mt-4">
-          <div className="flex items-start gap-4 border-y border-surface-outline py-3">
-            <div className="flex-1 flex items-center gap-4">
-              <div className="w-8 pl-1">
-                <Icons.IconHash className="text-content-base" size={16} />
-              </div>
-
-              <div className="flex-1 flex items-center gap-4">
-                <div className="w-2/3 flex items-center">
-                  <div className="text-xs font-bold">NAME</div>
-                </div>
-
-                <div className="w-12 flex items-center">
-                  <div className="text-xs font-bold">FROM</div>
-                </div>
-
-                <div className="w-12 flex items-center relative">
-                  <div className="text-xs font-bold">TO</div>
-                </div>
-
-                <div className="w-20 flex items-center relative">
-                  <div className="text-xs font-bold">UNIT</div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="flex items-center gap-4 border-b border-surface-outline py-2">
-            <div className="w-8">
-              <Icons.IconCircle1Filled className="text-accent-1" size={24} />
-            </div>
-
-            <div className="flex-1 flex items-center gap-4">
-              <div className="w-2/3 flex items-center relative">
-                <TextInput2 form={form} placeholder="e.g. Reduce Average Onboarding Time" />
-              </div>
-
-              <div className="w-12 flex items-center relative">
-                <TextInput2 form={form} placeholder="30" />
-              </div>
-
-              <div className="w-12 flex items-center relative">
-                <TextInput2 form={form} placeholder="15" />
-              </div>
-
-              <div className="w-20 flex items-center relative">
-                <TextInput2 form={form} placeholder="minutes" />
-                <Icons.IconChevronDown className="text-content-secondary" size={20} />
-              </div>
-            </div>
-          </div>
-
-          <div className="flex items-center gap-4 border-b border-surface-outline py-2">
-            <div className="w-8">
-              <Icons.IconCircle2Filled className="text-accent-1" size={24} />
-            </div>
-
-            <div className="flex-1 flex items-center gap-4">
-              <div className="w-2/3 flex items-center relative">
-                <TextInput2 form={form} placeholder="" />
-              </div>
-
-              <div className="w-12 flex items-center relative">
-                <TextInput2 form={form} placeholder="" />
-              </div>
-
-              <div className="w-12 flex items-center relative">
-                <TextInput2 form={form} placeholder="" />
-              </div>
-
-              <div className="w-20 flex items-center relative">
-                <TextInput2 form={form} placeholder="" />
-              </div>
-            </div>
-          </div>
-
-          <div className="flex items-center gap-4 border-b border-surface-outline py-2">
-            <div className="w-8">
-              <Icons.IconCircle3Filled className="text-accent-1" size={24} />
-            </div>
-
-            <div className="flex-1 flex items-center gap-4">
-              <div className="w-2/3 flex items-center relative">
-                <TextInput2 form={form} placeholder="" />
-              </div>
-
-              <div className="w-12 flex items-center relative">
-                <TextInput2 form={form} placeholder="" />
-              </div>
-
-              <div className="w-12 flex items-center relative">
-                <TextInput2 form={form} placeholder="" />
-              </div>
-
-              <div className="w-20 flex items-center relative">
-                <TextInput2 form={form} placeholder="" />
-              </div>
-            </div>
-          </div>
+          <TargetHeader />
+          <Target form={form} index={1} placeholders={["e.g. Reduce Average Onboarding Time", "30", "15", "minutes"]} />
+          <Target form={form} index={2} />
+          <Target form={form} index={3} />
         </div>
-        <div className="font-bold text-lg mt-8">People</div>
+        <div className="font-bold text-lg mt-12">People</div>
         TODO
       </div>
 
@@ -153,7 +60,7 @@ function Form({ form }) {
 function TextInput({ form, autoFocus = false, placeholder }) {
   return (
     <input
-      className="border-b border-surface-outline px-0 py-1 w-full"
+      className="border-b border-surface-outline px-0 py-1 w-full placeholder:text-content-subtle focus:bg-surface-highlight bg-transparent"
       placeholder={placeholder}
       autoFocus={autoFocus}
     />
@@ -161,7 +68,13 @@ function TextInput({ form, autoFocus = false, placeholder }) {
 }
 
 function TextInput2({ form, autoFocus = false, placeholder }) {
-  return <input className="px-0 py-1 w-full" placeholder={placeholder} autoFocus={autoFocus} />;
+  return (
+    <input
+      className="px-0 py-1 w-full group-hover:bg-surface-highlight"
+      placeholder={placeholder}
+      autoFocus={autoFocus}
+    />
+  );
 }
 
 function TimeframeSelector({ form }) {
@@ -188,50 +101,6 @@ function ContributorSearch({ title, subtitle, onSelect, defaultValue, inputId })
           loader={loader}
         />
       </div>
-    </div>
-  );
-}
-
-function Targets({ form }) {
-  return (
-    <div className="flex flex-col gap-6">
-      <label className="font-bold mb-1 block">Targets</label>
-
-      <div className="flex flex-col gap-4">
-        {form.targetList.targets.map((target, index) => (
-          <Target key={index} form={form} target={target} index={index} />
-        ))}
-
-        <Button onClick={form.targetList.addTarget} variant="secondary">
-          Add Target
-        </Button>
-      </div>
-    </div>
-  );
-}
-
-function Target({ form, target, index }) {
-  return (
-    <div className="flex flex-col gap-2">
-      <Forms.TextInput
-        label="Name"
-        value={target.name}
-        onChange={target.setName}
-        placeholder="e.g. Increase user retention"
-        data-test-id={`target-${index}-name-input`}
-      />
-
-      <Forms.SelectBox
-        label="Unit"
-        value={target.unit}
-        options={form.targetList.unitOptions}
-        onChange={target.setUnit}
-        data-test-id={`target-${index}-unit-input`}
-      />
-
-      <Button onClick={() => form.targetList.removeTarget(target.id)} variant="secondary">
-        Remove Target
-      </Button>
     </div>
   );
 }
