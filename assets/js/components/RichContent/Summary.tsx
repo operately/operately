cce377e744bca212e2b97eef408965d234a009c2
Igor Šarčević <igor@renderedtext.com>
2023-12-14 16:00:10 +0100

    Fix people mention in Rich Text summaries (#294)
    * Show mentioned people in rich content summaries
    
    * Write tests for mention extraction

--- assets/js/components/RichContent/Summary.tsx
+++ assets/js/components/RichContent/Summary.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import * as TipTapEditor from "@/components/Editor";
+import { extract } from "./textExtract";
 
 export function Summary({ jsonContent, characterCount }): JSX.Element {
   const { editor } = TipTapEditor.useEditor({
@@ -14,14 +15,39 @@ export function Summary({ jsonContent, characterCount }): JSX.Element {
     editor.commands.setContent(JSON.parse(jsonContent));
   }, [jsonContent]);
 
+  let summary: JSX.Element[] = React.useMemo(() => {
+    if (!editor) return [<></>];
+
+    const extracted = extract(editor.state.doc);
+    let result: JSX.Element[] = [];
+    let length = 0;
+    let contracted = false;
+
+    extracted.forEach((node, index) => {
+      if (typeof node === "string") {
+        if (length + node.length > characterCount) {
+          const remaining = characterCount - length;
+          const text = node.substring(0, remaining);
+
+          result.push(<React.Fragment key={index}>{text}...</React.Fragment>);
+          contracted = true;
+        } else {
+          result.push(<React.Fragment key={index}>{node}</React.Fragment>);
+        }
+
+        length += node.length;
+      } else {
+        result.push(
+          <span key={index} className="font-medium text-link-base">
+            @{node.label}
+          </span>,
+        );
+      }
+    });
+
+    return result;
+  }, [editor, jsonContent]);
+
   if (!editor) return <></>;
-
-  const doc = editor.state.doc;
-  const text = doc.textBetween(0, doc.nodeSize - 2, " ");
-
-  if (text.length > characterCount) {
-    return <>{text.slice(0, characterCount)}...</>;
-  } else {
-    return <>{text}</>;
-  }
+  return <div>{summary}</div>;
 }
