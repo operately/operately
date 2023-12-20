de7dc3daf87dce3d2bac1c6065fd2b8bbdab6898
Igor Šarčević <igor@renderedtext.com>
2023-12-20 12:55:48 +0000

    Fix problems with test splitter

--- scripts/split.rb
+++ scripts/split.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 
-batch_index = ARGV[0].to_i
-batch_count = ARGV[1].to_i
+batch_index = ARGV[0].to_i    # based on SEMAPHORE_JOB_INDEX (1-based)
+batch_count = ARGV[1].to_i    # based on SEMAPHORE_JOB_COUNT
+batch_index = batch_index - 1 # convert to 0-based
 
 files = $stdin.readlines.map(&:strip)
 
@@ -10,4 +11,11 @@ batch_size = (files.size / batch_count.to_f).ceil
 start_index = batch_index * batch_size
 end_index = start_index + batch_size
 
-puts files[start_index...end_index].join(" ")
+files = files[start_index...end_index]
+
+if files.empty?
+  puts "No files to process"
+  exit 1
+end
+
+puts files.join(" ")
