f56a11480ccdbca4ccfa538f8dcecef34d0a7e49
Igor Šarčević <igor@renderedtext.com>
2024-01-04 07:47:01 +0000

    Implement goal editing operation

--- lib/operately/operations/goal_editing.ex
+++ lib/operately/operations/goal_editing.ex
@@ -2,13 +2,28 @@ defmodule Operately.Operations.GoalEditing do
   alias Ecto.Multi
   alias Operately.Repo
   alias Operately.Activities
+  alias Operately.Goals.Goal
 
-  def run(creator, attrs) do
-    raise "Operation for GoalEditing not implemented"
+  def run(author, goal, attrs) do
+    changeset = Goal.changeset(goal, attrs)
 
-    # Multi.new()
-    # |> Multi.insert(:something, ...)
-    # |> Repo.transaction()
-    # |> Repo.extract_result(:goal)
+    Multi.new()
+    |> Multi.update(:goal, changeset)
+    |> Activities.insert(author.id, :goal_editing, fn changes ->
+      %{
+        company_id: changes.company.id,
+        goal_id: changes.goal.id,
+        old_name: goal.name,
+        new_name: changes.goal.name,
+        old_champion_id: goal.champion_id,
+        new_champion_id: changes.goal.champion_id,
+        old_reviewer_id: goal.reviewer_id,
+        new_reviewer_id: changes.goal.reviewer_id,
+        old_timeframe: goal.timeframe,
+        new_timeframe: changes.goal.timeframe,
+      }
+    end)
+    |> Repo.transaction()
+    |> Repo.extract_result(:goal)
   end
 end
