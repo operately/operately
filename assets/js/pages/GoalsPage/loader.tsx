04fcff7149ca547892f806802a5036d30309fb4f
Igor Šarčević <igor@renderedtext.com>
2024-01-08 09:51:39 +0000

    Bootstrap goals index page

--- assets/js/pages/GoalsPage/loader.tsx
+++ assets/js/pages/GoalsPage/loader.tsx
@@ -1,15 +1,23 @@
 import * as Pages from "@/components/Pages";
+import * as Goals from "@/models/goals";
+import * as Companies from "@/models/companies";
 
 interface LoaderResult {
+  company: Companies.Company;
+  goals: Goals.Goal[];
 }
 
-export async function loader({params}) : Promise<LoaderResult> {
-  // TODO: Implement
-
-  return {}
+export async function loader(): Promise<LoaderResult> {
+  return {
+    company: await Companies.getCompany(),
+    goals: await Goals.getGoals({
+      includeTargets: true,
+      includeSpace: true,
+    }),
+  };
 }
 
-export function useLoadedData() : LoaderResult {
+export function useLoadedData(): LoaderResult {
   return Pages.useLoadedData() as LoaderResult;
 }
 
