dbe1d6cf25909133ab49eeabd6e9746e9ad6df07
Igor Šarčević <igor@renderedtext.com>
2024-01-11 15:27:13 +0100

    By default, show only my projects on the projects page (#316)

--- assets/js/pages/ProjectsPage/loader.tsx
+++ assets/js/pages/ProjectsPage/loader.tsx
@@ -2,22 +2,51 @@ import * as Pages from "@/components/Pages";
 import * as Projects from "@/models/projects";
 import * as Companies from "@/models/companies";
 
+import { useSearchParams } from "react-router-dom";
+
 interface LoaderResult {
   company: Companies.Company;
   projects: Projects.Project[];
+  showingAllProjects: boolean;
 }
 
-export async function loader(): Promise<LoaderResult> {
+export async function loader({ request }): Promise<LoaderResult> {
+  const searchParams = new URL(request.url).searchParams;
+
+  let showAllProjects = (searchParams.get("allProjects") || "false") === "true";
+
   return {
     company: await Companies.getCompany(),
     projects: await Projects.getProjects({
       includeSpace: true,
       includeContributors: true,
       includeMilestones: true,
+      onlyMyProjects: !showAllProjects,
     }),
+    showingAllProjects: showAllProjects,
   };
 }
 
 export function useLoadedData(): LoaderResult {
   return Pages.useLoadedData() as LoaderResult;
+}
+
+export function useFilters() {
+  let [searchParams, setSearchParams] = useSearchParams();
+
+  const showMyProjects = () => {
+    setSearchParams(() => {
+      searchParams.delete("allProjects");
+      return searchParams;
+    });
+  };
+
+  const showAllProjects = () => {
+    setSearchParams({ allProjects: "true" });
+  };
+
+  return {
+    showMyProjects,
+    showAllProjects,
+  };
 }
--- lib/operately/projects/list_operation.ex
+++ lib/operately/projects/list_operation.ex
@@ -5,19 +5,19 @@ defmodule Operately.Projects.ListOperation do
   alias Operately.Projects.Project
 
   def run(person, filters) do
-    query = from p in Project
+    query = (from p in Project, as: :project)
 
     query = apply_visibility_filter(query, person)
     query = apply_group_filter(query, filters[:group_id])
     query = apply_goal_filter(query, filters[:goal_id])
     query = apply_company_filter(query, filters[:company_id])
+    query = apply_only_my_projects_filter(query, person, filters)
 
     Repo.all(query, [with_deleted: filters[:include_archived]])
   end
 
   defp apply_visibility_filter(query, person) do
     from p in query,
-      as: :project,
       where: not(p.private) 
         or exists(
         from c in Operately.Projects.Contributor, 
@@ -39,6 +39,18 @@ defmodule Operately.Projects.ListOperation do
   defp apply_company_filter(query, nil), do: query
   defp apply_company_filter(query, company_id) do
     from p in query, where: p.company_id == ^company_id
+  end
+
+  defp apply_only_my_projects_filter(query, person, filters) do
+    if filters[:only_my_projects] do
+      from p in query,
+        where: exists(
+          from c in Operately.Projects.Contributor, 
+            where: c.project_id == parent_as(:project).id and c.person_id == ^person.id
+        )
+    else
+      query
+    end
   end
 
 end
