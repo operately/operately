ef1a7b71ab1b6cb8c36fa450d12a9f24b7722072
Igor Šarčević <igor@renderedtext.com>
2024-01-23 14:21:26 +0100

    Include retrospective content into the project closed email (#326)

---
+++ lib/operately/operations/project_closed.ex
@@ -0,0 +1,23 @@
+defmodule Operately.Operations.ProjectClosed do
+  alias Ecto.Multi
+  alias Operately.Repo
+  alias Operately.Activities
+  alias Operately.Projects.Project
+
+  def run(author, project, retrospective) do
+    change = Project.changeset(project, %{
+      status: "closed",
+      closed_at: DateTime.utc_now(),
+      retrospective: Jason.decode!(retrospective)
+    })
+
+    Multi.new()
+    |> Multi.update(:project, change)
+    |> Activities.insert(author.id, :project_closed, fn _changes -> %{
+      company_id: project.company_id,
+      project_id: project.id
+    } end)
+    |> Repo.transaction()
+    |> Repo.extract_result(:project)
+  end
+end
