c753517b4ea3e01d4c36e9d2a3aa839824f1658c
Igor Šarčević <igor@renderedtext.com>
2024-02-02 10:29:50 +0000

    Use the goal check in feature

--- assets/js/pages/GoalCheckInEditPage/loader.tsx
+++ assets/js/pages/GoalCheckInEditPage/loader.tsx
@@ -1,15 +1,30 @@
+import client from "@/graphql/client";
+
 import * as Pages from "@/components/Pages";
+import * as Goals from "@/models/goals";
+import * as Updates from "@/graphql/Projects/updates";
 
 interface LoaderResult {
+  goal: Goals.Goal;
+  checkIn: Updates.Update;
 }
 
-export async function loader({params}) : Promise<LoaderResult> {
-  // TODO: Implement
+export async function loader({ params }): Promise<LoaderResult> {
+  let updateData = await client.query({
+    query: Updates.GET_STATUS_UPDATE,
+    variables: { id: params.id },
+    fetchPolicy: "network-only",
+  });
 
-  return {}
+  return {
+    goal: await Goals.getGoal(params.goalId, {
+      includeTargets: true,
+    }),
+    checkIn: updateData.data.update,
+  };
 }
 
-export function useLoadedData() : LoaderResult {
+export function useLoadedData(): LoaderResult {
   return Pages.useLoadedData() as LoaderResult;
 }
 
--- assets/js/pages/GoalCheckInEditPage/page.tsx
+++ assets/js/pages/GoalCheckInEditPage/page.tsx
@@ -3,17 +3,55 @@ import * as Paper from "@/components/PaperContainer";
 import * as Pages from "@/components/Pages";
 
 import { useLoadedData } from "./loader";
+import { FilledButton } from "@/components/Button";
+import { Form, useForm } from "@/features/GoalCheckInForm";
+
+import FormattedTime from "@/components/FormattedTime";
 
 export function Page() {
-  const data = useLoadedData();
+  const { goal, checkIn } = useLoadedData();
+
+  const form = useForm({ goal, checkIn, mode: "edit" });
 
   return (
-    <Pages.Page title={"GoalCheckInEditPage"}>
+    <Pages.Page title={["Edit Goal Check-In", goal.name]}>
       <Paper.Root>
         <Paper.Body>
-          <div className="text-content-accent text-3xl font-extrabold">GoalCheckInEditPage</div>
+          <Header form={form} checkIn={checkIn} />
+          <Form form={form} />
         </Paper.Body>
       </Paper.Root>
     </Pages.Page>
+  );
+}
+
+function Header({ form, checkIn }) {
+  return (
+    <div className="">
+      <Paper.Header className="bg-surface-dimmed">
+        <div className="flex items-end justify-between mx-10 my-2">
+          <h1 className="text-xl font-extrabold">
+            Editing the Check-In from <FormattedTime time={checkIn.insertedAt} format="long-date" />
+          </h1>
+
+          <div className="flex items-center gap-2">
+            <FilledButton type="secondary" linkTo={form.cancelPath} size="sm" testId="cancel-edit">
+              Cancel
+            </FilledButton>
+
+            <FilledButton
+              type="primary"
+              onClick={form.submit}
+              loading={form.submitting}
+              size="sm"
+              testId="save-changes"
+              bzzzOnClickFailure
+            >
+              {form.submitButtonLabel}
+            </FilledButton>
+          </div>
+        </div>
+      </Paper.Header>
+    </div>
   );
 }
