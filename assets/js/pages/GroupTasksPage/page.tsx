d858675e3c264b75c7e1ce96b8fe2d955df687ba
Igor Šarčević <igor@renderedtext.com>
2024-02-12 12:25:16 +0000

    Set up table

--- assets/js/pages/GroupTasksPage/page.tsx
+++ assets/js/pages/GroupTasksPage/page.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as Paper from "@/components/PaperContainer";
 import * as Pages from "@/components/Pages";
 
+import { Table } from "./Table";
 import { GroupPageNavigation } from "@/components/GroupPageNavigation";
 import { useLoadedData } from "./loader";
 
@@ -13,8 +14,28 @@ export function Page() {
       <Paper.Root size="large" fluid>
         <Paper.Body minHeight="500px" backgroundColor="bg-surface">
           <GroupPageNavigation group={group} activeTab="tasks" />
+
+          <TaskList />
         </Paper.Body>
       </Paper.Root>
     </Pages.Page>
   );
+}
+
+function TaskList() {
+  const columnSizes = [400, 150, 150, 150, 150, 150];
+  const headers = ["Task", "Priority", "Size", "Assignees", "Due Date", "Status"];
+
+  const rows = [
+    [
+      <div className="inline-flex justify-center font-medium">Renew SSL certificate for the staging environment</div>,
+      <div className="inline-flex justify-center">High</div>,
+      <div className="inline-flex justify-center">Small</div>,
+      <div className="inline-flex justify-center">John Doe</div>,
+      <div className="inline-flex justify-center">Feb 15th</div>,
+      <div className="inline-flex justify-center">Open</div>,
+    ],
+  ];
+
+  return <Table headers={headers} rows={rows} columnSizes={columnSizes} cellPadding="px-2 py-1.5" />;
 }
