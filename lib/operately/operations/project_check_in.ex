a6d875775066a5a397298b1d09e22a169691bc55
Igor Šarčević <igor@renderedtext.com>
2024-02-22 12:48:26 +0000

    Rename status update activity to check in activity

--- lib/operately/operations/project_check_in.ex
+++ lib/operately/operations/project_check_in.ex
@@ -2,19 +2,13 @@ defmodule Operately.Operations.ProjectCheckIn do
   alias Ecto.Multi
   alias Operately.{Repo, Activities, Updates.Update, Projects.Project}
 
-  def run(author, project, health, content) do
-    action = :project_status_update_submitted
-
-    health = Jason.decode!(health)
-    status = health["status"]["value"]
-
+  def run(author, project, status) do
     changeset = Update.changeset(%{
       updatable_type: :project,
       updatable_id: project.id,
       author_id: author.id,
       title: "",
-      type: :status_update,
-      content: Operately.Updates.Types.StatusUpdate.build(project, health, content)
+      type: :check_in
     })
 
     next_check_in = Operately.Time.calculate_next_check_in(project.next_update_scheduled_at, DateTime.utc_now())
@@ -22,7 +16,7 @@ defmodule Operately.Operations.ProjectCheckIn do
     Multi.new()
     |> Multi.insert(:update, changeset)
     |> Multi.update(:project, Project.changeset(project, %{health: status, next_update_scheduled_at: next_check_in}))
-    |> Activities.insert(author.id, action, fn changes -> %{update_id: changes.update.id, project_id: changes.project.id} end)
+    |> Activities.insert(author.id, :project_check_in_submitted, fn changes -> %{update_id: changes.update.id, project_id: changes.project.id} end)
     |> Repo.transaction()
     |> Repo.extract_result(:update)
   end
