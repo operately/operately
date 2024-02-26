5263d0ee548da9b1561f912b661840607ab415a1
Igor Šarčević <igor@renderedtext.com>
2024-02-26 12:38:30 +0000

    Clean up unused code

--- assets/js/graphql/Assignments/index.tsx
+++
@@ -1,8 +0,0 @@
-import type { Project } from "@/models/projects";
-import type { Milestone } from "@/models/milestones";
-
-export interface Assignments {
-  type: "milestone" | "project_status_update";
-  due: string;
-  resource: Project | Milestone;
-}
--- assets/js/graphql/Projects/index.tsx
+++
@@ -1,4 +0,0 @@
-export { useCreateProject } from "./mutations/create";
-export { useEditProjectTimeline } from "./mutations/edit_timeline";
-export { useEditProjectName } from "./mutations/edit_name";
-export { useArchiveForm } from "./mutations/archive";
