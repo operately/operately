ea6bba4d58bfe6b93d0966788d16918baa5f07be
Igor Šarčević <igor@renderedtext.com>
2024-02-28 10:29:45 +0000

    Remove deprecated account controlers

--- lib/operately_web/controllers/account_confirmation_html.ex
+++
@@ -1,5 +0,0 @@
-defmodule OperatelyWeb.AccountConfirmationHTML do
-  use OperatelyWeb, :html
-
-  embed_templates "account_confirmation_html/*"
-end
--- lib/operately_web/controllers/account_settings_html.ex
+++
@@ -1,5 +0,0 @@
-defmodule OperatelyWeb.AccountSettingsHTML do
-  use OperatelyWeb, :html
-
-  embed_templates "account_settings_html/*"
-end
