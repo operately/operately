743edc29f4fdcd0f38665563e28ab0164c67a26e
Igor Šarčević <igor@renderedtext.com>
2024-02-28 16:09:45 +0000

    Fix notification page loader

--- assets/js/pages/NotificationsPage/loader.tsx
+++ assets/js/pages/NotificationsPage/loader.tsx
@@ -198,7 +198,7 @@ const query = gql`
 
           ... on ActivityContentProjectCheckInCommented {
             projectId
-            statusUpdateId
+            checkInId
 
             project {
               name
