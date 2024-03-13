7beaa90a3a25318bc243da87d9b201840152802e
Igor Šarčević <igor@renderedtext.com>
2024-03-13 14:23:42 +0000

    Show a full company feed

--- assets/js/pages/FeedPage/loader.tsx
+++ assets/js/pages/FeedPage/loader.tsx
@@ -1,15 +1,17 @@
 import * as Pages from "@/components/Pages";
+import * as Companies from "@/models/companies";
 
 interface LoaderResult {
+  company: Companies.Company;
 }
 
-export async function loader({params}) : Promise<LoaderResult> {
-  // TODO: Implement
-
-  return {}
+export async function loader(): Promise<LoaderResult> {
+  return {
+    company: await Companies.getCompany(),
+  };
 }
 
-export function useLoadedData() : LoaderResult {
+export function useLoadedData(): LoaderResult {
   return Pages.useLoadedData() as LoaderResult;
 }
 
--- assets/js/pages/FeedPage/page.tsx
+++ assets/js/pages/FeedPage/page.tsx
@@ -2,18 +2,31 @@ import * as React from "react";
 import * as Paper from "@/components/PaperContainer";
 import * as Pages from "@/components/Pages";
 
+import { Feed, useItemsQuery } from "@/features/Feed";
 import { useLoadedData } from "./loader";
 
 export function Page() {
-  const data = useLoadedData();
+  const { company } = useLoadedData();
 
   return (
     <Pages.Page title={"FeedPage"}>
-      <Paper.Root>
+      <Paper.Root size="large">
         <Paper.Body>
-          <div className="text-content-accent text-3xl font-extrabold">FeedPage</div>
+          <div className="text-content-accent text-4xl font-extrabold">The Feed</div>
+          <div className="mb-8">All the latest activity in {company.name}</div>
+          <CompanyFeed />
         </Paper.Body>
       </Paper.Root>
     </Pages.Page>
   );
+}
+
+function CompanyFeed() {
+  const { company } = useLoadedData();
+  const { data, loading, error } = useItemsQuery("company", company.id);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error</div>;
+
+  return <Feed items={data.activities} testId="company-feed" page="company" />;
 }
