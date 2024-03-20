87739b924af411897645216e50f2ef41823dc155
Igor Šarčević <igor@renderedtext.com>
2024-03-20 10:23:55 +0000

    Add feed to profile page

--- assets/js/pages/ProfilePage/page.tsx
+++ assets/js/pages/ProfilePage/page.tsx
@@ -1,30 +1,58 @@
 import * as React from "react";
 import * as Paper from "@/components/PaperContainer";
 import * as Pages from "@/components/Pages";
+import * as People from "@/models/people";
 
 import { useLoadedData } from "./loader";
 import Avatar from "@/components/Avatar";
+import { Feed, useItemsQuery } from "@/features/Feed";
 
 export function Page() {
   const { person } = useLoadedData();
 
   return (
-    <Pages.Page title={"ProfilePage"}>
+    <Pages.Page title={[person.fullName, "Profile"]}>
       <Paper.Root>
         <Paper.Navigation>
           <Paper.NavItem linkTo={`/people`}>People</Paper.NavItem>
         </Paper.Navigation>
 
         <Paper.Body>
-          <div className="flex items-center gap-4">
-            <Avatar person={person} size={72} />
-            <div className="flex flex-col">
-              <div className="text-xl font-bold">{person.fullName}</div>
-              <div className="font-medium">{person.title}</div>
-            </div>
-          </div>
+          <Header />
+          <PersonFeed />
         </Paper.Body>
       </Paper.Root>
     </Pages.Page>
+  );
+}
+
+function Header() {
+  const { person } = useLoadedData();
+
+  return (
+    <div className="flex items-center gap-4">
+      <Avatar person={person} size={72} />
+      <div className="flex flex-col">
+        <div className="text-xl font-bold">{person.fullName}</div>
+        <div className="font-medium">{person.title}</div>
+      </div>
+    </div>
+  );
+}
+
+function PersonFeed() {
+  const { person } = useLoadedData();
+  const { data, loading, error } = useItemsQuery("person", person.id);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error</div>;
+
+  return (
+    <Paper.DimmedSection>
+      <div className="uppercase text-xs text-content-accent font-semibold mb-4">
+        {People.firstName(person)}'s Activity
+      </div>
+      <Feed items={data.activities} testId="profile-feed" page="profile" />
+    </Paper.DimmedSection>
   );
 }
--- lib/operately/activities/list_activities_operation.ex
+++ lib/operately/activities/list_activities_operation.ex
@@ -1,20 +1,29 @@
 defmodule Operately.Activities.ListActivitiesOperation do
   import Ecto.Query
-
   alias Operately.Repo
   alias Operately.Activities.Activity
 
   def run(scope_type, scope_id) do
-    query(scope_type, scope_id) |> Repo.all()
+    Activity
+    |> scope_query(scope_type, scope_id)
+    |> company_query()
+    |> order_desc()
+    |> Repo.all()
   end
 
-  def query(scope_type, scope_id) do
-    key = "#{scope_type}_id"
-    id = scope_id
+  def company_query(query) do
+    from a in query, where: fragment("? ->> ? IS NOT NULL", a.content, "company_id")
+  end
 
-    from a in Activity,
-      where: fragment("? ->> ? = ?", a.content, ^key, ^id),
-      where: fragment("? ->> ? IS NOT NULL", a.content, "company_id"),
-      order_by: [desc: a.inserted_at]
+  def scope_query(query, "person", scope_id) do
+    from a in query, where: a.author_id == ^scope_id
+  end
+
+  def scope_query(query, scope_type, scope_id) do
+    from a in query, where: fragment("? ->> ? = ?", a.content, ^"#{scope_type}_id", ^scope_id)
+  end
+
+  def order_desc(query) do
+    from a in query, order_by: [desc: a.inserted_at]
   end
 end
