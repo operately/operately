9d192b78d48ccc66e0d6d0fd2df783306de4865c
Igor Šarčević <igor@renderedtext.com>
2024-03-25 13:56:03 +0000

    Use goals tree to show goals on the space/goals page

--- assets/js/pages/GroupGoalsPage/loader.tsx
+++ assets/js/pages/GroupGoalsPage/loader.tsx
@@ -4,8 +4,10 @@ import * as Pages from "@/components/Pages";
 import * as Groups from "@/graphql/Groups";
 import * as Goals from "@/models/goals";
 import * as Companies from "@/models/companies";
+import * as Time from "@/utils/time";
 
 import { Company, Group } from "@/gql/generated";
+import { useSearchParams } from "react-router-dom";
 
 interface LoadedData {
   company: Company;
@@ -13,7 +15,9 @@ interface LoadedData {
   goals: Goals.Goal[];
 }
 
-export async function loader({ params }): Promise<LoadedData> {
+export async function loader({ request, params }): Promise<LoadedData> {
+  const timeframe = new URL(request.url).searchParams.get("timeframe") || Time.currentQuarter();
+
   const groupData = await client.query({
     query: Groups.GET_GROUP,
     variables: { id: params.id },
@@ -24,6 +28,7 @@ export async function loader({ params }): Promise<LoadedData> {
     company: await Companies.getCompany(),
     goals: await Goals.getGoals({
       spaceId: groupData.data.group.id,
+      timeframe,
       includeTargets: true,
     }),
     group: groupData.data.group,
@@ -32,4 +37,19 @@ export async function loader({ params }): Promise<LoadedData> {
 
 export function useLoadedData(): LoadedData {
   return Pages.useLoadedData() as LoadedData;
+}
+
+export function useTimeframeControles() {
+  let [searchParams, setSearchParams] = useSearchParams();
+  const timeframe = searchParams.get("timeframe") || Time.currentQuarter();
+
+  const next = () => {
+    setSearchParams({ timeframe: Time.nextQuarter(timeframe) });
+  };
+
+  const prev = () => {
+    setSearchParams({ timeframe: Time.prevQuarter(timeframe) });
+  };
+
+  return [timeframe, next, prev] as const;
 }
