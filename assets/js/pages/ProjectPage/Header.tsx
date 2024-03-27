5c3e0d7236b37d8e9ffb7749f676528696de6f07
Igor Šarčević <igor@renderedtext.com>
2024-03-27 17:34:30 +0000

    Show parent goal in header

--- assets/js/pages/ProjectPage/Header.tsx
+++ assets/js/pages/ProjectPage/Header.tsx
@@ -1,15 +1,17 @@
-import React from "react";
-
-import classnames from "classnames";
-import ContributorAvatar from "@/components/ContributorAvatar";
-
-import { Link } from "react-router-dom";
-import { Project } from "@/models/projects";
+import * as React from "react";
 import * as Icons from "@tabler/icons-react";
 import * as Projects from "@/models/projects";
 
-import { TextTooltip } from "@/components/Tooltip";
+import classnames from "classnames";
+import ContributorAvatar from "@/components/ContributorAvatar";
 import Options from "./Options";
+
+import { Link } from "react-router-dom";
+import { DivLink } from "@/components/Link";
+import { Project } from "@/models/projects";
+
+import { TextTooltip } from "@/components/Tooltip";
+import { Paths } from "@/routes/paths";
 import { GhostButton } from "@/components/Button";
 
 interface HeaderProps {
@@ -28,18 +30,41 @@ export default function Header({ project }: HeaderProps): JSX.Element {
 
 function ProjectName({ project }) {
   return (
-    <div className="flex items-center justify-between">
-      <div
-        className={classnames("flex gap-2 items-center", "font-bold", "break-all", "text-3xl", "text-content-accent")}
+    <div className="flex items-start justify-between truncate">
+      <div className="flex-1 truncate">
+        <ParentGoal project={project} />
+
+        <div className={classnames("flex gap-3 items-center", "text-content-accent", "truncate mr-12")}>
+          <div className="bg-indigo-500/10 p-1.5 rounded-lg">
+            <Icons.IconHexagons size={24} className="text-indigo-500" />
+          </div>
+
+          <div className="inline-flex items-center gap-2 truncate">
+            <div className="font-bold text-2xl text-content-accent truncate flex-1">{project.name}</div>
+          </div>
+
+          <PrivateIndicator project={project} />
+        </div>
+      </div>
+
+      <Options project={project} />
+    </div>
+  );
+}
+
+function ParentGoal({ project }) {
+  if (!project.goal) return null;
+
+  return (
+    <div className="flex items-center">
+      <div className="border-t-2 border-l-2 border-stroke-base rounded-tl w-7 h-2.5 ml-4 mb-1 mt-2.5 mr-1" />
+      <Icons.IconTarget size={14} className="text-red-500" />
+      <DivLink
+        to={Paths.goalPath(project.goal.id)}
+        className="text-sm text-content-dimmed mx-1 hover:underline font-medium"
       >
-        {project.name}
-
-        <PrivateIndicator project={project} />
-      </div>
-
-      <div className="flex gap-4 items-center">
-        <Options project={project} />
-      </div>
+        {project.goal.name}
+      </DivLink>
     </div>
   );
 }
