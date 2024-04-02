2a2491bca0d401af2ebdfdcede00512dd05077ee
Igor Šarčević <igor@renderedtext.com>
2024-04-02 14:28:15 +0000

    Gen profile goals page

---
+++ assets/js/pages/ProfileGoalsPage/page.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import * as Paper from "@/components/PaperContainer";
+import * as Pages from "@/components/Pages";
+
+import { useLoadedData } from "./loader";
+
+export function Page() {
+  const data = useLoadedData();
+
+  return (
+    <Pages.Page title={"ProfileGoalsPage"}>
+      <Paper.Root>
+        <Paper.Body>
+          <div className="text-content-accent text-3xl font-extrabold">ProfileGoalsPage</div>
+        </Paper.Body>
+      </Paper.Root>
+    </Pages.Page>
+  );
+}
