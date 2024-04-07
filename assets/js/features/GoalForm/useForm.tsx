741a343ae321e1029f4da7b9fb7b4f9d13c45c89
Igor Šarčević <igor@renderedtext.com>
2024-04-07 08:51:05 +0000

    Fix creation of goals that have parents

--- assets/js/features/GoalForm/useForm.tsx
+++ assets/js/features/GoalForm/useForm.tsx
@@ -253,7 +253,7 @@ function useSubmit(fields: Fields, config: FormConfig): [() => Promise<boolean>,
             reviewerID: fields.reviewer!.id,
             timeframe: fields.timeframe.value,
             description: prepareDescriptionForSave(fields),
-            parentGoal: config.parentGoal?.id,
+            parentGoalId: config.parentGoal?.id,
             targets: fields.targets
               .filter((t) => t.name.trim() !== "")
               .map((t, index) => ({
