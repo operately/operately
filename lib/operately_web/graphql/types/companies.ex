a06c963479757da1630039bf72dc0c08cfc3972c
Igor Šarčević <igor@operately.com>
2024-04-26 10:58:05 +0000

    Allow not setting mission for companies
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- lib/operately_web/graphql/types/companies.ex
+++ lib/operately_web/graphql/types/companies.ex
@@ -4,7 +4,7 @@ defmodule OperatelyWeb.Graphql.Types.Companies do
   object :company do
     field :id, non_null(:id)
     field :name, non_null(:string)
-    field :mission, non_null(:string)
+    field :mission, :string
     field :trusted_email_domains, list_of(:string)
     field :enabled_experimental_features, list_of(:string)
     field :company_space_id, :string
