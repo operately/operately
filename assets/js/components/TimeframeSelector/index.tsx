c0245a452d22458c9c3291ed938ad1b59c769c68
Igor Šarčević <igor@operately.com>
2024-04-27 16:44:51 +0000

    Clean up timeframe selector interface
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- assets/js/components/TimeframeSelector/index.tsx
+++ assets/js/components/TimeframeSelector/index.tsx
@@ -10,6 +10,8 @@ import { YearPicker } from "./YearPicker";
 import { CustomRangePicker } from "./CustomRangePicker";
 import { Timeframe, TimeframeType, SetTimeframe } from "./timeframe";
 import { formatTimeframe } from "./formatTimeframe";
+import { SegmentedControl } from "./SegmentedControl";
+import classNames from "classnames";
 
 interface TimeframeSelectorProps {
   timeframe: Timeframe;
@@ -39,35 +41,49 @@ function TimeframeSelectorFormElement(props: TimeframeSelectorProps) {
 }
 
 function PopeverContent(props: TimeframeSelectorProps) {
+  const className = classNames(
+    "z-[100] overflow-hidden",
+    "border border-surface-outline",
+    "rounded-lg shadow-xl",
+    "bg-surface",
+    "flex flex-col items-start p-6",
+  );
+
   return (
     <Popover.Portal>
-      <Popover.Content
-        className={"rounded-lg border border-surface-outline z-[100] shadow-xl overflow-hidden bg-surface"}
-        align="start"
-        sideOffset={5}
-      >
-        <div className="flex flex-col items-start p-6">
-          <div className="flex items-center justify-between gap-10 w-full border-b border-stroke-base pb-3 mb-3">
-            <div className="">
-              <div className="font-bold shrink-0">Select Timeframe</div>
-              <div className="text-content-dimmed text-xs">{formatTimeframe(props.timeframe)}</div>
-            </div>
-            <SegmentedControl
-              options={[
-                { label: "Month", value: "month" },
-                { label: "Quarter", value: "quarter" },
-                { label: "Year", value: "year" },
-                { label: "Custom", value: "days" },
-              ]}
-              value={props.timeframe.type}
-              onChange={(value) => props.setTimeframe({ ...props.timeframe, type: value as TimeframeType })}
-            />
-          </div>
-
-          <TimeframeSelectorContent timeframe={props.timeframe} setTimeframe={props.setTimeframe} />
-        </div>
+      <Popover.Content className={className} align="start" sideOffset={5}>
+        <TimeframeSelectorHeader timeframe={props.timeframe} setTimeframe={props.setTimeframe} />
+        <TimeframeSelectorContent timeframe={props.timeframe} setTimeframe={props.setTimeframe} />
       </Popover.Content>
     </Popover.Portal>
+  );
+}
+
+function TimeframeSelectorHeader(props: { timeframe: Timeframe; setTimeframe: SetTimeframe }) {
+  return (
+    <div className="flex items-center justify-between gap-10 w-full border-b border-stroke-base pb-3 mb-3">
+      <div className="">
+        <div className="font-bold shrink-0">Select Timeframe</div>
+        <div className="text-content-dimmed text-xs">{formatTimeframe(props.timeframe)}</div>
+      </div>
+
+      <TimeframeSelectorTypeSelector timeframe={props.timeframe} setTimeframe={props.setTimeframe} />
+    </div>
+  );
+}
+
+function TimeframeSelectorTypeSelector(props: { timeframe: Timeframe; setTimeframe: SetTimeframe }) {
+  return (
+    <SegmentedControl
+      options={[
+        { label: "Month", value: "month" },
+        { label: "Quarter", value: "quarter" },
+        { label: "Year", value: "year" },
+        { label: "Custom", value: "days" },
+      ]}
+      value={props.timeframe.type}
+      onChange={(value) => props.setTimeframe({ ...props.timeframe, type: value as TimeframeType })}
+    />
   );
 }
 
@@ -78,30 +94,4 @@ function TimeframeSelectorContent({ timeframe, setTimeframe }: { timeframe: Time
     .with("year", () => <YearPicker timeframe={timeframe} setTimeframe={setTimeframe} />)
     .with("days", () => <CustomRangePicker timeframe={timeframe} setTimeframe={setTimeframe} />)
     .exhaustive();
-}
-
-function SegmentedControl({
-  options,
-  value,
-  onChange,
-}: {
-  options: any[];
-  value: string;
-  onChange: (value: string) => void;
-}) {
-  return (
-    <div className="flex items-center bg-surface-dimmed p-0.5 rounded-lg">
-      {options.map((option) => (
-        <button
-          key={option.value}
-          className={`w-full px-2.5 py-1 text-sm font-medium rounded-lg ${
-            option.value === value ? "bg-surface border border-stroke-base" : "bg-transparent"
-          }`}
-          onClick={() => onChange(option.value)}
-        >
-          {option.label}
-        </button>
-      ))}
-    </div>
-  );
 }
