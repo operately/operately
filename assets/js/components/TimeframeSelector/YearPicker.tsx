7dbc4581d9fd2b66bec8127615d670d4a8a4654a
Igor Šarčević <igor@operately.com>
2024-04-28 11:46:00 +0000

    Fix year selector range
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- assets/js/components/TimeframeSelector/YearPicker.tsx
+++ assets/js/components/TimeframeSelector/YearPicker.tsx
@@ -4,6 +4,8 @@ import DatePicker from "react-datepicker";
 import { LeftChevron, RightChevron } from "./Chevrons";
 import { Timeframe, SetTimeframe } from "./timeframe";
 
+const YEAR_OPTION_COUNT = 6;
+
 export function YearPicker({ timeframe, setTimeframe }: { timeframe: Timeframe; setTimeframe: SetTimeframe }) {
   return (
     <DatePicker
@@ -12,7 +14,7 @@ export function YearPicker({ timeframe, setTimeframe }: { timeframe: Timeframe; 
       onChange={(date) => setTimeframe({ ...timeframe, startDate: date })}
       calendarClassName="w-full"
       showYearPicker
-      yearItemNumber={6}
+      yearItemNumber={YEAR_OPTION_COUNT}
       renderYearContent={renderYearContent}
       renderCustomHeader={Header}
     />
@@ -20,10 +22,16 @@ export function YearPicker({ timeframe, setTimeframe }: { timeframe: Timeframe; 
 }
 
 function Header({ date, decreaseYear, increaseYear }) {
+  const year = date.getFullYear();
+  const end = Math.ceil(year / YEAR_OPTION_COUNT) * YEAR_OPTION_COUNT;
+  const start = end - (YEAR_OPTION_COUNT - 1);
+
   return (
     <div className="flex items-center w-full px-1 pb-1 gap-2 font-medium mb-2">
       <LeftChevron onClick={decreaseYear} />
-      <div>{date.getFullYear()}</div>
+      <div>
+        {start} - {end}
+      </div>
       <RightChevron onClick={increaseYear} />
     </div>
   );
