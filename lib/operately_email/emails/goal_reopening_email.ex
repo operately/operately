4eb740fc376791e5a992b73b2dbd438242d72147
Igor Šarčević <igor@operately.com>
2024-05-14 14:39:42 +0000

    Send email for reopening
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- lib/operately_email/emails/goal_reopening_email.ex
+++ lib/operately_email/emails/goal_reopening_email.ex
@@ -1,16 +1,24 @@
 defmodule OperatelyEmail.Emails.GoalReopeningEmail do
   import OperatelyEmail.Mailers.ActivityMailer
+  alias Operately.{Repo, Goals}
 
   def send(person, activity) do
-    raise "Email for GoalReopening not implemented"
+    author = Repo.preload(activity, :author).author
+    company = Repo.preload(author, :company).company
+    goal = Goals.get_goal!(activity.content["goal_id"])
+    space = Operately.Groups.get_group!(goal.group_id)
+    link = OperatelyEmail.goal_activity_url(goal.id, activity.id)
+    message = Operately.Repo.preload(activity, :comment_thread).comment_thread.message
 
-    # author = Repo.preload(activity, :author).author
-
-    # company
-    # |> new()
-    # |> to(person)
-    # |> subject(who: author, action: "did something")
-    # |> assign(:author, author)
-    # |> render("goal_reopening")
+    company
+    |> new()
+    |> from(author)
+    |> to(person)
+    |> subject(where: space.name, who: author, action: "reopened the #{goal.name} goal")
+    |> assign(:goal, goal)
+    |> assign(:author, author)
+    |> assign(:link, link)
+    |> assign(:message, message)
+    |> render("goal_reopening")
   end
 end
