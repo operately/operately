71b3cbd64963855f8bea880addc1e25373c9878a
Igor Šarčević <igor@operately.com>
2024-05-15 11:34:06 +0000

    Visual adjustments for the update progress page
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- assets/js/features/goals/GoalCheckInForm/Form.tsx
+++ assets/js/features/goals/GoalCheckInForm/Form.tsx
@@ -10,73 +10,62 @@ export function Form({ form }: { form: FormState }) {
   return (
     <>
       <Header />
+      <TargetInputs form={form} />
       <Editor form={form} />
     </>
   );
 }
 
 function Header() {
-  return (
-    <div>
-      <div className="uppercase text-content-accent tracking-wide w-full mb-1 text-sm font-semibold">CHECK-IN</div>
-      <div className="text-4xl font-bold mx-auto">What's new since the last check-in?</div>
-    </div>
-  );
+  return <div className="text-3xl font-bold mb-8">Update Progress</div>;
 }
 
 function Editor({ form }: { form: FormState }) {
   const contentError = form.errors.find((e) => e.field === "content");
 
   return (
-    <div className="mt-4">
-      <TipTapEditor.Root editor={form.editor.editor}>
-        <TipTapEditor.Toolbar editor={form.editor.editor} />
-
-        <div
-          className="text-content-accent text-lg relative border-b border-stroke-base"
-          style={{ minHeight: "350px" }}
-        >
-          <TipTapEditor.EditorContent editor={form.editor.editor} />
-        </div>
-        {contentError && <div className="text-red-500 text-sm font-medium mt-1">Check-In message is required</div>}
-
-        <p className="font-bold text-lg mt-8">Success Conditions</p>
-        <p className="text-content-dimmed">What are the current values of your success conditions?</p>
-
-        <TargetInputs form={form} />
-      </TipTapEditor.Root>
+    <div className="mt-8">
+      <div className="font-bold mb-2">Describe your progress and any learnings</div>
+      {contentError && <div className="text-red-500 text-sm font-medium mt-1">Required</div>}
+      <div className="border border-surface-outline rounded overflow-hidden">
+        <TipTapEditor.StandardEditorForm editor={form.editor.editor} />
+      </div>
     </div>
   );
 }
 
 function TargetInputs({ form }: { form: FormState }) {
   return (
-    <div className="my-4 flex flex-col gap-4">
-      {form.targets.map((target, index) => {
-        return (
-          <div
-            className="flex items-center justify-between bg-surface-dimmed border border-stroke-base p-3 rounded"
-            key={index}
-          >
-            <div className="flex flex-col">
-              <div className="font-semibold text-content-accent">{target.name}</div>
-              <div className="text-content-dimmed text-sm">
-                Target: {target.to} {target.unit}
+    <div>
+      <div className="font-bold mb-2">Success Conditions</div>
+
+      <div className="flex flex-col gap-4">
+        {form.targets.map((target, index) => {
+          return (
+            <div
+              className="flex items-center justify-between bg-surface-dimmed border border-stroke-base p-3 rounded"
+              key={index}
+            >
+              <div className="flex flex-col">
+                <div className="font-semibold text-content-accent">{target.name}</div>
+                <div className="text-content-dimmed text-sm">
+                  Target: {target.to} {target.unit}
+                </div>
+              </div>
+
+              <div className="">
+                <Forms.TextInputNoLabel
+                  id={target.id}
+                  testId={createTestId("target", target.name)}
+                  value={target.value?.toString() || ""}
+                  onChange={(value: string) => form.updateTarget(target.id, value === "" ? null : Number(value))}
+                  error={!!form.errors.find((e) => e.field === target.id)}
+                />
               </div>
             </div>
-
-            <div className="">
-              <Forms.TextInputNoLabel
-                id={target.id}
-                testId={createTestId("target", target.name)}
-                value={target.value?.toString() || ""}
-                onChange={(value: string) => form.updateTarget(target.id, value === "" ? null : Number(value))}
-                error={!!form.errors.find((e) => e.field === target.id)}
-              />
-            </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 }
--- assets/js/pages/GoalProgressUpdateNewPage/index.tsx
+++ assets/js/pages/GoalProgressUpdateNewPage/index.tsx
@@ -1,2 +1,83 @@
-export { loader } from "./loader";
-export { Page } from "./page";
+import * as Pages from "@/components/Pages";
+import * as Paper from "@/components/PaperContainer";
+import * as Goals from "@/models/goals";
+import * as People from "@/models/people";
+import * as React from "react";
+
+import { FilledButton } from "@/components/Button";
+import { Form, useForm } from "@/features/goals/GoalCheckInForm";
+import { InlinePeopleList } from "@/components/InlinePeopleList";
+
+interface LoaderResult {
+  goal: Goals.Goal;
+  me: People.Person;
+}
+
+export async function loader({ params }): Promise<LoaderResult> {
+  return {
+    goal: await Goals.getGoal({
+      id: params.goalId,
+      includeTargets: true,
+    }),
+    me: await People.getMe({}),
+  };
+}
+
+export function Page() {
+  const { goal, me } = Pages.useLoadedData<LoaderResult>();
+
+  const form = useForm({ goal, mode: "create" });
+
+  return (
+    <Pages.Page title={["Check-In", goal.name]}>
+      <Paper.Root>
+        <Navigation goal={goal} />
+
+        <Paper.Body>
+          <Form form={form} />
+          <WhoWillBeNotified goal={goal} me={me} />
+          <SubmitButton form={form} />
+        </Paper.Body>
+      </Paper.Root>
+    </Pages.Page>
+  );
+}
+
+function Navigation({ goal }) {
+  return (
+    <Paper.Navigation>
+      <Paper.NavItem linkTo={`/goals/${goal.id}`}>{goal.name}</Paper.NavItem>
+    </Paper.Navigation>
+  );
+}
+
+function SubmitButton({ form }) {
+  return (
+    <div className="mt-8">
+      <div className="flex items-center gap-4">
+        <FilledButton
+          type="primary"
+          onClick={form.submit}
+          loading={form.submitting}
+          testId="submit-update"
+          bzzzOnClickFailure
+        >
+          Submit Update
+        </FilledButton>
+      </div>
+    </div>
+  );
+}
+
+function WhoWillBeNotified({ goal, me }: { goal: Goals.Goal; me: People.Person }) {
+  const people = [goal.champion!, goal.reviewer!].filter((person) => person.id !== me.id);
+
+  return (
+    <div className="mt-8 font-medium">
+      <p className="font-bold">When you submit:</p>
+      <div className="inline-flex gap-1 flex-wrap mt-1">
+        <InlinePeopleList people={people} /> will be notified.
+      </div>
+    </div>
+  );
+}
