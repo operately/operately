4cc3e858df0ab4d18f02ad832161d5de1be28d04
Igor Šarčević <igor@operately.com>
2024-05-16 10:10:11 +0000

    Define page for starting discussions
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- assets/js/features/DiscussionForm/Form.tsx
+++ assets/js/features/DiscussionForm/Form.tsx
@@ -1,60 +1,21 @@
-import React from "react";
-
-import classnames from "classnames";
+import * as React from "react";
 import * as TipTapEditor from "@/components/Editor";
+
 import { Spacer } from "@/components/Spacer";
 import { FormState } from "./useForm";
+import { FormTitleInput } from "@/components/FormTitleInput";
 
 export function Form({ form }: { form: FormState }) {
   return (
     <>
-      <Title form={form} />
+      <FormTitleInput
+        value={form.title}
+        onChange={form.setTitle}
+        error={!!form.errors.find((e) => e.field === "name")}
+      />
       <Message editor={form.editor} />
       <Spacer size={4} />
     </>
-  );
-}
-
-function Title({ form }: { form: FormState }) {
-  const className = classnames(
-    "bg-surface",
-    "text-3xl",
-    "font-semibold",
-    "border-none",
-    "outline-none",
-    "focus:outline-none",
-    "focus:ring-0",
-    "px-0 py-1",
-    "w-full",
-    "resize-none",
-    "ring-0",
-    "placeholder:text-content-subtle",
-    "leading-wide",
-    {
-      "bg-red-400/10": form.errors.find((e) => e.field === "name"),
-    },
-  );
-
-  const textareaRef = React.useRef<HTMLTextAreaElement>(null);
-
-  React.useEffect(() => {
-    if (!textareaRef.current) return;
-
-    textareaRef.current.style.height = "0px";
-    const scrollHeight = textareaRef.current.scrollHeight;
-    textareaRef.current.style.height = scrollHeight + "px";
-  }, [form.title]);
-
-  return (
-    <textarea
-      ref={textareaRef}
-      autoFocus
-      className={className}
-      placeholder="Title&hellip;"
-      value={form.title}
-      onChange={(e) => form.setTitle(e.target.value)}
-      data-test-id="discussion-title"
-    ></textarea>
   );
 }
 
--- assets/js/pages/GoalDiscussionNewPage/index.tsx
+++ assets/js/pages/GoalDiscussionNewPage/index.tsx
@@ -2,33 +2,73 @@ import * as React from "react";
 import * as Pages from "@/components/Pages";
 import * as Paper from "@/components/PaperContainer";
 import * as Goals from "@/models/goals";
+import * as People from "@/models/people";
+import * as TipTapEditor from "@/components/Editor";
+
+import { FormTitleInput } from "@/components/FormTitleInput";
+import { FilledButton } from "@/components/Button";
+import { DimmedLink } from "@/components/Link";
+import { Paths } from "@/routes/paths";
+import { GoalSubpageNavigation } from "@/features/goals/GoalSubpageNavigation";
+import { InlinePeopleList } from "@/components/InlinePeopleList";
 
 interface LoaderResult {
   goal: Goals.Goal;
+  me: People.Person;
 }
 
 export async function loader({ params }): Promise<LoaderResult> {
   return {
     goal: await Goals.getGoal({ id: params.goalId, includeParentGoal: true }),
+    me: await People.getMe({}),
   };
 }
 
 export function Page() {
-  const { goal } = Pages.useLoadedData<LoaderResult>();
+  const { goal, me } = Pages.useLoadedData<LoaderResult>();
+
+  const [title, setTitle] = React.useState("");
+  const editor = TipTapEditor.useEditor({
+    placeholder: "Start a new discussion...",
+    className: "min-h-[350px] py-2 text-lg",
+  });
 
   return (
     <Pages.Page title={["New Discussion", goal.name]}>
       <Paper.Root>
-        <Paper.Body>
-          <Form form={form} />
-        </Paper.Body>
+        <GoalSubpageNavigation goal={goal} />
 
-        <div className="flex justify-center items-center mt-8">
-          <FilledButton loading={form.submitting} testId="post-discussion" onClick={form.submit} size="lg">
-            {form.submitButtonLabel}
-          </FilledButton>
-        </div>
+        <Paper.Body>
+          <FormTitleInput value={title} onChange={setTitle} error={false} testID="discussion-title" />
+
+          <div className="mt-2 border-y border-stroke-base text-content-base font-medium ">
+            <TipTapEditor.StandardEditorForm editor={editor.editor} />
+          </div>
+
+          <WhoWillBeNotified goal={goal} me={me} />
+
+          <div className="flex items-center gap-4 mt-4">
+            <FilledButton testId="post-discussion" onClick={() => null}>
+              Post Discussion
+            </FilledButton>
+
+            <DimmedLink to={Paths.goalDiscussionsPath(goal.id)}>Cancel</DimmedLink>
+          </div>
+        </Paper.Body>
       </Paper.Root>
     </Pages.Page>
+  );
+}
+
+function WhoWillBeNotified({ goal, me }: { goal: Goals.Goal; me: People.Person }) {
+  const people = [goal.champion!, goal.reviewer!].filter((person) => person.id !== me.id);
+
+  return (
+    <div className="mt-10 font-medium">
+      <p className="font-bold">When you submit:</p>
+      <div className="inline-flex gap-1 flex-wrap mt-1">
+        <InlinePeopleList people={people} /> will be notified.
+      </div>
+    </div>
   );
 }
