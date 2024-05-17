de868031b2ed774c38444d576518d2cad4bbae94
Igor Šarčević <igor@operately.com>
2024-05-17 07:34:11 +0000

    Implement mailing
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- lib/operately_email/emails/goal_discussion_creation_email.ex
+++ lib/operately_email/emails/goal_discussion_creation_email.ex
@@ -1,16 +1,27 @@
 defmodule OperatelyEmail.Emails.GoalDiscussionCreationEmail do
   import OperatelyEmail.Mailers.ActivityMailer
+  alias Operately.{Repo, Goals}
 
   def send(person, activity) do
-    raise "Email for GoalDiscussionCreation not implemented"
+    author = Repo.preload(activity, :author).author
+    company = Repo.preload(author, :company).company
+    goal = Goals.get_goal!(activity.content["goal_id"])
 
-    # author = Repo.preload(activity, :author).author
+    comment_thread = Repo.preload(activity, :comment_thread).comment_thread
+    title = comment_thread.title
+    message = comment_thread.message
+    link = OperatelyEmail.goal_activity_url(goal.id, activity.id)
 
-    # company
-    # |> new()
-    # |> to(person)
-    # |> subject(who: author, action: "did something")
-    # |> assign(:author, author)
-    # |> render("goal_discussion_creation")
+    company
+    |> new()
+    |> from(author)
+    |> to(person)
+    |> subject(where: goal.name, who: author, action: "posted: #{title}")
+    |> assign(:author, author)
+    |> assign(:goal, goal)
+    |> assign(:title, title)
+    |> assign(:message, message)
+    |> assign(:link, link)
+    |> render("goal_discussion_creation")
   end
 end
