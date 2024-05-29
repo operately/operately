4c9e4658d61595d4cb7368f45b41ccd149830189
Igor Šarčević <igisar@gmail.com>
2024-05-29 18:37:35 +0200

    One of operator (#495)
    * Add one_of operator
    
    Signed-off-by: Igor Šarčević <igor@operately.com>
    
    * Allow the creation of sum types
    
    Signed-off-by: Igor Šarčević <igor@operately.com>
    
    * Always validate created fields
    
    Signed-off-by: Igor Šarčević <igor@operately.com>
    
    ---------
    
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- assets/js/api/index.tsx
+++ assets/js/api/index.tsx
@@ -1,3 +1,37 @@
+export interface Activity {
+  id: string;
+  scopeType: string;
+  scopeId: string;
+  resourceId: string;
+  resourceType: string;
+  actionType: string;
+  insertedAt: Date;
+  updatedAt: Date;
+  commentThread: CommentThread;
+  author: Person;
+  person: Person;
+  resource: Project | Milestone | Comment;
+}
+
+export interface Comment {
+  id: string;
+  insertedAt: Date;
+  content: string;
+  author: Person;
+  reactions: Reaction[];
+}
+
+export interface CommentThread {
+  id: string;
+  insertedAt: Date;
+  title: string;
+  message: string;
+  reactions: Reaction[];
+  comments: Comment[];
+  commentsCount: number;
+  author: Person;
+}
+
 export interface Company {
   id: string;
   name: string;
@@ -7,6 +41,24 @@ export interface Company {
   enabledExperimentalFeatures: string[];
   admins: Person[];
   people: Person[];
+}
+
+export interface Milestone {
+  id: string;
+  title: string;
+  status: string;
+  insertedAt: Date;
+  deadlineAt: Date;
+  completedAt: Date;
+  description: string;
+  comments: MilestoneComment[];
+  tasksKanbanState: string;
+}
+
+export interface MilestoneComment {
+  id: string;
+  action: string;
+  comment: Comment;
 }
 
 export interface Person {
@@ -27,4 +79,68 @@ export interface Person {
   theme: string;
   reports: Person[];
   peers: Person[];
+}
+
+export interface Project {
+  id: string;
+  name: string;
+  insertedAt: Date;
+  updatedAt: Date;
+  startedAt: Date;
+  deadline: Date;
+  nextUpdateScheduledAt: Date;
+  nextCheckInScheduledAt: Date;
+  private: boolean;
+  status: string;
+  closedAt: Date;
+  retrospective: string;
+  description: string;
+  lastCheckIn: ProjectCheckIn;
+  milestones: Milestone[];
+  contributors: ProjectContributor[];
+  keyResources: ProjectKeyResource[];
+  closedBy: Person;
+  isOutdated: boolean;
+  spaceId: string;
+  myRole: string;
+  permissions: string;
+  nextMilestone: Milestone;
+  isPinned: boolean;
+  isArchived: boolean;
+  archivedAt: Date;
+  champion: Person;
+  reviewer: Person;
+}
+
+export interface ProjectCheckIn {
+  id: string;
+  status: string;
+  insertedAt: Date;
+  description: string;
+  author: Person;
+  project: Project;
+  acknowledgedAt: Date;
+  acknowledgedBy: Person;
+  reactions: Reaction[];
+}
+
+export interface ProjectContributor {
+  id: string;
+  responsibility: string;
+  role: string;
+  person: Person;
+}
+
+export interface ProjectKeyResource {
+  id: string;
+  title: string;
+  link: string;
+  resourceType: string;
+}
+
+export interface Reaction {
+  id: string;
+  emoji: string;
+  reactionType: string;
+  person: Person;
 }
--- lib/operately_web/api.ex
+++ lib/operately_web/api.ex
@@ -39,4 +39,188 @@ defmodule OperatelyWeb.Api do
     field :people, list_of(:person)
   end
 
+  object :reaction do
+    field :id, :string
+    field :emoji, :string
+    field :reaction_type, :string
+    field :person, :person
+  end
+
+  object :comment do
+    field :id, :string
+    field :inserted_at, :datetime
+    field :content, :string
+    field :author, :person
+    field :reactions, list_of(:reaction)
+  end
+
+  object :comment_thread do
+    field :id, :string
+    field :inserted_at, :datetime
+    field :title, :string
+    field :message, :string
+    field :reactions, list_of(:reaction)
+    field :comments, list_of(:comment)
+    field :comments_count, :integer
+    field :author, :person
+  end
+
+  object :project_check_in do
+    field :id, :string
+    field :status, :string
+    field :inserted_at, :datetime
+    field :description, :string
+    field :author, :person
+    field :project, :project
+    field :acknowledged_at, :datetime
+    field :acknowledged_by, :person
+    field :reactions, list_of(:reaction)
+  end
+
+  object :milestone do
+    field :id, :string
+    field :title, :string
+    field :status, :string
+    field :inserted_at, :datetime
+    field :deadline_at, :datetime
+    field :completed_at, :datetime
+    field :description, :string
+    field :comments, list_of(:milestone_comment)
+    field :tasks_kanban_state, :string
+  end
+
+  object :milestone_comment do
+    field :id, :string
+    field :action, :string
+    field :comment, :comment
+  end
+
+  object :project_contributor do
+    field :id, :string
+    field :responsibility, :string
+    field :role, :string
+    field :person, :person
+  end
+
+  object :project_key_resource do
+    field :id, :string
+    field :title, :string
+    field :link, :string
+    field :resource_type, :string
+  end
+
+  object :project do
+    field :id, :string
+    field :name, :string
+    field :inserted_at, :datetime
+    field :updated_at, :datetime
+    field :started_at, :datetime
+    field :deadline, :datetime
+    field :next_update_scheduled_at, :datetime
+    field :next_check_in_scheduled_at, :datetime
+    field :private, :boolean
+    field :status, :string
+    field :closed_at, :datetime
+    field :retrospective, :string
+    field :description, :string
+    # field :goal, :goal
+    field :last_check_in, :project_check_in
+    field :milestones, list_of(:milestone)
+    field :contributors, list_of(:project_contributor)
+    field :key_resources, list_of(:project_key_resource)
+    field :closed_by, :person
+    field :is_outdated, :boolean
+    field :space_id, :string
+    # field :space, :group
+    field :my_role, :string
+    field :permissions, :string
+    field :next_milestone, :milestone
+    field :is_pinned, :boolean
+    field :is_archived, :boolean
+    field :archived_at, :datetime
+    field :champion, :person
+    field :reviewer, :person
+  end
+
+  object :activity do
+    field :id, :string
+    field :scope_type, :string
+    field :scope_id, :string
+    field :resource_id, :string
+    field :resource_type, :string
+    field :action_type, :string
+
+    field :inserted_at, :datetime
+    field :updated_at, :datetime
+
+    field :comment_thread, :comment_thread
+    field :author, :person
+    field :person, :person
+
+    field :resource, one_of([
+      :project, 
+      # :update, 
+      :milestone, 
+      :comment
+    ])
+
+    # field :event_data, one_of([
+      # :activity_event_data_project_create, 
+      # :activity_event_data_milestone_create, 
+      # :activity_event_data_comment_post
+    # ])
+
+    # field :content, :activity_content, one_of([
+      # :activity_content_comment_added,
+      # :activity_content_discussion_comment_submitted,
+      # :activity_content_discussion_editing,
+      # :activity_content_discussion_posting,
+      # :activity_content_goal_archived,
+      # :activity_content_goal_check_in,
+      # :activity_content_goal_check_in_acknowledgement,
+      # :activity_content_goal_check_in_edit,
+      # :activity_content_goal_closing,
+      # :activity_content_goal_created,
+      # :activity_content_goal_discussion_creation,
+      # :activity_content_goal_discussion_editing,
+      # :activity_content_goal_editing,
+      # :activity_content_goal_reopening,
+      # :activity_content_goal_reparent,
+      # :activity_content_goal_timeframe_editing,
+      # :activity_content_group_edited,
+      # :activity_content_project_archived,
+      # :activity_content_project_check_in_acknowledged,
+      # :activity_content_project_check_in_commented,
+      # :activity_content_project_check_in_edit,
+      # :activity_content_project_check_in_submitted,
+      # :activity_content_project_closed,
+      # :activity_content_project_contributor_addition,
+      # :activity_content_project_created,
+      # :activity_content_project_discussion_submitted,
+      # :activity_content_project_goal_connection,
+      # :activity_content_project_goal_disconnection,
+      # :activity_content_project_milestone_commented,
+      # :activity_content_project_moved,
+      # :activity_content_project_pausing,
+      # :activity_content_project_renamed,
+      # :activity_content_project_resuming,
+      # :activity_content_project_review_acknowledged,
+      # :activity_content_project_review_commented,
+      # :activity_content_project_review_request_submitted,
+      # :activity_content_project_review_submitted,
+      # :activity_content_project_timeline_edited,
+      # :activity_content_space_joining,
+      # :activity_content_task_adding,
+      # :activity_content_task_assignee_assignment,
+      # :activity_content_task_closing,
+      # :activity_content_task_description_change,
+      # :activity_content_task_name_editing,
+      # :activity_content_task_priority_change,
+      # :activity_content_task_reopening,
+      # :activity_content_task_size_change,
+      # :activity_content_task_status_change,
+      # :activity_content_task_update
+    # ])
+  end
+
 end
