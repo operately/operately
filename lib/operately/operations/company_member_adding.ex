4b8b78547fca204706d4eac5001733d189421b2f
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-05-29 13:42:48 -0300

    Add insert_invitation_token to CompanyMemberAdding operation
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- lib/operately/operations/company_member_adding.ex
+++ lib/operately/operations/company_member_adding.ex
@@ -1,36 +1,32 @@
 defmodule Operately.Operations.CompanyMemberAdding do
   alias Ecto.Multi
   alias Operately.Repo
-  alias Operately.People.Account
-  alias Operately.People.Person
-  alias Operately.Invitations.Invitation
-  alias Operately.Activities
-
 
   def run(admin, attrs) do
     Multi.new()
     |> insert_account(attrs)
     |> insert_person(admin, attrs)
     |> insert_invitation(admin)
+    |> insert_invitation_token()
     |> insert_activity(admin)
     |> Repo.transaction()
     |> Repo.extract_result(:person)
   end
 
-  def insert_account(multi, attrs) do
-    password = get_random_password()
+  defp insert_account(multi, attrs) do
+    password = get_random_string()
 
     Multi.insert(multi, :account,
-      Account.registration_changeset(%{email: attrs.email, password: password})
+      Operately.People.Account.registration_changeset(%{email: attrs.email, password: password})
     )
   end
 
-  def insert_person(multi, admin, attrs) do
+  defp insert_person(multi, admin, attrs) do
     attrs = Map.put(attrs, :company_id, admin.company_id)
     attrs = Map.put(attrs, :company_role, :member)
 
     Multi.insert(multi, :person, fn changes ->
-      Person.changeset(%{
+      Operately.People.Person.changeset(%{
         company_id: admin.company_id,
         account_id: changes[:account].id,
         full_name: attrs.full_name,
@@ -40,9 +36,9 @@ defmodule Operately.Operations.CompanyMemberAdding do
     end)
   end
 
-  def insert_invitation(multi, admin) do
+  defp insert_invitation(multi, admin) do
     Multi.insert(multi, :invitation, fn changes ->
-      Invitation.changeset(%{
+      Operately.Invitations.Invitation.changeset(%{
         member_id: changes[:person].id,
         admin_id: admin.id,
         admin_name: admin.full_name,
@@ -50,8 +46,19 @@ defmodule Operately.Operations.CompanyMemberAdding do
     end)
   end
 
-  def insert_activity(multi, admin) do
-    Activities.insert_sync(multi, admin.id, :company_member_added, fn changes ->
+  defp insert_invitation_token(multi) do
+    token = get_random_string()
+
+    Multi.insert(multi, :invitation_token, fn changes ->
+      Operately.Invitations.InvitationToken.changeset(%{
+        token: token,
+        invitation_id: changes[:invitation].id,
+      })
+    end)
+  end
+
+  defp insert_activity(multi, admin) do
+    Operately.Activities.insert_sync(multi, admin.id, :company_member_added, fn changes ->
       %{
         company_id: admin.company_id,
         invitatition_id: changes[:invitation].id,
@@ -62,9 +69,9 @@ defmodule Operately.Operations.CompanyMemberAdding do
     end)
   end
 
-  defp get_random_password do
-    :crypto.strong_rand_bytes(64)
+  defp get_random_string(length \\ 64) do
+    :crypto.strong_rand_bytes(length)
     |> Base.encode64
-    |> binary_part(0, 64)
+    |> binary_part(0, length)
   end
 end
