d94be157d470dd1c0546030e6e722c48427ea948
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-05-29 09:32:06 -0300

    Adjustments to Invitation schema
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- priv/repo/migrations/20240529105412_create_invitations.exs
+++ priv/repo/migrations/20240529105412_create_invitations.exs
@@ -4,8 +4,9 @@ defmodule Operately.Repo.Migrations.CreateInvitations do
   def change do
     create table(:invitations, primary_key: false) do
       add :id, :binary_id, primary_key: true
-      add :admin_id, references(:accounts, on_delete: :nothing, type: :binary_id)
-      add :member_id, references(:accounts, on_delete: :nothing, type: :binary_id)
+      add :admin_id, references(:people, on_delete: :nothing, type: :binary_id)
+      add :member_id, references(:people, on_delete: :nothing, type: :binary_id)
+      add :admin_name, :string
 
       timestamps()
     end
