b2966764a1ed5e6e4d207628c1c0c01693bee9e0
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-05-29 11:54:25 -0300

    Add unit tests for Invitations context
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

---
+++ test/support/fixtures/invitations_fixtures.ex
@@ -0,0 +1,52 @@
+defmodule Operately.InvitationsFixtures do
+  import Operately.CompaniesFixtures
+  import Operately.PeopleFixtures
+
+  @doc """
+  Generate an invitation.
+  """
+  def invitation_fixture() do
+    company = company_fixture(%{name: "Test Company"})
+    admin = person_fixture(%{
+      email: "admin@test.com",
+      company_role: :admin,
+      company_id: company.id,
+    })
+    member = person_fixture(%{
+      email: "member@test.com",
+      company_role: :member,
+      company_id: company.id,
+    })
+
+    {:ok, invitation} = Operately.Invitations.create_invitation(%{
+      member_id: member.id,
+      admin_id: admin.id,
+      admin_name: admin.full_name,
+    })
+
+    invitation
+  end
+
+  def invitation_fixture(attrs) do
+    {:ok, invitation} = Operately.Invitations.create_invitation(attrs)
+
+    invitation
+  end
+
+  @doc """
+  Generate an invitation_token.
+  """
+  def invitation_token_fixture() do
+    invitation = invitation_fixture()
+
+    {:ok, invitation_token} = Operately.Invitations.create_invitation_token(invitation.id)
+
+    invitation_token
+  end
+
+  def invitation_token_fixture(invitation_id) do
+    {:ok, invitation_token} = Operately.Invitations.create_invitation_token(invitation_id)
+
+    invitation_token
+  end
+end
