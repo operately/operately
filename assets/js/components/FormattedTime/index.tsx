66130996c319ac88368ec9ac69c585c83d94cb00
Jhonatan Silvério <68076306+Jh0wjso@users.noreply.github.com>
2024-05-30 11:47:46 -0300

    Timezone setting on feed (#498)
    * Set timezone for Activities queries
    
    Signed-off-by: Jh0wjso <jhonatansilverio1258@gmail.com>
    
    * Timezone on ListActivities query
    
    Signed-off-by: Jh0wjso <jhonatansilverio1258@gmail.com>
    
    * Timezone on feed and timezone on FormattedTime
    
    Signed-off-by: Jh0wjso <jhonatansilverio1258@gmail.com>
    
    * Fix timezone required
    
    Signed-off-by: Jh0wjso <jhonatansilverio1258@gmail.com>
    
    * Naive time to UTC time
    
    Signed-off-by: Jh0wjso <jhonatansilverio1258@gmail.com>
    
    * CLI Formatter on test helper
    
    Signed-off-by: Jh0wjso <jhonatansilverio1258@gmail.com>
    
    * London for null timezone on FormmatedTime
    
    Signed-off-by: Jh0wjso <jhonatansilverio1258@gmail.com>
    
    * Removing useless comments
    
    Signed-off-by: Jh0wjso <jhonatansilverio1258@gmail.com>
    
    ---------
    
    Signed-off-by: Jh0wjso <jhonatansilverio1258@gmail.com>

--- assets/js/components/FormattedTime/index.tsx
+++ assets/js/components/FormattedTime/index.tsx
@@ -22,6 +22,7 @@ type Format =
 interface FormattedTimeProps {
   time: string | Date;
   format: Format;
+  timezone?: string;
 }
 
 export default function FormattedTime(props: FormattedTimeProps): JSX.Element {
@@ -31,6 +32,8 @@ export default function FormattedTime(props: FormattedTimeProps): JSX.Element {
     return <>Invalid date</>;
   }
 
+  const options = { timeZone: props.timezone || "Europe/London" };
+  
   switch (props.format) {
     case "relative-day":
       return <RelativeDay time={parsedTime} />;
@@ -48,7 +51,7 @@ export default function FormattedTime(props: FormattedTimeProps): JSX.Element {
       return (
         <>
           {parsedTime
-            .toLocaleTimeString("en-US", { timeStyle: "short", hour12: true })
+            .toLocaleTimeString("en-US", { timeStyle: "short", hour12: true, ...options })
             .replace(" AM", "am")
             .replace(" PM", "pm")}
         </>
