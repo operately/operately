ee4d81891b13e9036207222d5554970807d294f1
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-05-30 20:44:27 -0300

    Replace GetInvitator by GetInvitation graphql query
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- lib/operately_web/graphql/types/invitations.ex
+++ lib/operately_web/graphql/types/invitations.ex
@@ -5,6 +5,18 @@ defmodule OperatelyWeb.Graphql.Types.Invitations do
     field :id, non_null(:id)
     field :admin_name, non_null(:string)
 
+    field :admin, non_null(:person) do
+      resolve(fn invitation, _, _ ->
+        {:ok, Operately.People.get_person!(invitation.admin_id)}
+      end)
+    end
+
+    field :member, non_null(:person) do
+      resolve(fn invitation, _, _ ->
+        {:ok, Operately.People.get_person!(invitation.member_id)}
+      end)
+    end
+
     # Retrieving an invitation token always creates a new token
     # create_invitation_token!/1 ensures that the previous token is deleted
     field :token, non_null(:string) do
