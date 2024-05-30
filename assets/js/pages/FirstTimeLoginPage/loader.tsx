5106cba5cd4bef6d154e8365119327d56402816d
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-05-30 20:52:37 -0300

    Add useForm hook for changing password
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- assets/js/pages/FirstTimeLoginPage/loader.tsx
+++ assets/js/pages/FirstTimeLoginPage/loader.tsx
@@ -1,7 +1,28 @@
+import { redirect } from "react-router-dom";
+
+import * as Pages from "@/components/Pages";
+import * as Invitations from "@/models/invitations";
+import { Invitation } from "@/gql";
+
+
 interface LoaderResult {
-  // TODO: Define what is loaded when you visit this page
+  invitation: Invitation;
+  token:  string;
 }
 
-export async function loader() : Promise<LoaderResult> {
-  return {} // TODO: Load data here
+export async function loader({request}) : Promise<any> {
+  const url = new URL(request.url);
+  const token = url.searchParams.get('token');
+  
+  if (!token)return redirect("/");
+  
+  const invitation = await Invitations.getInvitation(token);
+  
+  if (!invitation)return redirect("/");
+  
+  return { invitation, token };
+}
+
+export function useLoadedData(): LoaderResult {
+  return Pages.useLoadedData() as LoaderResult;
 }
--- assets/js/pages/FirstTimeLoginPage/page.tsx
+++ assets/js/pages/FirstTimeLoginPage/page.tsx
@@ -1,19 +1,73 @@
 import * as React from "react";
+
 import * as Pages from "@/components/Pages";
 import * as Paper from "@/components/PaperContainer";
+import * as Forms from "@/components/Form";
+
+import { Spacer } from "@/components/Spacer";
+import { useLoadedData } from "./loader";
+import { useForm } from "./useForm";
 
 
 export function Page() {
-  // const data = Pages.useLoadedData<LoaderResult>();
+  const { invitation } = useLoadedData();
 
   return (
     <Pages.Page title="Welcome to Operately!">
       <Paper.Root>
         <Paper.Body>    
-          <div className="text-content-accent text-2xl font-extrabold">Welcome to Operately!</div>
-          <div className="text-content-accent">Let&apos;s set up your company.</div>
+          <div className="text-content-accent text-2xl font-extrabold text-center">Welcome to Operately!</div>
+        
+          <Spacer size={8} />
+
+          <div className="text-content-accent">You were invited by {invitation.admin.fullName} to join ... .</div>
+          <div className="text-content-accent">Please choose a password to activate your account.</div>
+          
+          <Spacer size={2} />
+
+          <Form />
         </Paper.Body>
       </Paper.Root>
     </Pages.Page>
+  );
+}
+
+
+function Form() {
+  const { fields, submit, submitting, errors } = useForm();
+  
+  return (
+    <Forms.Form
+      isValid={true}
+      loading={submitting}
+      onSubmit={submit}
+    >
+      <Forms.TextInput 
+        label="Password"
+        onChange={fields.setPassword}
+        value={fields.password}
+        error={!!errors.find((e) => e.field === "password")?.message}
+        type="password"
+        testId="password"
+      />
+      <Forms.TextInput 
+        label="Repeat password"
+        onChange={fields.setPasswordConfirmation}
+        value={fields.passwordConfirmation}
+        error={!!errors.find((e) => e.field === "passwordConfirmation")?.message}
+        type="password"
+        testId="password-confirmation"
+      />
+
+      {errors.map((e, idx) => (
+        <div key={idx} className="text-red-500 text-sm">{e.message}</div>
+      ))}
+
+      <div className="flex items-center justify-center">
+        <Forms.SubmitButton data-test-id="submit-form">
+          Submit
+        </Forms.SubmitButton>
+      </div>
+    </Forms.Form>
   );
 }
