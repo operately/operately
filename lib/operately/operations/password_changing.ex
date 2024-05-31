f6416510c501b9d568f94dfcaa10b0361b75d604
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-05-31 10:55:55 -0300

    Add insert_activity to PasswordChanging operation
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- lib/operately/operations/password_changing.ex
+++ lib/operately/operations/password_changing.ex
@@ -1,28 +1,35 @@
 defmodule Operately.Operations.PasswordChanging do
-  import Ecto.Query
   alias Ecto.Multi
   alias Operately.Repo
-  alias Operately.Invitations.InvitationToken
 
-  def run(attrs) do
+  def run(attrs, invitation) do
+    invitation = Repo.preload(invitation, [:admin, member: [:account]])
+
     Multi.new()
-    |> change_password(attrs)
+    |> change_password(attrs, invitation)
+    |> insert_activity(invitation)
     |> Repo.transaction()
   end
 
-  defp change_password(multi, attrs) do
-    hashed_token = InvitationToken.hash_token(attrs.token)
-
-    query = from t in InvitationToken,
-      where: t.hashed_token == ^hashed_token,
-      join: inv in assoc(t, :invitation),
-      join: member in assoc(inv, :member),
-      join: account in assoc(member, :account),
-      select: account
-
-    account = Repo.one(query)
-    changeset = Operately.People.Account.password_changeset(account, attrs)
+  defp change_password(multi, attrs, invitation) do
+    changeset = Operately.People.Account.password_changeset(
+      invitation.member.account,
+      attrs
+    )
 
     Multi.update(multi, :password, changeset)
+  end
+
+  defp insert_activity(multi, invitation) do
+    Operately.Activities.insert_sync(multi, invitation.member_id, :password_first_time_changed, fn _changes ->
+      %{
+        invitatition_id: invitation.id,
+        company_id: invitation.admin.company_id,
+        admin_name: invitation.admin.full_name,
+        admin_email: invitation.admin.email,
+        member_name: invitation.member.full_name,
+        member_email: invitation.member.email,
+      }
+    end)
   end
 end
