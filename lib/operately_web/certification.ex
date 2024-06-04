8e45a505d368c250d86a91c541e2951437ba0e8e
Igor Šarčević <igor@operately.com>
2024-06-04 15:36:21 +0000

    Don't check cert options if the mode is set to manual
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- lib/operately_web/certification.ex
+++ lib/operately_web/certification.ex
@@ -28,31 +28,33 @@ defmodule OperatelyWeb.Certification do
   end
 
   def verify_config_presence do
-    unless domain() do
-      raise """
-      You need to set the CERT_DOMAIN environment variable to run Operately with TLS support.
-      """
-    end
+    if mode() != :manual do
+      unless domain() do
+        raise """
+        You need to set the CERT_DOMAIN environment variable to run Operately with TLS support.
+        """
+      end
 
-    unless folder() do
-      raise """
-      You need to set the CERT_DB_DIR environment variable, where the certificates will be stored. 
-      Make sure this is outside of the deployment folder. Otherwise, the deploy may delete the 
-      folder, which will effectively remove the generated key and certificate files.
-      """
-    end
+      unless folder() do
+        raise """
+        You need to set the CERT_DB_DIR environment variable, where the certificates will be stored. 
+        Make sure this is outside of the deployment folder. Otherwise, the deploy may delete the 
+        folder, which will effectively remove the generated key and certificate files.
+        """
+      end
 
-    unless File.exists?(folder()) do
-      raise """
-      The folder specified in CERT_DB_DIR does not exist. Please create it before starting the server.
-      """
-    end
+      unless File.exists?(folder()) do
+        raise """
+        The folder specified in CERT_DB_DIR does not exist. Please create it before starting the server.
+        """
+      end
 
-    unless File.stat!(folder()).access == :read_write do
-      raise """
-      The folder specified in CERT_DB_DIR does not have write permissions. Please make sure the 
-      folder is writable by the user running the server.
-      """
+      unless File.stat!(folder()).access == :read_write do
+        raise """
+        The folder specified in CERT_DB_DIR does not have write permissions. Please make sure the 
+        folder is writable by the user running the server.
+        """
+      end
     end
   end
 
