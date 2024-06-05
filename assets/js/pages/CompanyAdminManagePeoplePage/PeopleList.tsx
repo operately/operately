df9a811d5e96ef55881d1c42524db514ca307def
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-06-05 08:49:43 -0300

    Add remove-member-confirmation modal
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- assets/js/pages/CompanyAdminManagePeoplePage/PeopleList.tsx
+++ assets/js/pages/CompanyAdminManagePeoplePage/PeopleList.tsx
@@ -1,11 +1,13 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { useLoadedData } from "./loader";
 import { useRemoveMemberMutation } from "@/models/companies";
 import { Person } from "@/gql";
-import { GhostButton } from "@/components/Button";
+import { FilledButton, GhostButton } from "@/components/Button";
+import Modal from "@/components/Modal";
 import Avatar from "@/components/Avatar";
+
 
 
 export function PeopleList() {
@@ -20,7 +22,44 @@ export function PeopleList() {
   );
 }
 
+
 function PersonRow({ person }: { person: Person }) {
+  const [showRemoveMember, setShowRemoveMember] = useState(false);
+  
+  return (
+    <>
+      <div className="flex flex-col gap-4 items-center border border-stroke-base rounded-lg p-4 py-6">
+        <Avatar person={person} size="xlarge" />
+
+        <div className="flex flex-col text-center items-center">
+          <div className="text-content-primary font-bold">{person.fullName}</div>
+          <div className="text-content-secondary text-sm">{person.title}</div>
+          <div className="text-content-secondary text-sm break-all">{person.email}</div>
+        </div>
+
+        <div>
+          <GhostButton
+            onClick={() => setShowRemoveMember(true)}
+            size="xxs"
+            type="secondary"
+            testId="remove-member"
+          >
+            Remove
+          </GhostButton>
+        </div>
+      </div>
+
+      <RemoveMemberModal
+        isOpen={showRemoveMember}
+        hideModal={() => setShowRemoveMember(false)}
+        person={person}
+      />
+    </>
+  );
+}
+
+
+function RemoveMemberModal({isOpen, hideModal, person}: {isOpen: boolean, hideModal: ()=>void, person: Person}) {
   const navigate = useNavigate();
   const [remove, { loading }] = useRemoveMemberMutation({
     onCompleted: () => {
@@ -35,25 +74,22 @@ function PersonRow({ person }: { person: Person }) {
   }
 
   return (
-    <div className="flex flex-col gap-4 items-center border border-stroke-base rounded-lg p-4 py-6">
-      <Avatar person={person} size="xlarge" />
-
-      <div className="flex flex-col text-center items-center">
-        <div className="text-content-primary font-bold">{person.fullName}</div>
-        <div className="text-content-secondary text-sm">{person.title}</div>
-        <div className="text-content-secondary text-sm break-all">{person.email}</div>
-      </div>
-
-      <div>
-        <GhostButton
+    <Modal
+      title="Remove Company Member"
+      isOpen={isOpen}
+      hideModal={hideModal}
+      minHeight="150px"
+    >
+      <div>Are you sure you want to remove {person.fullName} from the company?</div>
+      <div className="mt-8 flex justify-center">
+        <FilledButton
           onClick={handleRemoveMember}
-          size="xxs"
+          type="primary"
           loading={loading}
-          testId="remove-member"
         >
-          Remove
-        </GhostButton>
+          Remove Member
+        </FilledButton>
       </div>
-    </div>
+    </Modal>
   );
 }
