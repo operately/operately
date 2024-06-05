088b9f03c5fd445cf91510adc3495e108417ca0e
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-06-05 15:02:41 -0300

    Add new_invitation_token mutation
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

---
+++ lib/operately/activities/notifications/company_invitation_token_created.ex
@@ -0,0 +1,6 @@
+defmodule Operately.Activities.Notifications.CompanyInvitationTokenCreated do
+  def dispatch(_activity) do
+    # Notification dispatcher for CompanyInvitationTokenCreated not implemented yet
+    {:ok, []}
+  end
+end
