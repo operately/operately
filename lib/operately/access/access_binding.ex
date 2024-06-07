b2b768ff6e2deb597c4e88c5731d70c93c3870be
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-06-07 09:37:11 -0300

    Change access_level field to integer
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- lib/operately/access/access_binding.ex
+++ lib/operately/access/access_binding.ex
@@ -1,16 +1,19 @@
 defmodule Operately.Access.Binding do
-  use Ecto.Schema
-  import Ecto.Changeset
+  use Operately.Schema
 
-  @primary_key {:id, :binary_id, autogenerate: true}
-  @foreign_key_type :binary_id
   schema "access_bindings" do
     belongs_to :group, Operately.Access.Group
     belongs_to :context, Operately.Access.Context
-    field :access_level, Ecto.Enum, values: [:full_access, :edit_access, :comment_access, :view_access]
+    field :access_level, :integer
 
     timestamps()
   end
+
+  # view -> 10
+  # comment -> 40
+  # edit -> 70
+  # full -> 100
+  @valid_access_levels [10, 40, 70, 100]
 
   def changeset(attrs) do
     changeset(%__MODULE__{}, attrs)
@@ -19,6 +22,7 @@ defmodule Operately.Access.Binding do
   def changeset(binding, attrs) do
     binding
     |> cast(attrs, [:group_id, :context_id, :access_level])
+    |> validate_inclusion(:access_level, @valid_access_levels, message: "invalid access level")
     |> validate_required([:group_id, :context_id, :access_level])
   end
 end
--- priv/repo/migrations/20240606222753_create_access_bindings.exs
+++ priv/repo/migrations/20240606222753_create_access_bindings.exs
@@ -4,7 +4,7 @@ defmodule Operately.Repo.Migrations.CreateBindings do
   def change do
     create table(:access_bindings, primary_key: false) do
       add :id, :binary_id, primary_key: true
-      add :access_level, :string
+      add :access_level, :integer
       add :group_id, references(:access_groups, on_delete: :nothing, type: :binary_id)
       add :context_id, references(:access_contexts, on_delete: :nothing, type: :binary_id)
 
