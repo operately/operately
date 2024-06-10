e21aff8a59f82a79409cd5d5687bf4e260838b68
Igor Šarčević <igisar@gmail.com>
2024-06-10 12:16:54 +0200

    Use http for local development server (#538)
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- config/dev.exs
+++ config/dev.exs
@@ -22,7 +22,7 @@ config :operately, OperatelyWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
   http: [ip: {0, 0, 0, 0}, port: 4000],
-  https: [port: 4001],
+  https: false,
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
