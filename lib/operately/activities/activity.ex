6bea09efbcc6561f8d7766582c266db4b4bf0281
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-06-10 12:25:49 -0300

    Add access_context to activities
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- lib/operately/activities/activity.ex
+++ lib/operately/activities/activity.ex
@@ -17,6 +17,8 @@ defmodule Operately.Activities.Activity do
     belongs_to :author, Operately.People.Person
     belongs_to :comment_thread, Operately.Comments.CommentThread
 
+    has_one :access_context, Operately.Access.Context, foreign_key: :activity_id
+
     field :action, :string
     field :content, :map
 
