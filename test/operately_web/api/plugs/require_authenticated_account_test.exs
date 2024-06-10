91676c8fe1cb99cf5790ba31de7f79970e86a8c9
Igor Šarčević <igor@operately.com>
2024-06-10 09:08:20 +0000

    Use Plug.Test for constructing conns
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- test/operately_web/api/plugs/require_authenticated_account_test.exs
+++ test/operately_web/api/plugs/require_authenticated_account_test.exs
@@ -1,9 +1,10 @@
 defmodule OperatelyWeb.Api.Plugs.RequireAuthenticatedAccountTest do
   use ExUnit.Case
-  import Plug.Conn
+  use Plug.Test
 
   test "it returns 401 if the current account is nil" do
-    conn = %Plug.Conn{assigns: %{current_account: nil}}
+    conn = conn(:get, "/")
+    conn = assign(conn, :current_account, nil)
     conn = OperatelyWeb.Api.Plugs.RequireAuthenticatedAccount.call(conn, nil)
 
     assert conn.status == 401
@@ -11,7 +12,8 @@ defmodule OperatelyWeb.Api.Plugs.RequireAuthenticatedAccountTest do
   end
 
   test "it returns the connection if the current account is not nil" do
-    conn = %Plug.Conn{assigns: %{current_account: %Operately.People.Account{}}}
+    conn = conn(:get, "/")
+    conn = assign(conn, :current_account, %Operately.People.Account{})
     conn = OperatelyWeb.Api.Plugs.RequireAuthenticatedAccount.call(conn, nil)
 
     assert conn.status == nil
