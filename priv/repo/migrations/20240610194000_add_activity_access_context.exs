6d2610798ee10ea9726245fa1734706e325057bb
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-06-11 08:49:21 -0300

    Change activity and access context relationship
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- priv/repo/migrations/20240610194000_add_activity_access_context.exs
+++ priv/repo/migrations/20240610194000_add_activity_access_context.exs
@@ -2,10 +2,10 @@ defmodule Operately.Repo.Migrations.AddActivityAccessContext do
   use Ecto.Migration
 
   def change do
-    alter table(:access_contexts) do
-      add :activity_id, references(:activities, on_delete: :nothing, type: :binary_id), null: true
+    alter table(:activities) do
+      add :context_id, references(:access_contexts, on_delete: :nothing, type: :binary_id), null: true
     end
 
-    create unique_index(:access_contexts, [:activity_id])
+    create index(:activities, [:context_id])
   end
 end
