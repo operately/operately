90d8fbb4cbd720a5756d80fdc3f042b3ae0ab14e
Jh0wjso <jhonatansilverio1258@gmail.com>
2024-06-12 10:51:34 -0300

    Remove useless migration
    Signed-off-by: Jh0wjso <jhonatansilverio1258@gmail.com>

--- priv/repo/migrations/20240609153530_add_storage_type.exs
+++
@@ -1,9 +0,0 @@
-defmodule Operately.Repo.Migrations.AddStorageType do
-  use Ecto.Migration
-
-  def change do
-    alter table(:blobs) do
-      add :storage_type, :string, default: "s3"
-    end
-  end
-end
