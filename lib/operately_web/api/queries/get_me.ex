a411cf0c7f51ed2539d257741b9df53b901c6a63
Igor Šarčević <igor@operately.com>
2024-06-12 09:31:59 +0000

    Implement get me query
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- lib/operately_web/api/queries/get_me.ex
+++ lib/operately_web/api/queries/get_me.ex
@@ -1,15 +1,55 @@
 defmodule OperatelyWeb.Api.Queries.GetMe do
   use TurboConnect.Query
 
+  alias Operately.Repo
+
   inputs do
-    # TODO: Define input fields
+    field :include_manager, :boolean
   end
 
   outputs do
-    # TODO: Define output fields
+    field :me, :person
   end
 
-  def call(_conn, _inputs) do
-    raise "Not implemented"
+  def call(conn, inputs) do
+    conn.assigns.current_account.person
+    |> preload_manager(inputs[:include_manager])
+    |> serialize(inputs[:include_manager])
+    |> ok_tuple()
+  end
+
+  defp preload_manager(me, true), do: Repo.preload(me, [:manager])
+  defp preload_manager(me, _), do: me
+
+  defp serialize(me, include_manager) do
+    %{me: 
+      %{
+        id: me.id,
+        full_name: me.full_name,
+        email: me.email,
+        timezone: me.timezone,
+
+        send_daily_summary: me.send_daily_summary,
+        notify_on_mention: me.notify_on_mention,
+        notify_about_assignments: me.notify_about_assignments,
+
+        theme: me.theme || "system",
+        manager: include_manager && serialize_manager(me.manager)
+      }
+    }
+  end
+
+  defp serialize_manager(nil), do: nil
+  defp serialize_manager(manager) do
+    %{
+      id: manager.id,
+      full_name: manager.full_name,
+      email: manager.email,
+      avatar_url: manager.avatar_url
+    }
+  end
+
+  defp ok_tuple(value) do
+    {:ok, value}
   end
 end
--- test/operately_web/api/queries/get_me_test.exs
+++ test/operately_web/api/queries/get_me_test.exs
@@ -1,3 +1,71 @@
 defmodule OperatelyWeb.Api.Queries.GetMeTest do
-  use OperatelyWeb.ConnCase
+  use OperatelyWeb.TurboCase
+
+  import Operately.PeopleFixtures
+
+  describe "security" do
+    test "it requires authentication", ctx do
+      assert {401, _} = query(ctx.conn, :get_me, %{})
+    end
+  end
+
+  describe "get_me functionality" do
+    setup :register_and_log_in_account
+
+    test "it returns the current account's information", ctx do
+      assert {200, %{me: data}} = query(ctx.conn, :get_me, %{})
+
+      assert data == %{
+        id: ctx.person.id,
+        full_name: ctx.person.full_name,
+        email: ctx.person.email,
+        timezone: ctx.person.timezone,
+        send_daily_summary: ctx.person.send_daily_summary,
+        notify_on_mention: ctx.person.notify_on_mention,
+        notify_about_assignments: ctx.person.notify_about_assignments,
+        theme: "system",
+        manager: nil,
+      }
+    end
+
+    test "includes manager information when requested", ctx do
+      manager = person_fixture(company_id: ctx.company.id, full_name: "John Doe")
+      {:ok, me} = Operately.People.update_person(ctx.person, %{manager_id: manager.id})
+
+      assert {200, %{me: data}} = query(ctx.conn, :get_me, %{include_manager: true})
+
+      assert data == %{
+        id: me.id,
+        full_name: me.full_name,
+        email: me.email,
+        timezone: me.timezone,
+        send_daily_summary: me.send_daily_summary,
+        notify_on_mention: me.notify_on_mention,
+        notify_about_assignments: me.notify_about_assignments,
+        theme: "system",
+        manager: %{
+          id: manager.id,
+          full_name: manager.full_name,
+          email: manager.email,
+          avatar_url: nil,
+        },
+      }
+    end
+
+    test "when the account has no manager, it returns null even when requested", ctx do
+      assert {200, %{me: data}} = query(ctx.conn, :get_me, %{include_manager: true})
+
+      assert data == %{
+        id: ctx.person.id,
+        full_name: ctx.person.full_name,
+        email: ctx.person.email,
+        timezone: ctx.person.timezone,
+        send_daily_summary: ctx.person.send_daily_summary,
+        notify_on_mention: ctx.person.notify_on_mention,
+        notify_about_assignments: ctx.person.notify_about_assignments,
+        theme: "system",
+        manager: nil
+      }
+    end
+  end
 end 
