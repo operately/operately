9cf85112a05f6eb112d6a2208b6272c98258aa57
Jh0wjso <jhonatansilverio1258@gmail.com>
2024-06-12 11:04:00 -0300

    Reverting to older storage-type migration
    Signed-off-by: Jh0wjso <jhonatansilverio1258@gmail.com>

--- priv/repo/migrations/20240612125730_add_storage_type_to_blob.exs
+++
@@ -1,9 +0,0 @@
-defmodule Operately.Repo.Migrations.AddStorageTypeToBlob do
-  use Ecto.Migration
-
-  def change do
-    alter table(:blobs) do
-      add :storage_type, :string
-    end
-  end
-end
