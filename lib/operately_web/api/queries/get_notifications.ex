ed853a00cfc74b62317e9322a965f8350bc89089
Igor Šarčević <igor@operately.com>
2024-06-17 15:37:59 +0000

    Implement get notifications query
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- lib/operately_web/api/queries/get_notifications.ex
+++ lib/operately_web/api/queries/get_notifications.ex
@@ -1,15 +1,56 @@
 defmodule OperatelyWeb.Api.Queries.GetNotifications do
   use TurboConnect.Query
 
+  alias Operately.Notifications.Notification
+  alias Operately.Activities.Activity
+  alias Operately.Activities.Preloader
+
+  import Ecto.Query, only: [from: 2]
+
   inputs do
-    # TODO: Define input fields
+    field :page, :integer
+    field :per_page, :integer
   end
 
   outputs do
-    # TODO: Define output fields
+    field :notifications, list_of(:notification)
   end
 
-  def call(_conn, _inputs) do
-    raise "Not implemented"
+  @default_per_page 100
+
+  def call(conn, inputs) do
+    me = conn.assigns.current_account.person
+    page = inputs[:page] || 1
+    per_page = inputs[:per_page] || @default_per_page
+
+    offset = per_page * (page - 1)
+    limit = per_page
+
+    query = from n in Notification,
+      join: a in assoc(n, :activity),
+      where: a.action not in ^Activity.deprecated_actions(),
+      where: n.person_id == ^me.id,
+      order_by: [desc: n.inserted_at],
+      offset: ^offset,
+      limit: ^limit,
+      preload: [activity: [:author]]
+
+    notifications = query |> Operately.Repo.all() |> load_data_for_activities()
+
+    {:ok, %{notifications: notifications}}
+  end
+
+  def load_data_for_activities(notifications) do
+    activities = 
+      notifications
+      |> Enum.map(fn n -> n.activity end)
+      |> Enum.map(&Operately.Activities.cast_content/1)
+      |> Preloader.preload()
+
+    Enum.map(notifications, fn notification ->
+      activity = Enum.find(activities, fn a -> a.id == notification.activity_id end)
+
+      %{notification | activity: activity}
+    end)
   end
 end
