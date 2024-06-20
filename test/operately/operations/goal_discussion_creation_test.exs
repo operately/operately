63288456f2462698536d866e12e3e2dbb4e3e5de
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-06-20 06:59:55 -0300

    Adjust tests for GoalDiscussionCreation operation
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- test/operately/operations/goal_discussion_creation_test.exs
+++ test/operately/operations/goal_discussion_creation_test.exs
@@ -10,21 +10,24 @@ defmodule Operately.Operations.GoalDiscussionCreationTest do
 
   alias Operately.Repo
   alias Operately.Activities.Activity
+  alias Operately.Notifications.Notification
 
   setup do
     company = company_fixture()
     author = person_fixture_with_account(%{company_id: company.id})
+    reader = person_fixture_with_account(%{company_id: company.id})
     group = group_fixture(author)
     goal= goal_fixture(author, %{space_id: group.id, targets: []})
 
-    {:ok, author: author, goal: goal}
+    {:ok, author: author, reader: reader, goal: goal}
   end
 
   test "GoalDiscussionCreation operation creates activity and thread", ctx do
     title = "some title"
+    message = "{\"type\":\"doc\",\"content\":[{\"type\":\"paragraph\",\"content\":[{\"type\":\"mention\",\"attrs\":{\"id\":\"#{ctx.reader.id}\",\"label\":\"#{ctx.reader.full_name}\"}}]}]}"
 
     Oban.Testing.with_testing_mode(:manual, fn ->
-      Operately.Operations.GoalDiscussionCreation.run(ctx.author, ctx.goal.id, title, "{}")
+      Operately.Operations.GoalDiscussionCreation.run(ctx.author, ctx.goal, title, message)
     end)
 
     activity = from(a in Activity,
@@ -35,5 +38,13 @@ defmodule Operately.Operations.GoalDiscussionCreationTest do
 
     assert activity.comment_thread_id != nil
     assert activity.comment_thread.title == title
+
+    assert 0 == Repo.aggregate(Notification, :count, :id)
+
+    Oban.Testing.perform_job(Operately.Activities.NotificationDispatcher, %{activity_id: activity.id}, [])
+
+    assert 1 == Repo.aggregate(Notification, :count, :id)
+
+    assert nil != from(n in Notification, where: n.person_id == ^ctx.reader.id) |> Repo.one()
   end
 end
