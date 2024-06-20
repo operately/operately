7c56faca8d31f57c1d72e832bc93ff9e0cf35534
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-06-20 08:55:14 -0300

    Adjust tests for GoalClosing operation
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- test/operately/operations/goal_closing_test.exs
+++ test/operately/operations/goal_closing_test.exs
@@ -1,5 +1,6 @@
 defmodule Operately.Operations.GoalClosingTest do
   use Operately.DataCase
+  use Operately.Support.Notifications
 
   import Ecto.Query, only: [from: 2]
 
@@ -9,13 +10,20 @@ defmodule Operately.Operations.GoalClosingTest do
   import Operately.GroupsFixtures
 
   alias Operately.Repo
+  alias Operately.Goals
   alias Operately.Activities.Activity
 
   setup do
     company = company_fixture()
     author = person_fixture_with_account(%{company_id: company.id})
+    champion = person_fixture_with_account(%{company_id: company.id})
     group = group_fixture(author)
-    goal = goal_fixture(author, %{space_id: group.id, targets: []})
+
+    Oban.Testing.with_testing_mode(:manual, fn ->
+      goal_fixture(author, %{space_id: group.id, champion_id: champion.id, targets: []})
+    end)
+
+    goal = Goals.list_goals() |> hd()
 
     {:ok, author: author, goal: goal}
   end
@@ -34,7 +42,7 @@ defmodule Operately.Operations.GoalClosingTest do
     assert goal.closed_by_id == ctx.author.id
   end
 
-  test "GoalClosing operation creates activity and thread", ctx do
+  test "GoalClosing operation creates activity, thread and notification", ctx do
     Oban.Testing.with_testing_mode(:manual, fn ->
       Operately.Operations.GoalClosing.run(ctx.author, ctx.goal.id, "success", "{}")
     end)
@@ -42,5 +50,13 @@ defmodule Operately.Operations.GoalClosingTest do
     activity = from(a in Activity, where: a.action == "goal_closing" and a.content["goal_id"] == ^ctx.goal.id) |> Repo.one()
 
     assert activity.comment_thread_id != nil
+
+    assert 0 == notifications_count()
+
+    perform_job(activity.id)
+
+    assert 1 == notifications_count()
+
+    assert nil != fetch_notification(activity.id)
   end
 end
