65330ceb4153be698d18999712ba839a2ee3ad93
Igor Šarčević <igor@operately.com>
2024-06-20 22:02:04 +0000

    Introduce protocols for serializing
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- lib/operately_web/api/queries/get_projects.ex
+++ lib/operately_web/api/queries/get_projects.ex
@@ -2,6 +2,7 @@ defmodule OperatelyWeb.Api.Queries.GetProjects do
   use TurboConnect.Query
   use OperatelyWeb.Api.Helpers
 
+  alias OperatelyWeb.Api.Serializer
   alias Operately.Projects.Project
   alias Operately.Projects.Contributor
 
@@ -27,7 +28,8 @@ defmodule OperatelyWeb.Api.Queries.GetProjects do
 
   def call(conn, inputs) do
     projects = load(me(conn), inputs)
-    {:ok, %{projects: serialize(projects, inputs)}}
+
+    {:ok, %{projects: Serializer.serialize(projects)}}
   end
 
   defp load(person, inputs) do
@@ -72,104 +74,4 @@ defmodule OperatelyWeb.Api.Queries.GetProjects do
 
   defp set_next_milestones(projects, true), do: Project.set_next_milestone(projects)
   defp set_next_milestones(projects, _), do: projects
-
-  defp serialize(projects, inputs) when is_list(projects) do
-    Enum.map(projects, fn p -> serialize(p, inputs) end)
-  end
-
-  defp serialize(project = %Operately.Projects.Project{}, inputs) do
-    %{
-      id: project.id,
-      name: project.name,
-      private: project.private,
-      inserted_at: project.inserted_at,
-      updated_at: project.updated_at,
-      started_at: project.started_at,
-      closed_at: project.closed_at,
-      deadline: project.deadline,
-      is_archived: project.deleted_at != nil,
-      is_outdated: Operately.Projects.outdated?(project),
-      status: project.status,
-    }
-    |> extend_map_if(inputs[:include_space], fn -> %{space: serialize_space(project.group)} end)
-    |> extend_map_if(inputs[:include_champion], fn -> %{champion: serialize_champion(project.champion)} end)
-    |> extend_map_if(inputs[:include_goal], fn -> %{goal: serialize_goal(project.goal)} end)
-    |> extend_map_if(inputs[:include_milestones], fn -> %{milestones: serialize_milestones(project.milestones)} end)
-    |> extend_map_if(inputs[:include_contributors], fn -> %{contributors: serialize_contributors(project.contributors)} end)
-    |> extend_map_if(inputs[:include_last_check_in], fn -> %{last_check_in: serialize_last_check_in(project.last_check_in)} end)
-    |> extend_map_if(inputs[:include_milestones], fn -> %{next_milestone: serialize_milestone(project.next_milestone)} end)
-  end
-
-  defp serialize_space(space) do
-    %{
-      id: space.id,
-      name: space.name,
-    }
-  end
-
-  defp serialize_champion(nil), do: nil
-  defp serialize_champion(champion) do
-    %{
-      id: champion.id,
-      full_name: champion.full_name,
-      avatar_url: champion.avatar_url,
-      title: champion.title,
-    }
-  end
-
-  defp serialize_goal(nil), do: nil
-  defp serialize_goal(goal) do
-    %{
-      id: goal.id,
-      name: goal.name,
-    }
-  end
-
-  def serialize_milestones(milestones) do
-    Enum.map(milestones, fn milestone -> serialize_milestone(milestone) end)
-  end
-
-  defp serialize_milestone(nil), do: nil
-  defp serialize_milestone(milestone) do
-    %{
-      id: milestone.id,
-      title: milestone.title,
-      status: milestone.status,
-      inserted_at: milestone.inserted_at,
-      deadline_at: milestone.deadline_at,
-    }
-  end
-
-  defp serialize_last_check_in(nil), do: nil
-  defp serialize_last_check_in(last_check_in) do
-    %{
-      id: last_check_in.id,
-      status: last_check_in.status,
-      description: last_check_in.description,
-      inserted_at: last_check_in.inserted_at,
-
-      author: %{
-        id: last_check_in.author.id,
-        full_name: last_check_in.author.full_name,
-        avatar_url: last_check_in.author.avatar_url,
-        title: last_check_in.author.title,
-      }
-    }
-  end
-
-  defp serialize_contributors(contributors) do
-    Enum.map(contributors, fn contributor ->
-      %{
-        id: contributor.id,
-        role: contributor.role,
-        responsibility: contributor.responsibility,
-        person: %{
-          id: contributor.person.id,
-          full_name: contributor.person.full_name,
-          avatar_url: contributor.person.avatar_url,
-          title: contributor.person.title,
-        }
-      }
-    end)
-  end
 end
---
+++ lib/operately_web/api/serializer.ex
@@ -0,0 +1,106 @@
+defprotocol OperatelyWeb.Api.Serializer do
+  @fallback_to_any true
+
+  def serialize(data)
+end
+
+defimpl OperatelyWeb.Api.Serializer, for: Any do
+  def serialize(nil), do: nil
+  def serialize(%Ecto.Association.NotLoaded{}), do: nil
+end
+
+defimpl OperatelyWeb.Api.Serializer, for: List do
+  def serialize(nil), do: nil
+  def serialize(data), do: Enum.map(data, &OperatelyWeb.Api.Serializer.serialize/1)
+end
+
+defimpl OperatelyWeb.Api.Serializer, for: Operately.People.Person do
+  def serialize(data) do
+    %{
+      id: data.id,
+      full_name: data.full_name,
+      avatar_url: data.avatar_url,
+      title: data.title,
+    }
+  end
+end
+
+defimpl OperatelyWeb.Api.Serializer, for: Operately.Goals.Goal do
+  def serialize(data) do
+    %{
+      id: data.id,
+      name: data.name,
+    }
+  end
+end
+
+defimpl OperatelyWeb.Api.Serializer, for: Operately.Groups.Group do
+  def serialize(space) do
+    %{
+      id: space.id,
+      name: space.name,
+    }
+  end
+end
+
+defimpl OperatelyWeb.Api.Serializer, for: Operately.Projects.Milestone do
+  def serialize(milestone) do
+    %{
+      id: milestone.id,
+      title: milestone.title,
+      status: milestone.status,
+      inserted_at: milestone.inserted_at,
+      deadline_at: milestone.deadline_at,
+    }
+  end
+end
+
+defimpl OperatelyWeb.Api.Serializer, for: Operately.Projects.Contributor do
+  def serialize(contributors) do
+    Enum.map(contributors, fn contributor ->
+      %{
+        id: contributor.id,
+        role: contributor.role,
+        responsibility: contributor.responsibility,
+        person: OperatelyWeb.Api.Serializer.serialize(contributor.person),
+      }
+    end)
+  end
+end
+
+defimpl OperatelyWeb.Api.Serializer, for: Operately.Projects.CheckIn do
+  def serialize(check_in) do
+    %{
+      id: check_in.id,
+      status: check_in.status,
+      description: check_in.description,
+      inserted_at: check_in.inserted_at,
+      author: OperatelyWeb.Api.Serializer.serialize(check_in.author),
+    }
+  end
+end
+
+defimpl OperatelyWeb.Api.Serializer, for: Operately.Projects.Project do
+  def serialize(project) do
+    %{
+      id: project.id,
+      name: project.name,
+      private: project.private,
+      inserted_at: project.inserted_at,
+      updated_at: project.updated_at,
+      started_at: project.started_at,
+      closed_at: project.closed_at,
+      deadline: project.deadline,
+      is_archived: project.deleted_at != nil,
+      is_outdated: Operately.Projects.outdated?(project),
+      status: project.status,
+      space: OperatelyWeb.Api.Serializer.serialize(project.group),
+      champion: OperatelyWeb.Api.Serializer.serialize(project.champion),
+      goal: OperatelyWeb.Api.Serializer.serialize(project.goal),
+      milestones: OperatelyWeb.Api.Serializer.serialize(project.milestones),
+      contributors: OperatelyWeb.Api.Serializer.serialize(project.contributors),
+      last_check_in: OperatelyWeb.Api.Serializer.serialize(project.last_check_in),
+      next_milestone: OperatelyWeb.Api.Serializer.serialize(project.next_milestone),
+    }
+  end
+end
