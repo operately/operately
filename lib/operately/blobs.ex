488b5e8568e973d31af736be4a945877178a6eed
Jh0wjso <jhonatansilverio1258@gmail.com>
2024-06-21 08:21:37 -0300

    S3 presigned_url update
    Signed-off-by: Jh0wjso <jhonatansilverio1258@gmail.com>

--- lib/operately/blobs.ex
+++ lib/operately/blobs.ex
@@ -30,14 +30,22 @@ defmodule Operately.Blobs do
     Blob.changeset(blob, attrs)
   end
 
+  def get_host do
+    case Application.get_env(:operately, :storage_host) do
+      "localhost" -> "#{Application.get_env(:operately, :storage_host)}:9090"
+      _ -> Application.get_env(:operately, :storage_host)
+    end
+  end
+
   def get_signed_get_url(%Blob{} = blob) do
     case Application.get_env(:operately, :storage_type) do
       "s3" ->
-        host = Application.get_env(:operately, :storage_host)
+        host = get_host()
         bucket = Application.get_env(:operately, :storage_bucket)
         path = "#{blob.company_id}-#{blob.id}"
+        post_link = get_s3_post_link(host, bucket, path)
 
-        "http://#{host}:9090/#{bucket}/#{path}"
+        "http://#{host}/#{bucket}/#{path}"
 
       "local" ->
         host = OperatelyWeb.Endpoint.url()
@@ -59,9 +67,7 @@ defmodule Operately.Blobs do
         host = Application.get_env(:operately, :storage_host)
         config = ExAws.Config.new(:s3, host: host)
 
-        ExAws.S3.put_object(bucket, blob.filename, path) |> ExAws.request!()
-        {:ok, "http://#{host}:9090/#{bucket}/#{path}" |> IO.inspect()}
-
+        ExAws.S3.presigned_url(config, :put_object, bucket, path, expires_in: 3600) |> IO.inspect()
       "local" ->
         host = OperatelyWeb.Endpoint.url()
         token = Operately.Blobs.Tokens.gen_upload_token(path)
