a9312456d4b126b31e47b70456adf67b774c3577
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2024-06-21 09:28:24 -0300

    Add insert_sync to goal operations (#618)
    * Add insert_sync to GoalClosing operation
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    * Add insert_sync to GoalDiscussionCreation operation
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    * Add insert_sync to GoalReopening operation
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    * Add insert_sync to GoalTimeframeEditing operation
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    * Run new tests in manual mode
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    * Adjust tests for GoalDiscussionCreation operation
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    * Add notifications.ex
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    * Add notification_message to support/notifications.ex
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    * Adjust tests for GoalClosing operation
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    * Adjust tests for GoalReopening operation
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    * Adjust tests for GoalReopening operation
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    * Adjust tests for GoalTimeframeEditing operation
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    * insert_sync optionally dispatches notification
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    * Adjust operations
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    ---------
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

---
+++ test/support/notifications.ex
@@ -0,0 +1,46 @@
+defmodule Operately.Support.Notifications do
+  import Ecto.Query, only: [from: 2]
+
+  alias Operately.Repo
+  alias Operately.People.Person
+  alias Operately.Notifications.Notification
+
+  defmacro __using__(_opts) do
+    quote do
+       import Operately.Support.Notifications
+    end
+  end
+
+  def notifications_count do
+    Repo.aggregate(Notification, :count, :id)
+  end
+
+  def perform_job(activity_id) do
+    Oban.Testing.perform_job(Operately.Activities.NotificationDispatcher, %{activity_id: activity_id}, [])
+  end
+
+  def fetch_notification(activity_id) do
+    from(n in Notification, where: n.activity_id == ^activity_id) |> Repo.one()
+  end
+
+  def notification_message(%Person{id: id, full_name: full_name}) do
+    %{
+      type: :doc,
+      content: [
+        %{
+          type: :paragraph,
+          content: [
+            %{
+              type: :mention,
+              attrs: %{
+                id: id,
+                label: full_name
+              }
+            }
+          ]
+        }
+      ]
+    }
+    |> Jason.encode!()
+  end
+end
