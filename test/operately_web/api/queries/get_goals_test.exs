59ac15c5b712b76be5bc790ce8e21a4465eecab1
Igor Šarčević <igor@operately.com>
2024-06-23 11:59:26 +0000

    Fix include_last_check_in
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- test/operately_web/api/queries/get_goals_test.exs
+++ test/operately_web/api/queries/get_goals_test.exs
@@ -19,15 +19,18 @@ defmodule OperatelyWeb.Api.Queries.GetGoalsTest do
       goal2 = goal_fixture(ctx.person, company_id: ctx.company.id, space_id: ctx.company.company_space_id)
 
       update1 = update_fixture(%{type: :goal_check_in, updatable_id: goal1.id, updatable_type: :goal, author_id: ctx.person.id})
-      update2 = update_fixture(%{type: :goal_check_in, updatable_id: goal1.id, updatable_type: :goal, author_id: ctx.person.id})
+      _update2 = update_fixture(%{type: :goal_check_in, updatable_id: goal1.id, updatable_type: :goal, author_id: ctx.person.id})
 
       update3 = update_fixture(%{type: :goal_check_in, updatable_id: goal2.id, updatable_type: :goal, author_id: ctx.person.id})
-      update4 = update_fixture(%{type: :goal_check_in, updatable_id: goal2.id, updatable_type: :goal, author_id: ctx.person.id})
+      _update4 = update_fixture(%{type: :goal_check_in, updatable_id: goal2.id, updatable_type: :goal, author_id: ctx.person.id})
+
+      update1 = Operately.Repo.preload(update1, :author)
+      update3 = Operately.Repo.preload(update3, :author)
 
       assert {200, res} = query(ctx.conn, :get_goals, %{include_last_check_in: true})
       assert length(res.goals) == 2
-      assert Enum.at(res.goals, 0).last_check_in == serialize(update1, level: :essential)
-      assert Enum.at(res.goals, 1).last_check_in == serialize(update3, level: :essential)
+      assert Enum.at(res.goals, 0).last_check_in == serialize(update1, level: :full)
+      assert Enum.at(res.goals, 1).last_check_in == serialize(update3, level: :full)
     end
 
     test "include_parent_goal", ctx do
