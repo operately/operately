ba0a7819f827d05ece5a353a2ae1863f22b36950
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-06-25 11:08:13 -0300

    Update GroupCreation operation
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- lib/operately/operations/group_creation.ex
+++ lib/operately/operations/group_creation.ex
@@ -14,7 +14,43 @@ defmodule Operately.Operations.GroupCreation do
     |> Multi.insert(:creator, fn %{group: group} ->
       Member.changeset(%{group_id: group.id, person_id: creator.id})
     end)
+    |> insert_members_access_group()
+    |> insert_managers_access_group(creator)
     |> Repo.transaction()
     |> Repo.extract_result(:group)
+  end
+
+  defp insert_members_access_group(multi) do
+    multi
+    |> Multi.insert(:members_access_group, fn changes ->
+      Operately.Access.Group.changeset(%{group_id: changes.group.id})
+    end)
+    |> Multi.insert(:members_access_binding, fn changes ->
+      Operately.Access.Binding.changeset(%{
+        access_group_id: changes.members_access_group.id,
+        access_context_id: changes.context.id,
+        access_level: 10,
+      })
+    end)
+  end
+
+  defp insert_managers_access_group(multi, creator) do
+    multi
+    |> Multi.insert(:managers_access_group, fn changes ->
+      Operately.Access.Group.changeset(%{group_id: changes.group.id})
+    end)
+    |> Multi.insert(:managers_access_binding, fn changes ->
+      Operately.Access.Binding.changeset(%{
+        access_group_id: changes.managers_access_group.id,
+        access_context_id: changes.context.id,
+        access_level: 100,
+      })
+    end)
+    |> Multi.insert(:creator_in_managers, fn changes ->
+      Operately.Access.GroupMembership.changeset(%{
+        access_group_id: changes.managers_access_group.id,
+        person_id: creator.id,
+      })
+    end)
   end
 end
