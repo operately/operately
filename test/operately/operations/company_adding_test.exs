3d7bfa8dbe29a29d9f0804fb639779f45dd60958
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-06-27 06:51:34 -0300

    Add tests for CompanyAdding operation
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

---
+++ test/operately/operations/company_adding_test.exs
@@ -0,0 +1,60 @@
+defmodule Operately.Operations.CompanyAddingTest do
+  use Operately.DataCase
+
+  alias Operately.Companies
+  alias Operately.Groups
+  alias Operately.People
+  alias Operately.Access
+
+  @email "john@your-company.com"
+
+  @company_attrs %{
+    :company_name => "Acme Co.",
+    :full_name => "John Doe",
+    :email => @email,
+    :role => "CEO",
+    :password => "Aa12345#&!123",
+    :password_confirmation => "Aa12345#&!123"
+  }
+
+  test "CompanyAdding operation creates company without admin" do
+    assert 0 == Companies.count_companies()
+
+    {:ok, company} = Operately.Operations.CompanyAdding.run(@company_attrs)
+
+    assert 1 == Companies.count_companies()
+    assert nil != Companies.get_company_by_name("Acme Co.")
+    assert [] == People.list_people(company.id)
+  end
+
+  test "CompanyAdding operation creates company with admin" do
+    assert 0 == Companies.count_companies()
+
+    {:ok, company} = Operately.Operations.CompanyAdding.run(@company_attrs, create_admin: true)
+    person = People.get_person_by_email(company, @email)
+
+    assert 1 == Companies.count_companies()
+    assert nil != Companies.get_company_by_name("Acme Co.")
+
+    assert 1 == length(People.list_people(company.id))
+    assert person.company_role == :admin
+    assert person.full_name == "John Doe"
+    assert person.title == "CEO"
+
+    assert nil != People.get_account_by_email(@email)
+  end
+
+  test "CompanyAdding operation creates company group" do
+    {:ok, company} = Operately.Operations.CompanyAdding.run(@company_attrs)
+
+    assert nil != company.company_space_id
+    assert nil != Groups.get_group(company.company_space_id)
+  end
+
+  test "CompanyAdding operation creates company and group access contexts" do
+    {:ok, company} = Operately.Operations.CompanyAdding.run(@company_attrs)
+
+    assert nil != Access.get_context!(company_id: company.id)
+    assert nil != Access.get_context!(group_id: company.company_space_id)
+  end
+end
