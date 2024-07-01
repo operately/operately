5afc2dea0883ac0af5f740becea83dce2111b89b
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-07-01 10:21:06 -0300

    Asjust tests
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- test/operately/operations/group_creation_test.exs
+++ test/operately/operations/group_creation_test.exs
@@ -12,7 +12,8 @@ defmodule Operately.Operations.GroupCreationTest do
     name: "my group",
     mission: "my mission",
     icon: "IconBuildingEstate",
-    color: "text-cyan-500"
+    color: "text-cyan-500",
+    company_permissions: Binding.comment_access(),
   }
 
   setup do
@@ -46,17 +47,23 @@ defmodule Operately.Operations.GroupCreationTest do
     assert nil != context
     assert 5 == length(access_groups) # 1 user's + 2 company's + 2 space's
 
-    space_members = Access.get_group(group_id: group.id, tag: :standard)
-    space_managers = Access.get_group(group_id: group.id, tag: :full_access)
-
-    assert nil != Access.get_binding(group_id: space_members.id, context_id: context.id, access_level: Binding.comment_access())
-    assert nil != Access.get_binding(group_id: space_managers.id, context_id: context.id, access_level: Binding.full_access())
+    assert nil != Access.get_group(group_id: group.id, tag: :standard)
+    assert nil != Access.get_group(group_id: group.id, tag: :full_access)
 
     company_members = Access.get_group(company_id: group.company_id, tag: :standard)
     company_admins = Access.get_group(company_id: group.company_id, tag: :full_access)
 
-    assert nil != Access.get_binding(group_id: company_members.id, context_id: context.id, access_level: Binding.view_access())
+    assert nil != Access.get_binding(group_id: company_members.id, context_id: context.id, access_level: Binding.comment_access())
     assert nil != Access.get_binding(group_id: company_admins.id, context_id: context.id, access_level: Binding.full_access())
+  end
+
+  test "GroupCreation operation creates full_access binding with creator", ctx do
+    {:ok, group} = Operately.Operations.GroupCreation.run(ctx.creator, @group_attrs)
+
+    context = Access.get_context!(group_id: group.id)
+    access_group = Access.get_group!(person_id: ctx.creator.id)
+
+    assert nil != Access.get_binding(group_id: access_group.id, context_id: context.id, access_level: Binding.full_access())
   end
 
   test "GroupCreation operation adds creator to managers group", ctx do
