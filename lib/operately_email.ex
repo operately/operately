19a26f13af8e7433284aa76b622ea1c479b5ea73
Igor Šarčević <igor@operately.com>
2024-07-02 13:10:37 +0000

    Change email urls
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- lib/operately_email.ex
+++ lib/operately_email.ex
@@ -6,18 +6,50 @@ defmodule OperatelyEmail do
 
   # URls used in emails
 
-  def app_url(), do: OperatelyWeb.Endpoint.url()
-  def project_url(project_id), do: app_url() <> "/projects/#{project_id}"
-  def project_review_url(project_id, review_id), do: app_url() <> "/projects/#{project_id}/reviews/#{review_id}"
-  def project_review_request_url(project_id, request_id), do: app_url() <> "/projects/#{project_id}/reviews/request/#{request_id}"
-  def project_check_in_new_url(project_id), do: app_url() <> "/projects/#{project_id}/check-ins/new"
-  def project_check_in_url(project_id, check_in_id), do: app_url() <> "/projects/#{project_id}/check-ins/#{check_in_id}"
-  def project_discussion_url(project_id, discussion_id), do: app_url() <> "/projects/#{project_id}/discussions/#{discussion_id}"
-  def project_milestone_url(project_id, milestone_id), do: app_url() <> "/projects/#{project_id}/milestones/#{milestone_id}"
-  def project_retrospective_url(project_id), do: app_url() <> "/projects/#{project_id}/retrospective"
-  def goal_url(goal_id), do: app_url() <> "/goals/#{goal_id}"
-  def goal_check_in_url(goal_id, check_in_id), do: app_url() <> "/goals/#{goal_id}/progress-updates/#{check_in_id}"
-  def goal_new_check_in_url(goal_id), do: app_url() <> "/goals/#{goal_id}/progress-updates/new"
-  def goal_activity_url(goal_id, activity_id), do: app_url() <> "/goals/#{goal_id}/activities/#{activity_id}"
-  def discussion_url(space_id, discussion_id), do: app_url() <> "/spaces/#{space_id}/discussions/#{discussion_id}"
+  def project_url(company_id, project_id) do
+    create_url([company_id, "projects", project_id])
+  end
+
+  def project_check_in_new_url(company_id, project_id) do 
+    create_url([company_id, "projects", project_id, "check-ins", "new"])
+  end
+
+  def project_check_in_url(company_id, project_id, check_in_id) do
+    create_url([company_id, "projects", project_id, "check-ins", check_in_id])
+  end
+
+  def project_discussion_url(company_id, project_id, discussion_id) do
+    create_url([company_id, "projects", project_id, "discussions", discussion_id])
+  end
+
+  def project_milestone_url(company_id, project_id, milestone_id) do
+    create_url([company_id, "projects", project_id, "milestones", milestone_id])
+  end
+
+  def project_retrospective_url(company_id, project_id) do
+    create_url([company_id, "projects", project_id, "retrospective"])
+  end
+
+  def goal_url(company_id, goal_id) do
+    create_url([company_id, "goals", goal_id])
+  end
+
+  def goal_check_in_url(company_id, goal_id, check_in_id) do
+    create_url([company_id, "goals", goal_id, "check-ins", check_in_id])
+  end
+  
+  def goal_new_check_in_url(company_id, goal_id) do
+    create_url([company_id, "goals", goal_id, "check-ins", "new"])
+  end
+
+  def goal_activity_url(company_id, goal_id, activity_id) do
+    create_url([company_id, "goals", goal_id, "activities", activity_id])
+  end
+
+  def discussion_url(company_id, space_id, discussion_id) do
+    create_url([company_id, "spaces", space_id, "discussions", discussion_id])
+  end
+
+  defp app_url(), do: OperatelyWeb.Endpoint.url()
+  defp create_url(parts), do: app_url() <> "/" <> Enum.join(parts, "/")
 end
