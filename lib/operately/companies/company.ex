a662177afa18d91997ebd0359202e865d1386277
Igor Šarčević <igor@operately.com>
2024-07-04 15:00:11 +0000

    Don't count suspended members in the operately lobby
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- lib/operately/companies/company.ex
+++ lib/operately/companies/company.ex
@@ -5,7 +5,7 @@ defmodule Operately.Companies.Company do
     has_one :access_context, Operately.Access.Context, foreign_key: :company_id
 
     has_many :access_groups, Operately.Access.Group, foreign_key: :company_id
-    has_many :people, Operately.People.Person, foreign_key: :company_id
+    has_many :people, Operately.People.Person, foreign_key: :company_id, where: [suspended_at: nil]
 
     field :mission, :string
     field :name, :string
