c758041917be26ce79d5abe7462eb1f2f9b36435
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-07-05 06:23:00 -0300

    Add ProjectContributorRemoved operation
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

---
+++ lib/operately/activities/content/project_contributor_removed.ex
@@ -0,0 +1,23 @@
+defmodule Operately.Activities.Content.ProjectContributorRemoved do
+  use Operately.Activities.Content
+
+  embedded_schema do
+    belongs_to :company, Operately.Companies.Company
+    belongs_to :project, Operately.Projects.Project
+    belongs_to :person, Operately.People.Person
+    belongs_to :contributor, Operately.Projects.Contributor
+
+    field :responsibility, :string
+    field :role, :string
+  end
+
+  def changeset(attrs) do
+    %__MODULE__{}
+    |> cast(attrs, __schema__(:fields))
+    |> validate_required(__schema__(:fields))
+  end
+
+  def build(params) do
+    changeset(params)
+  end
+end
---
+++ lib/operately/activities/notifications/project_contributor_removed.ex
@@ -0,0 +1,6 @@
+defmodule Operately.Activities.Notifications.ProjectContributorRemoved do
+  def dispatch(_activity) do
+    # Notification dispatcher for ProjectContributorRemoved not implemented yet
+    {:ok, []}
+  end
+end
