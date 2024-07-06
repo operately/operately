f4159b17c9654f43dfd723b7c72e3aa56e83e4b1
Igor Šarčević <igisar@gmail.com>
2024-07-07 00:38:32 +0200

    chore: Remove person queries in GQL (#720)
    * Remove person queries in GQL
    
    Signed-off-by: Igor Šarčević <igor@operately.com>
    
    * Remove assignments queries
    
    Signed-off-by: Igor Šarčević <igor@operately.com>
    
    * Remove goal queries
    
    Signed-off-by: Igor Šarčević <igor@operately.com>
    
    * Clean up non-used update queries
    
    Signed-off-by: Igor Šarčević <igor@operately.com>
    
    * Remove non-used fields in updates
    
    Signed-off-by: Igor Šarčević <igor@operately.com>
    
    * Remove deprecated tests
    
    Signed-off-by: Igor Šarčević <igor@operately.com>
    
    ---------
    
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- lib/operately_web/graphql/queries/updates.ex
+++ lib/operately_web/graphql/queries/updates.ex
@@ -2,7 +2,6 @@ defmodule OperatelyWeb.Graphql.Queries.Updates do
   use Absinthe.Schema.Notation
 
   input_object :updates_filter do
-    field :project_id, :id
     field :goal_id, :id
     field :type, :string
   end
@@ -12,19 +11,11 @@ defmodule OperatelyWeb.Graphql.Queries.Updates do
       arg :filter, non_null(:updates_filter)
 
       resolve fn args, _ ->
-        if args.filter[:project_id] do
-          updateble_id = args.filter[:project_id]
-          updateble_type = "project"
-          update_type = args.filter[:type] && String.to_existing_atom(args.filter.type)
+        updateble_id = args.filter[:goal_id]
+        updateble_type = "goal"
+        update_type = args.filter[:type] && String.to_existing_atom(args.filter.type)
 
-          {:ok, Operately.Updates.list_updates(updateble_id, updateble_type, update_type)}
-        else
-          updateble_id = args.filter[:goal_id]
-          updateble_type = "goal"
-          update_type = args.filter[:type] && String.to_existing_atom(args.filter.type)
-
-          {:ok, Operately.Updates.list_updates(updateble_id, updateble_type, update_type)}
-        end
+        {:ok, Operately.Updates.list_updates(updateble_id, updateble_type, update_type)}
       end
     end
 
