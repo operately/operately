6572e0f9958c27f8c252f08232e39cd92deb40ff
Igor Šarčević <igisar@gmail.com>
2024-07-06 21:20:13 +0200

    chore: Move add reaction to new API (#719)
    * Move add reaction to new API
    
    Signed-off-by: Igor Šarčević <igor@operately.com>
    
    * Fix types for reactions
    
    Signed-off-by: Igor Šarčević <igor@operately.com>
    
    ---------
    
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- test/operately_web/api/mutations/add_reaction_test.exs
+++ test/operately_web/api/mutations/add_reaction_test.exs
@@ -1,3 +1,43 @@
 defmodule OperatelyWeb.Api.Mutations.AddReactionTest do
-  use OperatelyWeb.ConnCase
+  use OperatelyWeb.TurboCase
+
+  import Operately.UpdatesFixtures
+  alias Operately.Support.RichText
+
+  describe "security" do
+    test "it requires authentication", ctx do
+      assert {401, _} = mutation(ctx.conn, :add_reaction, %{})
+    end
+  end
+
+  describe "mark_all_as_read functionality" do
+    setup :register_and_log_in_account
+
+    test "add reaction to a discussion", ctx do
+      discussion = create_discussion(ctx)
+
+      assert {200, res} = mutation(ctx.conn, :add_reaction, %{
+        entity_id: Paths.discussion_id(discussion),
+        entity_type: "update",
+        emoji: "👍"
+      })
+
+      reaction = hd(Operately.Updates.list_reactions(discussion.id, :update))
+      assert reaction.emoji == "👍"
+      assert res.reaction == Serializer.serialize(reaction, level: :essential)
+    end
+  end
+
+  def create_discussion(ctx, title \\ "Hello World", body \\ "How are you doing?") do
+    update_fixture(%{
+      author_id: ctx.person.id,
+      updatable_id: ctx.company.company_space_id,
+      updatable_type: :space,
+      type: :project_discussion,
+      content: %{
+        title: title,
+        body: RichText.rich_text(body)
+      }
+    })
+  end
 end 
