9be8c213e8a06a597781521ca3e8c60c4d54f224
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-07-15 15:46:14 -0300

    Add Review page
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

---
+++ assets/js/pages/ReviewPage/loader.tsx
@@ -0,0 +1,20 @@
+import * as Pages from "@/components/Pages";
+import { Assignment, getAssignments } from "@/api";
+
+interface LoaderResult {
+  assignments: Assignment[];
+  assignmentsCount: number;
+}
+
+export async function loader() : Promise<LoaderResult> {
+  const data = await getAssignments({});
+  
+  return {
+    assignments: data.assignments || [],
+    assignmentsCount: data.assignments?.length || 0,
+  }
+}
+
+export function useLoadedData(): LoaderResult {
+  return Pages.useLoadedData() as LoaderResult;
+}
