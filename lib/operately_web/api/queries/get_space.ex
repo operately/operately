720642bb5bd025545fcc733bb0218cc9c7f76a8d
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-07-18 08:50:10 -0300

    Update get_space query
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- lib/operately_web/api/queries/get_space.ex
+++ lib/operately_web/api/queries/get_space.ex
@@ -11,6 +11,7 @@ defmodule OperatelyWeb.Api.Queries.GetSpace do
     field :id, :string
     field :include_members, :boolean
     field :include_access_levels, :boolean
+    field :include_members_access_levels, :boolean
   end
 
   outputs do
@@ -34,6 +35,7 @@ defmodule OperatelyWeb.Api.Queries.GetSpace do
     (from s in Group, where: s.id == ^id, preload: [:company])
     |> Group.scope_company(person.company_id)
     |> include_requested(requested)
+    |> load_members_access_level(inputs[:include_members_access_levels], id)
     |> Repo.one()
     |> preload_is_member(person)
     |> sort_members()
@@ -45,6 +47,7 @@ defmodule OperatelyWeb.Api.Queries.GetSpace do
       case include do
         :include_members -> from p in q, preload: [:members]
         :include_access_levels -> q # this is done after the load
+        :include_members_access_levels -> q # this is done in a separate function
         e -> raise "Unknown include filter: #{inspect e}"
       end
     end)
@@ -61,4 +64,7 @@ defmodule OperatelyWeb.Api.Queries.GetSpace do
   defp load_access_levels(nil, _), do: nil
   defp load_access_levels(group, true), do: Group.preload_access_levels(group)
   defp load_access_levels(group, _), do: group
+
+  defp load_members_access_level(query, true, space_id), do: Group.preload_members_access_level(query, space_id)
+  defp load_members_access_level(query, _, _), do: query
 end
