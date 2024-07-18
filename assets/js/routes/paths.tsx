48a37575fe808cd87abc824a1c9e837ae6e85f5f
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-07-18 07:34:05 -0300

    Add SpaceAccessManagementPage
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- assets/js/routes/paths.tsx
+++ assets/js/routes/paths.tsx
@@ -183,6 +183,10 @@ export class Paths {
     return createCompanyPath(["spaces", spaceId, "appearance"]);
   }
 
+  static spaceAccessManagementPath(spaceId: string) {
+    return createCompanyPath(["spaces", spaceId, "access-management"]);
+  }
+
   static goalClosePath(goalId: string) {
     return createCompanyPath(["goals", goalId, "complete"]);
   }
