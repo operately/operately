c662fcc8cb27cdf0ec1a51c8155db01ec92d738b
Igor Šarčević <igor@operately.com>
2024-07-31 08:58:20 +0000

    Handle account and person separately in socket connections
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- lib/operately_web/api/subscriptions/unread_notifications_count.ex
+++ lib/operately_web/api/subscriptions/unread_notifications_count.ex
@@ -2,7 +2,7 @@ defmodule OperatelyWeb.Api.Subscriptions.UnreadNotificationsCount do
   use TurboConnect.Subscription
 
   def join(_name, _payload, socket) do
-    topic = socket.assigns.account.person.id
+    topic = socket.assigns.person.id
 
     {:ok, socket, [topic]}
   end
