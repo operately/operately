21069962522f5faa251efa9550b33fb565244915
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-08-01 11:44:51 -0300

    Improve permissions filter
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- lib/operately/access/filters.ex
+++ lib/operately/access/filters.ex
@@ -4,29 +4,28 @@ defmodule Operately.Access.Filters do
   alias Operately.Access.Binding
 
   def filter_by_view_access(query, person_id, opts \\ []) do
-    filter(query, person_id, Binding.view_access(), opts)
+    query
+    |> maybe_join_parent(opts)
+    |> filter(person_id, Binding.view_access())
   end
 
-  defp filter(query, person_id, access_level, join_parent: parent) do
+  defp filter(query, person_id, access_level) do
+    from([context: c] in query,
+      join: b in assoc(c, :bindings),
+      join: g in assoc(b, :group),
+      join: m in assoc(g, :memberships),
+      where: m.person_id == ^person_id and b.access_level >= ^access_level,
+      distinct: true
+    )
+  end
+
+  defp maybe_join_parent(query, join_parent: parent) do
     from(item in query,
       join: p in assoc(item, ^parent),
-      join: c in assoc(p, :access_context),
-      join: b in assoc(c, :bindings),
-      join: g in assoc(b, :group),
-      join: m in assoc(g, :memberships),
-      where: m.person_id == ^person_id and b.access_level >= ^access_level,
-      distinct: true
+      join: c in assoc(p, :access_context), as: :context
     )
   end
-
-  defp filter(query, person_id, access_level, _) do
-    from(item in query,
-      join: c in assoc(item, :access_context),
-      join: b in assoc(c, :bindings),
-      join: g in assoc(b, :group),
-      join: m in assoc(g, :memberships),
-      where: m.person_id == ^person_id and b.access_level >= ^access_level,
-      distinct: true
-    )
+  defp maybe_join_parent(query, _) do
+    from(item in query, join: c in assoc(item, :access_context), as: :context)
   end
 end
