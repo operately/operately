a120d3ba48e92ed4c7f2e868ae579aa6db450208
Igor Šarčević <igor@operately.com>
2024-08-02 08:18:58 +0000

    Unify various ways of creating a company
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- test/support/fixtures/companies_fixtures.ex
+++ test/support/fixtures/companies_fixtures.ex
@@ -1,12 +1,18 @@
 defmodule Operately.CompaniesFixtures do
+  import Operately.PeopleFixtures
+
   def company_fixture(attrs \\ %{}) do
+    creator = account_fixture()
+
     attrs = attrs |> Enum.into(%{
       mission: "some mission",
       company_name: "some name",
-      trusted_email_domains: []
+      trusted_email_domains: [],
+      role: "Founder"
     })
 
-    {:ok, company} = Operately.Companies.create_company(attrs)
+    {:ok, company} = Operately.Companies.create_company(attrs, creator)
+
     company
   end
 end
