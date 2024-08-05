4a101bee4c526eb89c3fc9bdc9d66cd6d12815b3
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2024-08-05 07:52:25 -0300

    feat: Add permissions filter to GetMilestone query (#830)
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- lib/operately_web/api/queries/get_milestone.ex
+++ lib/operately_web/api/queries/get_milestone.ex
@@ -2,6 +2,10 @@ defmodule OperatelyWeb.Api.Queries.GetMilestone do
   use TurboConnect.Query
   use OperatelyWeb.Api.Helpers
 
+  import Operately.Access.Filters, only: [filter_by_view_access: 3]
+
+  alias Operately.Projects.Milestone
+
   inputs do
     field :id, :string
     field :include_comments, :boolean
@@ -13,11 +17,24 @@ defmodule OperatelyWeb.Api.Queries.GetMilestone do
     field :milestone, :milestone
   end
 
-  def call(_conn, inputs) do
+  def call(conn, inputs) do
     {:ok, id} = decode_id(inputs.id)
-    milestone = Operately.Projects.get_milestone!(id, [with_deleted: true])
-    milestone = Operately.Repo.preload(milestone, [:project, comments: [comment: [:author, reactions: :person]]])
 
-    {:ok, %{milestone: Serializer.serialize(milestone)}}
+    milestone = load(me(conn), id)
+
+    if milestone do
+      {:ok, %{milestone: Serializer.serialize(milestone)}}
+    else
+      {:error, :not_found}
+    end
+  end
+
+  defp load(person, id) do
+    from(m in Milestone,
+      preload: [:project, comments: [comment: [:author, reactions: :person]]],
+      where: m.id == ^id
+    )
+    |> filter_by_view_access(person.id, join_parent: :project)
+    |> Repo.one()
   end
 end
