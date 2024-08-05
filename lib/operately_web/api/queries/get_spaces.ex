447e9c93cc34a4ea40f4b5f16d388f2d96868931
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-08-05 11:38:51 -0300

    Add permissions filter to get_spaces
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- lib/operately_web/api/queries/get_spaces.ex
+++ lib/operately_web/api/queries/get_spaces.ex
@@ -2,6 +2,8 @@ defmodule OperatelyWeb.Api.Queries.GetSpaces do
   use TurboConnect.Query
   use OperatelyWeb.Api.Helpers
 
+  import Operately.Access.Filters, only: [filter_by_view_access: 2, filter_by_view_access: 3]
+
   alias Operately.Groups.Group
 
   outputs do
@@ -9,13 +11,35 @@ defmodule OperatelyWeb.Api.Queries.GetSpaces do
   end
 
   def call(conn, _inputs) do
-    spaces = load(me(conn))
+    spaces = load(me(conn), company(conn))
 
     {:ok, %{spaces: Serializer.serialize(spaces, level: :full)}}
   end
 
-  defp load(me) do
-    query = from g in Group, where: g.company_id == ^me.company_id, preload: [:company], order_by: [asc: g.name]
-    Operately.Repo.all(query)
+  defp load(me, company) do
+    spaces = load_spaces(me)
+    company_space = load_company_space(me, company.company_space_id)
+
+    spaces ++ company_space
+    |> Enum.sort_by(&(&1.name))
+  end
+
+  defp load_spaces(me) do
+    from(g in Group,
+      where: g.company_id == ^me.company_id,
+      preload: [:company]
+    )
+    |> filter_by_view_access(me.id)
+    |> Operately.Repo.all()
+  end
+
+  defp load_company_space(me, id) do
+    from(g in Group,
+      join: c in assoc(g, :company), as: :company,
+      where: g.id == ^id,
+      preload: [company: c]
+    )
+    |> filter_by_view_access(me.id, named_binding: :company)
+    |> Operately.Repo.all()
   end
 end
--- test/operately_web/api/queries/get_spaces_test.exs
+++ test/operately_web/api/queries/get_spaces_test.exs
@@ -5,6 +5,10 @@ defmodule OperatelyWeb.Api.Queries.GetSpacesTest do
   import Operately.PeopleFixtures
   import Operately.GroupsFixtures
 
+  alias Operately.Repo
+  alias Operately.Groups
+  alias Operately.Access.Binding
+
   describe "security" do
     test "it requires authentication", ctx do
       assert {401, _} = query(ctx.conn, :get_space, %{id: "1"})
@@ -25,18 +29,70 @@ defmodule OperatelyWeb.Api.Queries.GetSpacesTest do
     end
   end
 
+  describe "permissions" do
+    setup ctx do
+      ctx = register_and_log_in_account(ctx)
+      creator = person_fixture(%{company_id: ctx.company.id})
+
+      Map.merge(ctx, %{creator: creator})
+    end
+
+    test "members have access to company space only", ctx do
+      Enum.each(1..3, fn _ ->
+        group_fixture(ctx.creator, company_id: ctx.company.id)
+      end)
+      spaces = Groups.list_groups_for_company(ctx.company.id)
+
+      company_space = Groups.get_group!(ctx.company.company_space_id) |> Repo.preload(:company)
+
+      assert {200, res} = query(ctx.conn, :get_spaces, %{})
+      assert length(spaces) == 4
+      assert_spaces(res, [company_space], 1)
+    end
+
+    test "members have access to spaces they are part of", ctx do
+      spaces = Enum.map(1..3, fn _ ->
+        space = group_fixture(ctx.creator, company_id: ctx.company.id)
+        add_person_to_space(ctx, space)
+        Repo.preload(space, :company)
+      end)
+      Enum.each(1..3, fn _ ->
+        group_fixture(ctx.creator, company_id: ctx.company.id)
+      end)
+
+      assert {200, res} = query(ctx.conn, :get_spaces, %{})
+      assert_spaces(res, spaces, 4)
+    end
+
+    test "members have access to spaces they are NOT part of", ctx do
+      Enum.each(1..2, fn _ ->
+        group_fixture(ctx.creator, company_id: ctx.company.id)
+      end)
+      spaces = Enum.map(1..2, fn _ ->
+        space = group_fixture(ctx.creator, [
+          company_id: ctx.company.id,
+          company_permissions: Binding.view_access(),
+        ])
+        Repo.preload(space, :company)
+      end)
+
+      assert {200, res} = query(ctx.conn, :get_spaces, %{})
+      assert_spaces(res, spaces, 3)
+    end
+  end
+
   describe "get_spaces functionality" do
     setup :register_and_log_in_account
 
     test "get_spaces", ctx do
-      s1 = group_fixture(ctx.person, company_id: ctx.company.id)
-      s2 = group_fixture(ctx.person, company_id: ctx.company.id)
+      s1 = group_fixture(ctx.person, company_id: ctx.company.id, name: "Space 1")
+      s2 = group_fixture(ctx.person, company_id: ctx.company.id, name: "Space 2")
 
       assert {200, res} = query(ctx.conn, :get_spaces, %{})
 
       company_space = Operately.Groups.get_group!(ctx.company.company_space_id)
-      
-      [s1, s2, company_space] 
+
+      [s1, s2, company_space]
       |> Enum.map(fn s -> Operately.Repo.preload(s, :company) end)
       |> Enum.sort_by(& &1.name)
       |> Enum.with_index()
@@ -44,5 +100,23 @@ defmodule OperatelyWeb.Api.Queries.GetSpacesTest do
         assert Enum.at(res.spaces, i) == Serializer.serialize(space, level: :full)
       end)
     end
-  end 
-end 
+  end
+
+  #
+  # Helpers
+  #
+
+  defp assert_spaces(res, spaces, count) do
+    assert length(res.spaces) == count
+    Enum.each(spaces, fn s ->
+      assert Enum.find(res.spaces, &(&1 == Serializer.serialize(s, level: :full)))
+    end)
+  end
+
+  defp add_person_to_space(ctx, space) do
+    Operately.Groups.add_members(ctx.person, space.id, [%{
+      id: ctx.person.id,
+      permissions: Binding.view_access(),
+    }])
+  end
+end
