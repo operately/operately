569d7f31648105683ef879f5c7926f68dd1dbad7
Igor Šarčević <igor@operately.com>
2024-08-06 14:43:11 +0000

    Create a single-host release
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- rel/single-host/build.ex
+++ rel/single-host/build.ex
@@ -1,65 +1,60 @@
 #!/usr/bin/env elixir
 
 defmodule ReleaseBuilder do
-  def run() do
-    IO.puts("Building release...")
+  def run(version) do
+    validate_version(version)
 
-    path = create_build_dir()
+    path = "build/v#{version}"
 
-    build_docker_compose_file(path)
+    IO.puts("Building release in #{path}")
+
+    create_build_dir(path)
+    build_docker_compose_file(path, version)
     build_docker_env_file(path)
-    zip_path = zip_build_dir(path)
+    zip_build_dir(path)
 
-    IO.puts("")
-    IO.puts("Release built! 🎉")
-    IO.puts(zip_path)
+    IO.puts("\nRelease built! 🎉")
   end
 
-  def create_build_dir() do
-    IO.write("* Creating build directory: ")
-    build_path = Path.join([System.tmp_dir!(), "release-build-#{:rand.uniform(20)}"])
-    File.mkdir_p!(build_path)
-
-    IO.puts(build_path)
-    build_path
+  def create_build_dir(path) do
+    IO.puts("* Creating build directory")
+    File.mkdir_p!(path)
   end
 
-  def build_docker_compose_file(build_path) do
-    IO.write("* Building docker-compose file: ")
+  def build_docker_compose_file(build_path, version) do
+    IO.puts("* Injecting docker-compose.yml")
     template_path = get_template_path("docker-compose.yml.eex")
     output_path = Path.join([build_path, "docker-compose.yml"])
-    content = EEx.eval_file(template_path, [version: "1.0.0"])
-
-    IO.puts(output_path)
-
+    content = EEx.eval_file(template_path, [version: version])
     File.write!(output_path, content)
   end
 
   def build_docker_env_file(build_path) do
-    IO.write("* Building docker env file: ")
+    IO.puts("* Injecting operately.env")
     template_path = get_template_path("operately.env")
     output_path = Path.join([build_path, "operately.env"])
     content = File.read!(template_path)
-
-    IO.puts(output_path)
-
     File.write!(output_path, content)
   end
 
   def get_template_path(file_name) do
     cwd = __ENV__.file |> Path.dirname() 
-
     Path.join([cwd, "templates", file_name])
   end
 
   def zip_build_dir(build_path) do
-    IO.write("* Zipping build directory: ")
-    zip_path = Path.join([System.tmp_dir!(), "release-build-#{:rand.uniform(20)}.zip"])
-    System.cmd("zip", ["-r", zip_path, build_path])
+    zip_path = "#{build_path}.zip"
+    IO.puts("* Zipping to #{zip_path}")
+    System.cmd("zip", ["-r", "-q", zip_path, build_path])
+  end
 
-    IO.puts(zip_path)
-    zip_path
+  def validate_version(version) do
+    if version == nil || version == "" || String.length(version) < 2 do
+      IO.puts("Error: Version is required, e.g elixir rel/single-host/build.ex 1.0.0")
+      System.halt(1)
+    end
   end
 end
 
-ReleaseBuilder.run()
+version = System.argv() |> List.first()
+ReleaseBuilder.run(version)
