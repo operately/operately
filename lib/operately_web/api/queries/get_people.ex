c0f0b2c99c08fa8c0bff6d6efeb3b0f80631a09f
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-08-06 11:26:32 -0300

    Add permissions filter to get_people
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- lib/operately_web/api/queries/get_people.ex
+++ lib/operately_web/api/queries/get_people.ex
@@ -2,6 +2,11 @@ defmodule OperatelyWeb.Api.Queries.GetPeople do
   use TurboConnect.Query
   use OperatelyWeb.Api.Helpers
 
+  import Operately.Access.Filters, only: [filter_by_view_access: 2]
+
+  alias Operately.People.Person
+  alias Operately.Companies.Company
+
   inputs do
     field :include_suspended, :boolean
     field :include_manager, :boolean
@@ -12,29 +17,35 @@ defmodule OperatelyWeb.Api.Queries.GetPeople do
   end
 
   def call(conn, inputs) do
-    company_id = me(conn).company_id
-    people = load_people(company_id, inputs)
+    people = load(me(conn), inputs)
 
     {:ok, %{people: Serializer.serialize(people, level: :full)}}
   end
 
-  defp load_people(company_id, inputs) do
-    query = from p in Operately.People.Person, where: p.company_id == ^company_id
-
-    query = if inputs[:include_suspended] do 
-      query
-    else 
-      from p in query, where: is_nil(p.suspended_at)
-    end
-
-    query = if inputs[:include_manager] do
-      from p in query, preload: [:manager]
-    else
-      query
-    end
-
-    query = from p in query, order_by: [asc: p.full_name]
-
-    Repo.all(query)
+  defp load(person, inputs) do
+    from(c in Company,
+      where: c.id == ^person.company_id,
+      select: c.id
+    )
+    |> filter_by_view_access(person.id)
+    |> Repo.one()
+    |> load_people(inputs)
   end
+
+  defp load_people(nil, _), do: []
+  defp load_people(company_id, inputs) do
+    from(p in Person,
+      where: p.company_id == ^company_id,
+      order_by: [asc: p.full_name]
+    )
+    |> include_suspended(inputs[:include_suspended])
+    |> include_manager(inputs[:include_manager])
+    |> Repo.all()
+  end
+
+  defp include_suspended(q, true), do: q
+  defp include_suspended(q, _), do: from(p in q, where: is_nil(p.suspended_at))
+
+  defp include_manager(q, true), do: from(p in q, preload: [:manager])
+  defp include_manager(q, _), do: q
 end
