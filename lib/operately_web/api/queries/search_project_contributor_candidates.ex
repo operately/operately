72b8506f5f3e1651ea2b03fa9c6b6e91762f1143
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2024-08-07 07:02:32 -0300

    feat: Add permissions filter to SearchProjectContributorCandidates query (#855)
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- lib/operately_web/api/queries/search_project_contributor_candidates.ex
+++ lib/operately_web/api/queries/search_project_contributor_candidates.ex
@@ -2,7 +2,10 @@ defmodule OperatelyWeb.Api.Queries.SearchProjectContributorCandidates do
   use TurboConnect.Query
   use OperatelyWeb.Api.Helpers
 
+  import Operately.Access.Filters, only: [filter_by_view_access: 2]
+
   alias Operately.Projects
+  alias Operately.Projects.Project
 
   inputs do
     field :project_id, :string
@@ -13,11 +16,20 @@ defmodule OperatelyWeb.Api.Queries.SearchProjectContributorCandidates do
     field :people, list_of(:person)
   end
 
-  def call(_conn, inputs) do
+  def call(conn, inputs) do
     {:ok, project_id} = decode_id(inputs.project_id)
 
-    people = Projects.list_project_contributor_candidates(project_id, inputs.query, [], 10)
+    if has_permissions?(me(conn), project_id) do
+      people = Projects.list_project_contributor_candidates(project_id, inputs.query, [], 10)
+      {:ok, %{people: Serializer.serialize(people)}}
+    else
+      {:ok, %{people: []}}
+    end
+  end
 
-    {:ok, %{people: Serializer.serialize(people)}}
+  defp has_permissions?(person, project_id) do
+    from(p in Project, where: p.id == ^project_id)
+    |> filter_by_view_access(person.id)
+    |> Repo.exists?()
   end
 end
