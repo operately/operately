b63277f97168cdbf41d52ca7c2da3865d65fd5f8
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2024-08-07 07:03:15 -0300

    feat: Add permissions filter to SearchPotentialSpaceMembers query (#854)
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- lib/operately_web/api/queries/search_potential_space_members.ex
+++ lib/operately_web/api/queries/search_potential_space_members.ex
@@ -2,6 +2,11 @@ defmodule OperatelyWeb.Api.Queries.SearchPotentialSpaceMembers do
   use TurboConnect.Query
   use OperatelyWeb.Api.Helpers
 
+  import Operately.Access.Filters, only: [filter_by_view_access: 2]
+
+  alias Operately.People.Person
+  alias Operately.Groups.{Group, Member}
+
   inputs do
     field :group_id, :string
     field :query, :string
@@ -13,15 +18,41 @@ defmodule OperatelyWeb.Api.Queries.SearchPotentialSpaceMembers do
     field :people, list_of(:person)
   end
 
-  def call(_conn, inputs) do
+  def call(conn, inputs) do
     {:ok, space_id} = decode_id(inputs.group_id)
 
-    query = inputs[:query] || ""
-    excluded_ids = inputs[:exclude_ids] || []
+    if has_permissions?(me(conn), space_id) do
+      people = load_members(inputs, space_id)
+      {:ok, %{people: Serializer.serialize(people)}}
+    else
+      {:ok, %{people: []}}
+    end
+  end
+
+  defp has_permissions?(person, space_id) do
+    from(g in Group, where: g.id == ^space_id)
+    |> filter_by_view_access(person.id)
+    |> Repo.exists?()
+  end
+
+  defp load_members(inputs, space_id) do
     limit = inputs[:limit] || 10
 
-    people = Operately.Groups.list_potential_members(space_id, query, excluded_ids, limit)
+    from(p in Person,
+      left_join: m in Member, on: p.id == m.person_id and m.group_id == ^space_id,
+      where: is_nil(m) and not p.suspended,
+      limit: ^limit
+    )
+    |> exclude_ids(inputs[:exclude_ids])
+    |> string_query(inputs[:query])
+    |> Repo.all()
+  end
 
-    {:ok, %{people: Serializer.serialize(people)}}
+  defp exclude_ids(q, nil), do: q
+  defp exclude_ids(q, exclude_ids), do: from(p in q, where: p.id not in ^exclude_ids)
+
+  defp string_query(q, nil), do: q
+  defp string_query(q, str) do
+    from(p in q, where: ilike(p.full_name, ^"%#{str}%") or ilike(p.title, ^"%#{str}%"))
   end
 end
