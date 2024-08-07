956ee176e812f9e07107cab3432d7b83bf9c4032
Igor Šarčević <igor@operately.com>
2024-08-07 09:59:02 +0000

    Calc next tag based on release type
    Signed-off-by: Igor Šarčević <igor@operately.com>

---
+++ scripts/calc_next_tag.sh
@@ -0,0 +1,27 @@
+git fetch --tags
+
+latest=$(git tag | sort --version-sort | tail -n 1)
+
+if [ -z "$latest" ]; then
+  echo "v0.0.1"
+  exit 0
+fi
+
+case $1 in
+  Major)
+    next=$(echo $$latest | cut -c 2- | awk -F '.' '{ print "v" $$1+1 ".0.0" }')
+    echo $$next
+    ;;
+  Minor)
+    next=$(echo $$latest | cut -c 2- | awk -F '.' '{ print "v" $$1 "." $$2 + 1 ".0" }')
+    echo $$next
+    ;;
+  Patch)
+    next=$(echo $$latest | cut -c 2- | awk -F '.' '{ print "v" $$1 "." $$2 "." $$3+1 }')
+    echo $$next
+    ;;
+  *)
+    echo "Invalid release type: $RELEASE_TYPE"
+    exit 1
+    ;;
+esac
