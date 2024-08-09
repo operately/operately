f6ca4d3d13aa1186cbeda0c39537506f3b0c5848
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-08-09 14:22:24 -0300

    Add CompanyAdminRemoving operation
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

---
+++ lib/operately/activities/notifications/company_admin_removed.ex
@@ -0,0 +1,6 @@
+defmodule Operately.Activities.Notifications.CompanyAdminRemoved do
+  def dispatch(_activity) do
+    # Notification dispatcher for CompanyAdminRemoved not implemented yet
+    {:ok, []}
+  end
+end
