63f2fccba96c0609a808cbf3b8436a638cbf5ce3
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-08-13 17:55:59 -0300

    feat: Add permissions filter to TaskDescriptionChange mutation
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- lib/operately/operations/task_description_change.ex
+++ lib/operately/operations/task_description_change.ex
@@ -1,15 +1,11 @@
 defmodule Operately.Operations.TaskDescriptionChange do
-  import Ecto.Query, only: [from: 2]
-
   alias Ecto.Multi
   alias Operately.Repo
   alias Operately.Activities
   alias Operately.Tasks.Task
 
-  def run(author, task_id, description) do
-    task = from(t in Task, where: t.id == ^task_id, preload: :group) |> Repo.one()
+  def run(author, task, description) do
     space = task.group
-
     changeset = Task.changeset(task, %{description: description})
 
     Multi.new()
