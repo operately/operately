540cde28e91a506fef50e67cab0ec7da2eab9d17
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2024-08-14 06:10:20 -0300

    feat: Add permissions filter to CreateTask mutation (#912)
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- lib/operately_web/api/mutations/create_task.ex
+++ lib/operately_web/api/mutations/create_task.ex
@@ -2,6 +2,10 @@ defmodule OperatelyWeb.Api.Mutations.CreateTask do
   use TurboConnect.Mutation
   use OperatelyWeb.Api.Helpers
 
+  import Operately.Access.Filters, only: [filter_by_edit_access: 3, forbidden_or_not_found: 3]
+
+  alias Operately.Repo
+
   inputs do
     field :name, :string
     field :assignee_ids, list_of(:string)
@@ -14,22 +18,31 @@ defmodule OperatelyWeb.Api.Mutations.CreateTask do
   end
 
   def call(conn, inputs) do
+    author = me(conn)
     {:ok, milestone_id} = decode_id(inputs.milestone_id)
+    {:ok, assignee_ids} = decode_id(inputs.assignee_ids)
 
     inputs = Map.merge(inputs, %{
       milestone_id: milestone_id,
-      assignee_ids: decode_assignees(inputs.assignee_ids),
+      assignee_ids: assignee_ids,
     })
 
-    creator = me(conn)
-    {:ok, task} = Operately.Operations.TaskAdding.run(creator, inputs)
-    {:ok, %{task: Serializer.serialize(task, level: :essential)}}
+    if has_permissions?(author, milestone_id) do
+      {:ok, task} = Operately.Operations.TaskAdding.run(author, inputs)
+      {:ok, %{task: Serializer.serialize(task, level: :essential)}}
+    else
+      query(milestone_id)
+      |> forbidden_or_not_found(author.id, join_parent: :project)
+    end
   end
 
-  defp decode_assignees(ids) do
-    Enum.map(ids, fn id ->
-      {:ok, id} = decode_id(id)
-      id
-    end)
+  defp has_permissions?(author, milestone_id) do
+    query(milestone_id)
+    |> filter_by_edit_access(author.id, join_parent: :project)
+    |> Repo.exists?()
+  end
+
+  defp query(milestone_id) do
+    from(m in Operately.Projects.Milestone, where: m.id == ^milestone_id)
   end
 end
