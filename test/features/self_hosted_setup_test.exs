e5da2b38f317cfaf1c3cdf39416a15d3ad8b722d
Igor Šarčević <igor@operately.com>
2024-08-15 17:27:30 +0000

    Refactor self-hosted setup specs
    Signed-off-by: Igor Šarčević <igor@operately.com>

---
+++ test/features/self_hosted_setup_test.exs
@@ -0,0 +1,25 @@
+defmodule Operately.Features.SelfHostedSetupTest do
+  use Operately.FeatureCase
+  alias Operately.Support.Features.SelfHostedSetupSteps, as: Steps
+
+  @company_info %{
+    company_name: "Acme Co.",
+    full_name: "John Doe",
+    email: "john@your-company.com",
+    title: "CEO",
+    password: "Aa12345#&!123",
+    password_confirmation: "Aa12345#&!123"
+  }
+
+  feature "setting up a self-hosted instance", ctx do
+    ctx
+    |> Steps.open_operately()
+    |> Steps.assert_on_the_setup_page()
+    |> Steps.fill_out_setup_form(@company_info)
+    |> Steps.submit_setup_form()
+    |> Steps.assert_account_and_company_created(@company_info)
+    |> Steps.assert_on_company_home()
+    |> Steps.setup_page_is_no_longer_accessible()
+  end
+
+end
