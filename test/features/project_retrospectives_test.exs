27af6989ae81fe0850c1a12d0c6f42b73c9fe40c
Igor Šarčević <igor@operately.com>
2024-08-15 15:57:04 +0000

    Fix email testing steps
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- test/features/project_retrospectives_test.exs
+++ test/features/project_retrospectives_test.exs
@@ -1,90 +1,33 @@
 defmodule Operately.Features.ProjectRetrospectivesTest do
   use Operately.FeatureCase
+  alias Operately.Support.Features.ProjectRetrospectiveSteps, as: Steps
 
-  alias Operately.Support.Features.ProjectSteps
-  alias Operately.Support.Features.FeedSteps
-  alias Operately.Support.Features.NotificationsSteps
-  alias Operately.Support.Features.EmailSteps
+  setup ctx, do: Steps.setup(ctx)
 
-  setup ctx do
-    ctx = ProjectSteps.create_project(ctx, name: "Test Project")
-    ctx = ProjectSteps.login(ctx)
+  @tag login_as: :champion
+  feature "closing a project and submitting a retrospective", ctx do
+    params = %{
+      "author" => ctx.champion,
+      "what-went-well" => "We built the thing",
+      "what-could-ve-gone-better" => "We built the thing",
+      "what-did-you-learn" => "We learned the thing"
+    }
 
-    {:ok, ctx}
+    ctx
+    |> Steps.initiate_project_closing()
+    |> Steps.fill_in_retrospective(params)
+    |> Steps.submit_retrospective()
+    |> Steps.assert_project_retrospective_posted(params)
+    |> Steps.assert_email_sent()
+    |> Steps.assert_notification_sent()
   end
 
   @tag login_as: :champion
-  feature "closing a project with all milestones completed", ctx do
+  feature "project can't be closed without filling in the retrospective", ctx do
     ctx
-    |> add_milestone("Build a thing", :done)
-    |> add_milestone("Launch the thing", :done)
-
-    ctx
-    |> ProjectSteps.visit_project_page()
-    |> ProjectSteps.visit_close_project_page()
-    |> fill_rich_text_in("what-went-well", "We built the thing")
-    |> fill_rich_text_in("what-could-ve-gone-better", "We built the thing")
-    |> fill_rich_text_in("what-did-you-learn", "We learned the thing")
-    |> UI.click(testid: "submit")
-
-    ctx
-    |> FeedSteps.assert_project_retrospective_posted(author: ctx.champion)
-    |> UI.assert_text("This project was closed on")
-    |> UI.click(testid: "project-retrospective-link")
-    |> UI.assert_text(ctx.champion.full_name)
-    |> UI.assert_text("We built the thing")
-    |> UI.assert_text("We learned the thing")
-    |> UI.assert_text("We built the thing")
-    |> UI.refute_has(testid: "close-project-button")
-
-    ctx
-    |> EmailSteps.assert_activity_email_sent(%{
-      where: ctx.project.name,
-      to: ctx.reviewer,
-      author: ctx.champion,
-      action: "closed the project and submitted a retrospective"
-    })
-
-    email = UI.list_sent_emails(ctx) |> List.last()
-    assert email.html_body =~ "We built the thing"
-    assert email.html_body =~ "We learned the thing"
-    assert email.html_body =~ "We built the thing"
-
-    ctx
-    |> UI.login_as(ctx.reviewer)
-    |> NotificationsSteps.assert_project_retrospective_sent(author: ctx.champion)
+    |> Steps.initiate_project_closing()
+    |> Steps.submit_retrospective()
+    |> Steps.assert_retrospective_error()
   end
 
-  @tag login_as: :champion
-  feature "can't close a project with an emtpy retrospective", ctx do
-    ctx
-    |> ProjectSteps.visit_project_page()
-    |> ProjectSteps.visit_close_project_page()
-    |> UI.click(testid: "submit")
-    |> UI.assert_text("Please fill in this field")
-  end
-
-  #
-  # Helpers
-  #
-
-  defp add_milestone(ctx, name, status) do
-    changeset = Operately.Projects.Milestone.changeset(%{
-      title: name,
-      deadline_at: NaiveDateTime.utc_now(),
-      status: status,
-      project_id: ctx.project.id
-    })
-
-    {:ok, _} = Operately.Repo.insert(changeset)
-
-    ctx
-  end
-
-  defp fill_rich_text_in(ctx, testid, content) do
-    ctx
-    |> UI.find(UI.query(testid: testid), fn el ->
-      UI.fill_rich_text(el, content)
-    end)
-  end
 end
---
+++ test/support/features/ui/emails.ex
@@ -0,0 +1,129 @@
+defmodule Operately.Support.Features.UI.Emails do
+  use ExUnit.CaseTemplate
+
+  alias __MODULE__.SentEmail
+  alias __MODULE__.SentEmails
+
+  def assert_email_sent(subject, receiver) do
+    {found, emails} = retry(times: 10, sleep: 200, fun: fn -> 
+      emails = list_sent_emails()
+      found = SentEmails.any?(emails, %{subject: subject, to: receiver})
+
+      if found do
+        {:ok, {found, emails}}
+      else
+        {:error, {found, emails}}
+      end
+    end)
+
+    error = assert_email_error_message(emails, subject, receiver, "Expected email to be sent")
+    assert found, error
+  end
+
+  def refute_email_sent(subject, receiver) do
+    {found, emails} = retry(times: 10, sleep: 200, fun: fn -> 
+      emails = list_sent_emails()
+      found = SentEmails.any?(emails, %{subject: subject, to: receiver})
+
+      if !found do
+        {:ok, {found, emails}}
+      else
+        {:error, {found, emails}}
+      end
+    end)
+
+    error = assert_email_error_message(emails, subject, receiver, "Expected email not to be sent")
+    refute !found, error
+  end
+
+  def list_sent_emails do
+    {:messages, messages} = Process.info(self(), :messages)
+
+    messages
+    |> Enum.filter(fn m -> match?({:delivered_email, _}, m) end)
+    |> Enum.map(fn {:delivered_email, email} -> email end)
+    |> SentEmails.new()
+  end
+
+  def last_sent_email() do
+    list_sent_emails() |> List.last()
+  end
+
+  def find_link(email, text) do
+    email.html
+    |> Floki.find("a[href]") 
+    |> Enum.filter(fn el -> Floki.text(el) == text end)
+    |> Floki.attribute("href")
+    |> case do
+      [] -> 
+        raise "No links found in email with text: #{text}"
+
+      links ->
+        links
+        |> List.first()
+        |> String.replace(OperatelyWeb.Endpoint.url(), "")
+    end
+  end
+
+  defp assert_email_error_message(emails, title, to, error_title) do
+    """
+    #{error_title}
+    #{SentEmail.as_string(%{subject: title, to: [to]})}
+
+    Sent emails:
+    #{SentEmails.as_string(emails)}
+    """
+  end
+
+  defp retry(times: times, sleep: sleep, fun: fun) do
+    case fun.() do
+      {:ok, result} -> result
+
+      {:error, _result} when times > 0 ->
+        Process.sleep(sleep)
+        retry(times: times - 1, sleep: sleep, fun: fun)
+
+      {:error, result} when times == 0 ->
+        result
+    end
+  end
+
+  defmodule SentEmail do
+    defstruct subject: nil, to: nil, html: nil, text: nil
+
+    def new(bamboo_email = %Bamboo.Email{}) do
+      %SentEmail{
+        subject: bamboo_email.subject,
+        to: Enum.map(bamboo_email.to, fn {_name, email} -> email end),
+        html: bamboo_email.html_body,
+        text: bamboo_email.text_body
+      }
+    end
+
+    def as_string(email) do
+      Enum.join([
+        "- Subject: #{inspect(email.subject)}",
+        "  To: #{Enum.join(email.to, ", ")}",
+      ], "\n")
+    end
+
+    def matches?(email, %{subject: subject, to: to}) do
+      email.subject == subject && Enum.any?(email.to, fn t -> t == to end)
+    end
+  end
+
+  defmodule SentEmails do
+    def new(emails) when is_list(emails) do
+      emails |> Enum.map(fn email -> SentEmail.new(email) end)
+    end
+
+    def as_string(emails) when is_list(emails) do
+      emails |> Enum.map(fn email -> SentEmail.as_string(email) end) |> Enum.join("\n")
+    end
+
+    def any?(emails, %{subject: subject, to: to}) when is_list(emails) do
+      Enum.any?(emails, fn email -> SentEmail.matches?(email, %{subject: subject, to: to}) end)
+    end
+  end
+
+end
