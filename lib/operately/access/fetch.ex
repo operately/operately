689e2e19468d9715fc77dcb6c396a5d10b36ad65
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2024-08-19 08:51:04 -0300

    feat: Add permissions filter to JoinSpace mutation (#944)
    * Add Operately.Access.Fetch
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    * feat: Add permissions filter to JoinSpace mutation
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    * Adjust tests
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    ---------
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

---
+++ lib/operately/access/fetch.ex
@@ -0,0 +1,46 @@
+defmodule Operately.Access.Fetch do
+  import Ecto.Query, only: [from: 2]
+
+  alias Operately.Repo
+  alias Operately.Access.Binding
+
+  def get_resource_and_access_level(query, person_id) do
+    query = join_resources(query, person_id)
+
+    from([resource: r, binding: b] in query,
+      where: b.access_level >= ^Binding.view_access(),
+      group_by: r.id,
+      select: {r, max(b.access_level)}
+    )
+    |> Repo.one()
+    |> case do
+      nil -> {:error, :not_found}
+      {r, level} -> {:ok, r, level}
+    end
+  end
+
+  def get_access_level(query, person_id) do
+    query = join_resources(query, person_id)
+
+    from([binding: b] in query,
+      where: b.access_level >= ^Binding.view_access(),
+      select: max(b.access_level)
+    )
+    |> Repo.one()
+    |> case do
+      nil -> Binding.no_access()
+      level -> level
+    end
+  end
+
+  defp join_resources(query, person_id) do
+    from([resource: r] in query,
+      join: c in assoc(r, :access_context),
+      join: b in assoc(c, :bindings), as: :binding,
+      join: g in assoc(b, :group),
+      join: m in assoc(g, :memberships),
+      join: p in assoc(m, :person),
+      where: m.person_id == ^person_id and is_nil(p.suspended_at)
+    )
+  end
+end
--- lib/operately/groups/permissions.ex
+++ lib/operately/groups/permissions.ex
@@ -6,6 +6,7 @@ defmodule Operately.Groups.Permissions do
     :can_edit,
     :can_edit_members_permissions,
     :can_edit_permissions,
+    :can_join,
   ]
 
   def calculate_permissions(access_level) do
@@ -14,6 +15,7 @@ defmodule Operately.Groups.Permissions do
       can_edit: can_edit(access_level),
       can_edit_members_permissions: can_edit_members_permissions(access_level),
       can_edit_permissions: can_edit_permissions(access_level),
+      can_join: can_join(access_level),
     }
   end
 
@@ -21,4 +23,5 @@ defmodule Operately.Groups.Permissions do
   def can_edit(access_level), do: access_level >= Binding.edit_access()
   def can_edit_members_permissions(access_level), do: access_level >= Binding.full_access()
   def can_edit_permissions(access_level), do: access_level >= Binding.full_access()
+  def can_join(access_level), do: access_level >= Binding.full_access()
 end
