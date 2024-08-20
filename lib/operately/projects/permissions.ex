0faff1580ecb0fcdcaa3128740bdfdf9ae733867
Igor Šarčević <igor@operately.com>
2024-08-20 19:55:21 +0000

    feat: Security for update_project_description
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- lib/operately/projects/permissions.ex
+++ lib/operately/projects/permissions.ex
@@ -51,6 +51,7 @@ defmodule Operately.Projects.Permissions do
     %__MODULE__{
       can_edit_name: can_edit_name(access_level),
       can_edit_permissions: can_edit_permissions(access_level),
+      can_edit_description: can_edit_description(access_level),
       can_edit_task: can_edit_task(access_level),
     }
   end
@@ -76,6 +77,7 @@ defmodule Operately.Projects.Permissions do
   def can_edit_name(access_level), do: access_level >= Binding.edit_access()
   def can_edit_permissions(access_level), do: access_level >= Binding.full_access()
   def can_edit_task(access_level), do: access_level >= Binding.edit_access()
+  def can_edit_description(access_level), do: access_level >= Binding.edit_access()
 
   def check(access_level, permission) do
     permissions = calculate_permissions(access_level)
--- lib/operately_web/api/mutations/update_project_description.ex
+++ lib/operately_web/api/mutations/update_project_description.ex
@@ -2,6 +2,9 @@ defmodule OperatelyWeb.Api.Mutations.UpdateProjectDescription do
   use TurboConnect.Mutation
   use OperatelyWeb.Api.Helpers
 
+  alias Operately.Projects
+  alias Operately.Projects.Permissions
+
   inputs do
     field :project_id, :string
     field :description, :string
@@ -11,14 +14,29 @@ defmodule OperatelyWeb.Api.Mutations.UpdateProjectDescription do
     field :project, :project
   end
 
-  def call(_conn, inputs) do
-    {:ok, id} = decode_id(inputs.project_id)
-    project = Operately.Projects.get_project!(id)
+  def call(conn, inputs) do
+    Action.new()
+    |> run(:me, fn -> find_me(conn) end)
+    |> run(:project_id, fn -> decode_id(inputs.project_id) end)
+    |> run(:project, fn ctx -> Projects.get_project_with_access_level(ctx.project_id, ctx.me.id) end)
+    |> run(:check_permissions, fn ctx -> Permissions.check(ctx.project.requester_access_level, :can_edit_description) end)
+    |> run(:operation, fn ctx -> update_project_description(ctx.project, inputs) end)
+    |> run(:serialized, fn ctx -> {:ok, %{project: Serializer.serialize(ctx.operation)}} end)
+    |> respond()
+  end
 
-    {:ok, project} = Operately.Projects.update_project(project, %{
-      description: Jason.decode!(inputs.description)
-    })
+  def update_project_description(project, inputs) do
+    Operately.Projects.update_project(project, %{description: Jason.decode!(inputs.description)})
+  end
 
-    {:ok, %{project: OperatelyWeb.Api.Serializer.serialize(project)}}
+  def respond(result) do
+    case result do
+      {:ok, ctx} -> {:ok, ctx.serialized}
+      {:error, :project_id, _} -> {:error, :bad_request}
+      {:error, :project, _} -> {:error, :not_found}
+      {:error, :check_permissions, _} -> {:error, :forbidden}
+      {:error, :operation, _} -> {:error, :internal_server_error}
+      _ -> {:error, :internal_server_error}
+    end
   end
 end
