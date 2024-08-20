a863208d018aa81dce3e4791e5edf274ffd3b65b
Igor Šarčević <igor@operately.com>
2024-08-20 13:43:56 +0000

    Set up new layout for people management page
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- assets/js/pages/CompanyAdminManagePeoplePage/page.tsx
+++ assets/js/pages/CompanyAdminManagePeoplePage/page.tsx
@@ -4,27 +4,58 @@ import * as Pages from "@/components/Pages";
 
 import { useLoadedData } from "./loader";
 import { PeopleList } from "./PeopleList";
-import { AddPeopleButton } from "./AddPeopleButton";
 import { Paths } from "@/routes/paths";
+import { FilledButton } from "@/components/Button";
 
 export function Page() {
   const { company } = useLoadedData();
 
   return (
     <Pages.Page title={["Manage People", company.name!]}>
-      <Paper.Root>
-        <Paper.Navigation>
-          <Paper.NavItem linkTo={Paths.companyAdminPath()}>Company Administration</Paper.NavItem>
-        </Paper.Navigation>
-        <Paper.Body>
-          <div className="text-content-accent text-2xl font-extrabold text-center leading-none">
-            People in {company.name}
-          </div>
+      <Paper.Root size="large">
+        <Navigation />
 
-          <AddPeopleButton />
+        <Paper.Body>
+          <Header />
           <PeopleList />
         </Paper.Body>
       </Paper.Root>
     </Pages.Page>
+  );
+}
+
+function Navigation() {
+  return (
+    <Paper.Navigation>
+      <Paper.NavItem linkTo={Paths.companyAdminPath()}>Company Administration</Paper.NavItem>
+    </Paper.Navigation>
+  );
+}
+
+function Header() {
+  return (
+    <div className="flex items-start justify-between">
+      <Title />
+      <AddMemberButton />
+    </div>
+  );
+}
+
+function Title() {
+  return (
+    <div>
+      <div className="text-content-accent text-3xl font-extrabold leading-none">People Management</div>
+      <div className="mt-2">Invite new members, edit profiles, or remove members from the company.</div>
+    </div>
+  );
+}
+
+function AddMemberButton() {
+  const addPeoplePath = Paths.companyManagePeopleAddPeoplePath();
+
+  return (
+    <FilledButton type="primary" linkTo={addPeoplePath} testId="add-person">
+      Invite a new member
+    </FilledButton>
   );
 }
