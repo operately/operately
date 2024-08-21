d1423c403e203e9813ec9e69666e13dfe50e6a79
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2024-08-21 12:32:45 -0300

    feat: Move project to space security (#979)
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- lib/operately_web/api/mutations/move_project_to_space.ex
+++ lib/operately_web/api/mutations/move_project_to_space.ex
@@ -2,20 +2,39 @@ defmodule OperatelyWeb.Api.Mutations.MoveProjectToSpace do
   use TurboConnect.Mutation
   use OperatelyWeb.Api.Helpers
 
+  alias Operately.Groups
+  alias Operately.Projects
+  alias Operately.Projects.Permissions
+  alias Operately.Operations.ProjectSpaceMoving
+
   inputs do
     field :project_id, :string
     field :space_id, :string
   end
 
   def call(conn, inputs) do
-    author = me(conn)
+    Action.new()
+    |> run(:me, fn -> find_me(conn) end)
+    |> run(:project_id, fn -> decode_id(inputs.project_id) end)
+    |> run(:space_id, fn -> decode_id(inputs.space_id) end)
+    |> run(:project, fn ctx -> Projects.get_project_with_access_level(ctx.project_id, ctx.me.id) end)
+    |> run(:project_permissions, fn ctx -> Permissions.check(ctx.project.requester_access_level, :can_edit_space) end)
+    |> run(:space_access_level, fn ctx -> {:ok, Groups.get_access_level(ctx.space_id, ctx.me.id)} end)
+    |> run(:space_permissions, fn ctx -> Groups.Permissions.check(ctx.space_access_level, :can_view) end)
+    |> run(:operation, fn ctx -> ProjectSpaceMoving.run(ctx.me, ctx.project, ctx.space_id) end)
+    |> respond()
+  end
 
-    {:ok, project_id} = decode_id(inputs.project_id)
-    {:ok, space_id} = decode_id(inputs.space_id)
-
-    project = Operately.Projects.get_project!(project_id)
-    {:ok, _project} = Operately.Operations.ProjectSpaceMoving.run(author, project, space_id)
-
-    {:ok, %{}}
+  defp respond(result) do
+    case result do
+      {:ok, _} -> {:ok, %{}}
+      {:error, :project_id, _} -> {:error, :bad_request}
+      {:error, :space_id, _} -> {:error, :bad_request}
+      {:error, :project, _} -> {:error, :not_found}
+      {:error, :project_permissions, _} -> {:error, :forbidden}
+      {:error, :space_permissions, _} -> {:error, :not_found}
+      {:error, :operation, _} -> {:error, :internal_server_error}
+      _ -> {:error, :internal_server_error}
+    end
   end
 end
