3e660a1edaa08727c6277ee254d3063f60cb90b6
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-08-21 11:20:45 -0300

    feat: Move project to space security
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- lib/operately/projects/permissions.ex
+++ lib/operately/projects/permissions.ex
@@ -52,13 +52,14 @@ defmodule Operately.Projects.Permissions do
   defp calculate_permissions(access_level) do
     %__MODULE__{
       can_edit_check_in: can_edit_check_in(access_level),
-      can_edit_timeline: can_edit_timeline(access_level),
-      can_edit_resources: can_edit_resources(access_level),
-      can_edit_name: can_edit_name(access_level),
-      can_edit_permissions: can_edit_permissions(access_level),
       can_edit_description: can_edit_description(access_level),
       can_edit_milestone: can_edit_milestone(access_level),
+      can_edit_name: can_edit_name(access_level),
+      can_edit_permissions: can_edit_permissions(access_level),
+      can_edit_resources: can_edit_resources(access_level),
+      can_edit_space: can_edit_space(access_level),
       can_edit_task: can_edit_task(access_level),
+      can_edit_timeline: can_edit_timeline(access_level),
       can_pause: can_pause(access_level),
     }
   end
@@ -82,13 +83,14 @@ defmodule Operately.Projects.Permissions do
   end
 
   def can_edit_check_in(access_level), do: access_level >= Binding.full_access()
-  def can_edit_timeline(access_level), do: access_level >= Binding.edit_access()
-  def can_edit_resources(access_level), do: access_level >= Binding.edit_access()
-  def can_edit_name(access_level), do: access_level >= Binding.edit_access()
-  def can_edit_permissions(access_level), do: access_level >= Binding.full_access()
-  def can_edit_task(access_level), do: access_level >= Binding.edit_access()
   def can_edit_description(access_level), do: access_level >= Binding.edit_access()
   def can_edit_milestone(access_level), do: access_level >= Binding.edit_access()
+  def can_edit_name(access_level), do: access_level >= Binding.edit_access()
+  def can_edit_permissions(access_level), do: access_level >= Binding.full_access()
+  def can_edit_resources(access_level), do: access_level >= Binding.edit_access()
+  def can_edit_space(access_level), do: access_level >= Binding.edit_access()
+  def can_edit_task(access_level), do: access_level >= Binding.edit_access()
+  def can_edit_timeline(access_level), do: access_level >= Binding.edit_access()
   def can_pause(access_level), do: access_level >= Binding.edit_access()
 
   def check(access_level, permission) do
--- test/operately_web/api/mutations/move_project_to_space_test.exs
+++ test/operately_web/api/mutations/move_project_to_space_test.exs
@@ -3,6 +3,9 @@ defmodule OperatelyWeb.Api.Mutations.MoveProjectToSpaceTest do
 
   import Operately.ProjectsFixtures
   import Operately.GroupsFixtures
+  import Operately.PeopleFixtures
+
+  alias Operately.Access.Binding
 
   describe "security" do
     test "it requires authentication", ctx do
@@ -10,26 +13,151 @@ defmodule OperatelyWeb.Api.Mutations.MoveProjectToSpaceTest do
     end
   end
 
+  describe "permissions" do
+    @project_table [
+      %{company: :no_access,      space: :no_access,      project: :no_access,      expected: 404},
+      %{company: :no_access,      space: :no_access,      project: :comment_access, expected: 403},
+      %{company: :no_access,      space: :no_access,      project: :edit_access,    expected: 200},
+      %{company: :no_access,      space: :no_access,      project: :full_access,    expected: 200},
+
+      %{company: :no_access,      space: :comment_access, project: :no_access,      expected: 403},
+      %{company: :no_access,      space: :edit_access,    project: :no_access,      expected: 200},
+      %{company: :no_access,      space: :full_access,    project: :no_access,      expected: 200},
+
+      %{company: :comment_access, space: :no_access,      project: :no_access,      expected: 403},
+      %{company: :edit_access,    space: :no_access,      project: :no_access,      expected: 200},
+      %{company: :full_access,    space: :no_access,      project: :no_access,      expected: 200},
+    ]
+
+    @new_space_table [
+      %{company: :no_access,      space: :no_access,      expected: 404},
+      %{company: :no_access,      space: :comment_access, expected: 200},
+      %{company: :no_access,      space: :edit_access,    expected: 200},
+      %{company: :no_access,      space: :full_access,    expected: 200},
+
+      %{company: :comment_access, space: :no_access,      expected: 200},
+      %{company: :edit_access,    space: :no_access,      expected: 200},
+      %{company: :full_access,    space: :no_access,      expected: 200},
+    ]
+
+    setup ctx do
+      ctx = register_and_log_in_account(ctx)
+      creator = person_fixture(%{company_id: ctx.company.id})
+      space = group_fixture(creator, %{company_id: ctx.company.id})
+
+      Map.merge(ctx, %{space: space, creator: creator})
+    end
+
+    tabletest @project_table do
+      test "if caller has levels company=#{@test.company}, space=#{@test.space}, project=#{@test.project} on the project, then expect code=#{@test.expected}", ctx do
+        new_space = create_space(ctx, company_permissions: :view_access)
+        project = create_project(ctx, @test.company, @test.space, @test.project)
+
+        assert {code, res} = mutation(ctx.conn, :move_project_to_space, %{
+          project_id: Paths.project_id(project),
+          space_id: Paths.space_id(new_space)
+        })
+
+        assert code == @test.expected
+
+        case @test.expected do
+          200 -> assert assert_space_changed(project, new_space)
+          403 -> assert res.message == "You don't have permission to perform this action"
+          404 -> assert res.message == "The requested resource was not found"
+        end
+      end
+    end
+
+    tabletest @new_space_table do
+      test "if caller has levels company=#{@test.company}, space=#{@test.space} on the new space, then expect code=#{@test.expected}", ctx do
+        new_space = create_space(ctx, company_permissions: @test.company)
+        project = create_project(ctx, :full_access, :full_access, :full_access)
+
+        if @test.space != :no_access do
+          {:ok, _} = Operately.Groups.add_members(ctx.creator, new_space.id, [%{
+            id: ctx.person.id,
+            permissions: Binding.from_atom(@test.space)
+          }])
+        end
+
+        assert {code, res} = mutation(ctx.conn, :move_project_to_space, %{
+          project_id: Paths.project_id(project),
+          space_id: Paths.space_id(new_space)
+        })
+
+        assert code == @test.expected
+
+        case @test.expected do
+          200 -> assert assert_space_changed(project, new_space)
+          403 -> assert res.message == "You don't have permission to perform this action"
+          404 -> assert res.message == "The requested resource was not found"
+        end
+      end
+    end
+  end
+
   describe "move_project_to_space functionality" do
     setup :register_and_log_in_account
 
     test "it moves a project to a space", ctx do
-      person = ctx.person
       project = project_fixture(%{
         company_id: ctx.company.id,
         group_id: ctx.company.company_space_id,
-        creator_id: person.id
+        creator_id: ctx.person.id
       })
 
-      space = group_fixture(person, %{company_id: ctx.company.id})
+      space = group_fixture(ctx.person, %{company_id: ctx.company.id})
 
       assert {200, %{}} = mutation(ctx.conn, :move_project_to_space, %{
         project_id: Paths.project_id(project),
         space_id: Paths.space_id(space)
       })
 
-      project = Operately.Projects.get_project!(project.id)
-      assert project.group_id == space.id
+      assert_space_changed(project, space)
     end
   end
-end 
+
+  defp assert_space_changed(project, space) do
+    project = Repo.reload(project)
+    assert project.group_id == space.id
+  end
+
+  #
+  # Helpers
+  #
+
+  def create_space(ctx, company_permissions: permission) do
+    group_fixture(ctx.creator, %{
+      company_id: ctx.company.id,
+      company_permissions: Binding.from_atom(permission),
+    })
+  end
+
+  def create_project(ctx, company_members_level, space_members_level, project_member_level) do
+    project = project_fixture(%{
+      company_id: ctx.company.id,
+      creator_id: ctx.creator.id,
+      group_id: ctx.space.id,
+      company_access_level: Binding.from_atom(company_members_level),
+      space_access_level: Binding.from_atom(space_members_level),
+    })
+
+    if space_members_level != :no_access do
+      {:ok, _} = Operately.Groups.add_members(ctx.creator, ctx.space.id, [%{
+        id: ctx.person.id,
+        permissions: Binding.from_atom(space_members_level)
+      }])
+    end
+
+    if project_member_level != :no_access do
+      {:ok, _} = Operately.Projects.create_contributor(ctx.creator, %{
+        project_id: project.id,
+        person_id: ctx.person.id,
+        permissions: Binding.from_atom(project_member_level),
+        responsibility: "some responsibility"
+      })
+    end
+
+    project
+  end
+end
