28beeff7970cff20114c0f6446d770070d58b9d9
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-08-22 14:13:41 -0300

    Security for UpdateProjectContributor mutation
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- lib/operately_web/api/mutations/update_project_contributor.ex
+++ lib/operately_web/api/mutations/update_project_contributor.ex
@@ -2,6 +2,10 @@ defmodule OperatelyWeb.Api.Mutations.UpdateProjectContributor do
   use TurboConnect.Mutation
   use OperatelyWeb.Api.Helpers
 
+  alias Operately.Projects
+  alias Operately.Projects.Permissions
+  alias Operately.Operations.ProjectContributorEditing
+
   inputs do
     field :contrib_id, :string
     field :person_id, :string
@@ -14,14 +18,29 @@ defmodule OperatelyWeb.Api.Mutations.UpdateProjectContributor do
   end
 
   def call(conn, inputs) do
-    {:ok, id} = decode_id(inputs.contrib_id)
+    Action.new()
+    |> run(:me, fn -> find_me(conn) end)
+    |> run(:attrs, fn -> parse_inputs(inputs) end)
+    |> run(:contrib, fn ctx -> Projects.get_contributor_with_project_and_access_level(ctx.attrs.contrib_id, ctx.me.id) end)
+    |> run(:check_permissions, fn ctx -> Permissions.check(ctx.contrib.project.requester_access_level, :can_edit_contributors) end)
+    |> run(:operation, fn ctx -> ProjectContributorEditing.run(ctx.me, ctx.contrib, ctx.attrs) end)
+    |> run(:serialized, fn ctx -> {:ok, %{contributor: Serializer.serialize(ctx.operation)}} end)
+    |> respond()
+  end
+
+  defp respond(result) do
+    case result do
+      {:ok, ctx} -> {:ok, ctx.serialized}
+      {:error, :attrs, _} -> {:error, :bad_request}
+      {:error, :contrib, _} -> {:error, :not_found}
+      {:error, :check_permissions, _} -> {:error, :forbidden}
+      {:error, :operation, _} -> {:error, :internal_server_error}
+      _ -> {:error, :internal_server_error}
+    end
+  end
+
+  defp parse_inputs(inputs) do
     {:ok, person_id} = decode_id(inputs.person_id)
-    contrib = Operately.Projects.get_contributor!(id)
-
-    attrs = Map.merge(inputs, %{person_id: person_id})
-
-    {:ok, contrib} = Operately.Operations.ProjectContributorEditing.run(me(conn), contrib, attrs)
-
-    {:ok, %{contributor: OperatelyWeb.Api.Serializer.serialize(contrib)}}
+    {:ok, %{inputs | person_id: person_id}}
   end
 end
