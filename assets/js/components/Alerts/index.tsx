326f84fabb2cc68e81e159531755beb4f2ac4707
Marko Anastasov <marko@operately.com>
2024-08-27 12:55:55 +0200

    chore: Refactor alerts to props.
    Signed-off-by: Marko Anastasov <marko@operately.com>

--- assets/js/components/Alerts/index.tsx
+++ assets/js/components/Alerts/index.tsx
@@ -1,20 +1,26 @@
 import React from "react";
+import { TestableElement } from "@/utils/testid";
 
 import { IconAlertTriangleFilled, IconCircleXFilled, IconInfoCircleFilled } from "@tabler/icons-react";
 
-export function InfoAlert({ message, description }: { message: string | JSX.Element, description?: string | JSX.Element }) {
+interface Props extends TestableElement {
+  message: string | JSX.Element;
+  description?: string | JSX.Element;
+}
+
+export function InfoAlert(props: Props) {
   return (
-    <div className="rounded-md bg-blue-50 p-4">
+    <div data-test-id={props.testId} className="rounded-md bg-blue-50 p-4">
       <div className="flex">
         <div className="flex-shrink-0">
           <IconInfoCircleFilled aria-hidden="true" className="h-5 w-5 text-blue-400" />
         </div>
         <div className="ml-3">
-          <h3 className="text-sm font-medium text-blue-800">{message}</h3>
-          {description ? (
+          <h3 className="text-sm font-medium text-blue-800">{props.message}</h3>
+          {props.description ? (
             <div className="mt-2 text-sm text-blue-700">
               <p>
-                {description}
+                {props.description}
               </p>
             </div>
           ) : null}
@@ -24,19 +30,19 @@ export function InfoAlert({ message, description }: { message: string | JSX.Elem
   )
 }
 
-export function WarningAlert({ message, description }: { message: string | JSX.Element, description?: string | JSX.Element }) {
+export function WarningAlert(props: Props) {
   return (
-    <div className="rounded-md bg-yellow-50 p-4">
+    <div data-test-id={props.testId} className="rounded-md bg-yellow-50 p-4">
       <div className="flex">
         <div className="flex-shrink-0">
           <IconAlertTriangleFilled aria-hidden="true" className="h-5 w-5 text-yellow-400" />
         </div>
         <div className="ml-3">
-          <h3 className="text-sm font-medium text-yellow-800">{message}</h3>
-          {description ? (
+          <h3 className="text-sm font-medium text-yellow-800">{props.message}</h3>
+          {props.description ? (
             <div className="mt-2 text-sm text-yellow-700">
               <p>
-                {description}
+                {props.description}
               </p>
             </div>
           ) : null}
@@ -46,19 +52,19 @@ export function WarningAlert({ message, description }: { message: string | JSX.E
   )
 }
 
-export function ErrorAlert({ message, description }: { message: string | JSX.Element, description?: string | JSX.Element }) {
+export function ErrorAlert(props: Props) {
   return (
-    <div className="rounded-md bg-red-50 p-4">
+    <div data-test-id={props.testId} className="rounded-md bg-red-50 p-4">
       <div className="flex">
         <div className="flex-shrink-0">
           <IconCircleXFilled aria-hidden="true" className="h-5 w-5 text-red-400" />
         </div>
         <div className="ml-3">
-          <h3 className="text-sm font-medium text-red-800">{message}</h3>
-          {description ? (
+          <h3 className="text-sm font-medium text-red-800">{props.message}</h3>
+          {props.description ? (
             <div className="mt-2 text-sm text-red-700">
               <p>
-                {description}
+                {props.description}
               </p>
             </div>
           ) : null}
