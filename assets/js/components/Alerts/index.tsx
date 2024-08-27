c7f1e2e967dfc48453ccbb7db4a2fd295947fa59
Marko Anastasov <marko@operately.com>
2024-08-27 11:34:37 +0200

    chore: Alerts in design system.
    We're going to need this in a UI change coming soon.
    
    Signed-off-by: Marko Anastasov <marko@operately.com>

---
+++ assets/js/components/Alerts/index.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+
+import { IconAlertTriangleFilled, IconCircleXFilled, IconInfoCircleFilled } from "@tabler/icons-react";
+
+export function InfoAlert({ message, description }: { message: string, description?: string }) {
+  return (
+    <div className="rounded-md bg-blue-50 p-4">
+      <div className="flex">
+        <div className="flex-shrink-0">
+          <IconInfoCircleFilled aria-hidden="true" className="h-5 w-5 text-blue-400" />
+        </div>
+        <div className="ml-3">
+          <h3 className="text-sm font-medium text-blue-800">{message}</h3>
+          {description ? (
+            <div className="mt-2 text-sm text-blue-700">
+              <p>
+                {description}
+              </p>
+            </div>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export function WarningAlert({ message, description }: { message: string, description?: string }) {
+  return (
+    <div className="rounded-md bg-yellow-50 p-4">
+      <div className="flex">
+        <div className="flex-shrink-0">
+          <IconAlertTriangleFilled aria-hidden="true" className="h-5 w-5 text-yellow-400" />
+        </div>
+        <div className="ml-3">
+          <h3 className="text-sm font-medium text-yellow-800">{message}</h3>
+          {description ? (
+            <div className="mt-2 text-sm text-yellow-700">
+              <p>
+                {description}
+              </p>
+            </div>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export function ErrorAlert({ message, description }: { message: string, description?: string }) {
+  return (
+    <div className="rounded-md bg-red-50 p-4">
+      <div className="flex">
+        <div className="flex-shrink-0">
+          <IconCircleXFilled aria-hidden="true" className="h-5 w-5 text-red-400" />
+        </div>
+        <div className="ml-3">
+          <h3 className="text-sm font-medium text-red-800">{message}</h3>
+          {description ? (
+            <div className="mt-2 text-sm text-red-700">
+              <p>
+                {description}
+              </p>
+            </div>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  )
+}
