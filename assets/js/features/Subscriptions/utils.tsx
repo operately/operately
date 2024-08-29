556425947be5775c2a875faaea49312f60f52900
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-08-29 10:37:52 -0300

    fix findNotifiableProjectContributors
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- assets/js/features/Subscriptions/utils.tsx
+++ assets/js/features/Subscriptions/utils.tsx
@@ -1,13 +1,15 @@
 import { Person } from "@/models/people";
 import { Project } from "@/models/projects";
 
-export function findNotifiableProjectContributors(project: Project): Person[] {
+export function findNotifiableProjectContributors(project: Project, me: Person): Person[] {
   const people = project
-    .contributors!.filter((contrib) => contrib.role !== "champion")
+    .contributors!.filter((contrib) => contrib.person!.id !== me.id)
     .map((contrib) => {
       switch (contrib.role) {
         case "reviewer":
           return { ...contrib.person, title: "Reviewer" };
+        case "champion":
+          return { ...contrib.person, title: "Champion" };
         default:
           return { ...contrib.person, title: contrib.responsibility };
       }
