9252be30991794107bfc3509ea47571a78303fca
Igor Šarčević <igor@operately.com>
2024-08-30 17:11:32 +0000

    Fix goal timeframe editing notification
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- lib/operately/activities/notifications/goal_discussion_creation.ex
+++ lib/operately/activities/notifications/goal_discussion_creation.ex
@@ -1,23 +1,19 @@
 defmodule Operately.Activities.Notifications.GoalDiscussionCreation do
   def dispatch(activity) do
     goal = Operately.Goals.get_goal!(activity.content["goal_id"])
+    goal = Operately.Repo.preload(goal, [:champion, :reviewer])
     message = Operately.Repo.preload(activity, :comment_thread).comment_thread.message
 
-    assigned = [
-      goal.champion_id,
-      goal.reviewer_id,
-    ]
-
+    assigned = [goal.champion, goal.reviewer]
     mentioned = Operately.RichContent.lookup_mentioned_people(message)
     people = Enum.uniq_by(assigned ++ mentioned, & &1.id)
 
     notifications =
       people
-      |> Enum.uniq()
-      |> Enum.filter(fn person_id -> person_id != activity.author_id end)
-      |> Enum.map(fn person_id ->
+      |> Enum.filter(fn p -> p.id != activity.author_id end)
+      |> Enum.map(fn p ->
         %{
-          person_id: person_id,
+          person_id: p.id,
           activity_id: activity.id,
           should_send_email: true,
         }
