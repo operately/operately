6f981da4d37b7fb16bc1a3b410e7e259ca3eb2ad
Igor Šarčević <igisar@gmail.com>
2024-09-02 18:20:20 +0200

    chore: Demo builder (#1036)
    * Bootstrap demo builder
    
    Signed-off-by: Igor Šarčević <igor@operately.com>
    
    * Refactor and clean up demo builder
    
    Signed-off-by: Igor Šarčević <igor@operately.com>
    
    * Mark everything as read
    
    Signed-off-by: Igor Šarčević <igor@operately.com>
    
    ---------
    
    Signed-off-by: Igor Šarčević <igor@operately.com>

---
+++ lib/operately/demo/company.ex
@@ -0,0 +1,42 @@
+defmodule Operately.Demo.Company do
+  @moduledoc """
+  Create a company for the demo.
+  """
+
+  import Ecto.Query
+
+  def create_company(context) do
+    cleanup_acme_companies()
+
+    {:ok, company} = Operately.Operations.CompanyAdding.run(%{
+      company_name: "Acme Inc.",
+      title: "Founder"
+    }, context.account)
+
+    owner = Operately.People.get_person!(context.account, company)
+
+    context
+    |> Map.put(:company, company)
+    |> Map.put(:owner, owner)
+  end
+
+  @doc """
+  As we don't have a way to delete companies (yet), we are cleaning up
+  the database by renaming companies that contain 'Acme Inc.' in their 
+  name.
+
+  We are doing this only in the development environment, to not risk
+  renaming companies in production by mistake.
+  """
+  def cleanup_acme_companies do
+    if Application.get_env(:operately, :app_env) == :dev do
+      companies = Operately.Repo.all(from c in Operately.Companies.Company)
+
+      companies |> Enum.each(fn c ->
+        if String.contains?(c.name, "Acme Inc.") do
+          Operately.Companies.update_company(c, %{name: "#{c.short_id}"})
+        end
+      end)
+    end
+  end
+end
---
+++ lib/operately/demo/goals.ex
@@ -0,0 +1,365 @@
+defmodule Operately.Demo.Goals do
+  @moduledoc """
+  Create several spaces for the demo.
+  """
+
+  import Ecto.Query
+
+  def create_goals_and_projects(context) do 
+    context
+    |> add_goal(%{
+      space: context.company_space,
+      name: "Achieve Product-Market Fit",
+      champion: find_person(context, "Emily Davis"),
+      reviewer: find_person(context, "Frank Miller"),
+      check_in: :default,
+    })
+    |> add_goal(%{
+      space: context.product_space,
+      name: "Release v1.0 of the Product",
+      champion: find_person(context, "Henry Taylor"),
+      reviewer: find_person(context, "Emily Davis"),
+      parent: find_goal(context, "Achieve Product-Market Fit"),
+      check_in: :default,
+    })
+    |> add_project(%{
+      space: context.product_space,
+      name: "Onboarding for new users",
+      champion: find_person(context, "Frank Miller"),
+      reviewer: find_person(context, "Emily Davis"),
+      parent: find_goal(context, "Release v1.0 of the Product"),
+      check_in: :default,
+      milestones: :default,
+    })
+    |> add_project(%{
+      space: context.product_space,
+      name: "Thighten the API security",
+      champion: find_person(context, "Olivia Hall"),
+      reviewer: find_person(context, "Emily Davis"),
+      parent: find_goal(context, "Release v1.0 of the Product"),
+      check_in: :default,
+      milestones: :default,
+    })
+    |> add_project(%{
+      space: context.product_space,
+      name: "Improve the performance of the app",
+      champion: find_person(context, "Frank Miller"),
+      reviewer: find_person(context, "Emily Davis"),
+      parent: find_goal(context, "Release v1.0 of the Product"),
+      check_in: :default,
+      milestones: :default,
+    })
+    |> add_goal(%{
+      space: context.marketing_space,
+      name: "Launch Marketing Campaign",
+      champion: find_person(context, "Ivy Anderson"),
+      reviewer: find_person(context, "Emily Davis"),
+      parent: find_goal(context, "Achieve Product-Market Fit"),
+      check_in: :default,
+    })
+    |> add_project(%{
+      space: context.marketing_space,
+      name: "Build and Launch the Website",
+      champion: find_person(context, "Grace Wilson"),
+      reviewer: find_person(context, "Emily Davis"),
+      parent: find_goal(context, "Launch Marketing Campaign"),
+      check_in: :default,
+      milestones: :default,
+    })
+    |> add_project(%{
+      space: context.marketing_space,
+      name: "Launch the Social Media Campaign",
+      champion: find_person(context, "Emily Davis"),
+      reviewer: find_person(context, "Emily Davis"),
+      parent: find_goal(context, "Launch Marketing Campaign"),
+      check_in: :default,
+      milestones: :default,
+    })
+    |> add_goal(%{
+      space: context.people_space,
+      name: "Build a Strong Team",
+      champion: find_person(context, "Rachel King"),
+      reviewer: find_person(context, "Emily Davis"),
+      check_in: :default,
+    })
+    |> add_goal(%{
+      space: context.people_space,
+      name: "Recruit Key Talent",
+      champion: find_person(context, "Rachel King"),
+      reviewer: find_person(context, "Emily Davis"),
+      parent: find_goal(context, "Build a Strong Team"),
+      check_in: :default,
+    })
+    |> add_project(%{
+      space: context.people_space,
+      name: "Hire a Software Engineer",
+      description: rich_text("We want to hire a software engineer to enhance Operately's core functionalities, ensuring our product is robust, scalable, and user-friendly. The engineer will play a crucial role in developing new features, optimizing performance, and maintaining the codebase. This addition to our team is essential to accelerate our development process and meet our growing user demands."),
+      champion: find_person(context, "Emily Davis"),
+      reviewer: find_person(context, "Emily Davis"),
+      parent: find_goal(context, "Recruit Key Talent"),
+      check_in: %{
+        status: "on_track",
+        description: rich_text("We have several good candidates in the final step of the selection process. If everything goes well, we will have a hired engineer by the end of this week."),
+      },
+      milestones: [
+        "Candidates are selected for the second round",
+        "Engineer is hired"
+      ]
+    })
+    |> add_project(%{
+      space: context.people_space,
+      name: "Hire a Designer",
+      champion: find_person(context, "Jack Thomas"),
+      reviewer: find_person(context, "Emily Davis"),
+      parent: find_goal(context, "Recruit Key Talent"),
+      check_in: :default,
+      milestones: :default
+    })
+    |> add_project(%{
+      space: context.people_space,
+      name: "Hire a Support Specialist",
+      champion: find_person(context, "Tina Scott"),
+      reviewer: context.owner,
+      parent: find_goal(context, "Recruit Key Talent"),
+      check_in: :default,
+      milestones: :default
+    })
+    |> add_goal(%{
+      space: context.people_space,
+      name: "Develop a Strong Company Culture",
+      champion: find_person(context, "Jack Thomas"),
+      reviewer: find_person(context, "Emily Davis"),
+      parent: find_goal(context, "Build a Strong Team"),
+      check_in: :default,
+    })
+    |> add_project(%{
+      space: context.people_space,
+      name: "Employee Handbook",
+      champion: find_person(context, "Karen Martinez"),
+      reviewer: find_person(context, "Emily Davis"),
+      parent: find_goal(context, "Develop a Strong Company Culture"),
+      check_in: :default,
+      milestones: :default
+    })
+    |> add_project(%{
+      space: context.people_space,
+      name: "Employee Handbook",
+      champion: find_person(context, "Karen Martinez"),
+      reviewer: find_person(context, "Emily Davis"),
+      parent: find_goal(context, "Develop a Strong Company Culture"),
+      check_in: :default,
+      milestones: :default
+    })
+  end
+
+  def add_goal(context, attrs) do
+    {:ok, goal} = Operately.Operations.GoalCreation.run(context.owner, %{
+      space_id: attrs.space.id,
+      name: attrs.name,
+      champion_id: attrs.champion.id,
+      reviewer_id: attrs.reviewer.id,
+      timeframe: %{
+        start_date: ~D[2024-07-01],
+        end_date: ~D[2024-09-30],
+        type: "quarter"
+      },
+      targets: [
+        %{
+          index: 0,
+          name: "All the core features are implemented",
+          from: 1,
+          to: 17,
+          unit: "features"
+        },
+        %{
+          index: 1,
+          name: "Eliminate all the known bugs and issues before release",
+          from: 0,
+          to: 60,
+          unit: "bugs"
+        },
+        %{
+          index: 2,
+          name: "Obtain feedback from at least 100 beta testers",
+          from: 0,
+          to: 100,
+          unit: "testers"
+        },
+      ],
+      parent_goal_id: attrs[:parent] && attrs.parent.id,
+      anonymous_access_level: 0, 
+      company_access_level: 100,
+      space_access_level: 100,
+    })
+
+    goal = Operately.Repo.preload(goal, [:targets, :champion])
+
+    if attrs.check_in == :default do
+      targets = Enum.map(goal.targets, fn t ->
+        %{
+          "id" => t.id,
+          "value" => trunc(t.to/2) + :rand.uniform(trunc(t.to/2))
+        }
+      end)
+
+      {:ok, _} = Operately.Operations.GoalCheckIn.run(
+        goal.champion, 
+        goal, 
+        rich_text("Everything is going as planned! Last week we had a new batch of beta testers and they loved the product! We are now fully focusing on eliminating the leftover bugs"),
+        targets
+      )
+    end
+
+    context
+  end
+
+  def add_project(context, attrs) do
+    {:ok, project} = Operately.Operations.ProjectCreation.run(%Operately.Operations.ProjectCreation{
+      company_id: context.company.id,
+      name: attrs.name,
+      champion_id: attrs.champion.id,
+      reviewer_id: attrs.reviewer.id,
+      creator_id: context.owner.id,
+      creator_role: "contributor",
+      creator_is_contributor: "yes",
+      visibility: "everyone",
+      group_id: attrs.space.id,
+      goal_id: attrs[:parent] && attrs.parent.id,
+      anonymous_access_level: 0, 
+      company_access_level: 100,
+      space_access_level: 100,
+    })
+
+    if attrs[:description] do
+      {:ok, _} = Operately.Projects.update_project(project, %{
+        description: attrs.description
+      })
+    end
+
+    {:ok, project} = set_project_timeline(attrs.champion, project)
+    {:ok, _} = create_project_check_in(attrs.champion, project, attrs.check_in)
+    {:ok, _} = create_project_milestones(attrs.champion, project, attrs.milestones)
+    {:ok, _} = complete_project_milestones(project)
+
+    context
+  end
+
+  def set_project_timeline(champion, project) do
+    start = Date.utc_today() |> Date.add(:rand.uniform(20)) |> Date.add(-:rand.uniform(20))
+    deadline = start |> Date.add(10 + :rand.uniform(20))
+
+    Operately.Projects.EditTimelineOperation.run(champion, project, %{
+      project_start_date: DateTime.new!(start, ~T[00:00:00], "Etc/UTC"),
+      project_due_date: DateTime.new!(deadline, ~T[00:00:00], "Etc/UTC"),
+      milestone_updates: [],
+      new_milestones: []
+    })
+  end
+
+  def create_project_check_in(champion, project, :default) do
+    create_project_check_in(champion, project, %{
+      status: "on_track",
+      description: rich_text("Everything is going as planned! Last week we had a new batch of beta testers and they loved the product! We are now fully focusing on eliminating the leftover bugs"),
+    })
+  end
+
+  def create_project_check_in(champion, project, attrs) do
+    Operately.Operations.ProjectCheckIn.run(champion, project, Map.merge(attrs, %{
+      send_notifications_to_everyone: true,
+      subscriber_ids: []
+    }))
+  end
+
+  def create_project_milestones(champion, project, :default) do
+    create_project_milestones(champion, project, [
+      "Kickoff",
+      "Design",
+      "Development",
+      "Testing",
+      "Launch"
+    ])
+  end
+
+  def create_project_milestones(champion, project, milestone_names) do
+    {:ok, _} = Operately.Projects.EditTimelineOperation.run(champion, project, %{
+      project_start_date: project.started_at,
+      project_due_date: project.deadline,
+      milestone_updates: [],
+      new_milestones: Enum.with_index(milestone_names) |> Enum.map(fn {name, index} ->
+        due = Date.add(project.deadline, length(milestone_names) - index - 1)
+
+        %{
+          title: name,
+          due_time: due |> NaiveDateTime.new!(~T[00:00:00]),
+          description: rich_text(""),
+          status: :done,
+          tasks_kanban_state: %{}
+        }
+      end)
+    })
+  end
+
+  def complete_project_milestones(project) do
+    milestones = Operately.Repo.preload(project, :milestones).milestones 
+    completed = Enum.take(milestones, :rand.uniform(length(milestones)))
+
+    completed
+    |> Enum.each(fn milestone ->
+      {:ok, _} = Operately.Projects.update_milestone(milestone, %{
+        status: :done,
+        completed_at: NaiveDateTime.utc_now()
+      })
+    end)
+
+    {:ok, completed}
+  end
+
+  def add_project_contributors(context, project, attrs) do
+    [
+      find_person(context, "Grace Wilson"),
+      find_person(context, "Frank Miller"),
+      find_person(context, "Olivia Hall"),
+      find_person(context, "Mia Clark"),
+    ] 
+    |> Enum.filter(fn p -> p.id != attrs.champion.id end)
+    |> Enum.filter(fn p -> p.id != attrs.reviewer.id end)
+    |> Enum.each(fn p ->
+      {:ok, _} = Operately.Operations.ProjectContributorAddition.run(attrs.champion, %{
+        person_id: p.id,
+        project_id: project.id,
+        permissions: 100,
+        responsibility: "Build and Launch the Website",
+        role: :contributor
+      })
+    end)
+
+    context
+  end
+
+  defp find_person(context, name) do
+    Operately.People.get_person_by_name!(context.company, name)
+  end
+
+  defp find_goal(context, name) do
+    Operately.Repo.one!(from g in Operately.Goals.Goal, where: g.name == ^name and g.company_id == ^context.company.id)
+  end
+
+  defp rich_text(text) do
+    %{
+      type: :doc,
+      content: [
+        %{
+          type: :paragraph,
+          content: [
+            %{
+              type: :text,
+              text: text
+            }
+          ]
+        }
+      ]
+    }
+    |> Jason.encode!() 
+    |> Jason.decode!()
+  end
+end
