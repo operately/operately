3812e2841032d39ab115561bcedf6726ee3f5796
Igor Šarčević <igor@operately.com>
2024-09-04 21:21:43 +0000

    Make only the link clickable
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- assets/js/pages/ProjectPage/ContributorsSection.tsx
+++ assets/js/pages/ProjectPage/ContributorsSection.tsx
@@ -1,33 +1,32 @@
 import * as React from "react";
 import * as Projects from "@/models/projects";
 
-import { DivLink } from "@/components/Link";
 import { Paths } from "@/routes/paths";
 import { GhostButton } from "@/components/Button";
 
 import ContributorAvatar from "@/components/ContributorAvatar";
 
 export function ContributorsSection({ project }: { project: Projects.Project }) {
-  const contributorsPath = Paths.projectContributorsPath(project.id!);
   const contribs = Projects.sortContributorsByRole(project.contributors!);
 
   return (
-    <DivLink to={contributorsPath} testId="project-contributors" className="flex items-center gap-1 cursor-pointer">
+    <div className="flex items-center gap-1">
       {contribs!.map((c) => (
         <ContributorAvatar key={c.id} contributor={c} />
       ))}
 
       <ManageAccessButton project={project} />
-    </DivLink>
+    </div>
   );
 }
 
 function ManageAccessButton({ project }: { project: Projects.Project }) {
   if (!project.permissions!.canEditContributors) return null;
+  const path = Paths.projectContributorsPath(project.id!);
 
   return (
     <div className="ml-2">
-      <GhostButton size="xs" type="secondary" testId="manage-team-button">
+      <GhostButton size="xs" type="secondary" testId="manage-team-button" linkTo={path}>
         Manage Team &amp; Access
       </GhostButton>
     </div>
