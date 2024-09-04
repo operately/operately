7314a22377479f03e639f7d0e7a9c562fd61a4bf
Marko Anastasov <marko@operately.com>
2024-09-04 13:17:58 +0200

    chore: Improve proportions and colors of buttons, add to design system
    Signed-off-by: Marko Anastasov <marko@operately.com>

--- assets/js/pages/DesignPage/Buttons.tsx
+++ assets/js/pages/DesignPage/Buttons.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 
 import { Section, SectionTitle } from "./Section";
+import { FilledButton, GhostButton } from "@/components/Button";
 
 export function Buttons() {
   return (
@@ -10,79 +11,38 @@ export function Buttons() {
       <div className="max-w-2xl mb-8">
         <p className="mt-2">
           Operately uses rounded primary and secondary buttons. The primary button is used for the main action on a
-          page, while the secondary button is used for secondary actions or as a subtle call to action. UI copy is
-          written like a sentence, not capitalized.
+          page, while the secondary button is used for secondary actions or as a subtle call to action. We write UI copy in active voice, not capitalized.
         </p>
       </div>
 
       <h3 className="font-bold mb-4">Primary buttons</h3>
 
       <div className="flex items-center space-x-4">
-        <a
-          className="rounded-full bg-accent-1 px-2.5 py-1 text-xs font-semibold text-slate-50 shadow-sm hover:bg-accent-1-light focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-bg-accent-1"
-          href=""
-        >
-          Button text
-        </a>
-        <a
-          className="rounded-full bg-accent-1 px-2.5 py-1 text-sm font-semibold text-slate-50 shadow-sm hover:bg-accent-1-light focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-bg-accent-1"
-          href=""
-        >
-          Button text
-        </a>
-        <a
-          className="rounded-full bg-accent-1 px-3 py-1.5 text-sm font-semibold text-slate-50 shadow-sm hover:bg-accent-1-light focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-bg-accent-1"
-          href=""
-        >
-          Button text
-        </a>
-        <a
-          className="rounded-full bg-accent-1 px-3.5 py-2 text-sm font-semibold text-slate-50 shadow-sm hover:bg-accent-1-light focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-bg-accent-1"
-          href=""
-        >
-          Button text
-        </a>
-        <a
-          className="rounded-full bg-accent-1 px-4 py-2.5 text-sm font-semibold text-slate-50 shadow-sm hover:bg-accent-1-light focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-bg-accent-1"
-          href=""
-        >
-          Button text
-        </a>
+        <FilledButton size="xxs">Button text</FilledButton>
+        <FilledButton size="xs">Button text</FilledButton>
+        <FilledButton size="sm">Button text</FilledButton>
+        <FilledButton size="base">Button text</FilledButton>
+        <FilledButton size="lg">Button text</FilledButton>
       </div>
 
-      <h3 className="font-bold mt-8 mb-4">Secondary buttons</h3>
+      <h3 className="font-bold mt-8 mb-4">Ghost buttons - primary</h3>
 
-      <div className="flex items-center space-x-4">
-        <button
-          type="button"
-          className="rounded-full bg-white px-2.5 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-        >
-          Button text
-        </button>
-        <button
-          type="button"
-          className="rounded-full bg-white px-2.5 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-        >
-          Button text
-        </button>
-        <button
-          type="button"
-          className="rounded-full bg-white px-3 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-        >
-          Button text
-        </button>
-        <button
-          type="button"
-          className="rounded-full bg-white px-3.5 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-        >
-          Button text
-        </button>
-        <button
-          type="button"
-          className="rounded-full bg-white px-4 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-        >
-          Button text
-        </button>
+      <div className="flex items-center space-x-4 w-auto">
+        <GhostButton type="primary" size="xxs">Button text</GhostButton>
+        <GhostButton type="primary" size="xs">Button text</GhostButton>
+        <GhostButton type="primary" size="sm">Button text</GhostButton>
+        <GhostButton type="primary" size="base">Button text</GhostButton>
+        <GhostButton type="primary" size="lg">Button text</GhostButton>
+      </div>
+
+      <h3 className="font-bold mt-8 mb-4">Ghost buttons - secondary</h3>
+
+      <div className="flex items-center space-x-4 w-auto">
+        <GhostButton type="secondary" size="xxs">Button text</GhostButton>
+        <GhostButton type="secondary" size="xs">Button text</GhostButton>
+        <GhostButton type="secondary" size="sm">Button text</GhostButton>
+        <GhostButton type="secondary" size="base">Button text</GhostButton>
+        <GhostButton type="secondary" size="lg">Button text</GhostButton>
       </div>
     </Section>
   );
