01bf8f75567b7191abd02ac957ec5ff27530bc1e
Marko Anastasov <marko@operately.com>
2024-09-04 12:04:45 +0200

    Standard H3
    Signed-off-by: Marko Anastasov <marko@operately.com>

---
+++ assets/js/components/Text/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+// icon is optional
+export const H3 = ({ content, icon: Icon }) => {
+  return (
+    <div className="border-b border-surface-outline mb-4">
+      <div className="px-2 py-2">
+        <div className="font-medium text-content-base text-lg mb-2">
+          <div className="flex items-center gap-1">
+            {Icon && <Icon size={20} />}
+            {Icon && " "}
+            {content}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const DimmedLabel = ({ children, className = "" }) => (
+  <div className={`text-xs uppercase font-medium text-content-dimmed tracking-wider ${className}`}>
+    {children}
+  </div>
+);
