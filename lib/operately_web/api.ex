0fc258510e422cf3c1f3f71181155883e46e9500
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2024-09-05 06:57:59 -0300

    feat: Add UnsubscribeFromNotifications mutation (#1062)
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- lib/operately_web/api.ex
+++ lib/operately_web/api.ex
@@ -107,6 +107,7 @@ defmodule OperatelyWeb.Api do
   mutation :reopen_goal, M.ReopenGoal
   mutation :resume_project, M.ResumeProject
   mutation :subscribe_to_notifications, M.SubscribeToNotifications
+  mutation :unsubscribe_from_notifications, M.UnsubscribeFromNotifications
   mutation :update_group_appearance, M.UpdateGroupAppearance
   mutation :update_milestone, M.UpdateMilestone
   mutation :update_milestone_description, M.UpdateMilestoneDescription
