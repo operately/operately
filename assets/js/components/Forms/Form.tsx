880b22ea28c437394704e58b641045485993f2d6
Igor Šarčević <igisar@gmail.com>
2024-09-09 14:33:06 +0200

    chore: Make forms cancellable (#1091)
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- assets/js/components/Forms/Form.tsx
+++ assets/js/components/Forms/Form.tsx
@@ -4,13 +4,9 @@ import { FormContext } from "./FormContext";
 import { FormState } from "./FormState";
 
 export function Form({ form, children }: { form: FormState<any>; children: React.ReactNode }) {
-  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const onSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    if (!form.validate()) return;
-
-    form.setState("submitting");
-    await form.submit(form);
-    form.setState("idle");
+    await form.actions.submit(form);
   };
 
   return (
