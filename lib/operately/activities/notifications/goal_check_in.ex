40ca3affb431e68e5da1eaa76ba048bada6acfcf
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-09-17 06:58:52 -0300

    feat: GoalCheckIn notifications uses subscriptions list
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- lib/operately/activities/notifications/goal_check_in.ex
+++ lib/operately/activities/notifications/goal_check_in.ex
@@ -1,31 +1,15 @@
 defmodule Operately.Activities.Notifications.GoalCheckIn do
-  alias Operately.Goals.Update
+  alias Operately.Goals.Notifications
 
   def dispatch(activity) do
-    goal_id = activity.content["goal_id"]
-    update_id = activity.content["update_id"]
-
-    goal = Operately.Goals.get_goal!(goal_id)
-    goal = Operately.Repo.preload(goal, [:champion, :reviewer])
-
-    {:ok, update} = Update.get(:system, id: update_id)
-
-    people = (
-      Operately.RichContent.lookup_mentioned_people(update.message)
-      ++ [goal.champion]
-      ++ [goal.reviewer]
-    ) |> Enum.uniq_by(& &1.id)
-
-    notifications = Enum.map(people, fn person ->
+    Notifications.get_goal_update_subscribers(activity.content["update_id"], ignore: [activity.author_id])
+    |> Enum.map(fn person_id ->
       %{
-        person_id: person.id,
+        person_id: person_id,
         activity_id: activity.id,
         should_send_email: true,
       }
     end)
-
-    notifications = Enum.filter(notifications, fn n -> n.person_id != activity.author_id end)
-
-    Operately.Notifications.bulk_create(notifications)
+    |> Operately.Notifications.bulk_create()
   end
 end
