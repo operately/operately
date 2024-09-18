0f916821884436a27a97671e802009abf16b1bf6
Igor Šarčević <igor@operately.com>
2024-09-18 09:07:58 +0000

    feat: Expose get_binded_people via the API
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- lib/operately_web/api/queries/get_binded_people.ex
+++ lib/operately_web/api/queries/get_binded_people.ex
@@ -11,11 +11,12 @@ defmodule OperatelyWeb.Api.Queries.GetBindedPeople do
     field :people, list_of(:person)
   end
 
-  def call(_conn, inputs) do
+  def call(conn, inputs) do
     Action.new()
     |> Action.run(:id, fn -> decode_id(inputs[:resourse_id]) end)
     |> Action.run(:access_context, fn ctx -> load_access_context(inputs[:resourse_type], ctx.id) end)
     |> Action.run(:people, fn ctx -> {:ok, Operately.Access.BindedPeopleLoader.load(ctx.access_context.id)} end)
+    |> Action.run(:check_permissions, fn ctx -> check_permissions(ctx.people, me(conn)) end)
     |> Action.run(:serialized, fn ctx -> {:ok, %{people: Serializer.serialize(ctx.people, level: :essential)}} end)
     |> respond()
   end
@@ -29,7 +30,16 @@ defmodule OperatelyWeb.Api.Queries.GetBindedPeople do
       {:ok, ctx} -> {:ok, ctx.serialized}
       {:error, :inputs, _} -> {:error, :bad_request}
       {:error, :not_found, _} -> {:error, :not_found}
+      {:error, :check_permissions, _} -> {:error, :not_found}
       _ -> {:error, :internal_server_error}
+    end
+  end
+
+  def check_permissions(people, me) do
+    if Enum.any?(people, &(&1.id == me.id)) do
+      {:ok, people}
+    else
+      {:error, :not_found}
     end
   end
 
