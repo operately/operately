6fb59ae1e6e510e121e8da813d0b22338d376b66
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-09-19 07:44:51 -0300

    chore: Create subscriptions for existing messages
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

---
+++ lib/operately/data/change_029_create_subscriptions_list_for_messages.ex
@@ -0,0 +1,74 @@
+defmodule Operately.Data.Change029CreateSubscriptionsListForMessages do
+  import Ecto.Query, only: [from: 1, from: 2]
+
+  alias Operately.{Repo, Notifications}
+  alias Operately.Notifications.{Subscription, SubscriptionList}
+
+  def run do
+    Repo.transaction(fn ->
+      from(m in Operately.Messages.Message)
+      |> Repo.all()
+      |> create_subscriptions_list()
+      |> create_subscriptions()
+    end)
+  end
+
+  defp create_subscriptions_list(messages) when is_list(messages) do
+    Enum.map(messages, fn m ->
+      create_subscriptions_list(m)
+    end)
+  end
+
+  defp create_subscriptions_list(message) do
+    case SubscriptionList.get(:system, parent_id: message.id) do
+      {:error, :not_found} ->
+        {:ok, subscriptions_list} = Notifications.create_subscription_list(%{
+          parent_id: message.id,
+          parent_type: :message,
+        })
+        subscriptions_list
+
+      {:ok, subscriptions_list} -> subscriptions_list
+    end
+    |> update_message(message)
+  end
+
+  defp update_message(subscriptions_list, message) do
+    if subscriptions_list.id != message.subscription_list_id do
+      {:ok, _} = Operately.Messages.update_message(message, %{subscription_list_id: subscriptions_list.id})
+    end
+    subscriptions_list
+  end
+
+  defp create_subscriptions(subscriptions_list) when is_list(subscriptions_list) do
+    Enum.map(subscriptions_list, fn list ->
+      create_subscriptions(list)
+    end)
+  end
+
+  defp create_subscriptions(subscriptions_list) do
+    from(m in Operately.Messages.Message,
+      join: s in assoc(m, :space),
+      join: members in assoc(s, :members),
+      where: m.subscription_list_id == ^subscriptions_list.id,
+      select: members
+    )
+    |> Repo.all()
+    |> Enum.map(fn p ->
+      find_or_create_subscription(subscriptions_list, p)
+    end)
+  end
+
+  defp find_or_create_subscription(subscriptions_list, person) do
+    case Subscription.get(:system, subscription_list_id: subscriptions_list.id, person_id: person.id) do
+      {:error, :not_found} ->
+        {:ok, _} = Notifications.create_subscription(%{
+          subscription_list_id: subscriptions_list.id,
+          person_id: person.id,
+          type: :invited,
+        })
+      _ ->
+        :ok
+    end
+  end
+end
---
+++ test/operately/data/change_029_create_subscriptions_list_for_messages_test.exs
@@ -0,0 +1,39 @@
+defmodule Operately.Data.Change029CreateSubscriptionsListForMessagesTest do
+  use Operately.DataCase
+
+  import Operately.MessagesFixtures
+
+  alias Operately.Support.Factory
+  alias Operately.Notifications.Subscription
+
+  setup ctx do
+    ctx
+    |> Factory.setup()
+    |> Factory.add_space(:space)
+    |> Factory.add_space_member(:mike, :space)
+    |> Factory.add_space_member(:bob, :space)
+    |> Factory.add_space_member(:jane, :space)
+  end
+
+  test "creates subscriptions list for existing messages", ctx do
+    people = [ctx.creator, ctx.mike, ctx.bob, ctx.jane]
+
+    messages = Enum.map(1..3, fn _ ->
+      message_fixture(ctx.creator.id, ctx.space.id)
+    end)
+
+    Enum.each(messages, fn m ->
+      Enum.each(people, fn p ->
+        assert {:error, :not_found} = Subscription.get(:system, subscription_list_id: m.subscription_list_id, person_id: p.id)
+      end)
+    end)
+
+    Operately.Data.Change029CreateSubscriptionsListForMessages.run()
+
+    Enum.each(messages, fn m ->
+      Enum.each(people, fn p ->
+        assert {:ok, _} = Subscription.get(:system, subscription_list_id: m.subscription_list_id, person_id: p.id)
+      end)
+    end)
+  end
+end
