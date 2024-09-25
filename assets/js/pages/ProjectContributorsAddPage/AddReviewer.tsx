fecd602b8b9865ee778c2972edf1a04f51883599
Igor Šarčević <igor@operately.com>
2024-09-25 14:47:41 +0000

    chore: Refactor add contributor page
    Signed-off-by: Igor Šarčević <igor@operately.com>

---
+++ assets/js/pages/ProjectContributorsAddPage/AddReviewer.tsx
@@ -0,0 +1,70 @@
+import * as React from "react";
+import * as Pages from "@/components/Pages";
+import * as Paper from "@/components/PaperContainer";
+
+import { PermissionLevels } from "@/features/Permissions";
+import { ProjectContribsSubpageNavigation } from "@/components/ProjectPageNavigation";
+import { useAddProjectContributor } from "@/api";
+
+import Forms from "@/components/Forms";
+import { Paths } from "@/routes/paths";
+import { useNavigateTo } from "@/routes/useNavigateTo";
+import { LoaderResult } from ".";
+
+export function AddReviewer() {
+  const { project } = Pages.useLoadedData() as LoaderResult;
+  const form = useForm();
+
+  return (
+    <Paper.Root size="small">
+      <ProjectContribsSubpageNavigation project={project} />
+
+      <Paper.Body>
+        <div className="text-2xl font-extrabold">Add Reviewer</div>
+        <div className="text-medium">
+          Reviewers are responsible for acknowledging each check-in and have the authority to initiate corrective action
+          if needed. This is typically the person to whom the champion reports to.
+        </div>
+
+        <Forms.Form form={form}>
+          <Forms.FieldGroup>
+            <Forms.SelectPerson field={"person"} label="Reviewer" />
+          </Forms.FieldGroup>
+
+          <Forms.Submit saveText="Add Reviewer" />
+        </Forms.Form>
+      </Paper.Body>
+    </Paper.Root>
+  );
+}
+
+function useForm() {
+  const { project } = Pages.useLoadedData() as LoaderResult;
+  const [add] = useAddProjectContributor();
+  const goBack = useGoBack();
+
+  const form = Forms.useForm({
+    fields: {
+      person: "",
+    },
+    submit: async () => {
+      await add({
+        projectId: project.id,
+        personId: form.values.person,
+        responsibility: "",
+        permissions: PermissionLevels.FULL_ACCESS,
+        role: "reviewer",
+      });
+
+      goBack();
+    },
+    cancel: goBack,
+  });
+
+  return form;
+}
+
+function useGoBack() {
+  const { project } = Pages.useLoadedData() as LoaderResult;
+  return useNavigateTo(Paths.projectContributorsPath(project.id!));
+}
