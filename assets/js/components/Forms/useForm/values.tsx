784ec4d6bca669b9b48af1152024d2d1d21e02dc
Igor Šarčević <igisar@gmail.com>
2024-09-27 16:06:53 +0200

    fix: Adjust project levels based on the space's levels (#1199)
    * Adjust levels based on space levels
    
    Signed-off-by: Igor Šarčević <igor@operately.com>
    
    * Add tests for creating projects in a confidential space
    
    Signed-off-by: Igor Šarčević <igor@operately.com>
    
    * Introduce jest test to verify the transitions
    
    Signed-off-by: Igor Šarčević <igor@operately.com>
    
    * Set COMMENT as defualt space members access
    
    Signed-off-by: Igor Šarčević <igor@operately.com>
    
    ---------
    
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- assets/js/components/Forms/useForm/values.tsx
+++ assets/js/components/Forms/useForm/values.tsx
@@ -3,25 +3,31 @@ import React from "react";
 import { hasProperty, getProperty, setProperty } from "dot-prop";
 import { FieldObject, FieldValue } from "./field";
 
-export function useFieldValues<T extends FieldObject>(fields: T) {
-  const [values, setValues] = React.useState<T>(fields);
+export type OnChangeFn<T extends FieldObject> = ({ newValues, field }: { newValues: T; field: string | null }) => void;
+
+export function useFieldValues<T extends FieldObject>(fields: T, onChange?: OnChangeFn<T>) {
+  const [values, setValues] = React.useState<T>(() => {
+    if (onChange) onChange({ newValues: fields, field: null });
+
+    return fields;
+  });
 
   const getValue = (key: string) => {
     if (hasProperty(values, key)) {
       return getProperty(values, key);
     } else {
-      throw new Error(`Field ${key} does not exist in form`);
+      throw new Error(`Field ${key} does not exist in form. Available fields: ${Object.keys(values).join(", ")}`);
     }
   };
 
   const setValue = (key: string, value: FieldValue) => {
     setValues((prev) => {
-      if (hasProperty(prev, key)) {
-        setProperty(prev, key, value);
-        return { ...prev };
-      } else {
-        throw new Error(`Field ${key} does not exist in form`);
-      }
+      if (!hasProperty(prev, key)) throw new Error(`Field ${key} does not exist in form`);
+
+      setProperty(prev, key, value);
+      if (onChange) onChange({ newValues: prev, field: key });
+
+      return { ...prev };
     });
   };
 
