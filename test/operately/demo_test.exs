dc73906eb8c18942fd66fbc07b2b8dadd7192d59
Igor Šarčević <igor@operately.com>
2024-09-27 15:47:50 +0000

    feat: Build a demo org via the UI
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- test/operately/demo_test.exs
+++ test/operately/demo_test.exs
@@ -4,9 +4,10 @@ defmodule Operately.DemoTest do
   import Operately.PeopleFixtures
 
   test "it creates a demo company without failures" do
-    account_fixture(%{full_name: "Peter Parker", email: "peter.parker@localhost"})
+    account = account_fixture(%{full_name: "Peter Parker", email: "peter.parker@localhost"})
 
-    assert {:ok, _} = Operately.Demo.run("peter.parker@localhost")
+    assert {:ok, company} = Operately.Demo.run(account, "Acme Inc.", "CEO")
+    assert %Operately.Companies.Company{} = company
     assert Operately.Companies.get_company_by_name("Acme Inc.")
   end
 end
