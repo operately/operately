f79151ccd37730d6f1a3f4301e82936a5ba1f31b
Igor Šarčević <igor@operately.com>
2024-09-30 13:40:52 +0000

    Add more button for contribs
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- assets/js/pages/ProjectContributorsAddPage/AddContributors.tsx
+++ assets/js/pages/ProjectContributorsAddPage/AddContributors.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as Pages from "@/components/Pages";
 import * as Paper from "@/components/PaperContainer";
 import * as Projects from "@/models/projects";
+import * as Icons from "@tabler/icons-react";
 
 import { PERMISSIONS_LIST, PermissionLevels } from "@/features/Permissions";
 import { useAddProjectContributors } from "@/api";
@@ -10,50 +11,104 @@ import Forms from "@/components/Forms";
 import { Paths } from "@/routes/paths";
 import { useNavigateTo } from "@/routes/useNavigateTo";
 import { LoaderResult } from "./loader";
+import { SecondaryButton } from "@/components/Buttons";
+
+interface ContributorFields {
+  personId: string;
+  responsibility: string;
+  permissions: PermissionLevels;
+  role: string;
+}
 
 export function AddContributors() {
-  const { project, contribType } = Pages.useLoadedData() as LoaderResult;
+  const { project } = Pages.useLoadedData() as LoaderResult;
   const gotoContribPage = useNavigateTo(Paths.projectContributorsPath(project.id!));
   const personSearchFn = Projects.useContributorSearchFn(project!);
   const [add] = useAddProjectContributors();
 
   const form = Forms.useForm({
     fields: {
-      contributors: {
-        personId: "",
-        responsibility: "",
-        permissions: PermissionLevels.EDIT_ACCESS,
-        role: "contributor",
-      },
+      contributors: [
+        {
+          personId: "",
+          responsibility: "",
+          permissions: PermissionLevels.EDIT_ACCESS,
+          role: "contributor",
+        },
+      ],
     },
     submit: async () => {
       await add({
         projectId: project.id,
-        contributors: form.values.contributors,
+        // contributors: form.values.contributors,
       });
 
       gotoContribPage();
     },
-    cancel: async () => gotoContribPage(),
   });
 
   return (
-    <Forms.Form form={form}>
-      <Forms.FieldGroup>
-        <Forms.FieldGroup layout="grid">
-          <Forms.SelectPerson field={"person"} label="Contributor" searchFn={personSearchFn} />
-          <Forms.SelectBox field={"permissions"} label="Access Level" options={PERMISSIONS_LIST} />
-        </Forms.FieldGroup>
+    <Paper.Root size="small">
+      <Paper.NavigateBack to={Paths.projectContributorsPath(project.id!)} title="Back to Team & Access" />
+      <div className="text-2xl font-extrabold mb-4 text-center">Add contributors to {project.name}</div>
+
+      <Forms.Form form={form}>
+        <Contributors form={form} personSearchFn={personSearchFn} />
+        <AddMoreContributorsButton />
+
+        <Forms.Submit saveText="Add contributors" layout="centered" buttonSize="base" />
+      </Forms.Form>
+    </Paper.Root>
+  );
+}
+
+function Contributors({ form, personSearchFn }) {
+  return (
+    <div className="flex flex-col gap-6">
+      {form.values.contributors.map((_, i: number) => (
+        <Contributor key={i} field={`contributors[${i}]`} personSearchFn={personSearchFn} />
+      ))}
+    </div>
+  );
+}
+
+function Contributor({ field, personSearchFn }) {
+  return (
+    <Paper.Body>
+      <Forms.FieldGroup layout="horizontal">
+        <Forms.SelectPerson field={field + ".personId"} label="Contributor" searchFn={personSearchFn} />
+        <Forms.SelectBox field={field + ".permissions"} label="Access Level" options={PERMISSIONS_LIST} />
 
         <Forms.TextInput
-          field={"responsibility"}
+          field={field + ".responsibility"}
           placeholder="e.g. Project Manager"
           label="Responsibility"
-          required={contribType == "contributor"}
+          required={true}
         />
       </Forms.FieldGroup>
+    </Paper.Body>
+  );
+}
 
-      <Forms.Submit saveText="Add Contributor" />
-    </Forms.Form>
+function AddMoreContributorsButton() {
+  const [value, setValue] = Forms.useFieldValue<ContributorFields[]>("contributors");
+
+  const addMore = React.useCallback(() => {
+    const newContributor = {
+      personId: "",
+      responsibility: "",
+      permissions: PermissionLevels.EDIT_ACCESS,
+      role: "contributor",
+    };
+
+    setValue([...value, newContributor]);
+  }, [value, setValue]);
+
+  return (
+    <div className="flex justify-center" style={{ marginTop: "-18px" }}>
+      <SecondaryButton onClick={addMore}>
+        <Icons.IconPlus size={16} />
+      </SecondaryButton>
+    </div>
   );
 }
