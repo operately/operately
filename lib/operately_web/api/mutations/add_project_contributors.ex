55aedc158b0a830c57aac213b7c0f46841f837f6
Igor Šarčević <igor@operately.com>
2024-10-02 10:08:48 +0000

    feat: Add contributor addition emails
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- lib/operately_web/api/mutations/add_project_contributors.ex
+++ lib/operately_web/api/mutations/add_project_contributors.ex
@@ -2,48 +2,59 @@ defmodule OperatelyWeb.Api.Mutations.AddProjectContributors do
   use TurboConnect.Mutation
   use OperatelyWeb.Api.Helpers
 
-  import Operately.Access.Filters, only: [filter_by_full_access: 2, forbidden_or_not_found: 2]
+  alias Operately.Projects.Project
+  alias Operately.Projects.Permissions
+  alias Operately.Operations.ProjectContributorsAddition, as: Operation
 
   inputs do
     field :project_id, :string
-    field :person_id, :string
-    field :responsibility, :string
-    field :permissions, :integer
-    field :role, :string
+    field :contributors, list_of(:project_contributor_input)
   end
 
   outputs do
-    field :project_contributor, :project_contributor
+    field :success, :boolean
   end
 
   def call(conn, inputs) do
-    {:ok, project_id} = decode_id(inputs.project_id)
-    {:ok, person_id} = decode_id(inputs.person_id)
+    Action.new()
+    |> run(:me, fn -> find_me(conn) end)
+    |> run(:project_id, fn -> decode_id(inputs.project_id) end)
+    |> run(:contribs, fn -> decode_contributors(inputs.contributors) end)
+    |> run(:project, fn ctx -> Project.get(ctx.me, id: ctx.project_id) end)
+    |> run(:check_permissions, fn ctx -> Permissions.check(ctx.project.requester_access_level, :can_edit_contributors) end)
+    |> run(:operation, fn ctx -> Operation.run(ctx.me, ctx.project, ctx.contribs) end)
+    |> run(:serialized, fn -> {:ok, %{success: true}} end)
+    |> respond()
+  end
 
-    case check_permissions(me(conn), project_id) do
-      {:error, reason} ->
-        {:error, reason}
-
-      :ok ->
-        {:ok, contributor} = Operately.Operations.ProjectContributorAddition.run(me(conn), %{
-          project_id: project_id,
-          person_id: person_id,
-          responsibility: inputs.responsibility,
-          permissions: inputs.permissions,
-          role: inputs.role
-        })
-        {:ok, %{contributor: Serializer.serialize(contributor, level: :essential)}}
+  defp respond(result) do
+    case result do
+      {:ok, ctx} -> {:ok, ctx.serialized}
+      {:error, :me, _} -> {:error, :unauthorized}
+      {:error, :project_id, _} -> {:error, :bad_request}
+      {:error, :contribs, _} -> {:error, :bad_request}
+      {:error, :project, _} -> {:error, :not_found}
+      {:error, :check_permissions, _} -> {:error, :forbidden}
+      {:error, :operation, _} -> {:error, :internal_server_error}
+      _ -> {:error, :internal_server_error}
     end
   end
 
-  defp check_permissions(person, project_id) do
-    query = from(p in Operately.Projects.Project, where: p.id == ^project_id)
-    has_permissions = filter_by_full_access(query, person.id) |> Repo.exists?()
+  defp decode_contributors(contribs), do: decode_contributors(contribs, [])
 
-    if has_permissions do
-      :ok
-    else
-      forbidden_or_not_found(query, person.id)
+  defp decode_contributors([], acc), do: {:ok, acc}
+
+  defp decode_contributors([contrib| rest], acc) do
+    case decode_contributor(contrib) do
+      {:ok, contrib} -> decode_contributors(rest, [contrib | acc])
+      _ -> {:error, :bad_request}
+    end
+  end
+
+  defp decode_contributor(contributor) do
+    case decode_id(contributor.person_id) do
+      {:ok, person_id} -> {:ok, Map.merge(contributor, %{person_id: person_id, role: :contributor})}
+      _ -> {:error, :bad_request}
     end
   end
 end
