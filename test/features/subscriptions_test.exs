22eb7280c6b4ad2593285be93f9f92a5f332d616
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-10-04 15:01:15 -0300

    Add tests for project check-ins
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- test/features/subscriptions_test.exs
+++ test/features/subscriptions_test.exs
@@ -1,4 +1,79 @@
 defmodule Operately.Features.SubscriptionsTest do
   use Operately.FeatureCase
 
+  alias Operately.Support.Features.SubscriptionsSteps, as: Steps
+
+  setup ctx do
+    ctx
+    |> Factory.setup()
+    |> Factory.add_space(:space)
+  end
+
+  describe "Project Check-in" do
+    setup ctx do
+      ctx
+      |> Factory.add_company_member(:bob)
+      |> Factory.add_project(:project, :space, reviewer: :bob)
+      |> Factory.add_project_contributor(:fred, :project, :as_person)
+      |> Factory.add_project_contributor(:jane, :project, :as_person)
+      |> Factory.add_project_contributor(:john, :project, :as_person)
+      |> UI.login_as(ctx.creator)
+    end
+
+    feature "All contributors", ctx do
+      ctx
+      |> Steps.go_to_new_check_in_page()
+      |> Steps.fill_out_check_in_form(%{status: "on_track", description: "Going well"})
+      |> Steps.select_all_people()
+      |> Steps.submit_check_in_form()
+      |> Steps.asset_current_subscribers(%{count: 5, resource: "check-in"})
+    end
+
+    feature "Select specific contributors", ctx do
+      ctx
+      |> Steps.go_to_new_check_in_page()
+      |> Steps.fill_out_check_in_form(%{status: "on_track", description: "Going well"})
+      |> Steps.select_specific_people()
+      |> Steps.toggle_person_checkbox(ctx.john)
+      |> Steps.toggle_person_checkbox(ctx.jane)
+      |> Steps.save_people_selection()
+      |> Steps.submit_check_in_form()
+      |> Steps.asset_current_subscribers(%{count: 4, resource: "check-in"})
+    end
+
+    feature "No one", ctx do
+      ctx
+      |> Steps.go_to_new_check_in_page()
+      |> Steps.fill_out_check_in_form(%{status: "on_track", description: "Going well"})
+      |> Steps.select_no_one()
+      |> Steps.submit_check_in_form()
+      |> Steps.asset_current_subscribers(%{count: 2, resource: "check-in"})
+    end
+
+    feature "Subscribe and unsubribe", ctx do
+      ctx
+      |> Steps.go_to_new_check_in_page()
+      |> Steps.fill_out_check_in_form(%{status: "on_track", description: "Going well"})
+      |> Steps.select_all_people()
+      |> Steps.submit_check_in_form()
+      |> Steps.asset_current_subscribers(%{count: 5, resource: "check-in"})
+      |> Steps.open_current_subscriptions_form()
+      |> Steps.toggle_person_checkbox(ctx.bob)
+      |> Steps.toggle_person_checkbox(ctx.jane)
+      |> Steps.save_people_selection()
+      |> Steps.asset_current_subscribers(%{count: 3, resource: "check-in"})
+      |> Steps.unsubscribe()
+      |> Steps.asset_current_subscribers(%{count: 2, resource: "check-in"})
+      |> Steps.open_current_subscriptions_form()
+      |> Steps.deselect_everyone()
+      |> Steps.save_people_selection()
+      |> Steps.asset_current_subscribers(%{count: 0, resource: "check-in"})
+      |> Steps.subscribe()
+      |> Steps.asset_current_subscribers(%{count: 1, resource: "check-in"})
+      |> Steps.open_current_subscriptions_form()
+      |> Steps.select_everyone()
+      |> Steps.save_people_selection()
+      |> Steps.asset_current_subscribers(%{count: 5, resource: "check-in"})
+    end
+  end
 end
--- test/support/features/subscriptions_steps.ex
+++ test/support/features/subscriptions_steps.ex
@@ -1,4 +1,96 @@
 defmodule Operately.Support.Features.SubscriptionsSteps do
   use Operately.FeatureCase
 
+  #
+  # Project Check-in
+  #
+
+  step :go_to_new_check_in_page, ctx do
+    ctx
+    |> UI.visit(Paths.project_path(ctx.company, ctx.project))
+    |> UI.click(testid: "check-in-now")
+  end
+
+  step :fill_out_check_in_form, ctx, attrs do
+    ctx
+    |> UI.click(testid: "status-dropdown")
+    |> UI.click(testid: "status-dropdown-#{attrs.status}")
+    |> UI.fill_rich_text(attrs.description)
+  end
+
+  step :submit_check_in_form, ctx do
+    ctx
+    |> UI.click(testid: "post-check-in")
+  end
+
+  #
+  # Subscriptions widget
+  #
+
+  step :select_all_people, ctx do
+    ctx
+    |> UI.click(testid: "subscribe-all")
+  end
+
+  step :select_specific_people, ctx do
+    ctx
+    |> UI.click(testid: "subscribe-specific-people")
+  end
+
+  step :select_no_one, ctx do
+    ctx
+    |> UI.click(testid: "subscribe-no-one")
+  end
+
+  step :toggle_person_checkbox, ctx, person do
+    testid = "person-option-#{Paths.person_id(person)}"
+
+    ctx
+    |> UI.click(testid: testid)
+  end
+
+  step :open_current_subscriptions_form, ctx do
+    ctx
+    |> UI.click(testid: "add-remobe-subscribers")
+  end
+
+  step :save_people_selection, ctx do
+    ctx
+    |> UI.click(testid: "submit")
+  end
+
+  step :unsubscribe, ctx do
+    ctx
+    |> UI.click(testid: "unsubscribe")
+  end
+
+  step :subscribe, ctx do
+    ctx
+    |> UI.click(testid: "subscribe")
+  end
+
+  step :select_everyone, ctx do
+    ctx
+    |> UI.click(testid: "select-everyone")
+  end
+
+  step :deselect_everyone, ctx do
+    ctx
+    |> UI.click(testid: "select-no-one")
+  end
+
+  #
+  # Assertions
+  #
+
+  step :asset_current_subscribers, ctx, attrs do
+    text = case attrs.count do
+      0 -> "No one"
+      1 -> "1 person"
+      count -> "#{count} people"
+    end
+
+    ctx
+    |> UI.assert_text("#{text} will be notified when someone comments on this #{attrs.resource}.")
+  end
 end
