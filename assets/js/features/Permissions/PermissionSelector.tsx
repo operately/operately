15cfdea232caaac8489e189e3a867b1142bb418c
Igor Šarčević <igor@operately.com>
2024-10-09 08:38:45 +0000

    feat: Allow editing general access for a space
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- assets/js/features/Permissions/PermissionSelector.tsx
+++ assets/js/features/Permissions/PermissionSelector.tsx
@@ -1,40 +1,10 @@
 import React, { useEffect, useMemo } from "react";
 import PrivacyLevel from "./PrivacyLevel";
-import { AccessLevel, ResourceAccessLevel } from "./AccessLevel";
+import { ResourceAccessLevel } from "./AccessLevel";
 import { PermissionsState, ReducerActions } from "./usePermissionsState";
 import { compareIds } from "@/routes/paths";
 import { PermissionOptions } from ".";
 import { calculatePrivacyLevel } from "./utils";
-
-export function SpacePermissionSelector({ state }: { state: PermissionsState }) {
-  const { hasPermissions, permissions } = state;
-
-  const PRIVACY_OPTIONS = [
-    { label: "Public - Anyone on the internet", value: PermissionOptions.PUBLIC },
-    { label: "Internal - Only organization members", value: PermissionOptions.INTERNAL },
-    { label: "Confidential - Only people invited to the space", value: PermissionOptions.CONFIDENTIAL },
-  ];
-
-  const defaultPrivacyLevel = useMemo(() => {
-    if (hasPermissions) {
-      return calculatePrivacyLevel(permissions, { forSpace: true });
-    } else {
-      return PermissionOptions.CONFIDENTIAL;
-    }
-  }, []);
-
-  return (
-    <>
-      <PrivacyLevel
-        state={state}
-        description="Who can view information in this space?"
-        options={PRIVACY_OPTIONS}
-        defaultValue={defaultPrivacyLevel}
-      />
-      <AccessLevel state={state} />
-    </>
-  );
-}
 
 export function ResourcePermissionSelector({ state }: { state: PermissionsState }) {
   const { space, company, dispatch, hasPermissions, permissions } = state;
--- assets/js/pages/SpaceEditGeneralAccessPage/index.tsx
+++ assets/js/pages/SpaceEditGeneralAccessPage/index.tsx
@@ -1,25 +1,70 @@
 import * as React from "react";
 import * as Pages from "@/components/Pages";
 import * as Paper from "@/components/PaperContainer";
+import * as Spaces from "@/models/spaces";
+
+import Forms from "@/components/Forms";
+
+import { Paths } from "@/routes/paths";
+import { useNavigateTo } from "@/routes/useNavigateTo";
+import { initialAccessLevels, applyAccessLevelConstraints, AccessSelectors } from "@/features/spaces";
 
 interface LoaderResult {
-  // TODO: Define what is loaded when you visit this page
+  space: Spaces.Space;
 }
 
 export async function loader({ params }): Promise<LoaderResult> {
-  return {}; // TODO: Load data here
+  const space = await Spaces.getSpace({ id: params.id, includeAccessLevels: true });
+
+  return { space: space };
 }
 
 export function Page() {
-  const data = Pages.useLoadedData<LoaderResult>();
+  const { space } = Pages.useLoadedData<LoaderResult>();
 
   return (
-    <Pages.Page title={"SpaceEditGeneralAccessPage"}>
-      <Paper.Root>
+    <Pages.Page title={["Edit General Access", space.name!]}>
+      <Paper.Root size="small">
         <Paper.Body>
-          <div className="text-content-accent text-3xl font-extrabold">SpaceEditGeneralAccessPage</div>
+          <h1 className="text-2xl font-extrabold">Edit General Access</h1>
+          <Form />
         </Paper.Body>
       </Paper.Root>
     </Pages.Page>
+  );
+}
+
+function Form() {
+  const { space } = Pages.useLoadedData();
+
+  const navigateBack = useNavigateTo(Paths.spaceAccessManagementPath(space.id!));
+  const [edit] = Spaces.useEditSpacePermissions();
+
+  const form = Forms.useForm({
+    fields: {
+      access: initialAccessLevels(space.accessLevels),
+    },
+    onChange: ({ newValues }) => {
+      newValues.access = applyAccessLevelConstraints(newValues.access);
+    },
+    submit: async () => {
+      await edit({
+        spaceId: space.id!,
+        accessLevels: {
+          public: form.values.access.anonymous,
+          company: form.values.access.companyMembers,
+        },
+      });
+
+      navigateBack();
+    },
+    cancel: navigateBack,
+  });
+
+  return (
+    <Forms.Form form={form}>
+      <AccessSelectors />
+      <Forms.Submit />
+    </Forms.Form>
   );
 }
