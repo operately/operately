d47eb0313abddea491d33d48b3bfae43d9cb654f
Igor Šarčević <igor@operately.com>
2024-10-10 16:33:20 +0000

    Use new ID encoders
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- lib/operately_web/api/mutations/add_space_members.ex
+++ lib/operately_web/api/mutations/add_space_members.ex
@@ -3,32 +3,17 @@ defmodule OperatelyWeb.Api.Mutations.AddSpaceMembers do
   use OperatelyWeb.Api.Helpers
 
   inputs do
-    field :space_id, :string
+    field :space_id, :id
     field :members, list_of(:add_member_input)
   end
 
   def call(conn, inputs) do
     Action.new()
     |> run(:me, fn -> find_me(conn) end)
-    |> run(:space_id, fn -> decode_id(inputs.space_id) end)
-    |> run(:members, fn -> decode_member_ids(inputs) end)
-    |> run(:space, fn ctx -> Operately.Groups.Group.get(ctx.me, id: ctx.space_id) end)
+    |> run(:space, fn ctx -> Operately.Groups.Group.get(ctx.me, id: inputs.space_id) end)
     |> run(:check_permissions, fn ctx -> Operately.Groups.Permissions.check(ctx.space.request_info.access_level, :can_add_members) end)
     |> run(:operation, fn ctx -> Operately.Operations.GroupMembersAdding.run(ctx.me, ctx.space_id, ctx.members) end)
     |> respond()
-  end
-
-  defp decode_member_ids(inputs) do
-    Enum.reduce(inputs.members, [], fn member, acc ->
-      case acc do
-        {:error, _} -> acc
-        _ ->
-          case decode_id(member.id) do
-            {:ok, id} -> [Map.put(member, :id, id) | acc]
-            {:error, _} -> acc
-          end
-      end
-    end)
   end
 
   defp respond(result) do
