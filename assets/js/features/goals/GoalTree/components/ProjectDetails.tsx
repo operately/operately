b4ed4d0e9710cb5b49e600b837d3123a74864896
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2024-10-18 08:07:27 -0300

    feat: Add project progress chart to Goals V2 page (#1372)
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- assets/js/features/goals/GoalTree/components/ProjectDetails.tsx
+++ assets/js/features/goals/GoalTree/components/ProjectDetails.tsx
@@ -1,19 +1,22 @@
 import React, { useState } from "react";
 
 import Modal from "@/components/Modal";
-import { assertPresent } from "@/utils/assertions";
 import { IconArrowUpRight } from "@tabler/icons-react";
-import { StatusIndicator } from "@/features/ProjectListItem/StatusIndicator";
+import { splitByStatus } from "@/models/milestones";
 import { Project } from "@/models/projects";
+import { StatusIndicator } from "@/features/ProjectListItem/StatusIndicator";
 import { StatusSection } from "@/features/projectCheckIns/StatusSection";
 import { DescriptionSection } from "@/features/projectCheckIns/DescriptionSection";
+import { MiniPieChart } from "@/components/MiniPieChart";
+import { assertPresent } from "@/utils/assertions";
 
 import { ProjectNode } from "../tree";
 
 export default function ProjectDetails({ node }: { node: ProjectNode }) {
   return (
-    <div className="pl-[20px] flex">
+    <div className="pl-[20px] flex gap-8 items-center">
       <Status project={node.project} />
+      <MilestoneCompletion project={node.project} />
     </div>
   );
 }
@@ -26,7 +29,9 @@ function Status({ project }: { project: Project }) {
   };
 
   return (
-    <div>
+    // The 14px padding-right in the container is the same
+    // as the 14px offset in icon.
+    <div className="pr-[14px]">
       <div onClick={toggleShowCheckIn} className="relative cursor-pointer">
         <StatusIndicator project={project} size="sm" />
         <IconArrowUpRight size={12} className="absolute top-0 right-[-14px]" />
@@ -54,5 +59,21 @@ function LatestProjectCheckIn({ project, showCheckIn, toggleShowCheckIn }: Lates
       <StatusSection checkIn={project.lastCheckIn} reviewer={project.reviewer} />
       <DescriptionSection checkIn={project.lastCheckIn} />
     </Modal>
+  );
+}
+
+function MilestoneCompletion({ project }: { project: Project }) {
+  assertPresent(project.milestones, "milestones must be present in project");
+
+  let { pending, done } = splitByStatus(project.milestones);
+  const totalCount = pending.length + done.length;
+
+  if (totalCount === 0) return <></>;
+
+  return (
+    <div className="flex items-center gap-2 shrink-0">
+      <MiniPieChart completed={done.length} total={totalCount} />
+      {done.length}/{totalCount} completed
+    </div>
   );
 }
