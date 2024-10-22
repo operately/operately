499982cf4f19fac77a72baff023989b84a7addf0
Igor Šarčević <igor@operately.com>
2024-10-22 11:19:42 +0000

    chore: Stabilize contributor ordering in the API
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- lib/operately_web/api/serializers/project.ex
+++ lib/operately_web/api/serializers/project.ex
@@ -29,7 +29,7 @@ defimpl OperatelyWeb.Api.Serializable, for: Operately.Projects.Project do
       reviewer: OperatelyWeb.Api.Serializer.serialize(project.reviewer),
       goal: OperatelyWeb.Api.Serializer.serialize(project.goal),
       milestones: OperatelyWeb.Api.Serializer.serialize(project.milestones),
-      contributors: OperatelyWeb.Api.Serializer.serialize(exclude_suspended(project)),
+      contributors: OperatelyWeb.Api.Serializer.serialize(sort_contribs(exclude_suspended(project))),
       last_check_in: OperatelyWeb.Api.Serializer.serialize(project.last_check_in),
       next_milestone: OperatelyWeb.Api.Serializer.serialize(project.next_milestone),
       permissions: OperatelyWeb.Api.Serializer.serialize(project.permissions),
@@ -45,5 +45,20 @@ defimpl OperatelyWeb.Api.Serializable, for: Operately.Projects.Project do
       %Ecto.Association.NotLoaded{} -> []
       contributors -> Enum.filter(contributors, &(not is_nil(&1.person)))
     end
+  end
+
+  @roles_ordering [champion: 0, reviewer: 1, contributor: 2]
+
+  defp sort_contribs(contribs) do
+    Enum.sort(contribs, fn contrib1, contrib2 ->
+      order1 = Keyword.get(@roles_ordering, contrib1.role, 100)
+      order2 = Keyword.get(@roles_ordering, contrib2.role, 100)
+
+      if order1 == order2 do
+        contrib1.person.full_name < contrib2.person.full_name
+      else
+        order1 < order2
+      end
+    end)
   end
 end
