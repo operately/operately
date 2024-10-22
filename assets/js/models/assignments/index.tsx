a5f6a01934112c1856026b1fa60e4ad40a59b14b
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-10-22 15:10:07 -0300

    Review count refreshed when page loads
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- assets/js/models/assignments/index.tsx
+++ assets/js/models/assignments/index.tsx
@@ -1,5 +1,6 @@
 export { useAssignmentsCount } from "./useAssignmentsCount";
 export { getAssignments } from "@/api";
+export { useReviewRefreshSignal } from "@/signals";
 
 export type { ReviewAssignment } from "@/api";
 
