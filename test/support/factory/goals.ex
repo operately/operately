241dbaf3ce284c6dadf877d4f494ae43dbbb8a89
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2024-10-24 10:33:07 -0300

    chore: Add UI tests for Goals V2 page (#1418)
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- test/support/factory/goals.ex
+++ test/support/factory/goals.ex
@@ -2,12 +2,16 @@ defmodule Operately.Support.Factory.Goals do
   alias Operately.Access.Binding
 
   def add_goal(ctx, testid, space_name, opts \\ []) do
+    name = Keyword.get(opts, :name, "some name")
     company_access = Keyword.get(opts, :company_access, Binding.view_access())
     space_access = Keyword.get(opts, :space_access, Binding.comment_access())
     champion = Keyword.get(opts, :champion, :creator)
     reviewer = Keyword.get(opts, :reviewer, :creator)
+    parent_goal = Keyword.get(opts, :parent_goal)
 
     goal = Operately.GoalsFixtures.goal_fixture(ctx.creator, %{
+      name: name,
+      parent_goal_id: parent_goal && ctx[parent_goal].id,
       space_id: ctx[space_name].id,
       champion_id: ctx[champion].id,
       reviewer_id: ctx[reviewer].id,
