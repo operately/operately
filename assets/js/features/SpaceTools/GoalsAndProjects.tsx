1e8db43926293c0efb2fffe2017e47a121a7c6aa
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-10-25 13:04:27 -0300

    feat: Display goals and projects headers in space tools
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- assets/js/features/SpaceTools/GoalsAndProjects.tsx
+++ assets/js/features/SpaceTools/GoalsAndProjects.tsx
@@ -1,11 +1,65 @@
 import React from "react";
 
-import { Container, Title } from "./components";
+import { Goal } from "@/models/goals";
+import { Project } from "@/models/projects";
 
-export function GoalsAndProjects() {
+import { Container, Title } from "./components";
+import { calculateGoalsStatus, calculateProjectsStatus } from "./utils";
+import { PieChart } from "@/components/PieChart";
+
+interface GoalsAndProjectsProps {
+  goals: Goal[];
+  projects: Project[];
+}
+
+export function GoalsAndProjects({ goals, projects }: GoalsAndProjectsProps) {
   return (
     <Container>
       <Title title="Goals & Projects" />
+
+      <Header goals={goals} type="goals" />
+
+      <Header projects={projects} type="projects" />
     </Container>
+  );
+}
+
+interface GoalsHeader {
+  goals: Goal[];
+  type: "goals";
+}
+interface ProjectsHeader {
+  projects: Project[];
+  type: "projects";
+}
+interface ResourceStatus {
+  on_track: number;
+  caution: number;
+  issue: number;
+  total: number;
+}
+
+function Header(props: GoalsHeader | ProjectsHeader) {
+  const status: ResourceStatus = React.useMemo(() => {
+    switch (props.type) {
+      case "goals":
+        return calculateGoalsStatus(props.goals);
+      case "projects":
+        return calculateProjectsStatus(props.projects);
+    }
+  }, []);
+
+  return (
+    <div className="font-bold flex items-center gap-2 py-3 px-2">
+      <PieChart
+        total={status.total}
+        slices={[
+          { size: status.on_track, color: "green" },
+          { size: status.caution, color: "yellow" },
+          { size: status.issue, color: "red" },
+        ]}
+      />
+      {status.on_track}/{status.total} {props.type} on track
+    </div>
   );
 }
