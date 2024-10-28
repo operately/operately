579e2cc90c7931fe7d871354a61aa8216eecbe7e
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-10-28 11:52:56 -0300

    feat: tools container redirects to resoucers page
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- assets/js/features/SpaceTools/components.tsx
+++ assets/js/features/SpaceTools/components.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { DivLink } from "@/components/Link";
 
 export function Title({ title }: { title: string }) {
   return <div className="font-bold text-lg text-center py-2 border-b border-stroke-base">{title}</div>;
 }
 
-export function Container({ children }) {
-  return <div className="text-sm h-[380px] w-[285px] overflow-hidden border border-stroke-base">{children}</div>;
+export function Container({ children, path }: { children: NonNullable<React.ReactNode>; path: string }) {
+  return (
+    <DivLink to={path} className="text-sm h-[380px] w-[285px] overflow-hidden border border-stroke-base">
+      {children}
+    </DivLink>
+  );
 }
