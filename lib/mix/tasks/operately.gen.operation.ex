f8a117156794f19629c394022c86e4d85344ac63
Igor Šarčević <igor@operately.com>
2024-10-29 12:13:20 +0000

    Generate files
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- lib/mix/tasks/operately.gen.operation.ex
+++ lib/mix/tasks/operately.gen.operation.ex
@@ -1,7 +1,4 @@
 defmodule Mix.Tasks.Operately.Gen.Operation do
-  # import Mix.Operately, only: [generate_file: 2, indent: 2]
-
-  # @supported_types ~w(string integer float boolean)
   alias Mix.Tasks.Operately.Utils.StringUtils
 
   def run(_) do
@@ -24,7 +21,6 @@ defmodule Mix.Tasks.Operately.Gen.Operation do
     IO.puts(" * Email Text Template:  #{ctx.email_text_template_file_path}")
     IO.puts(" * Activity Item:        #{ctx.activity_item_file_path}")
 
-    IO.puts("")
     continue?()
 
     IO.puts("")
@@ -36,19 +32,20 @@ defmodule Mix.Tasks.Operately.Gen.Operation do
     activity_fields = get_activity_fields()
     ctx = Map.put(ctx, :activity_fields, activity_fields)
 
-    IO.inspect(ctx)
+    continue?()
 
-    System.halt(1)
+    generate_files(ctx)
+  end
 
-    # name = parse_name(name)
-    # fields = get_fields()
-
-    # gen_operation(name, fields)
-    # gen_activity_content_schema(name, fields)
-    # gen_notificaiton_dispatcher(name)
-    # gen_notification_item(name)
-    # gen_feed_item(name)
-    # gen_email_template(name)
+  def generate_files(ctx) do
+    Mix.Tasks.Operation.GenOperationModule.gen(ctx)
+    Mix.Tasks.Operation.GenApiMutation.gen(ctx)
+    Mix.Tasks.Operation.GenActivitySchema.gen(ctx)
+    Mix.Tasks.Operation.GenNotificationHandler.gen(ctx)
+    Mix.Tasks.Operation.GenEmailHandler.gen(ctx)
+    Mix.Tasks.Operation.GenEmailHandler.gen_html_template(ctx)
+    Mix.Tasks.Operation.GenEmailHandler.gen_text_template(ctx)
+    Mix.Tasks.Operation.GenActivityItem.gen(ctx)
   end
 
   defp initialize_context(resource, action, action_gerund) do
@@ -62,6 +59,11 @@ defmodule Mix.Tasks.Operately.Gen.Operation do
 
       operation_module_name: operation_module_name,
       activity_item_name: activity_item_name,
+      activity_action_name: "#{resource}_#{action_gerund}",
+      api_module_name: "#{Macro.camelize(action)}#{Macro.camelize(resource)}",
+      activity_schema_name: "#{Macro.camelize(resource)}#{Macro.camelize(action_gerund)}",
+      email_handler_module_name: "#{Macro.camelize(resource)}#{Macro.camelize(action_gerund)}Email",
+      activity_item_module_name: "#{Macro.camelize(resource)}#{Macro.camelize(action_gerund)}",
 
       operation_file_path: "lib/operately/operations/#{resource}_#{action_gerund}.ex",
       api_mutation_file_path: "lib/opertely_web/api/mutations/#{action}_#{resource}.ex",
@@ -74,11 +76,7 @@ defmodule Mix.Tasks.Operately.Gen.Operation do
     }
   end
 
-  def add_fields_to_context(ctx, fields) do
-    Map.put(ctx, :fields, fields)
-  end
-
-  defp  get_resource_name do
+  defp get_resource_name do
     name = IO.gets("Enter resource name: (e.g project_milestone): ") 
     name = String.trim(name)
 
@@ -107,194 +105,19 @@ defmodule Mix.Tasks.Operately.Gen.Operation do
     [action, gerund]
   end
 
-  #def gen_operation(name, fields) do
-  #  module_name = name
-  #  action_name = Macro.underscore(name)
-  #  file_name = Macro.underscore(module_name)
-  #  activity_fields = Enum.map(fields, fn {field_name, _field_type} -> "    \#   #{field_name}: \"TODO\"" end)
-
-  #  generate_file("lib/operately/operations/#{file_name}.ex", fn _ ->
-  #    """
-  #    defmodule Operately.Operations.#{module_name} do
-  #      alias Ecto.Multi
-  #      alias Operately.Repo
-  #      alias Operately.Activities
-
-  #      def run(creator, attrs) do
-  #        raise "Operation for #{module_name} not implemented"
-
-  #        # Multi.new()
-  #        # |> Multi.insert(:something, ...)
-  #        # |> Activities.insert_sync(creator.id, :#{action_name}, fn changes ->
-  #        #   %{
-  #    #{activity_fields}
-  #        #   }
-  #        # end)
-  #        # |> Repo.transaction()
-  #        # |> Repo.extract_result(:something)
-  #      end
-  #    end
-  #    """
-  #  end)
-  #end
-
-  #def gen_activity_content_schema(name, fields) do
-  #  module_name = name
-  #  file_name = Macro.underscore(name)
-
-  #  generate_file("lib/operately/activities/content/#{file_name}.ex", fn _ ->
-  #    """
-  #    defmodule Operately.Activities.Content.#{module_name} do
-  #      use Operately.Activities.Content
-
-  #      embedded_schema do
-  #        #{Enum.map(fields, fn {field_name, field_type} -> "field :#{field_name}, :#{field_type}" end) |> Enum.join("\n") |> indent(2)}
-  #      end
-
-  #      def changeset(attrs) do
-  #        %__MODULE__{}
-  #        |> cast(attrs, __schema__(:fields))
-  #        |> validate_required(__schema__(:fields))
-  #      end
-
-  #      def build(params) do
-  #        changeset(params)
-  #      end
-  #    end
-  #    """
-  #  end)
-  #end
-
-  #def gen_notificaiton_dispatcher(name) do
-  #  module_name = name
-  #  file_name = Macro.underscore(name)
-
-  #  generate_file("lib/operately/activities/notifications/#{file_name}.ex", fn _ ->
-  #    """
-  #    defmodule Operately.Activities.Notifications.#{module_name} do
-  #      def dispatch(activity) do
-  #        {:ok, []}
-  #      end
-  #    end
-  #    """
-  #  end)
-  #end
-
-  #def gen_notification_item(name) do
-  #  generate_file("assets/js/pages/NotificationsPage/NotificationItem/#{name}.tsx", fn _ ->
-  #    """
-  #    // import * as React from "react";
-
-  #    export default function({ notification }) {
-  #      throw "Not implemented";
-  #    }
-  #    """
-  #  end)
-  #end
-
-  #def gen_feed_item(name) do
-  #  generate_file("assets/js/features/Feed/FeedItems/#{name}.tsx", fn _ ->
-  #    """
-  #    import { FeedItem } from "../FeedItem";
-
-  #    export const #{name}: FeedItem = {
-  #      typename: "ActivityContent#{name}",
-  #      contentQuery: ``,
-  #      component: () => null,
-  #    };
-  #    """
-  #  end)
-  #end
-
-  #def gen_email_template(name) do
-  #  generate_file("lib/operately_email/emails/#{Macro.underscore(name <> "Email")}.ex", fn _ ->
-  #    """
-  #    defmodule OperatelyEmail.Emails.#{name <> "Email"} do
-  #      import OperatelyEmail.Mailers.ActivityMailer
-
-  #      def send(person, activity) do
-  #        raise "Email for #{name} not implemented"
-
-  #        # author = Repo.preload(activity, :author).author
-
-  #        # company
-  #        # |> new()
-  #        # |> to(person)
-  #        # |> subject(who: author, action: "did something")
-  #        # |> assign(:author, author)
-  #        # |> render("#{Macro.underscore(name)}")
-  #      end
-  #    end
-  #    """
-  #  end)
-
-  #  generate_file("lib/operately_email/templates/#{Macro.underscore(name)}.html.eex", fn _ ->
-  #    """
-  #    <%= raise "HTML Email for #{name} not implemented" %>
-  #    """
-  #  end)
-
-  #  generate_file("lib/operately_email/templates/#{Macro.underscore(name)}.text.eex", fn _ ->
-  #    """
-  #    <%= raise "Text Email for #{name} not implemented" %>
-  #    """
-  #  end)
-  #end
-
-  #defp parse_name(name) do
-  #  if String.contains?(name, "-") do
-  #    raise """
-  #    Operation name should be camel case. Example: ProjectDiscussionSubmitted
-  #    """
-  #  end
-
-  #  if String.contains?(name, "_") do
-  #    raise """
-  #    Operation name should be camel case. Example: ProjectDiscussionSubmitted
-  #    """
-  #  end
-
-  #  name
-  #end
-
-  #defp get_fields do
-  #  fields = IO.gets("Enter fields (e.g. company_id:string space_id:string name:string): ") 
-  #  fields = fields |> String.trim() |> String.split(~r/\s+/)
-
-  #  if Enum.empty?(fields) do
-  #    raise """
-  #    Activity should have at least one field. Example: mix operately.gen.activity.type ProjectCreation company_id:string space_id:string name:string
-  #    """
-  #  end
-
-  #  Enum.each(fields, fn field ->
-  #    unless String.contains?(field, ":") do
-  #      raise """
-  #      Every field should have a type. Example: mix operately.gen.activity.type ProjectCreation company_id:string space_id:string name:string
-  #      """
-  #    end
-      
-  #    [name, fieldType] = String.split(field, ":")
-
-  #    unless Enum.member?(@supported_types, fieldType) do
-  #      raise """
-  #      #{fieldType} is not a supported type in #{name}:#{fieldType}. Supported types are: #{@supported_types}
-  #      """
-  #    end
-  #  end)
-
-  #  fields |> Enum.map(fn field ->
-  #    [name, fieldType] = String.split(field, ":")
-  #    {name, fieldType}
-  #  end)
-  #end
-
   def get_api_inputs do
     IO.puts("")
     IO.puts("1. What are the inputs for the api mutation?")
     IO.puts("   e.g company_id:string space_id:string name:string")
     IO.puts("")
-    IO.gets(">  ") |> String.trim() |> String.split(~r/\s+/)
+    
+    IO.gets(">  ") 
+    |> String.trim() 
+    |> String.split(~r/\s+/) 
+    |> Enum.map(fn field ->
+      [name, fieldType] = String.split(field, ":")
+      {name, fieldType}
+    end)
   end
 
   def get_activity_fields do
@@ -302,13 +125,21 @@ defmodule Mix.Tasks.Operately.Gen.Operation do
     IO.puts("2. What data should be stored in the activity?")
     IO.puts("   e.g company_id:string space_id:string name:string")
     IO.puts("")
-    IO.gets(">  ") |> String.trim() |> String.split(~r/\s+/)
+
+    IO.gets(">  ") 
+    |> String.trim() 
+    |> String.split(~r/\s+/) 
+    |> Enum.map(fn field ->
+      [name, fieldType] = String.split(field, ":")
+      {name, fieldType}
+    end)
   end
 
   def continue? do
+    IO.puts("")
     case IO.gets("Continue? (y/n): ") do
-      "y\n" -> true
-      "n\n" -> false
+      "y\n" -> IO.puts("")
+      "n\n" -> System.halt(1)
       _ -> continue?()
     end
   end
