b0b73b94e213c67d89635e284b4bbfe18dfcd232
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-11-05 09:25:23 -0300

    Add feature tests
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- test/features/goal_tree_test.exs
+++ test/features/goal_tree_test.exs
@@ -115,4 +115,69 @@ defmodule Operately.Features.GoalTreeTest do
       |> Steps.refute_project_visible(:project_omega)
     end
   end
+
+  describe "view options are saved to local storage" do
+    feature "expand/collapse goal's children", ctx do
+      ctx
+      |> Steps.visit_goal_tree_page()
+      |> Steps.assert_all_goals_and_projects_are_visible_by_default()
+      |> Steps.collapse_goal(ctx.goal_1)
+      |> Steps.assert_subgoals_and_projects_are_hidden()
+      |> Steps.visit_goal_page(ctx.goal_1)
+      |> Steps.visit_goal_tree_page()
+      |> Steps.assert_subgoals_and_projects_are_hidden()
+    end
+
+    feature "expand/collapse goal's success conditions", ctx do
+      ctx
+      |> Steps.visit_goal_tree_page()
+      |> Steps.assert_goal_success_conditions_are_hidden_by_default()
+      |> Steps.expand_goal_success_conditions(ctx.goal_1)
+      |> Steps.assert_goal_success_conditions_are_visible()
+      |> Steps.visit_goal_page(ctx.goal_1)
+      |> Steps.visit_goal_tree_page()
+      |> Steps.assert_goal_success_conditions_are_visible()
+    end
+
+    feature "active projects", ctx do
+      ctx
+      |> Steps.visit_goal_tree_page()
+      |> Steps.assert_all_goals_and_projects_are_visible_by_default()
+      |> Steps.toggle_active_filter()
+      |> Steps.refute_project_visible(:project_alpha)
+      |> Steps.refute_project_visible(:project_beta)
+      |> Steps.visit_goal_page(ctx.goal_1)
+      |> Steps.visit_goal_tree_page()
+      |> Steps.refute_project_visible(:project_alpha)
+      |> Steps.refute_project_visible(:project_beta)
+    end
+
+    feature "paused projects", ctx do
+      ctx
+      |> Steps.given_project_is_paused(ctx.project_beta)
+      |> Steps.visit_goal_tree_page()
+      |> Steps.assert_paused_project_hidden(ctx.project_beta)
+      |> Steps.toggle_paused_filter()
+      |> Steps.assert_project_visible(:project_beta)
+      |> Steps.visit_goal_page(ctx.goal_1)
+      |> Steps.visit_goal_tree_page()
+      |> Steps.assert_project_visible(:project_beta)
+    end
+
+    feature "completed goals and projects", ctx do
+      ctx
+      |> Steps.given_goal_is_closed(ctx.goal_2)
+      |> Steps.given_project_is_closed(ctx.project_beta)
+      |> Steps.visit_goal_tree_page()
+      |> Steps.assert_closed_goal_hidden(ctx.goal_2)
+      |> Steps.assert_closed_project_hidden(ctx.project_beta)
+      |> Steps.toggle_completed_filter()
+      |> Steps.assert_goal_visible(:goal_2)
+      |> Steps.assert_project_visible(:project_beta)
+      |> Steps.visit_goal_page(ctx.goal_1)
+      |> Steps.visit_goal_tree_page()
+      |> Steps.assert_goal_visible(:goal_2)
+      |> Steps.assert_project_visible(:project_beta)
+    end
+  end
 end
