93c600bdbfa411825f67780873296af7c68d6779
Igor Šarčević <igor@operately.com>
2024-11-05 13:38:22 +0000

    Fix size misscalculations
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- assets/js/components/Avatar/index.tsx
+++ assets/js/components/Avatar/index.tsx
@@ -1,8 +1,7 @@
-import React from "react";
-import classnames from "classnames";
-
-import * as Icons from "@tabler/icons-react";
+import * as React from "react";
 import * as People from "@/models/people";
+
+import classnames from "classnames";
 
 export enum AvatarSize {
   Tiny = "tiny",
@@ -20,27 +19,27 @@ interface AvatarProps {
   size: AvatarSize | AvatarSizeString | number;
 }
 
-function SizeClasses({ size }: { size: AvatarSizeString | number }): string {
+function calculateSize(size: AvatarSize | AvatarSizeString | number): number {
   if (size.constructor.name === "Number") {
-    return "";
+    return size as number;
   }
 
   switch (size) {
     case AvatarSize.Tiny:
-      return "w-[20px] h-[20px]";
+      return 20;
     case AvatarSize.Small:
-      return "w-8 h-8";
+      return 32;
     case AvatarSize.Normal:
-      return "w-8 h-8";
+      return 32;
     case AvatarSize.Large:
-      return "w-10 h-10";
+      return 40;
     case AvatarSize.XLarge:
-      return "w-14 h-14";
+      return 56;
     case AvatarSize.XXLarge:
-      return "w-24 h-24";
+      return 96;
   }
 
-  return "";
+  throw new Error("Invalid size");
 }
 
 function TextClasses({ size }: { size: AvatarSizeString | number }): string {
@@ -56,15 +55,21 @@ function TextClasses({ size }: { size: AvatarSizeString | number }): string {
     if ((size as number) <= 24) {
       return "text-[11px] font-bold";
     }
+
+    if ((size as number) <= 40) {
+      return "text-base font-bold";
+    }
+
+    return "text-3xl font-bold";
   }
 
   switch (size) {
     case AvatarSize.Tiny:
       return "text-[9px] font-semibold";
     case AvatarSize.Small:
-      return "text font-extrabold";
+      return "text-[9px] font-extrabold";
     case AvatarSize.Normal:
-      return "text font-bold";
+      return "text-base font-bold";
     case AvatarSize.Large:
       return "text-xl font-bold";
     case AvatarSize.XLarge:
@@ -109,15 +114,14 @@ function BackupAvatar({ person, size }: AvatarProps): JSX.Element {
     "font-semibold",
   );
 
-  const sizeClass = SizeClasses({ size });
-  const textClass = TextClasses({ size });
-  const className = baseClass + " " + sizeClass + " " + textClass;
+  const className = baseClass + " " + TextClasses({ size });
 
-  const style = size.constructor.name === "Number" ? { width: size + "px", height: size + "px" } : {};
+  const sizeNumber = calculateSize(size);
+  const style = { width: `${sizeNumber}px`, height: `${sizeNumber}px` };
 
   return (
     <div title={person!.fullName!} className={around} style={style}>
-      <div title={person!.fullName!} className={className}>
+      <div className={className}>
         <div className="flex items-center justify-center h-full">{initials(person!.fullName!)}</div>
       </div>
     </div>
@@ -137,11 +141,10 @@ function BackupAvatar({ person, size }: AvatarProps): JSX.Element {
 function ImageAvatar({ person, size }: AvatarProps): JSX.Element {
   if (!person) return <></>;
 
-  const baseClass = "rounded-full overflow-hidden bg-white shrink-0 border border-stroke-base inline-block";
-  const sizeClass = SizeClasses({ size });
-  const className = `${baseClass} ${sizeClass}`;
+  const className = "rounded-full overflow-hidden bg-white shrink-0 border border-stroke-base inline-block";
 
-  const style = typeof size === "number" ? { width: `${size}px`, height: "100%" } : {};
+  const sizeNumber = calculateSize(size);
+  const style = { width: `${sizeNumber}px`, height: `${sizeNumber}px` };
 
   return (
     <div title={person.fullName!} className={className} style={style}>
@@ -149,20 +152,8 @@ function ImageAvatar({ person, size }: AvatarProps): JSX.Element {
         src={person.avatarUrl!}
         alt={person.fullName!}
         referrerPolicy="no-referrer"
-        style={{ height: "100%", width: "100%", display: "block" }}
+        style={{ height: "100%", width: "100%", display: "block", maxHeight: size + "px" }}
       />
-    </div>
-  );
-}
-
-function UnassingedAvatar({ size }: { size: AvatarSizeString | number }): JSX.Element {
-  const baseClass = "rounded-full overflow-hidden bg-surface-accent flex items-center justify-center shrink-0";
-  const sizeClass = SizeClasses({ size });
-  const className = baseClass + " " + sizeClass;
-
-  return (
-    <div title="Unassigned" className={className}>
-      <Icons.IconUser size={size} />
     </div>
   );
 }
@@ -175,6 +166,6 @@ export default function Avatar(props: AvatarProps): JSX.Element {
       return BackupAvatar(props);
     }
   } else {
-    return UnassingedAvatar(props);
+    return BackupAvatar({ person: { fullName: "?" }, size: props.size });
   }
 }
