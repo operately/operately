9e1cae417348ac645476fa0ac37348ae52ab3219
Igor Šarčević <igor@operately.com>
2024-11-05 21:20:39 +0000

    fix: Revalidate the notification count when visiting the page
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- assets/js/layouts/DefaultLayout/Bell.tsx
+++ assets/js/layouts/DefaultLayout/Bell.tsx
@@ -5,22 +5,27 @@ import { DivLink } from "@/components/Link";
 import { Paths } from "@/routes/paths";
 
 import * as Notifications from "@/models/notifications";
+import classNames from "classnames";
 
 export function Bell() {
   const count = Notifications.useUnreadCount();
+  const path = Paths.notificationsPath();
+  const style = { height: "32px", width: "32px" };
+
+  const className = classNames(
+    "flex items-center justify-center",
+    "cursor-pointer",
+    "relative group",
+    "rounded-full",
+    "bg-surface-accent",
+    "border border-surface-outline",
+  );
+
+  const iconClassName = classNames("text-content-dimmed", "group-hover:text-content-accent transition-all");
 
   return (
-    <DivLink
-      to={Paths.notificationsPath()}
-      className="flex items-center justify-center cursor-pointer relative group rounded-full bg-surface-accent border border-surface-outline"
-      style={{ height: "32px", width: "32px" }}
-      testId="notifications-bell"
-    >
-      <Icons.IconBell
-        size={20}
-        stroke={1.5}
-        className="text-content-dimmed group-hover:text-content-accent transition-all"
-      />
+    <DivLink to={path} className={className} style={style} testId="notifications-bell">
+      <Icons.IconBell size={20} stroke={1.5} className={iconClassName} />
       <UnreadIndicator count={count} />
     </DivLink>
   );
@@ -29,17 +34,19 @@ export function Bell() {
 function UnreadIndicator({ count }: { count: number }) {
   if (count === 0) return null;
 
+  const className = classNames(
+    "absolute -top-1 -right-1",
+    "rounded-full",
+    "bg-orange-600 text-white-1 group-hover:bg-orange-500",
+    "flex items-center justify-center",
+    "leading-none",
+    "transition-all",
+  );
+
+  const style = { height: "17px", width: "17px", fontSize: "9px", fontWeight: "900" };
+
   return (
-    <div
-      className="absolute -top-1 -right-1 rounded-full bg-orange-600 flex items-center justify-center text-white-1 leading-none group-hover:bg-orange-500 transition-all"
-      style={{
-        height: "17px",
-        width: "17px",
-        fontSize: "9px",
-        fontWeight: "900",
-      }}
-      data-test-id="unread-notifications-count"
-    >
+    <div className={className} style={style} data-test-id="unread-notifications-count">
       {count}
     </div>
   );
