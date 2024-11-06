9549d543638c81e6f7617e6ec5f3b9b6088313f9
Igor Šarčević <igisar@gmail.com>
2024-11-06 23:02:23 +0100

    chore: Format errors nicely to be less confusing (#1505)
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -6,5 +6,5 @@ Application.put_env(:wallaby, :screenshot_on_failure, true)
 
 Application.put_env(:operately, :storage_type, "local")
 
-ExUnit.configure(formatters: [JUnitFormatter, CustomFormatter, ExUnit.CLIFormatter])
+ExUnit.configure(formatters: [JUnitFormatter, CustomFormatter])
 ExUnit.start([])
