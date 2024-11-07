e1b3e0665aa5c11b808e5c98da9bf8d0b650d454
Igor Šarčević <igor@operately.com>
2024-11-07 21:47:42 +0000

    fix: Round up progress update numbers
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- assets/js/features/activities/GoalCheckIn/ConditionChanges.tsx
+++ assets/js/features/activities/GoalCheckIn/ConditionChanges.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import * as GoalCheckIns from "@/models/goalCheckIns";
 
+import { TargetChange, TargetProgress } from "@/features/goals/GoalCheckIn/TargetsSection";
+
 export function ConditionChanges({ update }: { update: GoalCheckIns.Update }) {
   const targets = (update.goalTargetUpdates ?? [])
     .map((t) => t!)
@@ -21,34 +23,4 @@ export function ConditionChanges({ update }: { update: GoalCheckIns.Update }) {
       </div>
     </div>
   );
-}
-
-function TargetProgress({ value, start, end }) {
-  const total = end - start;
-  const progress = (value - start) / total;
-  const width = 100 * progress;
-
-  return (
-    <div className="text-xs font-medium bg-gray-500 rounded px-1.5 py-0.5 text-white-1 relative w-[60px] text-right truncate">
-      <div className="absolute top-0 left-0 bottom-0 bg-accent-1 rounded" style={{ width: `${width}%` }} />
-
-      <div className="relative">
-        {value} / {end}
-      </div>
-    </div>
-  );
-}
-
-function TargetChange({ target }) {
-  const newProgress = target.value - target.from;
-  const oldProgress = target.previousValue - target.from;
-  const diff = newProgress - oldProgress;
-
-  if (newProgress > oldProgress) {
-    return <div className="text-green-600 font-bold shrink-0 text-sm">+ {Math.abs(diff)}</div>;
-  } else if (newProgress < oldProgress) {
-    return <div className="text-content-error font-bold shrink-0 test-sm">- {Math.abs(diff)}</div>;
-  } else {
-    return <div className="flex items-center gap-1 text-content-dimmed font-medium shrink-0 text-xs">No Change</div>;
-  }
 }
