45836966e6eb9c293539bb707816d8bcc99801fa
Igor Šarčević <igisar@gmail.com>
2024-11-07 11:06:25 +0100

    feat: Feed on homepage (#1520)
    * Home page with activity feed prototype
    
    Signed-off-by: Igor Šarčević <igor@operately.com>
    
    * feat: Show feed on home page
    
    Signed-off-by: Igor Šarčević <igor@operately.com>
    
    * Fix get_spaces tests
    
    Signed-off-by: Igor Šarčević <igor@operately.com>
    
    ---------
    
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- assets/js/components/Line/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-export function HorizontalLine({ className }: { className?: string }) {
-  const classes = `flex-1 border-t border-surface-outline ${className}`;
-  return <div className={classes}></div>;
-}
