5ca44051c0fb5b0edb51610ed283c4348316e3e7
Igor Šarčević <igor@operately.com>
2024-11-12 13:42:53 +0000

    chore: Account cleanup
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- test/operately_web/controllers/account_oauth_controller_test.exs
+++ test/operately_web/controllers/account_oauth_controller_test.exs
@@ -40,54 +40,6 @@ defmodule OperatelyWeb.AccountOauthControllerTest do
       assert saved_account.email == "i.exist@text.com"
     end
 
-    test "when a person with the given email doesn't exists and the email is allowed => it creates a new person/account combo", ctx do
-      {:ok, _} = Operately.Companies.update_company(ctx.company, %{trusted_email_domains: ["@text.com"]})
-
-      assert Operately.Repo.aggregate(Operately.People.Person, :count, :id) == 1 # company creator
-
-      conn = Plug.Conn.assign(ctx.conn, :ueberauth_auth, %{
-        info: %{
-          email: "new-email@text.com",
-          image: "http://example.com/image.png",
-          name: "Michael Bolton"
-        }
-      })
-
-      conn = get(conn, ~p"/accounts/auth/google/callback", %{"provider" => "google"})
-      assert conn.status == 302
-
-      people = Operately.People.list_people(ctx.company.id)
-      assert length(people) == 2 # company creator + new person
-
-      saved_person = Operately.People.get_person_by_email(ctx.company, "new-email@text.com")
-      assert saved_person.email == "new-email@text.com"
-      assert saved_person.account_id != nil
-      assert saved_person.title == "Unknown Role"
-      
-      saved_account = Operately.Repo.preload(saved_person, :account).account
-      assert saved_account.email == "new-email@text.com"
-    end
-
-    test "when a person with the given doesn't exists, but the email is not allowed => it rejects the request", ctx do
-      {:ok, _} = Operately.Companies.update_company(ctx.company, %{trusted_email_domains: ["@text.com"]})
-
-      assert Operately.Repo.aggregate(Operately.People.Person, :count, :id) == 1 # company creator
-
-      conn = Plug.Conn.assign(ctx.conn, :ueberauth_auth, %{
-        info: %{
-          email: "new-email@dmif.com",
-          image: "http://example.com/image.png",
-          name: "Michael Bolton"
-        }
-      })
-
-      conn = get(conn, ~p"/accounts/auth/google/callback", %{"provider" => "google"})
-      assert conn.status == 302
-      assert conn.assigns.flash == %{"error" => "Authentication failed"}
-
-      assert Operately.Repo.aggregate(Operately.People.Person, :count, :id) == 1 # company creator
-    end
-
     test "when the account exists, but the avatar_url is different, it updates the avatar_url", ctx do
       assert Operately.Repo.aggregate(Operately.People.Person, :count, :id) == 1 # company creator
 
