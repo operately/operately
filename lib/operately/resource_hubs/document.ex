a81b65582782c0b1a0a8624a62e47b82bd735bbe
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2024-11-21 07:19:57 -0300

    feat: Add GetResourceHubDocument query (#1608)
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- lib/operately/resource_hubs/document.ex
+++ lib/operately/resource_hubs/document.ex
@@ -5,6 +5,7 @@ defmodule Operately.ResourceHubs.Document do
   schema "resource_documents" do
     belongs_to :node, Operately.ResourceHubs.Node, foreign_key: :node_id
     belongs_to :subscription_list, Operately.Notifications.SubscriptionList, foreign_key: :subscription_list_id
+    has_one :access_context, through: [:node, :resource_hub, :access_context]
 
     field :content, :map
 
