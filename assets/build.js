f7c3a3b54f3c8d8f06968c1f050f9d86f0eb9e21
Igor Šarčević <igisar@gmail.com>
2024-11-22 15:34:31 +0100

    chore: Unify code under a single ee folder (#1620)
    * chore: Introduce a single ee folder
    
    Signed-off-by: Igor Šarčević <igor@operately.com>
    
    * chore: Move packages.json, packages.lock tot he root
    
    Signed-off-by: Igor Šarčević <igor@operately.com>
    
    * Clean up tailwind config and include ee
    
    Signed-off-by: Igor Šarčević <igor@operately.com>
    
    * Fix which folder is used for generation of content
    
    Signed-off-by: Igor Šarčević <igor@operately.com>
    
    * Don't cd into assets for npm install for docker images
    
    Signed-off-by: Igor Šarčević <igor@operately.com>
    
    * Inject package.json and package.lock into prod docker image
    
    Signed-off-by: Igor Šarčević <igor@operately.com>
    
    * Inject ee/assets
    
    Signed-off-by: Igor Šarčević <igor@operately.com>
    
    ---------
    
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- assets/build.js
+++ assets/build.js
@@ -10,7 +10,7 @@ const plugins = [];
 
 // Define esbuild options
 let opts = {
-  entryPoints: ["js/app.js"],
+  entryPoints: ["./js/app.js"],
   bundle: true,
   logLevel: "info",
   target: "es2020",
