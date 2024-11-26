ec184f2f9bae6a6b13a25f9641ecfc80cf84e8b6
Igor Šarčević <igor@operately.com>
2024-11-26 13:53:53 +0000

    chore: Remove deprecated template files
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- lib/operately_web/components/layouts.ex
+++ lib/operately_web/components/layouts.ex
@@ -1,13 +1,5 @@
 defmodule OperatelyWeb.Layouts do
   use OperatelyWeb, :html
 
-  def app_top_padding(assigns) do
-    if assigns[:breadcrumbs] do
-      "py-28"
-    else
-      "pt-20"
-    end
-  end
-
   embed_templates "layouts/*"
 end
