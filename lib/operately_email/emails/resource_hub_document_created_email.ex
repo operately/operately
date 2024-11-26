831e6fd09a41bacc27165d38833ea0094a5b27ca
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2024-11-26 10:14:27 -0300

    feat: Creating a document notifies subscribers (#1632)
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

---
+++ lib/operately_email/emails/resource_hub_document_created_email.ex
@@ -0,0 +1,25 @@
+defmodule OperatelyEmail.Emails.ResourceHubDocumentCreatedEmail do
+  import OperatelyEmail.Mailers.ActivityMailer
+
+  alias Operately.Repo
+  alias Operately.ResourceHubs.Document
+
+  def send(person, activity) do
+    author = Repo.preload(activity, :author).author
+    company = Repo.preload(author, :company).company
+
+    {:ok, document} = Document.get(:system, id: activity.content["document_id"], opts: [
+      preload: [resource_hub: :space],
+    ])
+
+    company
+    |> new()
+    |> from(author)
+    |> to(person)
+    |> subject(where: document.resource_hub.name, who: author, action: "added a document: #{document.node.name}")
+    |> assign(:author, author)
+    |> assign(:document, document)
+    |> assign(:cta_url, OperatelyWeb.Paths.document_path(company, document) |> OperatelyWeb.Paths.to_url())
+    |> render("resource_hub_document_created")
+  end
+end
