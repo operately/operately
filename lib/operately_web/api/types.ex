a28125a6ebeb37074480e3f3bd052080fd4c0036
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-11-27 10:18:21 -0300

    feat: Display document-edited event in feed
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- lib/operately_web/api/types.ex
+++ lib/operately_web/api/types.ex
@@ -430,6 +430,11 @@ defmodule OperatelyWeb.Api.Types do
     field :document, :resource_hub_document
   end
 
+  object :activity_content_resource_hub_document_edited do
+    field :resource_hub, :resource_hub
+    field :document, :resource_hub_document
+  end
+
   object :activity_content_resource_hub_document_commented do
     field :document, :resource_hub_document
     field :comment, :comment
