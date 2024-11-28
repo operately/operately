46f2db0df82d3392387f1f609d76fd934980115d
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-11-28 07:24:47 -0300

    feat: Display document-deleted event in feed
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- assets/js/features/activities/index.tsx
+++ assets/js/features/activities/index.tsx
@@ -108,6 +108,7 @@ export const DISPLAYED_IN_FEED = [
   "resource_hub_document_created",
   "resource_hub_document_edited",
   "resource_hub_document_commented",
+  "resource_hub_document_deleted",
   "resource_hub_folder_created",
   "space_added",
   "space_joining",
@@ -168,6 +169,7 @@ import ProjectTimelineEdited from "@/features/activities/ProjectTimelineEdited";
 import ResourceHubDocumentCreated from "@/features/activities/ResourceHubDocumentCreated";
 import ResourceHubDocumentEdited from "@/features/activities/ResourceHubDocumentEdited";
 import ResourceHubDocumentCommented from "@/features/activities/ResourceHubDocumentCommented";
+import ResourceHubDocumentDeleted from "@/features/activities/ResourceHubDocumentDeleted";
 import ResourceHubFolderCreated from "@/features/activities/ResourceHubFolderCreated";
 import SpaceAdded from "@/features/activities/SpaceAdded";
 import SpaceJoining from "@/features/activities/SpaceJoining";
@@ -224,6 +226,7 @@ function handler(activity: Activity) {
     .with("resource_hub_document_created", () => ResourceHubDocumentCreated)
     .with("resource_hub_document_edited", () => ResourceHubDocumentEdited)
     .with("resource_hub_document_commented", () => ResourceHubDocumentCommented)
+    .with("resource_hub_document_deleted", () => ResourceHubDocumentDeleted)
     .with("resource_hub_folder_created", () => ResourceHubFolderCreated)
     .with("space_added", () => SpaceAdded)
     .with("space_joining", () => SpaceJoining)
---
+++ lib/operately_web/api/serializers/activity_content/resource_hub_document_deleted.ex
@@ -0,0 +1,12 @@
+defimpl OperatelyWeb.Api.Serializable, for: Operately.Activities.Content.ResourceHubDocumentDeleted do
+  alias OperatelyWeb.Api.Serializer
+
+  def serialize(content, level: :essential) do
+    document = Map.put(content["document"], :node, content["node"])
+
+    %{
+      resource_hub: Serializer.serialize(content["resource_hub"], level: :essential),
+      document: Serializer.serialize(document, level: :essential),
+    }
+  end
+end
