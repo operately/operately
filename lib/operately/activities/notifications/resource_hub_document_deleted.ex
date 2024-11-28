55cad06a2d87bbf44208d45636e8021c43c2e0dd
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-11-28 08:17:50 -0300

    feat: Deleting document notifies subscribers
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- lib/operately/activities/notifications/resource_hub_document_deleted.ex
+++ lib/operately/activities/notifications/resource_hub_document_deleted.ex
@@ -1,6 +1,15 @@
 defmodule Operately.Activities.Notifications.ResourceHubDocumentDeleted do
-  def dispatch(_activity) do
-    # Notification dispatcher for ResourceHubDocumentDeleted not implemented yet
-    {:ok, []}
+  alias Operately.ResourceHubs.Notifications
+
+  def dispatch(activity) do
+    Notifications.get_document_subscribers(activity.content["document_id"], ignore: [activity.author_id], with_deleted: true)
+    |> Enum.map(fn person_id ->
+      %{
+        person_id: person_id,
+        activity_id: activity.id,
+        should_send_email: true,
+      }
+    end)
+    |> Operately.Notifications.bulk_create()
   end
 end
---
+++ lib/operately_email/emails/resource_hub_document_deleted_email.ex
@@ -0,0 +1,26 @@
+defmodule OperatelyEmail.Emails.ResourceHubDocumentDeletedEmail do
+  import OperatelyEmail.Mailers.ActivityMailer
+
+  alias Operately.Repo
+  alias Operately.ResourceHubs.Document
+
+  def send(person, activity) do
+    author = Repo.preload(activity, :author).author
+    company = Repo.preload(author, :company).company
+
+    {:ok, document} = Document.get(:system, id: activity.content["document_id"], opts: [
+      preload: [:node, :resource_hub],
+      with_deleted: true,
+    ])
+
+    company
+    |> new()
+    |> from(author)
+    |> to(person)
+    |> subject(where: document.resource_hub.name, who: author, action: "deleted a document: #{document.node.name}")
+    |> assign(:author, author)
+    |> assign(:document, document)
+    |> assign(:cta_url, OperatelyWeb.Paths.resource_hub_path(company, document.resource_hub) |> OperatelyWeb.Paths.to_url())
+    |> render("resource_hub_document_deleted")
+  end
+end
