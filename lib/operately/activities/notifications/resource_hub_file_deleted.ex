94e7da0b3046ba2b107a533b1762c545668d88bf
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2024-12-04 12:32:31 -0300

    feat: Deleting file notifies subscribers (#1682)
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- lib/operately/activities/notifications/resource_hub_file_deleted.ex
+++ lib/operately/activities/notifications/resource_hub_file_deleted.ex
@@ -1,6 +1,15 @@
 defmodule Operately.Activities.Notifications.ResourceHubFileDeleted do
-  def dispatch(_activity) do
-    # Notification dispatcher for ResourceHubFileDeleted not implemented yet
-    {:ok, []}
+  alias Operately.ResourceHubs.Notifications
+
+  def dispatch(activity) do
+    Notifications.get_file_subscribers(activity.content["file_id"], ignore: [activity.author_id], with_deleted: true)
+    |> Enum.map(fn person_id ->
+      %{
+        person_id: person_id,
+        activity_id: activity.id,
+        should_send_email: true,
+      }
+    end)
+    |> Operately.Notifications.bulk_create()
   end
 end
