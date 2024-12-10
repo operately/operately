97bf038c06e2bb5470313367738088e79c1e9d9e
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2024-12-10 10:31:02 -0300

    feat: Commenting on file notifies subscribers (#1695)
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- lib/operately/activities/notifications/resource_hub_file_commented.ex
+++ lib/operately/activities/notifications/resource_hub_file_commented.ex
@@ -1,6 +1,15 @@
 defmodule Operately.Activities.Notifications.ResourceHubFileCommented do
-  def dispatch(_activity) do
-    # Notification dispatcher for ResourceHubFileCommented not implemented yet
-    {:ok, []}
+  alias Operately.ResourceHubs.Notifications
+
+  def dispatch(activity) do
+    Notifications.get_file_subscribers(activity.content["file_id"], ignore: [activity.author_id])
+    |> Enum.map(fn person_id ->
+      %{
+        person_id: person_id,
+        activity_id: activity.id,
+        should_send_email: true,
+      }
+    end)
+    |> Operately.Notifications.bulk_create()
   end
 end
---
+++ lib/operately_email/emails/resource_hub_file_commented_email.ex
@@ -0,0 +1,24 @@
+defmodule OperatelyEmail.Emails.ResourceHubFileCommentedEmail do
+  import OperatelyEmail.Mailers.ActivityMailer
+
+  alias OperatelyWeb.Paths
+  alias Operately.{Repo, Updates}
+  alias Operately.ResourceHubs.File
+
+  def send(person, activity) do
+    %{author: author = %{company: company}} = Repo.preload(activity, author: :company)
+    {:ok, file} = File.get(:system, id: activity.content["file_id"], opts: [preload: [:node, :resource_hub]])
+    comment = Updates.get_comment!(activity.content["comment_id"])
+
+    company
+    |> new()
+    |> from(author)
+    |> to(person)
+    |> subject(where: file.resource_hub.name, who: author, action: "commented on: #{file.node.name}")
+    |> assign(:author, author)
+    |> assign(:comment, comment)
+    |> assign(:name, file.node.name)
+    |> assign(:cta_url, Paths.file_path(company, file, comment) |> Paths.to_url())
+    |> render("resource_hub_file_commented")
+  end
+end
