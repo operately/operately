3b2bb8936ea435ac9133d195f113e575e094e5f1
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2024-12-12 09:37:15 -0300

    feat: Display resource hub item's icon and subtitle on space page (#1708)
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- assets/js/features/ResourceHub/index.tsx
+++ assets/js/features/ResourceHub/index.tsx
@@ -1,3 +1,4 @@
 export { NodesList } from "./NodesList";
 export { ZeroNodes } from "./ZeroNodes";
 export { AddFilesButtonAndForms } from "./AddNewFiles";
+export { findIcon, findSubtitle, NodeType } from "./utils";
