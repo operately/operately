780b5acb249444f65c205ee75a5043b45eb83eb3
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-12-13 08:38:48 -0300

    fix: Display resource hub actions only if user has permissions
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- assets/js/features/ResourceHub/AddFilesButton.tsx
+++ assets/js/features/ResourceHub/AddFilesButton.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 import { IconFile, IconFolder, IconUpload } from "@tabler/icons-react";
+import { ResourceHubPermissions } from "@/models/resourceHubs";
 import { OptionsButton } from "@/components/Buttons";
 
 import { useNewFileModalsContext } from "./contexts/NewFileModalsContext";
 
-export function AddFilesButton() {
+export function AddFilesButton({ permissions }: { permissions: ResourceHubPermissions }) {
   const { navigateToNewDocument, toggleShowAddFolder, showAddFilePopUp } = useNewFileModalsContext();
 
   return (
@@ -13,9 +14,27 @@ export function AddFilesButton() {
       <OptionsButton
         align="start"
         options={[
-          { icon: IconFile, label: "Write a new document", action: navigateToNewDocument, testId: "new-document" },
-          { icon: IconFolder, label: "Create a new folder", action: toggleShowAddFolder, testId: "new-folder" },
-          { icon: IconUpload, label: "Upload files", action: showAddFilePopUp, testId: "upload-files" },
+          {
+            icon: IconFile,
+            label: "Write a new document",
+            action: navigateToNewDocument,
+            testId: "new-document",
+            hidden: !permissions.canCreateDocument,
+          },
+          {
+            icon: IconFolder,
+            label: "Create a new folder",
+            action: toggleShowAddFolder,
+            testId: "new-folder",
+            hidden: !permissions.canCreateFolder,
+          },
+          {
+            icon: IconUpload,
+            label: "Upload files",
+            action: showAddFilePopUp,
+            testId: "upload-files",
+            hidden: !permissions.canCreateFile,
+          },
         ]}
         testId="add-options"
       />
