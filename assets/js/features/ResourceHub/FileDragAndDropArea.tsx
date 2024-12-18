7cb5ad367be4d2be0eda32589f57559a6a0e8f98
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2024-12-18 19:11:27 -0300

    chore: Improve drag-and-drop animation (#1736)
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- assets/js/features/ResourceHub/FileDragAndDropArea.tsx
+++ assets/js/features/ResourceHub/FileDragAndDropArea.tsx
@@ -8,18 +8,26 @@ export function FileDragAndDropArea({ children }) {
   const { setFile } = useNewFileModalsContext();
   const { isFileDragging, onDragLeave, onDragOver, onDrop } = useFileDragAndDrop(setFile);
 
+  const overlayClassName = classNames(
+    "fixed inset-0 bg-black bg-opacity-50 transition-opacity duration-300 z-50",
+    isFileDragging ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none",
+  );
+
   const messageClassName = classNames(
-    "p-4 bg-callout-info text-white text-sm rounded-md shadow-lg",
-    "fixed bottom-0 left-1/2 transform -translate-x-1/2",
-    "transition-transform duration-300",
-    isFileDragging ? "translate-y-[-5vh]" : "translate-y-full",
+    "p-6 bg-callout-info text-white text-sm rounded-md shadow-lg",
+    "transition-transform transform scale-95 duration-300",
+    isFileDragging ? "scale-100 opacity-100" : "scale-95 opacity-0",
   );
 
   return (
     <div onDragOver={onDragOver} onDragLeave={onDragLeave} onDrop={onDrop} className="">
       {children}
 
-      <div className={messageClassName}>Drop files to upload them to Resource Hub</div>
+      <div className={overlayClassName}>
+        <div className="flex items-center justify-center h-full">
+          <div className={messageClassName}>Drop files to upload them to Resource Hub</div>
+        </div>
+      </div>
     </div>
   );
 }
