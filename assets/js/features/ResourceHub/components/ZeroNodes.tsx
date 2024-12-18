858ed6e528c376ae1e97183c599448b6059e6ed5
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-12-18 10:49:32 -0300

    chore: Improve resource hub zero-state copy
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- assets/js/features/ResourceHub/components/ZeroNodes.tsx
+++ assets/js/features/ResourceHub/components/ZeroNodes.tsx
@@ -6,8 +6,9 @@ export function ZeroNodes() {
     <div className="border border-dashed border-stroke-base p-4 w-[500px] mx-auto mt-12 flex gap-4">
       <IconFile size={48} className="text-gray-600" />
       <div>
-        <div className="font-bold">Nothing here just yet.</div>A place to share rich text documents, images, videos, and
-        other files.
+        <div className="font-bold">Ready for your first document</div>
+        <br />
+        <div>Your team's central hub for sharing documents, images, videos, and files. Click 'Add' to get started.</div>
       </div>
     </div>
   );
