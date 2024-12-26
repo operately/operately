956b3215e03cf23e5ab0ffaddc88aee7607b9003
Adriano Lazzaretti <lazzaretti136@gmail.com>
2024-12-26 09:21:15 -0300

    feat: Links can be created
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- assets/js/features/ResourceHub/utils.tsx
+++ assets/js/features/ResourceHub/utils.tsx
@@ -1,4 +1,4 @@
-import { IconFile, IconFileTypePdf, IconFolder, IconMovie, IconMusic, IconPhoto } from "@tabler/icons-react";
+import { IconFile, IconFileTypePdf, IconFolder, IconLink, IconMovie, IconMusic, IconPhoto } from "@tabler/icons-react";
 import { ResourceHubNode } from "@/models/resourceHubs";
 
 import { richContentToString } from "@/components/RichContent";
@@ -6,7 +6,7 @@ import { Paths } from "@/routes/paths";
 import { assertPresent } from "@/utils/assertions";
 import { truncateString } from "@/utils/strings";
 
-export type NodeType = "document" | "folder" | "file";
+export type NodeType = "document" | "folder" | "file" | "link";
 
 export function findIcon(nodeType: NodeType, node: ResourceHubNode) {
   switch (nodeType) {
@@ -14,6 +14,8 @@ export function findIcon(nodeType: NodeType, node: ResourceHubNode) {
       return IconFile;
     case "folder":
       return IconFolder;
+    case "link":
+      return IconLink;
     case "file":
       assertPresent(node.file?.blob, "file.blob must be present in node");
 
@@ -31,6 +33,8 @@ export function findPath(nodeType: NodeType, node: ResourceHubNode) {
       return Paths.resourceHubDocumentPath(node.document!.id!);
     case "folder":
       return Paths.resourceHubFolderPath(node.folder!.id!);
+    case "link":
+      return "";
     case "file":
       return Paths.resourceHubFilePath(node.file!.id!);
   }
@@ -40,16 +44,18 @@ export function findSubtitle(nodeType: NodeType, node: ResourceHubNode) {
   switch (nodeType) {
     case "document":
       assertPresent(node.document?.content, "content must be present in node.document");
-
       const content = richContentToString(JSON.parse(node.document.content));
       return truncateString(content, 60);
+
     case "folder":
       assertPresent(node.folder?.childrenCount, "childrenCount must be present in node.folder");
-
       return node.folder.childrenCount === 1 ? "1 item" : `${node.folder.childrenCount} items`;
+
+    case "link":
+      return "";
+
     case "file":
       assertPresent(node.file?.description, "description must be present in node.file");
-
       const description = richContentToString(JSON.parse(node.file.description));
       return truncateString(description, 50);
   }
---
+++ assets/js/pages/ResourceHubNewLinkPage/loader.tsx
@@ -0,0 +1,20 @@
+import * as Pages from "@/components/Pages";
+import { getResourceHub, ResourceHub } from "@/models/resourceHubs";
+
+interface LoaderResult {
+  resourceHub: ResourceHub;
+}
+
+export async function loader({ params }): Promise<LoaderResult> {
+  return {
+    resourceHub: await getResourceHub({
+      id: params.id,
+      includeSpace: true,
+      includePotentialSubscribers: true,
+    }).then((res) => res.resourceHub!),
+  };
+}
+
+export function useLoadedData(): LoaderResult {
+  return Pages.useLoadedData() as LoaderResult;
+}
