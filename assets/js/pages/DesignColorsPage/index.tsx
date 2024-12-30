ca6fccc5ad92148d4f26c20a5b42f8731a60574c
Igor Šarčević <igor@operately.com>
2024-12-30 13:48:55 +0000

    chore: Add new design pages
    Signed-off-by: Igor Šarčević <igor@operately.com>

---
+++ assets/js/pages/DesignColorsPage/index.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import * as Pages from "@/components/Pages";
+import * as Paper from "@/components/PaperContainer";
+
+interface LoaderResult {
+  // TODO: Define what is loaded when you visit this page
+}
+
+export async function loader({params}) : Promise<LoaderResult> {
+  return {} // TODO: Load data here
+}
+
+export function Page() {
+  const data = Pages.useLoadedData<LoaderResult>();
+
+  return (
+    <Pages.Page title={"DesignColorsPage"}>
+      <Paper.Root>
+        <Paper.Body>
+          <div className="text-content-accent text-3xl font-extrabold">DesignColorsPage</div>
+        </Paper.Body>
+      </Paper.Root>
+    </Pages.Page>
+  );
+}
