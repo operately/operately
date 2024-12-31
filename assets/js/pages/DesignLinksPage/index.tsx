33e58bb3ea776260d897166b8e8e6e603527906e
Igor Šarčević <igisar@gmail.com>
2024-12-31 13:42:41 +0100

    fix: Link to lobby page on design pages (#1812)
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- assets/js/pages/DesignLinksPage/index.tsx
+++ assets/js/pages/DesignLinksPage/index.tsx
@@ -11,7 +11,7 @@ export function Page() {
     <Pages.Page title={"Links"}>
       <Paper.Root>
         <Paper.Navigation>
-          <Paper.NavItem linkTo="/__design__">Lobby</Paper.NavItem>
+          <Paper.NavItem linkTo="/">Lobby</Paper.NavItem>
           <Paper.NavSeparator />
           <Paper.NavItem linkTo="/__design__">Design System</Paper.NavItem>
         </Paper.Navigation>
