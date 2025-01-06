96bd1fa84d8574d362a0755c3bc8287609053970
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2025-01-06 07:33:27 -0300

    fix: Include name of space in feed items outside of space (#1828)
    * fix: Include name of space in feed items outside of space
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    * Fix tests
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    ---------
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- lib/operately_web/api/serializers/activity_content/resource_hub_file_created.ex
+++ lib/operately_web/api/serializers/activity_content/resource_hub_file_created.ex
@@ -6,6 +6,7 @@ defimpl OperatelyWeb.Api.Serializable, for: Operately.Activities.Content.Resourc
       file_id: OperatelyWeb.Paths.file_id(content["file"]),
       file_name: content["file_name"],
       resource_hub: Serializer.serialize(content["resource_hub"], level: :essential),
+      space: Serializer.serialize(content["space"], level: :essential),
     }
   end
 end
--- lib/operately_web/api/serializers/activity_content/resource_hub_link_created.ex
+++ lib/operately_web/api/serializers/activity_content/resource_hub_link_created.ex
@@ -5,6 +5,7 @@ defimpl OperatelyWeb.Api.Serializable, for: Operately.Activities.Content.Resourc
     link = Map.put(content["link"], :node, content["node"])
 
     %{
+      space: Serializer.serialize(content["space"], level: :essential),
       resource_hub: Serializer.serialize(content["resource_hub"], level: :essential),
       link: Serializer.serialize(link, level: :essential),
     }
