9101ab307c5b0ac1f9bb0caf0119fb91ece37981
Adriano Lazzaretti <lazzaretti136@gmail.com>
2025-01-09 11:41:54 -0300

    chore: CreateBlob mutation can be used to create multiple blobs
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- test/operately_web/api/mutations/create_blob_test.exs
+++ test/operately_web/api/mutations/create_blob_test.exs
@@ -11,11 +11,14 @@ defmodule OperatelyWeb.Api.Mutations.CreateBlobTest do
     setup :register_and_log_in_account
 
     test "it creates a new blob record in the database", ctx do
-      assert {200, blob} = mutation(ctx.conn, :create_blob, %{
-        filename: "test.txt",
-        size: 1024,
-        content_type: "text/plain"
-      })
+      assert {200, res} = mutation(ctx.conn, :create_blob, %{files: [
+        %{filename: "test.txt",
+          size: 1024,
+          content_type: "text/plain"
+        }
+      ]})
+
+      blob = hd(res.blobs)
 
       assert blob.id != nil
       assert blob.url == "/blobs/#{blob.id}"
@@ -24,5 +27,35 @@ defmodule OperatelyWeb.Api.Mutations.CreateBlobTest do
       blob = Operately.Blobs.get_blob!(blob.id)
       assert blob.storage_type != nil
     end
+
+    test "it creates multiple blob records in the database", ctx do
+      assert {200, res} = mutation(ctx.conn, :create_blob, %{files: [
+        %{filename: "test.txt",
+          size: 1024,
+          content_type: "text/plain"
+        },
+        %{filename: "test.txt",
+          size: 1024,
+          content_type: "image/jpeg",
+          width: 1920,
+          height: 1080,
+        },
+        %{filename: "test.txt",
+          size: 1024,
+          content_type: "image/jpeg",
+        }
+      ]})
+
+      assert length(res.blobs) == 3
+
+      Enum.each(res.blobs, fn blob ->
+        assert blob.id != nil
+        assert blob.url == "/blobs/#{blob.id}"
+        assert blob.signed_upload_url != nil
+
+        blob = Operately.Blobs.get_blob!(blob.id)
+        assert blob.storage_type != nil
+      end)
+    end
   end
-end 
+end
