6fbcf4a35323d464a7b7dfdeb44ee7a811ada66f
Adriano Lazzaretti <lazzaretti136@gmail.com>
2025-01-09 11:41:54 -0300

    chore: CreateBlob mutation can be used to create multiple blobs
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- lib/operately_web/api/mutations/create_blob.ex
+++ lib/operately_web/api/mutations/create_blob.ex
@@ -3,41 +3,60 @@ defmodule OperatelyWeb.Api.Mutations.CreateBlob do
   use OperatelyWeb.Api.Helpers
 
   inputs do
-    field :filename, :string
-    field :size, :integer
-    field :content_type, :string
-    field :width, :integer
-    field :height, :integer
+    field :files, list_of(:blob_creation_input)
   end
 
   outputs do
-    field :id, :string
-    field :url, :string
-    field :signed_upload_url, :string
-    field :upload_strategy, :string # "direct", "multipart"
+    field :blobs, list_of(:blob_creation_output)
   end
 
   def call(conn, inputs) do
-    person = me(conn)
+    Action.new()
+    |> run(:me, fn -> find_me(conn) end)
+    |> run(:blobs, fn ctx -> create_blobs(ctx.me, inputs.files) end)
+    |> run(:serialized, fn ctx -> serialize(ctx.blobs) end)
+    |> respond()
+  end
 
-    {:ok, blob} = Operately.Blobs.create_blob(%{
-      company_id: person.company_id,
-      author_id: person.id,
-      status: :pending,
-      filename: inputs.filename,
-      size: inputs.size,
-      content_type: inputs.content_type,
-      width: inputs[:width],
-      height: inputs[:height],
-    })
+  def respond(result) do
+    case result do
+      {:ok, ctx} -> {:ok, ctx.serialized}
+      {:error, :blobs, _} -> {:error, :bad_request}
+      _ -> {:error, :internal_server_error}
+    end
+  end
 
-    {:ok, url} = Operately.Blobs.get_signed_upload_url(blob)
+  defp create_blobs(person, files) do
+    Repo.transaction(fn ->
+      Enum.reduce(files, [], fn file, acc ->
+        {:ok, blob} = Operately.Blobs.create_blob(%{
+          company_id: person.company_id,
+          author_id: person.id,
+          status: :pending,
+          filename: file.filename,
+          size: file.size,
+          content_type: file.content_type,
+          width: file[:width],
+          height: file[:height],
+        })
 
-    {:ok, %{
-      id: blob.id,
-      url: Operately.Blobs.Blob.url(blob),
-      signed_upload_url: url,
-      upload_strategy: Operately.Blobs.Blob.upload_strategy(blob),
-    }}
+        [blob | acc]
+      end)
+    end)
+  end
+
+  defp serialize(blobs) do
+    files = Enum.map(blobs, fn blob ->
+      {:ok, url} = Operately.Blobs.get_signed_upload_url(blob)
+
+      %{
+        id: blob.id,
+        url: Operately.Blobs.Blob.url(blob),
+        signed_upload_url: url,
+        upload_strategy: Operately.Blobs.Blob.upload_strategy(blob),
+      }
+    end)
+
+    {:ok, %{blobs: files}}
   end
 end
