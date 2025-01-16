4758a004233fd6d1f84aff85a16aa15d6bf187f9
Igor Šarčević <igor@operately.com>
2025-01-16 14:11:46 +0000

    chore: Display warning if not all subitems are closed
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- assets/js/pages/GoalClosingPage/page.tsx
+++ assets/js/pages/GoalClosingPage/page.tsx
@@ -1,82 +1,134 @@
 import * as React from "react";
 import * as Paper from "@/components/PaperContainer";
 import * as Pages from "@/components/Pages";
-import * as Forms from "@/components/Form";
-import * as Editor from "@/components/Editor";
+import * as Goals from "@/models/goals";
 
-import { PrimaryButton } from "@/components/Buttons";
-import { DimmedLink } from "@/components/Link";
+import Forms from "@/components/Forms";
+
+import { Paths } from "@/routes/paths";
+import { BlackLink } from "@/components/Link";
+import { WarningCallout } from "@/components/Callouts";
 
 import { useLoadedData } from "./loader";
-import { useForm, FormData } from "./useForm";
-import { Paths } from "@/routes/paths";
+import { useNavigateTo } from "@/routes/useNavigateTo";
 
 export function Page() {
   const { goal } = useLoadedData();
-  const form = useForm(goal);
 
   return (
     <Pages.Page title={"Closing " + goal.name}>
       <Paper.Root>
-        <Paper.Navigation>
-          <Paper.NavItem linkTo={Paths.goalPath(goal.id!)}>{goal.name}</Paper.NavItem>
-        </Paper.Navigation>
+        <Navigation />
 
         <Paper.Body minHeight="none">
           <PageTitle />
-          <SuccessQuestion form={form} />
-          <Retrospective form={form} />
-
-          <div className="flex items-center gap-6 mt-8">
-            <SubmitButton form={form} />
-            <DimmedLink to={form.cancelPath}>Cancel</DimmedLink>
-          </div>
+          <ActiveSubitemsWarning />
+          <Form />
         </Paper.Body>
       </Paper.Root>
     </Pages.Page>
   );
 }
 
+function Form() {
+  const { goal } = useLoadedData();
+
+  const [close] = Goals.useCloseGoal();
+  const navigateToGoal = useNavigateTo(Paths.goalPath(goal.id!));
+
+  const form = Forms.useForm({
+    fields: {
+      success: "yes",
+      retrospective: "",
+    },
+    submit: async () => {
+      await close({
+        goalId: goal.id,
+        success: form.values.success,
+        retrospective: JSON.stringify(form.values.retrospective),
+      });
+      navigateToGoal();
+    },
+    cancel: navigateToGoal,
+  });
+
+  return (
+    <Forms.Form form={form}>
+      <Forms.FieldGroup>
+        <AccomplishedOrDropped />
+        <RetrospectiveNotes />
+      </Forms.FieldGroup>
+
+      <Forms.Submit saveText="Close Goal" />
+    </Forms.Form>
+  );
+}
+
+function AccomplishedOrDropped() {
+  return (
+    <Forms.RadioButtons
+      field="success"
+      label="Was this goal accomplished or dropped?"
+      options={[
+        { value: "yes", label: "Accomplished" },
+        { value: "no", label: "Dropped" },
+      ]}
+    />
+  );
+}
+
+function RetrospectiveNotes() {
+  const { goal } = useLoadedData();
+
+  return (
+    <Forms.RichTextArea
+      field="retrospective"
+      label="Retrospective notes"
+      mentionSearchScope={{ type: "goal", id: goal.id! }}
+      placeholder="What went well? What didn't? What did you learn?"
+      required
+    />
+  );
+}
+
+function Navigation() {
+  const { goal } = useLoadedData();
+
+  return (
+    <Paper.Navigation>
+      <Paper.NavItem linkTo={Paths.goalPath(goal.id!)}>{goal.name}</Paper.NavItem>
+    </Paper.Navigation>
+  );
+}
+
 function PageTitle() {
-  return <div className="text-content-accent text-3xl font-extrabold">Close Goal</div>;
+  return <div className="text-content-accent text-2xl font-extrabold">Review &amp; Close Goal</div>;
 }
 
-function Retrospective({ form }: { form: FormData }) {
+function ActiveSubitemsWarning() {
   return (
-    <div className="mt-6">
-      <div className="font-bold mb-2">Retrospective notes:</div>
-
-      <div className="border border-surface-outline rounded overflow-hidden">
-        <Editor.StandardEditorForm editor={form.retrospectiveEditor.editor} />
-      </div>
+    <div className="my-6">
+      <WarningCallout
+        message="This goal contains 3 active sub-goals and 1 project that will remain active:"
+        description={
+          <div>
+            <ul className="flex flex-col gap-1 mt-2">
+              <li>
+                <BlackLink to={Paths.goalPath("123")}>Document core business processes in company playbook</BlackLink>
+              </li>
+              <li>
+                <BlackLink to={Paths.goalPath("123")}>Ensure Financial Stability</BlackLink>
+              </li>
+              <li>
+                <BlackLink to={Paths.projectPath("123")}>Expand team capbilities</BlackLink>
+              </li>
+              <li>
+                <BlackLink to={Paths.projectPath("123")}>Website Redesign</BlackLink>
+              </li>
+            </ul>
+          </div>
+        }
+      />
     </div>
-  );
-}
-
-function SuccessQuestion({ form }: { form: FormData }) {
-  return (
-    <div className="mt-6">
-      <div className="font-bold mb-2">Did you accomplish this goal?</div>
-
-      <Forms.RadioGroup defaultValue="yes" onChange={form.setSuccess} name="success">
-        {form.successOptions.map((option) => (
-          <Forms.Radio
-            key={option.value}
-            label={option.label}
-            value={option.value}
-            disabled={false}
-            testId={`success-${option.value}`}
-          />
-        ))}
-      </Forms.RadioGroup>
-    </div>
-  );
-}
-
-function SubmitButton({ form }: { form: FormData }) {
-  return (
-    <PrimaryButton onClick={form.submit} testId="confirm-close-goal">
-      Close Goal
-    </PrimaryButton>
   );
 }
