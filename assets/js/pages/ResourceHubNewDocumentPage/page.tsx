9def32440b037983270d1a5764c6b76fbd55cd4d
Adriano Lazzaretti <lazzaretti136@gmail.com>
2025-01-21 17:36:35 -0300

    feat: Display complete navigation on new-resource pages
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- assets/js/pages/ResourceHubNewDocumentPage/page.tsx
+++ assets/js/pages/ResourceHubNewDocumentPage/page.tsx
@@ -1,41 +1,24 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
 
 import * as Pages from "@/components/Pages";
 import * as Paper from "@/components/PaperContainer";
 
-import { Paths } from "@/routes/paths";
-import { assertPresent } from "@/utils/assertions";
+import { NewResourcePageNavigation } from "@/features/ResourceHub";
 import { Form } from "./form";
 import { useLoadedData } from "./loader";
 
 export function Page() {
-  const location = useLocation();
-  const params = new URLSearchParams(location.search);
-  const folderId = params.get("folderId");
+  const { resourceHub, folder } = useLoadedData();
 
   return (
     <Pages.Page title="New Document">
       <Paper.Root>
-        <Navigation />
+        <NewResourcePageNavigation resourceHub={resourceHub} folder={folder} />
+
         <Paper.Body>
-          <Form folderId={folderId} />
+          <Form />
         </Paper.Body>
       </Paper.Root>
     </Pages.Page>
-  );
-}
-
-function Navigation() {
-  const { resourceHub } = useLoadedData();
-
-  assertPresent(resourceHub.space, "space must be present in resourceHub");
-
-  return (
-    <Paper.Navigation>
-      <Paper.NavItem linkTo={Paths.spacePath(resourceHub.space.id!)}>{resourceHub.space.name}</Paper.NavItem>
-      <Paper.NavSeparator />
-      <Paper.NavItem linkTo={Paths.resourceHubPath(resourceHub.id!)}>{resourceHub.name}</Paper.NavItem>
-    </Paper.Navigation>
   );
 }
--- assets/js/pages/ResourceHubNewLinkPage/page.tsx
+++ assets/js/pages/ResourceHubNewLinkPage/page.tsx
@@ -1,44 +1,24 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
 
 import * as Pages from "@/components/Pages";
 import * as Paper from "@/components/PaperContainer";
 
-import { Paths } from "@/routes/paths";
-import { assertPresent } from "@/utils/assertions";
-import { LinkOptions } from "@/features/ResourceHub";
-
+import { NewResourcePageNavigation } from "@/features/ResourceHub";
 import { Form } from "./form";
 import { useLoadedData } from "./loader";
 
 export function Page() {
-  const location = useLocation();
-  const params = new URLSearchParams(location.search);
-  const folderId = params.get("folderId");
-  const type = (params.get("type") || "other") as LinkOptions;
+  const { resourceHub, folder } = useLoadedData();
 
   return (
     <Pages.Page title="New Link">
       <Paper.Root>
-        <Navigation />
+        <NewResourcePageNavigation resourceHub={resourceHub} folder={folder} />
+
         <Paper.Body>
-          <Form folderId={folderId} type={type} />
+          <Form />
         </Paper.Body>
       </Paper.Root>
     </Pages.Page>
-  );
-}
-
-function Navigation() {
-  const { resourceHub } = useLoadedData();
-
-  assertPresent(resourceHub.space, "space must be present in resourceHub");
-
-  return (
-    <Paper.Navigation>
-      <Paper.NavItem linkTo={Paths.spacePath(resourceHub.space.id!)}>{resourceHub.space.name}</Paper.NavItem>
-      <Paper.NavSeparator />
-      <Paper.NavItem linkTo={Paths.resourceHubPath(resourceHub.id!)}>{resourceHub.name}</Paper.NavItem>
-    </Paper.Navigation>
   );
 }
