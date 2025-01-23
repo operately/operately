54a70927c75e2981ceeef7438998f38cdaa72c45
Adriano Lazzaretti <lazzaretti136@gmail.com>
2025-01-23 11:29:07 -0300

    Add tests for moving folders
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- test/features/resource_hub_folder_test.exs
+++ test/features/resource_hub_folder_test.exs
@@ -1,6 +1,6 @@
 defmodule Operately.Features.ResourceHubFolderTest do
   use Operately.FeatureCase
-  use Operately.Support.ResourceHub.Deletion
+  use Operately.Support.ResourceHub.{Deletion, Moving}
 
   alias Operately.Support.Features.ResourceHubFolderSteps, as: Steps
 
@@ -113,6 +113,33 @@ defmodule Operately.Features.ResourceHubFolderTest do
       |> Steps.create_folder(name)
       |> Steps.assert_folder_created(%{name: name, index: 0})
       |> delete_resource_from_nodes_list(name)
+    end
+  end
+
+  describe "Move" do
+    @resource_name "unique_folder"
+
+    feature "Moving folder to child folder", ctx do
+      ctx
+      |> Steps.given_nested_folders_exist()
+      |> Steps.given_folder_exists(:hub)
+      |> move_resource_to_child_folder(resource_name: @resource_name)
+    end
+
+    feature "Moving folder to parent folder", ctx do
+      ctx
+      |> Steps.given_nested_folders_exist()
+      |> Steps.visit_folder_page(:five)
+      |> Steps.create_folder(@resource_name)
+      |> move_resource_to_parent_folder(resource_name: @resource_name)
+    end
+
+    feature "Moving link to resource hub root", ctx do
+      ctx
+      |> Steps.given_nested_folders_exist()
+      |> Steps.visit_folder_page(:five)
+      |> Steps.create_folder(@resource_name)
+      |> move_resource_to_hub_root(resource_name: @resource_name)
     end
   end
 end
