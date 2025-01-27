6524676ef145b1de54bf4bbbb6d2615ee519eebc
Adriano Lazzaretti <lazzaretti136@gmail.com>
2025-01-27 16:10:41 -0300

    Add validation for reviewer and champion during goal creation
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- lib/operately_web/api/mutations/create_goal.ex
+++ lib/operately_web/api/mutations/create_goal.ex
@@ -2,19 +2,18 @@ defmodule OperatelyWeb.Api.Mutations.CreateGoal do
   use TurboConnect.Mutation
   use OperatelyWeb.Api.Helpers
 
-  alias Operately.Groups
-  alias Operately.Groups.Permissions
+  alias Operately.Groups.{Group, Permissions}
   alias Operately.Operations.GoalCreation
 
   inputs do
-    field :space_id, :string
+    field :space_id, :id
     field :name, :string
-    field :champion_id, :string
-    field :reviewer_id, :string
+    field :champion_id, :id
+    field :reviewer_id, :id
     field :timeframe, :timeframe
     field :targets, list_of(:create_target_input)
     field :description, :string
-    field :parent_goal_id, :string
+    field :parent_goal_id, :id
     field :anonymous_access_level, :integer
     field :company_access_level, :integer
     field :space_access_level, :integer
@@ -27,10 +26,11 @@ defmodule OperatelyWeb.Api.Mutations.CreateGoal do
   def call(conn, inputs) do
     Action.new()
     |> run(:me, fn -> find_me(conn) end)
-    |> run(:attrs, fn -> decode_inputs(inputs) end)
-    |> run(:space, fn ctx -> Groups.get_group_with_access_level(ctx.attrs.space_id, ctx.me.id) end)
-    |> run(:check_permissions, fn ctx -> Permissions.check(ctx.space.requester_access_level, :can_create_goal) end)
-    |> run(:operation, fn ctx -> GoalCreation.run(ctx.me, ctx.attrs) end)
+    |> run(:space, fn ctx -> Group.get(ctx.me, id: inputs.space_id) end)
+    |> run(:check_permissions, fn ctx -> Permissions.check(ctx.space.request_info.access_level, :can_create_goal) end)
+    |> run(:champion_validation, fn ctx -> validate_champion_permissions(ctx.me, inputs) end)
+    |> run(:reviewer_validation, fn ctx -> validate_reviewer_permissions(ctx.me, inputs) end)
+    |> run(:operation, fn ctx -> GoalCreation.run(ctx.me, inputs) end)
     |> run(:serialized, fn ctx -> {:ok, %{goal: Serializer.serialize(ctx.operation, level: :essential)}} end)
     |> respond()
   end
@@ -38,25 +38,22 @@ defmodule OperatelyWeb.Api.Mutations.CreateGoal do
   defp respond(result) do
     case result do
       {:ok, ctx} -> {:ok, ctx.serialized}
-      {:error, :attrs, _} -> {:error, :bad_request}
       {:error, :space, _} -> {:error, :not_found}
       {:error, :check_permissions, _} -> {:error, :forbidden}
+      {:error, :champion_validation, _} -> {:error, :bad_request, "The selected champion doesn't have access to the selected space"}
+      {:error, :reviewer_validation, _} -> {:error, :bad_request, "The selected reviewer doesn't have access to the selected space"}
       {:error, :operation, _} -> {:error, :internal_server_error}
       _ -> {:error, :internal_server_error}
     end
   end
 
-  defp decode_inputs(inputs) do
-    {:ok, space_id} = decode_id(inputs.space_id)
-    {:ok, champion_id} = decode_id(inputs[:champion_id], :allow_nil)
-    {:ok, reviewer_id} = decode_id(inputs[:reviewer_id], :allow_nil)
-    {:ok, parent_goal_id} = decode_id(inputs[:parent_goal_id], :allow_nil)
+  defp validate_champion_permissions(me, inputs) when me.id == inputs.champion_id, do: {:ok, nil}
+  defp validate_champion_permissions(_, inputs) do
+    Group.get(inputs.champion_id, id: inputs.space_id)
+  end
 
-    {:ok, Map.merge(inputs, %{
-      space_id: space_id,
-      champion_id: champion_id,
-      reviewer_id: reviewer_id,
-      parent_goal_id: parent_goal_id,
-    })}
+  defp validate_reviewer_permissions(me, inputs) when me.id == inputs.reviewer_id, do: {:ok, nil}
+  defp validate_reviewer_permissions(_, inputs) do
+    Group.get(inputs.reviewer_id, id: inputs.space_id)
   end
 end
