cee73076433d63be4735d260f6d392a7ed840bb6
Igor Šarčević <igor@operately.com>
2025-02-26 14:54:29 +0000

    Update target dropdowns
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- assets/js/features/goals/GoalCheckInForm/Form.tsx
+++ assets/js/features/goals/GoalCheckInForm/Form.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Goal, Target } from "@/models/goals";
+import { Goal } from "@/models/goals";
 import { Update } from "@/models/goalCheckIns";
 
 import Forms from "@/components/Forms";
@@ -8,14 +8,12 @@ import { useSubscriptions } from "@/features/Subscriptions";
 import { Spacer } from "@/components/Spacer";
 import { useForm } from "./useForm";
 import { assertPresent } from "@/utils/assertions";
-import { DivLink } from "@/components/Link";
-import { Paths } from "@/routes/paths";
-import classNames from "classnames";
 import { PrimaryButton, SecondaryButton } from "@/components/Buttons";
 import AvatarList from "@/components/AvatarList";
 
 import { StatusPicker } from "./StatusPicker";
-import { Timeframe, TimeframeSelector } from "./Timeframe";
+import { Timeframe } from "./Timeframe";
+import { Targets } from "./Targets";
 
 export interface CreateProps {
   goal: Goal;
@@ -88,53 +86,6 @@ function Description({ goal }: { goal: Goal }) {
         fontSize="text-base"
         placeholder="Describe your progress, risks, and blockers..."
       />
-    </div>
-  );
-}
-
-function Targets() {
-  return (
-    <div className="mt-4">
-      <div className="mb-1 font-bold">Update targets</div>
-
-      <div className="grid grid-cols-1">
-        <Target name="Figure out how to open a new office in Brazil" value={0} total={0} progress={0} index={1} />
-        <Target name="Eliminate blockers for selling in China" value={4} total={20} progress={20} index={2} />
-        <Target name="Achieve 1000+ active users in new countries" value={700} total={1000} progress={70} index={3} />
-        <Target
-          name="Increase revenue by 20% from international sales"
-          value={"$ 1.2M"}
-          total={"$ 1M"}
-          progress={100}
-          index={4}
-        />
-      </div>
-    </div>
-  );
-}
-
-function Target({ name, value, total, progress, index }) {
-  return (
-    <DivLink className="hover:bg-surface-highlight px-2 py-2 -mx-2" to={Paths.targetPath(index.toString())}>
-      <div className="flex items-start justify-between">
-        <div className="font-medium">{name}</div>
-        <div className="tracking-wider text-sm font-medium">
-          {value} / {total}
-        </div>
-      </div>
-
-      <LargeProgress progress={progress} color="bg-accent-1" />
-    </DivLink>
-  );
-}
-
-function LargeProgress({ progress, color }) {
-  const outer = classNames("h-1.5 bg-stroke-base mt-2");
-  const inner = classNames("h-1.5", color);
-
-  return (
-    <div className={outer}>
-      <div className={inner} style={{ width: progress + "%" }} />
     </div>
   );
 }
