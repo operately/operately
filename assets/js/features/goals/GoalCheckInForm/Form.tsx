ad44dd29045289783cd869c878a1b7c0c6e21e9c
Igor Šarčević <igor@operately.com>
2025-02-26 13:11:22 +0000

    Implement status picker
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- assets/js/features/goals/GoalCheckInForm/Form.tsx
+++ assets/js/features/goals/GoalCheckInForm/Form.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import * as Time from "@/utils/time";
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 
 import { Goal, Target } from "@/models/goals";
 import { Update } from "@/models/goalCheckIns";
@@ -7,7 +8,6 @@ import { Update } from "@/models/goalCheckIns";
 import Forms from "@/components/Forms";
 import { useSubscriptions } from "@/features/Subscriptions";
 import { Spacer } from "@/components/Spacer";
-import { createTestId } from "@/utils/testid";
 import { useForm } from "./useForm";
 import { assertPresent } from "@/utils/assertions";
 import { DivLink } from "@/components/Link";
@@ -16,6 +16,8 @@ import classNames from "classnames";
 import { PrimaryButton, SecondaryButton } from "@/components/Buttons";
 import AvatarList from "@/components/AvatarList";
 import FormattedTime from "@/components/FormattedTime";
+import { IconCheck } from "@tabler/icons-react";
+import { match } from "ts-pattern";
 
 export interface CreateProps {
   goal: Goal;
@@ -28,19 +30,16 @@ export interface EditProps {
 }
 
 export function Form(props: CreateProps | EditProps) {
-  const { goal, mode } = props;
+  const { goal } = props;
 
   assertPresent(goal.reviewer, "reviewer must be present in goal");
 
-  const subscriptionsState = useSubscriptions(mode === "create" ? goal.potentialSubscribers! : [], {
+  const subscriptionsState = useSubscriptions(goal.potentialSubscribers!, {
     ignoreMe: true,
     notifyPrioritySubscribers: true,
   });
 
   const form = useForm(props, subscriptionsState);
-
-  const [start, setStart] = React.useState(Time.parse(goal.timeframe?.startDate!));
-  const [end, setEnd] = React.useState(Time.parse(goal.timeframe?.endDate!));
 
   return (
     <Forms.Form form={form}>
@@ -48,41 +47,8 @@ export function Form(props: CreateProps | EditProps) {
 
       <Forms.FieldGroup>
         <div className="flex items-start gap-8 mt-6">
-          <div>
-            <div className="font-bold mb-2">Status</div>
-
-            <div className="w-48">
-              <div className="border border-stroke-base shadow-sm bg-surface-dimmed text-sm rounded-lg px-2 py-1.5 flex items-center gap-2 relative overflow-hidden group cursor-pointer">
-                <div className="border-2 border-surface-outline border-dashed w-4 h-4 rounded-full"></div>
-                <div className="font-medium">Pick a status&hellip;</div>
-              </div>
-            </div>
-          </div>
-
-          <div>
-            <div className="mb-2 font-bold">Still on track for this deadline?</div>
-
-            <div className="flex items-center gap-4">
-              <div className="w-64">
-                <Chronograph start={start} end={end} />
-              </div>
-
-              <div className="flex items-center gap-2">
-                <div
-                  className="text-xs bg-surface-dimmed px-2 py-1 rounded cursor-pointer"
-                  onClick={() => setEnd(new Date(+end! - 1000 * 60 * 60 * 24 * 30))}
-                >
-                  -1 month
-                </div>
-                <div
-                  className="text-xs bg-surface-dimmed px-2 py-1 rounded cursor-pointer"
-                  onClick={() => setEnd(new Date(+end! + 1000 * 60 * 60 * 24 * 30))}
-                >
-                  +1 month
-                </div>
-              </div>
-            </div>
-          </div>
+          <StatusPicker />
+          <Timeframe goal={goal} />
         </div>
 
         <Targets />
@@ -92,18 +58,7 @@ export function Form(props: CreateProps | EditProps) {
       <Spacer size={4} />
 
       <div className="">
-        <div className="font-bold mb-1">When I post this, notify:</div>
-
-        <div className="flex items-center gap-2">
-          <AvatarList
-            people={subscriptionsState.subscribers.map((s) => s.person!)}
-            size={30}
-            stacked
-            stackSpacing={"-space-x-1"}
-          />
-          <SecondaryButton size="xs">Add/Remove</SecondaryButton>
-        </div>
-
+        <WhoToNotify subscriptionsState={subscriptionsState} />
         <div className="flex items-center gap-2 mt-8">
           <PrimaryButton>Post Update</PrimaryButton>
           <SecondaryButton>Cancel</SecondaryButton>
@@ -139,7 +94,7 @@ function Description({ goal }: { goal: Goal }) {
   );
 }
 
-function Chronograph({ start, end }: { start: string; end: string }) {
+function Chronograph({ start, end }: { start: Date; end: Date }) {
   const diff = new Date(end).getTime() - new Date(start).getTime();
   const diffToday = new Date().getTime() - new Date(start).getTime();
   const progress = (diffToday / diff) * 100;
@@ -212,6 +167,221 @@ function LargeProgress({ progress, color }) {
   return (
     <div className={outer}>
       <div className={inner} style={{ width: progress + "%" }} />
+    </div>
+  );
+}
+
+function StatusPickerOption({ status, description, color, isSelected, onClick }) {
+  return (
+    <DropdownMenu.Item asChild>
+      <div className="px-3 py-1.5 hover:bg-surface-highlight cursor-pointer rounded" onClick={onClick}>
+        <div className="flex items-center justify-between gap-3">
+          <div className="flex items-center gap-3">
+            <Circle size={20} color={color} />
+            <div>
+              <div className="text-sm font-bold leading-none">{status}</div>
+              <div className="text-xs mt-1 leading-none">{description}</div>
+            </div>
+          </div>
+
+          <div className={isSelected ? "opacity-100" : "opacity-0"}>
+            <IconCheck />
+          </div>
+        </div>
+      </div>
+    </DropdownMenu.Item>
+  );
+}
+
+function StatusTriggerValue({ status }) {
+  return match(status)
+    .with("not-set", () => (
+      <div className="flex items-center gap-2">
+        <Circle size={18} border="border-surface-outline" noFill borderSize={2} borderDashed />
+        <div className="font-medium">Pick a status&hellip;</div>
+      </div>
+    ))
+    .with("Pending", () => (
+      <div className="flex items-center gap-2">
+        <Circle size={18} color="bg-stone-500" />
+        <div className="font-medium">Pending</div>
+      </div>
+    ))
+    .with("On Track", () => (
+      <div className="flex items-center gap-2">
+        <Circle size={18} color="bg-accent-1" />
+        <div className="font-medium">On Track</div>
+      </div>
+    ))
+    .with("Concern", () => (
+      <div className="flex items-center gap-2">
+        <Circle size={18} color="bg-yellow-500" />
+        <div className="font-medium">Concern</div>
+      </div>
+    ))
+    .with("Off Track", () => (
+      <div className="flex items-center gap-2">
+        <Circle size={18} color="bg-red-500" />
+        <div className="font-medium">Off Track</div>
+      </div>
+    ))
+    .run();
+}
+
+function StatusPicker() {
+  const [status, setStatus] = React.useState<string>("not-set");
+
+  const trigger = (
+    <div className="w-48">
+      <div className="border border-stroke-base shadow-sm bg-surface-dimmed text-sm rounded-lg px-2 py-1.5 relative overflow-hidden group cursor-pointer">
+        <StatusTriggerValue status={status} />
+      </div>
+    </div>
+  );
+
+  const content = (
+    <div>
+      <StatusPickerOption
+        status="Pending"
+        color="bg-stone-500"
+        description="Work has not started yet."
+        isSelected={status === "Pending"}
+        onClick={() => setStatus("Pending")}
+      />
+
+      <StatusPickerOption
+        status="On Track"
+        color="bg-accent-1"
+        description="Progressing well. No blockers."
+        isSelected={status === "On Track"}
+        onClick={() => setStatus("On Track")}
+      />
+
+      <StatusPickerOption
+        status="Concern"
+        color="bg-yellow-500"
+        description="There are risks. Stefan should be aware."
+        isSelected={status === "Concern"}
+        onClick={() => setStatus("Concern")}
+      />
+
+      <StatusPickerOption
+        status="Off Track"
+        color="bg-red-500"
+        description="There are blockers. Stefan's help is needed."
+        isSelected={status === "Off Track"}
+        onClick={() => setStatus("Off Track")}
+      />
+    </div>
+  );
+
+  return (
+    <div>
+      <div className="font-bold mb-2">Status</div>
+      <OptionsMenu trigger={trigger} content={content} />
+    </div>
+  );
+}
+
+function Circle({
+  size,
+  color,
+  border,
+  noFill,
+  borderSize,
+  borderDashed,
+}: {
+  size: number;
+  color?: string;
+  border?: string;
+  noFill?: boolean;
+  borderSize?: number;
+  borderDashed?: boolean;
+}) {
+  const className = classNames(
+    "rounded-full",
+    border ? "border" : "",
+    border ? border : "",
+    noFill ? "" : color,
+    borderDashed ? "border-dashed" : "",
+  );
+
+  return (
+    <div
+      className={className}
+      style={{ width: size + "px", height: size + "px", borderWidth: borderSize ? borderSize + "px" : undefined }}
+    />
+  );
+}
+
+const menuContentClass = classNames(
+  "relative rounded-md mt-1 z-10 px-1 py-1.5",
+  "shadow-xl ring-1 transition ring-surface-outline",
+  "focus:outline-none",
+  "bg-surface-base",
+  "animateMenuSlideDown",
+);
+
+function OptionsMenu({ trigger, content }) {
+  return (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>{trigger}</DropdownMenu.Trigger>
+
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content className={menuContentClass} align="start">
+          {content}
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
+  );
+}
+
+function Timeframe({ goal }: { goal: Goal }) {
+  const [start, _] = React.useState(Time.parse(goal.timeframe?.startDate!));
+  const [end, setEnd] = React.useState(Time.parse(goal.timeframe?.endDate!));
+
+  return (
+    <div>
+      <div className="mb-2 font-bold">Still on track for this deadline?</div>
+
+      <div className="flex items-center gap-4">
+        <div className="w-64">
+          <Chronograph start={start!} end={end!} />
+        </div>
+
+        <div className="flex items-center gap-2">
+          <div
+            className="text-xs bg-surface-dimmed px-2 py-1 rounded cursor-pointer"
+            onClick={() => setEnd(new Date(+end! - 1000 * 60 * 60 * 24 * 30))}
+          >
+            -1 month
+          </div>
+          <div
+            className="text-xs bg-surface-dimmed px-2 py-1 rounded cursor-pointer"
+            onClick={() => setEnd(new Date(+end! + 1000 * 60 * 60 * 24 * 30))}
+          >
+            +1 month
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function WhoToNotify({ subscriptionsState }) {
+  return (
+    <div>
+      <div className="font-bold mb-1">When I post this, notify:</div>
+
+      <div className="flex items-center gap-2">
+        <AvatarList
+          people={subscriptionsState.subscribers.map((s: any) => s.person!)}
+          size={30}
+          stacked
+          stackSpacing={"-space-x-1"}
+        />
+        <SecondaryButton size="xs">Add/Remove</SecondaryButton>
+      </div>
     </div>
   );
 }
