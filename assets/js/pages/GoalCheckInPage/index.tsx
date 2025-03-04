16ddec0f63abdbc19ffea9c2f049dcb02c04e5c7
Adriano Lazzaretti <lazzaretti136@gmail.com>
2025-03-04 15:43:10 -0300

    Add new goal check-in page placeholder
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- assets/js/pages/GoalCheckInPage/index.tsx
+++ assets/js/pages/GoalCheckInPage/index.tsx
@@ -1,2 +1,30 @@
+<<<<<<< HEAD
 export { Page } from "./page";
 export { loader } from "./loader";
+=======
+import * as React from "react";
+import * as Pages from "@/components/Pages";
+import * as Paper from "@/components/PaperContainer";
+
+interface LoaderResult {
+  // TODO: Define what is loaded when you visit this page
+}
+
+export async function loader({params}) : Promise<LoaderResult> {
+  return {} // TODO: Load data here
+}
+
+export function Page() {
+  const data = Pages.useLoadedData<LoaderResult>();
+
+  return (
+    <Pages.Page title={"GoalCheckInPage"}>
+      <Paper.Root>
+        <Paper.Body>
+          <div className="text-content-accent text-3xl font-extrabold">GoalCheckInPage Placeholder</div>
+        </Paper.Body>
+      </Paper.Root>
+    </Pages.Page>
+  );
+}
+>>>>>>> 6538f6ef (Add new goal check-in page placeholder)
