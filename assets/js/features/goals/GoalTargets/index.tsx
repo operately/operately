c6044dca6e8958cd61f9810f5df1acbf8e627fc4
Igor Šarčević <igor@operately.com>
2025-03-04 14:47:15 +0000

    feat: Use new progress bars for targets on goal page
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- assets/js/features/goals/GoalTargets/index.tsx
+++ assets/js/features/goals/GoalTargets/index.tsx
@@ -1,13 +1,14 @@
 import * as React from "react";
 import * as Goals from "@/models/goals";
-import * as Icons from "@tabler/icons-react";
+
+import { ProgressBar } from "@/components/charts";
 
 export function GoalTargets({ goal }: { goal: Goals.Goal }) {
   return (
     <div className="flex flex-col gap-2">
-      <div className="text-sm font-bold uppercase mb-3">Targets</div>
+      <div className="text-sm font-bold uppercase mb-2">Targets</div>
 
-      <div className="flex flex-col gap-2.5">
+      <div className="flex flex-col gap-4">
         {goal.targets!.map((target) => (
           <TargetItem key={target!.id} target={target!} />
         ))}
@@ -17,51 +18,25 @@ export function GoalTargets({ goal }: { goal: Goals.Goal }) {
 }
 
 function TargetItem({ target }: { target: Goals.Target }) {
-  return (
-    <div className="flex justify-between border items-center gap-8 py-2 px-4 rounded-2xl">
-      <div className="flex items-center gap-2 flex-1">
-        <Icons.IconTimeline size={20} />
-        <div className="font-semibold flex-1">{target.name}</div>
-      </div>
-
-      <div className="shrink-0 w-40">
-        <ProgressBar target={target} />
-      </div>
-
-      <div className="shrink-0 w-32 flex flex-col">
-        <CurrentValue target={target} />
-        <Range target={target} />
-      </div>
-    </div>
-  );
-}
-
-function CurrentValue({ target }: { target: Goals.Target }) {
-  return (
-    <div className="flex items-baseline gap-1">
-      <div className="font-bold">{target.value}</div>
-      <div className="text-sm text-content-accent">{target.unit}</div>
-    </div>
-  );
-}
-
-function ProgressBar({ target }: { target: Goals.Target }) {
   const progress = Goals.targetProgressPercentage(target);
 
   return (
-    <div className="h-3 bg-stone-500/20 rounded relative overflow-hidden">
-      <div
-        className="bg-green-600 absolute top-0 bottom-0 left-0"
-        style={{ width: progress === 0 ? "3px" : progress + "%" }}
-      />
-    </div>
-  );
-}
+    <div>
+      <div className="flex items-end justify-between mb-1.5">
+        <div className="font-semibold flex-1">{target.name}</div>
+        <div className="text-sm">
+          {target.value} / {target.to}
+          {target.unit}
+        </div>
+      </div>
 
-function Range({ target }: { target: Goals.Target }) {
-  return (
-    <div className="text-xs inline-flex gap-1 items-center text-content-dimmed">
-      Target: {target.from} <Icons.IconArrowRight size={12} /> {target.to}
+      <ProgressBar
+        percentage={progress}
+        width="w-full"
+        height="h-1.5"
+        rounded={false}
+        bgColor="var(--color-stroke-base)"
+      />
     </div>
   );
 }
