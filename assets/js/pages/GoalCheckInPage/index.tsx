656c5921512a3220bae4f8bc8cc23c46d108172e
Igor Šarčević <igor@operately.com>
2025-03-04 20:12:25 +0000

    Redirect to new page if feature is enabled
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- assets/js/pages/GoalCheckInPage/index.tsx
+++ assets/js/pages/GoalCheckInPage/index.tsx
@@ -6,12 +6,12 @@ interface LoaderResult {
   // TODO: Define what is loaded when you visit this page
 }
 
-export async function loader({params}) : Promise<LoaderResult> {
-  return {} // TODO: Load data here
+export async function loader({}): Promise<LoaderResult> {
+  return {}; // TODO: Load data here
 }
 
 export function Page() {
-  const data = Pages.useLoadedData<LoaderResult>();
+  // const data = Pages.useLoadedData<LoaderResult>();
 
   return (
     <Pages.Page title={"GoalCheckInPage"}>
