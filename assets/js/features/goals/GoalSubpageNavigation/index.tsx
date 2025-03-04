07d506349e6a47a08cbe30bf8c4859379595954f
Igor Šarčević <igor@operately.com>
2025-03-04 21:18:24 +0000

    feat: Navigation for the check-in page
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- assets/js/features/goals/GoalSubpageNavigation/index.tsx
+++ assets/js/features/goals/GoalSubpageNavigation/index.tsx
@@ -3,12 +3,19 @@ import * as Paper from "@/components/PaperContainer";
 import * as Goals from "@/models/goals";
 
 import { Paths } from "@/routes/paths";
+import { assertPresent } from "@/utils/assertions";
 
 export function GoalSubpageNavigation({ goal }: { goal: Goals.Goal }) {
   const goalPath = Paths.goalPath(goal.id!);
 
+  assertPresent(goal.space, "space must be present in goal");
+
   return (
     <Paper.Navigation>
+      <Paper.NavSpaceLink space={goal.space} />
+      <Paper.NavSeparator />
+      <Paper.NavSpaceWorkMapLink space={goal.space} />
+      <Paper.NavSeparator />
       <Paper.NavItem linkTo={goalPath}>{goal.name}</Paper.NavItem>
     </Paper.Navigation>
   );
