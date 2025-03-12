b35aa85bc02e6161f7658768c8d09791ea8d787d
Igor Šarčević <igor@operately.com>
2025-03-12 10:43:07 +0000

    feat: Use goal names in the check-in title
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- assets/js/pages/GoalCheckInNewPage/index.tsx
+++ assets/js/pages/GoalCheckInNewPage/index.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
-
-import * as Goals from "@/models/goals";
 import * as Pages from "@/components/Pages";
 import * as Paper from "@/components/PaperContainer";
+import * as Goals from "@/models/goals";
 
+import { SubscribersSelector, useSubscriptions } from "@/features/Subscriptions";
+import { Spacer } from "@/components/Spacer";
 import { assertPresent } from "@/utils/assertions";
-import { Form } from "./Form";
+import { useForm, Form as CheckInForm } from "@/features/goals/GoalCheckIn";
+
+import Forms from "@/components/Forms";
 
 interface LoaderResult {
   goal: Goals.Goal;
@@ -27,14 +30,12 @@ export function Page() {
   const { goal } = Pages.useLoadedData<LoaderResult>();
 
   return (
-    <Pages.Page title={["Check-In", goal.name!]}>
+    <Pages.Page title={["Check-in", goal.name!]} testId="goal-check-in-new-page">
       <Paper.Root>
-        <Navigation />
+        <Navigation goal={goal} />
 
-        <Paper.Body>
-          <div className="text-3xl font-bold">Goal Check-in</div>
-          <div>Share the progress with the team</div>
-
+        <Paper.Body className="lg:px-28">
+          <Header goal={goal} />
           <Form goal={goal} />
         </Paper.Body>
       </Paper.Root>
@@ -42,9 +43,16 @@ export function Page() {
   );
 }
 
-function Navigation() {
-  const { goal } = Pages.useLoadedData<LoaderResult>();
+function Header({ goal }: { goal: Goals.Goal }) {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center">Check-in: {goal.name}</h1>
+      <p className="text-center mt-1">Share the progress with the team</p>
+    </div>
+  );
+}
 
+function Navigation({ goal }: { goal: Goals.Goal }) {
   assertPresent(goal.space, "space must be present in goal");
 
   return (
@@ -55,5 +63,25 @@ function Navigation() {
       <Paper.NavSeparator />
       <Paper.NavGoalLink goal={goal} />
     </Paper.Navigation>
+  );
+}
+
+function Form({ goal }: { goal: Goals.Goal }) {
+  assertPresent(goal.space, "space must be present in goal");
+  assertPresent(goal.potentialSubscribers, "potentialSubscribers must be present in goal");
+
+  const subscriptionsState = useSubscriptions(goal.potentialSubscribers, {
+    ignoreMe: true,
+    notifyPrioritySubscribers: true,
+  });
+
+  const form = useForm({ mode: "create", goal, subscriptionsState });
+
+  return (
+    <CheckInForm form={form} goal={goal} readonly={false}>
+      <Spacer size={4} />
+      <SubscribersSelector state={subscriptionsState} spaceName={goal.space.name!} />
+      <Forms.Submit saveText="Check In" buttonSize="base" />
+    </CheckInForm>
   );
 }
