ec475a3c1faa287242fb1ae7a0ab75ced0e4c30a
Igor Šarčević <igor@operately.com>
2025-03-17 14:31:47 +0100

    feat: Textual description of the goal's state (#2117)
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- assets/js/features/goals/GoalCheckIn/Form.tsx
+++ assets/js/features/goals/GoalCheckIn/Form.tsx
@@ -15,6 +15,8 @@ import { isPresent } from "@/utils/isPresent";
 import Forms from "@/components/Forms";
 import RichContent from "@/components/RichContent";
 import classNames from "classnames";
+import { match } from "ts-pattern";
+import { durationHumanized } from "@/utils/time";
 
 interface Props {
   form: any;
@@ -26,7 +28,7 @@ interface Props {
 export function Form({ form, readonly, goal, children }: Props) {
   return (
     <Forms.Form form={form} preventSubmitOnEnter>
-      <div className="space-y-6 mt-6">
+      <div className="space-y-8 mt-8">
         <StatusAndTimeframe goal={goal} readonly={readonly} />
         <Targets readonly={readonly} />
         <Description goal={goal} readonly={readonly} />
@@ -38,8 +40,86 @@ export function Form({ form, readonly, goal, children }: Props) {
 }
 
 function StatusAndTimeframe({ goal, readonly }: { goal: Goals.Goal; readonly: boolean }) {
-  if (readonly) return null;
+  if (readonly) {
+    return <TextualOverview goal={goal} />;
+  } else {
+    return <StatusAndTimeframeForm goal={goal} />;
+  }
+}
 
+function TextualOverview({ goal }: { goal: Goals.Goal }) {
+  return (
+    <div className="ProseMirror">
+      <Label text="Overview" />
+      <OverviewStatus goal={goal} /> <OverviewTimeframe />
+    </div>
+  );
+}
+
+function OverviewStatus({ goal }: { goal: Goals.Goal }) {
+  const [value] = Forms.useFieldValue("status");
+
+  return match(value)
+    .with("pending", () => <OverviewPending />)
+    .with("on_track", () => <OverviewOnTrack />)
+    .with("concern", () => <OverviewConcern goal={goal} />)
+    .with("issue", () => <OverviewIssue goal={goal} />)
+    .run();
+}
+
+function OverviewPending() {
+  return (
+    <span>
+      The goal is <span className="bg-stone-300">Pending</span>. Work has not started yet.
+      <br />
+    </span>
+  );
+}
+
+function OverviewOnTrack() {
+  return (
+    <span>
+      The goal is <mark data-highlight="bgGreen">on-track</mark>.
+    </span>
+  );
+}
+
+function OverviewConcern({ goal }: { goal: Goals.Goal }) {
+  return (
+    <span>
+      <mark data-highlight="bgYellow">Concern</mark>. There are risks.
+      {goal.reviewer && <span> {People.firstName(goal.reviewer)} should be aware.</span>}
+      <br />
+    </span>
+  );
+}
+
+function OverviewIssue({ goal }: { goal: Goals.Goal }) {
+  return (
+    <span>
+      <mark data-highlight="bgRed">Issue</mark>. The goal is blocked or significantly behind.{" "}
+      {goal.reviewer && <span> {People.firstName(goal.reviewer)}'s help is needed.</span>}
+      <br />
+    </span>
+  );
+}
+
+function OverviewTimeframe() {
+  const [timeframe] = Forms.useFieldValue<Timeframes.Timeframe>("timeframe");
+  if (!timeframe.endDate) return null;
+
+  if (timeframe.endDate < new Date()) {
+    return (
+      <span>
+        {durationHumanized(timeframe.endDate, new Date())} <mark data-highlight="bgRed">overdue</mark>.
+      </span>
+    );
+  } else {
+    return <span>{durationHumanized(new Date(), timeframe.endDate)} left to complete the goal.</span>;
+  }
+}
+
+function StatusAndTimeframeForm({ goal }: { goal: Goals.Goal }) {
   return (
     <Forms.FieldGroup>
       <div className="flex items-start gap-8">
@@ -100,7 +180,7 @@ function DescriptionEdit({ goal }: { goal: Goals.Goal }) {
 }
 
 function Label({ text, className = "" }: { text: string; className?: string }) {
-  return <div className={"text-lg font-bold mb-2 " + className}>{text}</div>;
+  return <div className={"font-bold mb-1.5 " + className}>{text}</div>;
 }
 
 function TimeframeSelector() {
@@ -108,7 +188,7 @@ function TimeframeSelector() {
 
   return (
     <div>
-      <div className="flex items-center gap-2 mb-2">
+      <div className="flex items-center gap-2 mb-1.5">
         <Label text="Timeframe" className="mb-0" />
         <TimeframeEditButton value={value} setValue={setValue} />
       </div>
