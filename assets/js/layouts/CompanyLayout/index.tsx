2ef0b970037e7c95a57e4446e29756b0f4617c47
Igor Šarčević <igor@operately.com>
2025-03-18 16:28:41 +0000

    feat: Mobile burger navigation
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- assets/js/layouts/CompanyLayout/index.tsx
+++ assets/js/layouts/CompanyLayout/index.tsx
@@ -3,6 +3,7 @@ import * as Icons from "@tabler/icons-react";
 import * as Api from "@/api";
 
 import { useLoaderData } from "react-router-dom";
+import { logOut } from "@/routes/auth";
 
 import { DivLink } from "@/components/Link";
 import { Outlet } from "react-router-dom";
@@ -17,14 +18,121 @@ import { HelpDropdown } from "./HelpDropdown";
 import { DevBar } from "@/features/DevBar";
 import { Paths } from "@/routes/paths";
 import { useScrollToTopOnNavigationChange } from "@/hooks/useScrollToTopOnNavigationChange";
-
-function NavigationContainer({ children }) {
-  return <div className="transition-all z-50 py-1.5 bg-base border-b border-surface-outline">{children}</div>;
-}
+import { useWindowSizeBreakpoints } from "@/components/Pages";
 
 function Navigation({ company }: { company: Api.Company }) {
+  const size = useWindowSizeBreakpoints();
+
+  if (size === "xs") {
+    return <MobileNavigation company={company} />;
+  } else {
+    return <DesktopNavigation company={company} />;
+  }
+}
+
+function MobileNavigation({ company }: { company: Api.Company }) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleLogOut = async () => {
+    const res = await logOut();
+
+    if (res === "success") {
+      window.location.href = "/";
+    }
+  };
+
   return (
-    <NavigationContainer>
+    <div className="transition-all z-50 py-2 bg-base border-b border-surface-outline">
+      <div className="flex items-center justify-between px-4">
+        <div className="flex items-center gap-3">
+          <DivLink className="flex items-center gap-2 cursor-pointer" to={Paths.homePath()}>
+            <OperatelyLogo />
+          </DivLink>
+          <div className="font-bold">{company.name}</div>
+        </div>
+
+        <div className="">
+          <Icons.IconMenu2 size={24} onClick={() => setOpen(!open)} />
+        </div>
+      </div>
+
+      {open && (
+        <div
+          className="flex flex-col bg-base absolute inset-0 top-10 bg-surface-bg border-t border-surface-outline"
+          onClick={() => setOpen(false)}
+        >
+          <MobileSectionLink to={Paths.homePath()} icon={Icons.IconHome2}>
+            Home
+          </MobileSectionLink>
+
+          <MobileSectionLink to={Paths.goalsPath()} icon={Icons.IconTargetArrow}>
+            Goals
+          </MobileSectionLink>
+
+          <MobileSectionLink to={Paths.projectsPath()} icon={Icons.IconTable}>
+            Projects
+          </MobileSectionLink>
+
+          <MobileSectionLink to={Paths.reviewPath()} icon={Icons.IconCoffee}>
+            Review
+          </MobileSectionLink>
+
+          <MobileSectionLink to={Paths.peoplePath()} icon={Icons.IconUserCircle}>
+            People
+          </MobileSectionLink>
+
+          <MobileSectionLink to={Paths.notificationsPath()} icon={Icons.IconBell}>
+            Notifications
+          </MobileSectionLink>
+
+          <MobileSectionLink to={Paths.accountPath()} icon={Icons.IconUser}>
+            Account
+          </MobileSectionLink>
+
+          <MobileSectionLink to={Paths.companyAdminPath()} icon={Icons.IconCircleKey}>
+            Company Admin
+          </MobileSectionLink>
+
+          <MobileSectionLink to={Paths.lobbyPath()} icon={Icons.IconSwitch}>
+            Switch Company
+          </MobileSectionLink>
+
+          <MobileSectionAction onClick={handleLogOut} icon={Icons.IconDoorExit}>
+            Log Out
+          </MobileSectionAction>
+        </div>
+      )}
+    </div>
+  );
+}
+
+function MobileSectionLink({ to, children, icon }) {
+  return (
+    <DivLink
+      to={to}
+      className="font-semibold flex items-center gap-1 cursor-pointer group hover:bg-surface-base px-4 py-2.5 border-b border-surface-outline first:border-t"
+    >
+      {React.createElement(icon, { size: 16 })}
+      {children}
+    </DivLink>
+  );
+}
+
+function MobileSectionAction({ onClick, children, icon }) {
+  return (
+    <div
+      onClick={onClick}
+      className="font-semibold flex items-center gap-1 cursor-pointer group hover:bg-surface-base px-4 py-2.5 border-b border-surface-outline first:border-t"
+    >
+      {React.createElement(icon, { size: 16 })}
+      {children}
+    </div>
+  );
+}
+
+function DesktopNavigation({ company }: { company: Api.Company }) {
+  return (
+    <div className="transition-all z-50 py-1.5 bg-base border-b border-surface-outline">
       <div className="flex items-center justify-between px-4">
         <div className="flex items-center">
           <DivLink className="flex items-center gap-2 cursor-pointer" to={Paths.homePath()}>
@@ -65,7 +173,7 @@ function Navigation({ company }: { company: Api.Company }) {
           </div>
         </div>
       </div>
-    </NavigationContainer>
+    </div>
   );
 }
 
