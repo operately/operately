be02997704a6c8ffbd91756ae746cd9b74fd66bc
Adriano Lazzaretti <lazzaretti136@gmail.com>
2025-03-19 16:46:54 -0300

    feat: Targets can be added inline on the new Goal Page
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- assets/js/features/goals/GoalTargetsV2/types.tsx
+++ assets/js/features/goals/GoalTargetsV2/types.tsx
@@ -1,7 +1,7 @@
 import * as Goals from "@/models/goals";
 import * as GoalCheckIns from "@/models/goalCheckIns";
 
-export type Target = GoalCheckIns.Target | Goals.Target;
+export type Target = (GoalCheckIns.Target | Goals.Target) & { isNew?: boolean };
 
 export function isCheckInTarget(target: Target): target is GoalCheckIns.Target {
   return "previousValue" in target;
--- assets/js/features/goals/GoalTargetsV2/utils.tsx
+++
@@ -1,6 +0,0 @@
-export function getReadonlyFlags(attrs: { readonly?: boolean; editValue?: boolean; editDefinition?: boolean }) {
-  return {
-    readonlyValue: Boolean(attrs.readonly || !attrs.editValue),
-    readonlyDefinition: Boolean(attrs.readonly || !attrs.editDefinition),
-  };
-}
