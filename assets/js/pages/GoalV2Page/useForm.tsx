a8a9a5451345a059859f8da6755ac0d3118e0697
Adriano Lazzaretti <lazzaretti136@gmail.com>
2025-03-19 16:46:54 -0300

    feat: Targets can be added inline on the new Goal Page
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- assets/js/pages/GoalV2Page/useForm.tsx
+++ assets/js/pages/GoalV2Page/useForm.tsx
@@ -38,6 +38,8 @@ export function useForm() {
     submit: async () => {
       assertPresent(goal.accessLevels, "accessLevels must be present in goal");
 
+      const { updated, added } = parseTargets(form.values.targets);
+
       await edit({
         goalId: goal.id,
         parentGoalId: form.values.parentGoal?.id || null,
@@ -46,8 +48,8 @@ export function useForm() {
         reviewerId: form.values.reviewer,
         timeframe: serializeTimeframe(form.values.timeframe, currTimeframe),
         description: JSON.stringify(form.values.description),
-        updatedTargets: parseTargets(form.values.targets),
-        addedTargets: [],
+        updatedTargets: updated,
+        addedTargets: added,
         anonymousAccessLevel: goal.accessLevels.public,
         companyAccessLevel: goal.accessLevels.company,
         spaceAccessLevel: goal.accessLevels.space,
