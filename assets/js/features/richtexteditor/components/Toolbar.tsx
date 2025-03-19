3196efa8e3cdca332d1d6b02e8d51edf28d567a8
Igor Šarčević <igor@operately.com>
2025-03-19 14:19:30 +0100

    fix: Mobile-friendly toolbar for the rich text editor (#2141)
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- assets/js/features/richtexteditor/components/Toolbar.tsx
+++ assets/js/features/richtexteditor/components/Toolbar.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import * as React from "react";
+import * as Popover from "@radix-ui/react-popover";
+import * as Icons from "@tabler/icons-react";
 
 import { AttachmentButton } from "./AttachmentButton";
 import { BlockquoteButton } from "./BlockquoteButton";
@@ -17,13 +19,24 @@ import { LinkEditForm } from "@/components/Editor";
 import { CodeBlockButton } from "./CodeBlockButton";
 import { ColorPicker } from "./ColorPicker";
 import { DividerButton } from "./DividerButton";
+import { useWindowSizeBreakpoints } from "@/components/Pages";
 
 interface Props {
   editor: any;
   noTopBorder?: boolean;
 }
 
-export function Toolbar({ editor, noTopBorder }: Props): JSX.Element {
+export function Toolbar(props: Props): JSX.Element {
+  const size = useWindowSizeBreakpoints();
+
+  if (size == "xs") {
+    return <MobileToolbar {...props} />;
+  } else {
+    return <DesktopToolbar {...props} />;
+  }
+}
+
+function DesktopToolbar({ editor, noTopBorder }: Props): JSX.Element {
   const border = noTopBorder ? "border-b" : "border-y";
 
   return (
@@ -65,6 +78,54 @@ export function Toolbar({ editor, noTopBorder }: Props): JSX.Element {
 
       <LinkEditForm editor={editor} />
     </div>
+  );
+}
+
+function MobileToolbar({ editor, noTopBorder }: Props): JSX.Element {
+  const border = noTopBorder ? "border-b" : "border-y";
+
+  return (
+    <div className="sticky bg-surface-base z-10 rounded-t-lg top-0">
+      <div className={"flex items-center gap-3 border-stroke-base" + " " + border}>
+        <div className="flex justify-between items-center w-full">
+          <div className="flex items-center">
+            <BoldButton editor={editor} iconSize={20} />
+            <ItalicButton editor={editor} iconSize={20} />
+            <StrikeButton editor={editor} iconSize={20} />
+            <H1Button editor={editor} iconSize={20} />
+            <H2Button editor={editor} iconSize={20} />
+            <ColorPicker editor={editor} iconSize={18} />
+            <BulletListButton editor={editor} iconSize={20} />
+            <AttachmentButton editor={editor} iconSize={20} />
+          </div>
+
+          <MobilePopupTools>
+            <NumberListButton editor={editor} iconSize={20} />
+            <BlockquoteButton editor={editor} iconSize={20} />
+            <CodeBlockButton editor={editor} iconSize={20} />
+            <DividerButton editor={editor} iconSize={20} />
+            <LinkButton editor={editor} iconSize={20} />
+            <UndoButton editor={editor} iconSize={20} />
+            <RedoButton editor={editor} iconSize={20} />
+          </MobilePopupTools>
+        </div>
+      </div>
+
+      <LinkEditForm editor={editor} />
+    </div>
+  );
+}
+
+function MobilePopupTools({ children }: { children: React.ReactNode }): JSX.Element {
+  return (
+    <Popover.Root>
+      <Popover.Trigger className="mr-2">
+        <Icons.IconChevronDown size={20} />
+      </Popover.Trigger>
+      <Popover.Content className="z-10 p-2 bg-surface-base rounded-lg shadow-lg border border-stroke-base">
+        <div className="flex flex-wrap gap-1">{children}</div>
+      </Popover.Content>
+    </Popover.Root>
   );
 }
 
