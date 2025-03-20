5aaf2e8fa67bc787f5a67e7bf4c16e5c39af6eb6
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2025-03-20 16:36:23 -0300

    fix: Add error handling Goal title (#2158)
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- assets/js/components/Forms/validations/presence.tsx
+++ assets/js/components/Forms/validations/presence.tsx
@@ -1,10 +1,12 @@
 import { AddErrorFn } from "../useForm/errors";
 
-export function validatePresence(required?: boolean) {
+export function validatePresence(required?: boolean, message?: string) {
   return (field: string, value: string, addError: AddErrorFn) => {
+    message = message ?? "Can't be empty";
+
     if (!required) return;
 
-    if (!value) return addError(field, "Can't be empty");
-    if (value.trim().length === 0) return addError(field, "Can't be empty");
+    if (!value) return addError(field, message);
+    if (value.trim().length === 0) return addError(field, message);
   };
 }
