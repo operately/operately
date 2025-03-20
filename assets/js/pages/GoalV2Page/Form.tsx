91d0d8a22684c0aabe12ff16ad7fb5fde3a653df
Adriano Lazzaretti <lazzaretti136@gmail.com>
2025-03-20 14:59:42 -0300

    fix: Goal status moved to sidebar
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- assets/js/pages/GoalV2Page/Form.tsx
+++ assets/js/pages/GoalV2Page/Form.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import Forms from "@/components/Forms";
 import { useIsViewMode } from "@/components/Pages";
 import { EditBar } from "@/components/Pages/EditBar";
+import { GoalTargetsField } from "@/features/goals/GoalTargetsV2";
+import { GoalStatusBadge } from "@/features/goals/GoalStatusBadge";
 
 import { Messages } from "./Messages";
 import { HorizontalRule, Title } from "./components";
@@ -14,7 +16,6 @@ import { GoalName } from "./Name";
 import { ParentGoal } from "./ParentGoal";
 import { useForm } from "./useForm";
 import { Description } from "./Description";
-import { GoalTargetsField } from "@/features/goals/GoalTargetsV2";
 
 export function Form() {
   const form = useForm();
@@ -34,6 +35,9 @@ export function Form() {
           <RelatedWork />
         </div>
         <div className="col-start-2 row-start-2 flex flex-col gap-4 sticky top-0 self-start">
+          <div className="flex">
+            <GoalStatusBadge status={form.values.status} className="mt-3" />
+          </div>
           <NextCheckIn />
           <Timeframe />
           <Champion />
