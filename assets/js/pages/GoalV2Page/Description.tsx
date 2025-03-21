51ba1ca07c7ee1037e956d0705fd2b7214f71429
Adriano Lazzaretti <lazzaretti136@gmail.com>
2025-03-21 07:36:48 -0300

    feat: Goal description can be expanded and collapsed
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- assets/js/pages/GoalV2Page/Description.tsx
+++ assets/js/pages/GoalV2Page/Description.tsx
@@ -1,21 +1,53 @@
 import React from "react";
 
 import Forms from "@/components/Forms";
-import { useFieldValue } from "@/components/Forms/FormContext";
 import { useIsViewMode } from "@/components/Pages";
-import { richContentToString } from "@/components/RichContent";
+import RichContent, { countCharacters, shortenContent } from "@/components/RichContent";
+import { ActionLink } from "@/components/Link";
 
 import { useLoadedData } from "./loader";
 
+const DESCRIPTION_CHAR_LIMIT = 200;
+
 export function Description() {
-  const { goal } = useLoadedData();
   const isViewMode = useIsViewMode();
-  const [description] = useFieldValue("description");
 
+  if (isViewMode) return <ViewDescription />;
+  return <EditDescription />;
+}
+
+function ViewDescription() {
+  const { goal } = useLoadedData();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const length = React.useMemo(() => {
+    return goal.description ? countCharacters(goal.description) : 0;
+  }, [goal.description]);
+
+  const description = React.useMemo(() => {
+    if (expanded || length <= DESCRIPTION_CHAR_LIMIT) {
+      return goal.description!;
+    }
+    return shortenContent(goal.description!, DESCRIPTION_CHAR_LIMIT, { suffix: "..." });
+  }, [length, expanded, goal.description]);
+
+  if (length === 0) return null;
+
+  return (
+    <div>
+      <RichContent jsonContent={description} />
+      {length > DESCRIPTION_CHAR_LIMIT && (
+        <ActionLink onClick={() => setExpanded(!expanded)} className="text-sm">
+          {expanded ? "Collapse" : "Expand"}
+        </ActionLink>
+      )}
+    </div>
+  );
+}
+
+function EditDescription() {
+  const { goal } = useLoadedData();
   const mentionSearchScope = { type: "goal", id: goal.id! } as const;
-  const strDescription = description ? richContentToString(description).trim() : "";
-
-  if (!strDescription && isViewMode) return null;
 
   return (
     <Forms.FieldGroup>
@@ -23,10 +55,10 @@ export function Description() {
         field="description"
         placeholder="Write here..."
         mentionSearchScope={mentionSearchScope}
-        readonly={isViewMode}
         height="3rem"
         horizontalPadding=""
         verticalPadding=""
+        fontWeight="font-normal"
         hideBorder
         hideToolbar
       />
