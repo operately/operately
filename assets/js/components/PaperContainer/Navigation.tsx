5d2a83795f1f42cdb66eba597eeb678911524e36
Igor Šarčević <igor@operately.com>
2025-03-24 22:08:32 +0000

    Initial prototype for hidding nav items
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- assets/js/components/PaperContainer/Navigation.tsx
+++ assets/js/components/PaperContainer/Navigation.tsx
@@ -22,37 +22,72 @@ interface Item {
   label: string;
 }
 
+const navigationClassName = classNames(
+  "bg-surface-dimmed",
+  "flex items-center gap-1 justify-center",
+  "px-2 pt-2 pb-1 mx-0 sm:mx-10",
+  "font-semibold rounded-t",
+  "border-b sm:border-b-0 sm:border-t sm:border-x border-surface-outline",
+);
+
 export function Navigation({ items, testId }: { items: Item[]; testId?: string }) {
-  const className = classNames(
-    "bg-surface-dimmed",
-    "flex items-center flex-wrap",
-    "justify-center gap-1",
-    "px-2 pt-2 pb-1 mx-0 sm:mx-10",
-    "font-semibold rounded-t",
-    "border-b sm:border-b-0 sm:border-t sm:border-x border-surface-outline",
-  );
+  const containerRef = React.useRef<HTMLDivElement>(null);
+  const [hiddenCount, setHiddenCount] = React.useState(0);
+
+  function handleResize() {
+    window.requestAnimationFrame(() => {
+      if (!containerRef.current) return;
+
+      const container = containerRef.current;
+      const width = container.getBoundingClientRect().width;
+      const itemWidths = items.map((item) => estimatedTextWidth(item.label));
+      const totalItemWidths = itemWidths.reduce((acc, cur) => acc + cur, 0);
+      const separators = (items.length - 1) * 25;
+
+      let overflow = totalItemWidths + separators - width;
+      let hidden = 0;
+
+      console.log("before", width, itemWidths, overflow);
+
+      while (overflow > 0 && itemWidths.length > 1) {
+        const last = itemWidths.shift();
+        overflow -= last!;
+        hidden++;
+      }
+
+      console.log("after", width, itemWidths, overflow, hidden);
+      setHiddenCount(hidden);
+    });
+  }
+
+  React.useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [containerRef, items]);
+
+  console.log("hiddenCount", hiddenCount);
 
   return (
-    <div className={className} data-test-id={testId}>
-      {items.map((item, index) => (
-        <>
-          {index > 0 && <NavSeparator key={`separator-${index}`} />}
-          <NavItem item={item} key={item.to} />
-        </>
+    <div className={navigationClassName} data-test-id={testId} ref={containerRef}>
+      {items.slice(hiddenCount).map((item, index) => (
+        <NavItem key={index + item.label} item={item} index={index} />
       ))}
     </div>
   );
 }
 
-interface NavItemProps {
-  item: Item;
-}
+function NavItem({ item, index }: { item: Item; index: number }) {
+  const className = classNames("truncate");
 
-function NavItem({ item }: NavItemProps) {
   return (
-    <Link to={item.to} testId={createTestId("nav-item", item.label)}>
-      {item.label}
-    </Link>
+    <React.Fragment key={`fragment-${index}`}>
+      {index > 0 && <NavSeparator key={`separator-${index}`} />}
+
+      <Link to={item.to} testId={createTestId("nav-item", item.label)} className={className}>
+        {item.label}
+      </Link>
+    </React.Fragment>
   );
 }
 
@@ -65,4 +100,18 @@ function NavSeparator() {
       <Icons.IconSlash size={iconSize} />
     </div>
   );
+}
+
+function estimatedTextWidth(text: string) {
+  let width = 0;
+
+  for (const char of text) {
+    if ("iIljtf".includes(char))
+      width += 6; // narrow chars
+    else if ("mwWM".includes(char))
+      width += 12; // wide chars
+    else width += 9; // average chars
+  }
+
+  return width;
 }
