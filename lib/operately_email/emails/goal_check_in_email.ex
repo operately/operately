7dc6109375d7df09d38709fc9454d46febf5226d
Igor Šarčević <igor@operately.com>
2025-03-25 23:10:57 +0000

    Include overview in check-in emails
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- lib/operately_email/emails/goal_check_in_email.ex
+++ lib/operately_email/emails/goal_check_in_email.ex
@@ -22,6 +22,7 @@ defmodule OperatelyEmail.Emails.GoalCheckInEmail do
     |> assign(:update, update)
     |> assign(:cta_url, cta_url)
     |> assign(:cta_text, cta_text)
+    |> assign(:overview, construct_overview(update, goal))
     |> render("goal_check_in")
   end
 
@@ -38,4 +39,43 @@ defmodule OperatelyEmail.Emails.GoalCheckInEmail do
   defp check_in_url(company, check_in) do
     Paths.goal_check_in_path(company, check_in) |> Paths.to_url()
   end
+
+  defp construct_overview(update, goal) do
+    construct_overview_status(update) <> construct_overview_due_date(goal.timeframe.end_date)
+  end
+
+  defp construct_overview_status(%Update{status: "pending"}) do
+    "The goal is pending. Work has not started yet."
+  end
+
+  defp construct_overview_status(%Update{status: "on_track"}) do
+    "The goal is on track. 3 months until the deadline."
+  end
+
+  defp construct_overview_status(%Update{status: "caution"}) do
+    "The goal needs attention, due to emerging risks. Frank should be aware. 3 months until the deadline."
+  end
+
+  defp construct_overview_status(%Update{status: "issue"}) do
+    "The goal is at risk due to blockers or significant delays. Frank's help is needed. 3 months until the deadline."
+  end
+
+  defp construct_overview_due_date(due_date) do
+    days = DateTime.diff(due_date, DateTime.utc_now(), :days)
+
+    cond do
+      days == 0 -> "The due date is today."
+      days == 1 -> "The due date is tomorrow."
+      days == -1 -> "The due date was yesterday."
+      days < 0 -> human_duration(abs(days)) <> " overdue."
+      days > 0 -> human_duration(abs(days)) <> " until the deadline."
+    end
+  end
+
+  defp human_duration(n) when n == 1, do: "1 day"
+  defp human_duration(n) when n < 7, do: "#{n} days"
+  defp human_duration(n) when n == 7, do: "1 week"
+  defp human_duration(n) when n < 30, do: "#{div(n, 7)} weeks"
+  defp human_duration(n) when n == 30, do: "1 month"
+  defp human_duration(n) when n < 365, do: "#{div(n, 30)} months"
 end
