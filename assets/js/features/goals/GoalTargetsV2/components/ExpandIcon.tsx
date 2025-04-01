322c703d3f4ac420302051974143d441f02c339c
Igor Šarčević <igor@operately.com>
2025-04-01 22:04:45 +0200

    chore: Standardize the target chevron
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- assets/js/features/goals/GoalTargetsV2/components/ExpandIcon.tsx
+++ assets/js/features/goals/GoalTargetsV2/components/ExpandIcon.tsx
@@ -1,7 +1,16 @@
 import React from "react";
-import { IconChevronDown, IconChevronLeft, IconProps } from "@tabler/icons-react";
+import classNames from "classnames";
+import { IconChevronDown } from "@tabler/icons-react";
 
-export function ExpandIcon({ expanded, ...rest }: IconProps & { expanded: boolean }) {
-  const Icon = expanded ? IconChevronDown : IconChevronLeft;
-  return <Icon {...rest} />;
+interface Props {
+  expanded: boolean;
+  className?: string;
+
+  onClick: () => void;
+}
+
+export function ExpandIcon({ expanded, onClick, className }: Props) {
+  className = classNames(className, "mt-2 transition-all", {"rotate-180": expanded })
+
+  return <IconChevronDown size={14} className={className} onClick={onClick} />;
 }
