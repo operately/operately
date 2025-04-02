d72b62ef983fe14a46fc440b454cde1d9b1543a3
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2025-04-02 12:38:41 -0300

    chore: Add DeleteGoal mutation (#2223)
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- lib/operately/goals/permissions.ex
+++ lib/operately/goals/permissions.ex
@@ -8,6 +8,7 @@ defmodule Operately.Goals.Permissions do
     :can_close,
     :can_archive,
     :can_reopen,
+    :can_delete,
   ]
 
   def calculate(access_level) do
@@ -18,6 +19,7 @@ defmodule Operately.Goals.Permissions do
       can_reopen: can_edit(access_level),
       can_archive: can_archive(access_level),
       can_close: can_edit(access_level),
+      can_delete: can_delete(access_level),
     }
   end
 
@@ -26,6 +28,7 @@ defmodule Operately.Goals.Permissions do
   def can_check_in(access_level), do: access_level >= Binding.full_access()
   def can_edit(access_level), do: access_level >= Binding.edit_access()
   def can_reopen(access_level), do: access_level >= Binding.edit_access()
+  def can_delete(access_level), do: access_level >= Binding.edit_access()
 
   def check(access_level, permission) when is_atom(permission) and is_integer(access_level) do
     permissions = calculate(access_level)
