e0c30a99f33f0da3386024f6e31bc1f4d92c9e5d
Igor Šarčević <igor@operately.com>
2025-04-08 21:57:11 +0200

    Add styles from the app
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- turboui/.storybook/preview.ts
+++ turboui/.storybook/preview.ts
@@ -1,3 +1,4 @@
+import React from "react";
 import type { Preview } from "@storybook/react";
 import "./global.css";
 
@@ -10,7 +11,23 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    backgrounds: {
+      default: 'light',
+      values: [
+        {
+          name: 'light',
+          value: 'rgba(254, 245, 237, 1)',
+        },
+      ],
+    },
   },
+  decorators: [
+    (Story) => (
+      <div className="light antialiased">
+        <Story />
+      </div>
+    ),
+  ],
 };
 
 export default preview;
--- turboui/tailwind.config.js
+++ turboui/tailwind.config.js
@@ -1,5 +1,11 @@
+// See the Tailwind configuration guide for advanced usage
+// https://tailwindcss.com/docs/configuration
+
+const plugin = require("tailwindcss/plugin");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
     ".storybook/**/*.{js,ts,jsx,tsx}",
@@ -7,17 +13,36 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'accent-1': 'var(--color-accent-1)',
-        'accent-1-light': 'var(--color-accent-1-light)',
-        'surface-base': 'var(--color-surface-base)',
-        'surface-accent': 'var(--color-surface-accent)',
-        'surface-outline': 'var(--color-surface-outline)',
-        'content-base': 'var(--color-content-base)',
-        'content-dimmed': 'var(--color-content-dimmed)',
-        'content-subtle': 'var(--color-content-subtle)',
-        'white-1': 'var(--color-white-1)',
-      },
-    },
+        "surface-bg": "var(--color-surface-bg)",
+        "surface-bg-highlight": "var(--color-surface-bg-highlight)",
+        "surface-base": "var(--color-surface-base)",
+        "surface-dimmed": "var(--color-surface-dimmed)",
+        "surface-outline": "var(--color-surface-outline)",
+        "surface-accent": "var(--color-surface-accent)",
+        "surface-highlight": "var(--color-surface-highlight)",
+
+        "content-base": "var(--color-content)",
+        "content-accent": "var(--color-content-accent)",
+        "content-dimmed": "var(--color-content-dimmed)",
+        "content-subtle": "var(--color-content-subtle)",
+        "content-error": "var(--color-content-error)",
+
+        "stroke-base": "var(--color-stroke-base)",
+        "stroke-dimmed": "var(--color-stroke-dimmed)",
+
+        "link-base": "var(--color-link-base)",
+        "link-hover": "var(--color-link-hover)",
+
+        "toggle-active": "var(--color-toggle-active)",
+
+        "accent-1": "var(--color-accent-1)",
+        "accent-1-light": "var(--color-accent-1-light)",
+
+        "callout-info": "var(--color-callout-info)",
+        "callout-info-icon": "var(--color-callout-info-icon)",
+        "callout-info-message": "var(--color-callout-info-message)",
+      }
+    }
   },
   plugins: [],
 }
