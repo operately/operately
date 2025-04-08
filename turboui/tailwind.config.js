74652dafb05505c3bd2ac4d51fc60bc56a051fee
Igor Šarčević <igor@operately.com>
2025-04-08 23:05:11 +0200

    Use proper colors
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- turboui/tailwind.config.js
+++ turboui/tailwind.config.js
@@ -41,8 +41,48 @@ module.exports = {
         "callout-info": "var(--color-callout-info)",
         "callout-info-icon": "var(--color-callout-info-icon)",
         "callout-info-message": "var(--color-callout-info-message)",
-      }
-    }
+
+        "callout-warning": "var(--color-callout-warning)",
+        "callout-warning-icon": "var(--color-callout-warning-icon)",
+        "callout-warning-message": "var(--color-callout-warning-message)",
+
+        "callout-error": "var(--color-callout-error)",
+        "callout-error-icon": "var(--color-callout-error-icon)",
+        "callout-error-message": "var(--color-callout-error-message)",
+
+        "callout-success": "var(--color-callout-success)",
+        "callout-success-icon": "var(--color-callout-success-icon)",
+        "callout-success-message": "var(--color-callout-success-message)",
+
+        brand: {
+          1: "#3185FF",
+          2: "#E3F2FF",
+        },
+
+        dark: {
+          1: "rgba(30,30,34,1)",
+          2: "rgba(35,35,39,1)",
+          3: "rgba(43,45,49,1)",
+          4: "rgba(49,51,56,1)",
+          5: "rgba(60,60,64,1)",
+          6: "rgba(65,65,69,1)",
+          7: "rgba(73,75,79,1)",
+          8: "rgba(79,81,86,1)",
+        },
+
+        shade: {
+          1: "rgba(255,255,255,0.05)",
+          2: "rgba(255,255,255,0.1)",
+          3: "rgba(255,255,255,0.2)",
+        },
+
+        white: {
+          1: "rgba(255,255,255,1.00)",
+          2: "rgba(255,255,255,0.50)",
+          3: "rgba(255,255,255,0.25)",
+        },
+      },
+    },
   },
   plugins: [],
 }
