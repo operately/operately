8cd17141ed73afb43eafb8f263f5c170ba742f09
Adriano Lazzaretti <lazzaretti136@gmail.com>
2025-04-09 18:41:16 -0300

    Link updated
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- turboui/src/index.tsx
+++ turboui/src/index.tsx
@@ -4,5 +4,6 @@ export * from "./Button";
 export * from "./Link";
 export * from "./Menu";
 export * from "./StatusBadge";
-export * from "./ProgressBar"
 export * from "./Chronometer";
+export * from "./ProgressBar";
+export * from "./Avatar";
