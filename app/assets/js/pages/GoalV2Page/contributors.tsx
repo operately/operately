11e83255b709ddb126c93506e38423a84ef833d8
Adriano Lazzaretti <lazzaretti136@gmail.com>
2025-04-09 18:45:31 -0300

    app imports AvatarList from turboui
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- app/assets/js/pages/GoalV2Page/contributors.tsx
+++ app/assets/js/pages/GoalV2Page/contributors.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 import Forms from "@/components/Forms";
-import AvatarList from "@/components/AvatarList";
 import { AvatarLink } from "@/components/Avatar";
 import { useIsViewMode } from "@/components/Pages";
 import plurarize from "@/utils/plurarize";
+import { AvatarList } from "turboui";
 
 import { useLoadedData } from "./loader";
 import { DisableInEditMode, Title } from "./components";
