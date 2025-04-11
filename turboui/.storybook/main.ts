69a83a5d446d933202a152cce76740f5d0a41b6b
Igor Šarčević <igor@operately.com>
2025-04-11 13:56:32 +0200

    chore: Add a light/dark toggle to the storybook (#2269)
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- turboui/.storybook/main.ts
+++ turboui/.storybook/main.ts
@@ -1,27 +1,30 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-essentials",
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    {
+      name: "@storybook/addon-essentials",
+      options: {
+        backgrounds: false,
+      },
+    },
     "@storybook/addon-interactions",
+    "@storybook/addon-themes",
   ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
   },
-  "core": {
-    "builder": "@storybook/builder-vite"
+  core: {
+    builder: "@storybook/builder-vite",
   },
   viteFinal: async (config) => {
     config.css = {
       postcss: true,
     };
     return config;
-  }
+  },
 };
 
 export default config;
