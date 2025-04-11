b99161e55a90c63245687f007e993c130f542df8
Igor Šarčević <igor@operately.com>
2025-04-11 22:44:02 +0200

    chore: Watch files in related folder (#2278)
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- app/vite.config.mjs
+++ app/vite.config.mjs
@@ -40,6 +40,7 @@ export default defineConfig({
   server: {
     watch: {
       usePolling: false,
+      ignored: ['**/node_modules/**', '**/.git/**', '!../turboui/src/**']
     },
     hmr: true,
     port: 4005,
