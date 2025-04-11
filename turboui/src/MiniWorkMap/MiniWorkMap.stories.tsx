1db8d5654a98ffe342b300ef2bbefabeda117e67
Igor Šarčević <igor@operately.com>
2025-04-11 15:57:47 +0200

    chore: Extract mini work map to turboui (#2271)
    Signed-off-by: Igor Šarčević <igor@operately.com>

---
+++ turboui/src/MiniWorkMap/MiniWorkMap.stories.tsx
@@ -0,0 +1,73 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { MiniWorkMap } from '.';
+
+const meta = {
+  title: 'Components/MiniWorkMap',
+  component: MiniWorkMap,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof MiniWorkMap>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    items: [
+      { 
+        id: 'goal-a', 
+        name: 'Goal A', 
+        type: "goal", 
+        link: "#", 
+        completed: false, 
+        progress: 0,
+        people: [],
+        status: "on_track",
+        subitems: [
+          {
+            id: 'goal-b', 
+            name: 'Goal B', 
+            type: "goal", 
+            link: "#", 
+            completed: false, 
+            progress: 10,
+            people: [],
+            subitems: [],
+            status: "caution"
+          },
+          {
+            id: 'project-a',
+            name: 'Project A', 
+            type: "project",
+            link: "#", 
+            completed: false, 
+            progress: 50,
+            people: [],
+            subitems: [],
+            status: "on_track"
+          },
+          {
+            id: 'project-b',
+            name: 'Project B', 
+            type: "project",
+            link: "#", 
+            completed: true,
+            progress: 100,
+            people: [],
+            subitems: [],
+            status: "issue"
+          }
+        ] 
+      }
+    ]
+  },
+  decorators: [
+    (Story) => (
+      <div className="w-[500px]">
+        <Story />
+      </div>
+    ),
+  ],
+};
---
+++ turboui/src/MiniWorkMap/types.tsx
@@ -0,0 +1,21 @@
+interface WorkItemPerson {
+  id: string;
+  name: string;
+  avatarUrl: string;
+}
+
+export interface WorkItem {
+  id: string;
+  type: "goal" | "project";
+  status: "on_track" | "caution" | "concern" | "issue" | "paused" | "outdated" | "pending";
+  name: string;
+  link: string;
+  progress: number;
+  subitems: WorkItem[];
+  completed: boolean;
+  people: WorkItemPerson[];
+}
+
+export interface MiniWorkMapProps {
+  items: WorkItem[];
+}
