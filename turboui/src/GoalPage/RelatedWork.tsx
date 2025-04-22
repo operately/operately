b9af5f365d4a8867a7e88698788e8720f1a84dfb
Igor Šarčević <igor@operately.com>
2025-04-22 18:20:14 +0200

    chore: Optimize zero state buttons for goals (#2319)
    * chore: Optimize zero state buttons
    
    Signed-off-by: Igor Šarčević <igor@operately.com>
    
    * chore: Reduce zero-state for non-editors
    
    Signed-off-by: Igor Šarčević <igor@operately.com>
    
    ---------
    
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- turboui/src/GoalPage/RelatedWork.tsx
+++ turboui/src/GoalPage/RelatedWork.tsx
@@ -4,20 +4,26 @@ import { GoalPage } from ".";
 import { SectionHeader } from "./SectionHeader";
 
 export function RelatedWork(props: GoalPage.Props) {
+  if (props.relatedWorkItems.length === 0 && !props.canEdit) return null;
+
   return (
     <div>
       <SectionHeader
-        title="Related Work"
+        title="Subgoals & Projects"
         buttons={
           <div className="flex items-center gap-2">
-            <SecondaryButton size="xxs">Add subgoal</SecondaryButton>
-            <SecondaryButton size="xxs">Add project</SecondaryButton>
+            <SecondaryButton size="xxs" onClick={() => {}}>
+              Add goal
+            </SecondaryButton>
+            <SecondaryButton size="xxs" onClick={() => {}}>
+              Add project
+            </SecondaryButton>
           </div>
         }
-        showButtons={props.relatedWorkItems.length > 0 && props.canEdit}
+        showButtons={props.canEdit}
       />
 
-      {props.relatedWorkItems.length > 0 ? <RelatedWorkContent {...props} /> : <RelatedWorkZeroState {...props} />}
+      {props.relatedWorkItems.length > 0 ? <RelatedWorkContent {...props} /> : <RelatedWorkZeroState />}
     </div>
   );
 }
@@ -30,21 +36,10 @@ function RelatedWorkContent(props: GoalPage.Props) {
   );
 }
 
-function RelatedWorkZeroState(props: GoalPage.Props) {
+function RelatedWorkZeroState() {
   return (
     <div className="mt-1">
-      <div className="text-content-dimmed text-sm">
-        {props.canEdit
-          ? "Break down the work on this goal into subgoals and projects."
-          : "Connections to supporting projects and subgoals will appear here."}
-      </div>
-
-      {props.canEdit && (
-        <div className="mt-2 flex items-center gap-2">
-          <SecondaryButton size="xs">Add subgoal</SecondaryButton>
-          <SecondaryButton size="xs">Add project</SecondaryButton>
-        </div>
-      )}
+      <div className="text-content-dimmed text-sm">Break down the work on this goal into subgoals and projects.</div>
     </div>
   );
 }
