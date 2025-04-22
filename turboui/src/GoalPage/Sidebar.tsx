bc1cf18e9e3ac64a0e12c1557ad2a321ec674f5c
Igor Šarčević <igor@operately.com>
2025-04-22 22:42:01 +0200

    chore: Handle missing reviewer on goals
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- turboui/src/GoalPage/Sidebar.tsx
+++ turboui/src/GoalPage/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { IconQuestionMark } from "@tabler/icons-react";
 import { GoalPage } from ".";
 import { Avatar, AvatarPerson } from "../Avatar";
 import { Chronometer } from "../Chronometer";
@@ -14,8 +15,8 @@ export function Sidebar(props: GoalPage.Props) {
       <div className="space-y-3">
         <div className="font-bold">Team</div>
 
-        {props.champion && <Contributor person={props.champion} description="Champion" />}
-        {props.reviewer && <Contributor person={props.reviewer} description="Reviewer" />}
+        <Champion {...props} />
+        <Reviewer {...props} />
       </div>
 
       {props.contributors.length > 0 && (
@@ -29,6 +30,44 @@ export function Sidebar(props: GoalPage.Props) {
       )}
     </div>
   );
+}
+
+function Champion(props: GoalPage.Props) {
+  if (!props.champion) {
+    const message = props.canEdit ?
+      "Assign a champion to get started" :
+      "No champion assigned";
+
+    return <MissingContributor role="No Champion" description={message} />;
+  } else {
+    return <Contributor person={props.champion} description="Champion" />;
+  }
+}
+
+function Reviewer(props: GoalPage.Props) {
+  if (!props.canEdit && !props.reviewer) return null;
+
+  if (!props.reviewer) {
+    return <MissingContributor role="No Reviewer" description="Assign a reviewer to get feedback" />;
+  } else {
+    return <Contributor person={props.reviewer} description="Reviewer" />;
+  }
+}
+
+function MissingContributor({ role, description }: { role: string, description: string }) {
+  return <div className="flex items-start gap-2 truncate">
+    <div className="bg-yellow-500/10 rounded-full h-[32px] w-[32px] flex items-center justify-center">
+      <IconQuestionMark
+        className="text-yellow-800"
+        size={20}
+      />
+    </div>
+
+    <div className="-mt-0.5 truncate">
+      <div className="text-sm font-medium">{role}</div>
+      <div className="text-xs truncate">{description}</div>
+    </div>
+  </div>
 }
 
 function Contributor({ person, description }: { person: AvatarPerson; description: string }) {
