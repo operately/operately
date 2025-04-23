2957f5deb69cb02480d622574b6d818cdd0d6d1d
Igor Šarčević <igor@operately.com>
2025-04-23 11:40:32 +0200

    chore: Extact state handler from goal target list (#2325)
    Signed-off-by: Igor Šarčević <igor@operately.com>

---
+++ turboui/src/GoalTargetList/useGoalTargetListState.tsx
@@ -0,0 +1,61 @@
+import { GoalTargetList } from ".";
+import { useListState } from "../utils/useListState";
+
+export type TargetState = GoalTargetList.Target & {
+  expanded: boolean;
+  editButtonVisible: boolean;
+};
+
+export interface State {
+  targets: TargetState[];
+
+  toggleExpand: (id: string) => void;
+
+  startEditing: (id: string) => void;
+  cancelEdit: (id: string) => void;
+  saveEdit: (id: string, newTarget: Partial<GoalTargetList.Target>) => void;
+
+  deleteTarget: (id: string) => void;
+  reorder: (item: any, targetId: string, indexInDropZone: number) => void;
+}
+
+export function useGoalTargetListState(props: GoalTargetList.Props): State {
+  const [targets, { update, remove, reorder }] = useListState<TargetState>(() => {
+    return props.targets.map((t) => ({
+      ...t,
+      editButtonVisible: !!props.showEditButton,
+      expanded: false,
+    }));
+  });
+
+  const state: State = {
+    targets,
+
+    toggleExpand: (id: string) => {
+      update(id, (t) => ({ ...t, expanded: !t.expanded }));
+    },
+
+    startEditing: (id: string) => {
+      update(id, (t) => ({ ...t, mode: "edit" as const }));
+    },
+
+    cancelEdit: (id: string) => {
+      update(id, (t) => ({ ...t, mode: "view" as const }));
+    },
+
+    saveEdit: (id: string, newTarget: Partial<GoalTargetList.Target>) => {
+      update(id, (t) => ({ ...t, ...newTarget, mode: "view" as const }));
+    },
+
+    deleteTarget: (id: string) => {
+      remove(id);
+    },
+
+    // Implements the interface from utils/DragAndDrop
+    reorder: (_: any, id: string, newIndex: number) => {
+      reorder(id, newIndex);
+    },
+  };
+
+  return state;
+}
