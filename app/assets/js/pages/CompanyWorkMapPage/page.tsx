d3731d078530ad22dfb3c5605e4a7678d4e5492d
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2025-05-06 11:32:54 -0300

    fix: Don't reload WorkMap data when tab changes (#2389)
    * fix: Don't reload WorkMap data when tab changes
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    * Update make gen
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    * Fix linter
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    ---------
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

---
+++ app/assets/js/pages/CompanyWorkMapPage/page.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+import { Page as PageContainer, WorkMap } from "turboui";
+import { useLoadedData } from "./loader";
+
+export function Page() {
+  const { workMap } = useLoadedData();
+  const title = "Company Work Map";
+
+  return (
+    <div className="py-6 px-2">
+      <PageContainer title={title} size="fullwidth">
+        <WorkMap title={title} items={workMap as WorkMap.Item[]} />
+      </PageContainer>
+    </div>
+  );
+}
