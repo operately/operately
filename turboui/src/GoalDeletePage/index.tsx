c72d9c8f9be22996dba82b909148e0d4a1bf1423
Igor Šarčević <igor@operately.com>
2025-05-07 09:23:59 +0200

    feat: Bootstrap page state that is not deletable
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- turboui/src/GoalDeletePage/index.tsx
+++ turboui/src/GoalDeletePage/index.tsx
@@ -1,7 +1,7 @@
-import { useForm } from "react-hook-form";
-import { Page } from "../Page";
 import { DangerButton, SecondaryButton } from "../Button";
 import { WarningCallout } from "../Callouts";
+import { MiniWorkMap } from "../MiniWorkMap";
+import { Page } from "../Page";
 
 namespace GoalDeletePage {
   export interface Props {
@@ -15,6 +15,8 @@ namespace GoalDeletePage {
 
     onSubmit: (id: string) => void;
     onCancel: () => void;
+
+    subitems: MiniWorkMap.WorkItem[];
   }
 
   export interface FormValues {
@@ -34,22 +36,41 @@ export function GoalDeletePage(props: GoalDeletePage.Props) {
       <div className="p-12 sm:px-24">
         <h1 className="text-2xl font-bold mb-6">Delete {props.goalName}</h1>
 
-        <form className="space-y-6" onSubmit={() => props.onSubmit(props.goalId)}>
-          <WarningCallout
-            message="This action cannot be undone"
-            description={`Deleting a goal is permanent and cannot be undone. Please confirm that you want to delete the ${props.goalName} goal.`}
-          />
-
-          <div className="flex items-center gap-2">
-            <DangerButton size="sm" type="submit">
-              Delete Forever
-            </DangerButton>
-            <SecondaryButton size="sm" onClick={props.onCancel}>
-              Cancel
-            </SecondaryButton>
-          </div>
-        </form>
+        {props.subitems.length > 0 ? <CantDeleteHasSubitems {...props} /> : <DeleteForm {...props} />}
       </div>
     </Page>
+  );
+}
+
+function CantDeleteHasSubitems(props: GoalDeletePage.Props) {
+  return (
+    <div className="space-y-6">
+      <WarningCallout
+        message="This goal cannot be deleted"
+        description="Please re-connect to another goal or delete the subitems before deleting."
+      />
+
+      <MiniWorkMap items={props.subitems} />
+    </div>
+  );
+}
+
+function DeleteForm(props: GoalDeletePage.Props) {
+  return (
+    <form className="space-y-6" onSubmit={() => props.onSubmit(props.goalId)}>
+      <WarningCallout
+        message="This action cannot be undone"
+        description={`Deleting a goal is permanent and cannot be undone. Please confirm that you want to delete the ${props.goalName} goal.`}
+      />
+
+      <div className="flex items-center gap-2">
+        <DangerButton size="sm" type="submit">
+          Delete Forever
+        </DangerButton>
+        <SecondaryButton size="sm" onClick={props.onCancel}>
+          Cancel
+        </SecondaryButton>
+      </div>
+    </form>
   );
 }
