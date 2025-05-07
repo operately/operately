a0041369d7ee825fbcfd23c74f00d59351294bf8
Igor Šarčević <igor@operately.com>
2025-05-07 09:44:36 +0200

    feat: Disconect conected before deleting
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- turboui/src/GoalDeletePage/index.tsx
+++ turboui/src/GoalDeletePage/index.tsx
@@ -34,8 +34,6 @@ export function GoalDeletePage(props: GoalDeletePage.Props) {
   return (
     <Page title={["Edit Goal"]} size="medium" navigation={navigation}>
       <div className="p-12 sm:px-24">
-        <h1 className="text-2xl font-bold mb-6">Delete {props.goalName}</h1>
-
         {props.subitems.length > 0 ? <CantDeleteHasSubitems {...props} /> : <DeleteForm {...props} />}
       </div>
     </Page>
@@ -44,33 +42,44 @@ export function GoalDeletePage(props: GoalDeletePage.Props) {
 
 function CantDeleteHasSubitems(props: GoalDeletePage.Props) {
   return (
-    <div className="space-y-6">
-      <WarningCallout
-        message="This goal cannot be deleted"
-        description="Please re-connect to another goal or delete the subitems before deleting."
-      />
+    <div>
+      <h1 className="text-2xl font-bold mb-1">Remove connections before deleting</h1>
+      <p className="mb-6">
+        You need to disconnect or reassign all connected goals and projects before you can delete this goal.
+      </p>
 
+      <div className="font-bold mb-2">Connected items</div>
       <MiniWorkMap items={props.subitems} />
+
+      <div className="flex items-center gap-2 mt-8">
+        <DangerButton size="sm">Delete Forever</DangerButton>
+        <SecondaryButton size="sm" onClick={props.onCancel}>
+          Cancel
+        </SecondaryButton>
+      </div>
     </div>
   );
 }
 
 function DeleteForm(props: GoalDeletePage.Props) {
   return (
-    <form className="space-y-6" onSubmit={() => props.onSubmit(props.goalId)}>
-      <WarningCallout
-        message="This action cannot be undone"
-        description={`Deleting a goal is permanent and cannot be undone. Please confirm that you want to delete the ${props.goalName} goal.`}
-      />
+    <div>
+      <h1 className="text-2xl font-bold mb-6">Delete {props.goalName}</h1>
+      <form className="space-y-6" onSubmit={() => props.onSubmit(props.goalId)}>
+        <WarningCallout
+          message="This action cannot be undone"
+          description={`Deleting a goal is permanent and cannot be undone. Please confirm that you want to delete the ${props.goalName} goal.`}
+        />
 
-      <div className="flex items-center gap-2">
-        <DangerButton size="sm" type="submit">
-          Delete Forever
-        </DangerButton>
-        <SecondaryButton size="sm" onClick={props.onCancel}>
-          Cancel
-        </SecondaryButton>
-      </div>
-    </form>
+        <div className="flex items-center gap-2">
+          <DangerButton size="sm" type="submit">
+            Delete Forever
+          </DangerButton>
+          <SecondaryButton size="sm" onClick={props.onCancel}>
+            Cancel
+          </SecondaryButton>
+        </div>
+      </form>
+    </div>
   );
 }
