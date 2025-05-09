320ef1a061753892b90f6d8eb17341b761693371
Igor Šarčević <igor@operately.com>
2025-05-09 12:33:51 +0200

    feat: Execute questions based on the workmap data
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- app/lib/operately/ai.ex
+++ app/lib/operately/ai.ex
@@ -6,30 +6,34 @@ defmodule Operately.AI do
   alias LangChain.Utils.ChainResult
   alias Operately.WorkMaps.GetWorkMapQuery
 
-  def run(person) do
-    prompt = """
-    You are a COO of a company. You are responsible for overseeing the company's
-    operations and ensuring that everything runs smoothly. You have access to a
-    work map that contains all the goals and projects of the company.
-
-    How many active projects do we have in the company?
+  def coo_prompt(question) do
     """
+    Act as a Chief Operating Officer (COO) of a company. You are responsible for
+    overseeing the company's operations and ensuring that everything runs
+    smoothly. You have access to a work map that contains all the goals and
+    projects of the company.
 
-    {:ok, updated_chain} =
-      LLMChain.new!(%{llm: ChatAnthropic.new!(), verbose: true})
+    #{question}
+    """
+  end
+
+  def run(person, prompt) do
+    {:ok, chain} =
+      LLMChain.new!(%{llm: ChatAnthropic.new!(), custom_context: %{}})
       |> LLMChain.add_tools(work_map_fn(person))
       |> LLMChain.add_message(Message.new_user!(prompt))
       |> LLMChain.run(mode: :while_needs_response)
 
-    IO.puts(ChainResult.to_string!(updated_chain))
+    ChainResult.to_string!(chain)
   end
 
   defp work_map_fn(person) do
     Function.new!(%{
       name: "get_work_map",
       description: "Returns all goals and projects for a given person.",
-      function: fn _ ->
-        {:ok, GetWorkMapQuery.execute(person, %{company_id: person.company_id})}
+      function: fn _, _ ->
+        {:ok, workmap} = GetWorkMapQuery.execute(person, %{company_id: person.company_id})
+        {:ok, inspect(workmap)}
       end
     })
   end
