43eea3a9b1f45f3d94dc9be1b98a347b0eda6201
Igor Šarčević <igor@operately.com>
2025-05-12 10:58:35 +0200

    feat: Integrate turboui goal page with dummy data
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- app/assets/js/pages/GoalV3Page/index.tsx
+++ app/assets/js/pages/GoalV3Page/index.tsx
@@ -1,6 +1,6 @@
-import * as Pages from "@/components/Pages";
-import * as Paper from "@/components/PaperContainer";
-import * as React from "react";
+import React from "react";
+import { GoalPage } from "turboui";
+import { Timeframe } from "turboui/src/utils/timeframes";
 
 interface LoaderResult {}
 
@@ -9,15 +9,37 @@ export async function loader({}): Promise<LoaderResult> {
 }
 
 export function Page() {
-  const data = Pages.useLoadedData<LoaderResult>();
+  const props: GoalPage.Props = {
+    targets: [],
+    checkIns: [],
+    messages: [],
+    contributors: [],
+    relatedWorkItems: [],
+    canEdit: false,
+    description: "",
+    status: "pending",
+    timeframe: {
+      startDate: new Date(),
+      endDate: new Date(),
+      type: "days" as const,
+    },
+    spaceLink: "",
+    workmapLink: "",
+    closeLink: "",
+    deleteLink: "",
+    parentGoal: null,
+    goalName: "Example Goal",
+    spaceName: "Example Space",
+    privacyLevel: "internal",
+    updateTimeframe: function (timeframe: Timeframe): Promise<void> {
+      console.log("updateTimeframe", timeframe);
+      throw new Error("Function not implemented.");
+    },
+  };
 
   return (
-    <Pages.Page title={"GoalV3Page"}>
-      <Paper.Root>
-        <Paper.Body>
-          <div className="text-content-accent text-3xl font-extrabold">GoalV3Page</div>
-        </Paper.Body>
-      </Paper.Root>
-    </Pages.Page>
+    <div className="sm:mt-8">
+      <GoalPage {...props} />
+    </div>
   );
 }
