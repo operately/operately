fd06edafb461c4df55a5cedb79a87a2554711b7e
Igor Šarčević <igor@operately.com>
2025-05-14 10:25:44 +0200

    feat: Prepopulate last_update_status on goals
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- app/lib/operately/goals/goal.ex
+++ app/lib/operately/goals/goal.ex
@@ -26,7 +26,7 @@ defmodule Operately.Goals.Goal do
     # Check-Ins (they are called updates for historical reasons)
     has_many :updates, Operately.Goals.Update
     belongs_to :last_update, Operately.Goals.Update, foreign_key: :last_check_in_id
-    field :last_update_status, :string
+    field :last_update_status, Ecto.Enum, values: [:on_track, :concern, :caution, :issue, :pending]
     field :next_update_scheduled_at, :utc_datetime
 
     embeds_one :timeframe, Operately.Goals.Timeframe, on_replace: :delete
