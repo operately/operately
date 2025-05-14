df52c5c84d7cf7869345cefde991a1cd9405a10b
Igor Šarčević <igor@operately.com>
2025-05-14 16:11:47 +0200

    fix: Fix typo on company admin page
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- app/assets/js/pages/CompanyAdminPage/page.tsx
+++ app/assets/js/pages/CompanyAdminPage/page.tsx
@@ -1,26 +1,26 @@
-import * as React from "react";
-import * as Paper from "@/components/PaperContainer";
 import * as Pages from "@/components/Pages";
+import * as Paper from "@/components/PaperContainer";
 import * as Icons from "@tabler/icons-react";
+import * as React from "react";
 
-import { NavigationBackToLobby } from "./NavigationBackToLobby";
-import { CompanyAdmins, CompanyOwners } from "./CompanyAdmins";
-import { useLoadedData } from "./loader";
-import { OptionsMenuItem } from "./OptionsMenu";
+import { useMe } from "@/contexts/CurrentCompanyContext";
 import { Paths, includesId } from "@/routes/paths";
 import { Link } from "turboui";
-import { useMe } from "@/contexts/CurrentCompanyContext";
+import { CompanyAdmins, CompanyOwners } from "./CompanyAdmins";
+import { useLoadedData } from "./loader";
+import { NavigationBackToLobby } from "./NavigationBackToLobby";
+import { OptionsMenuItem } from "./OptionsMenu";
 
 export function Page() {
   const { company } = useLoadedData();
 
   return (
-    <Pages.Page title={[company.name!, "Admininstration"]} testId="company-admin-page">
+    <Pages.Page title={[company.name!, "Administration"]} testId="company-admin-page">
       <Paper.Root size="small">
         <NavigationBackToLobby />
 
         <Paper.Body minHeight="none">
-          <div className="uppercase text-sm tracking-wide">Company Admininstration</div>
+          <div className="uppercase text-sm tracking-wide">Company Administration</div>
           <div className="text-content-accent text-3xl font-extrabold">{company.name}</div>
 
           <Paper.Section title="What's this?">
