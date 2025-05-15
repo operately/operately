35226090072f67317312a6e82b127ce98a587de5
Adriano Lazzaretti <lazzaretti136@gmail.com>
2025-05-15 17:55:13 -0300

    Creating Goal Discussion also creates subscriptions
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- app/assets/js/pages/GoalDiscussionNewPage/Form.tsx
+++ app/assets/js/pages/GoalDiscussionNewPage/Form.tsx
@@ -8,12 +8,18 @@ import { FormTitleInput } from "@/components/FormTitleInput";
 
 import { Paths } from "@/routes/paths";
 import { assertPresent } from "@/utils/assertions";
-import { SubscribersSelector, useSubscriptions } from "@/features/Subscriptions";
+import { SubscribersSelector, SubscriptionsState, useSubscriptions } from "@/features/Subscriptions";
 
 import { useForm } from "./useForm";
 
 export function Form({ goal }: { goal: Goals.Goal }) {
-  const form = useForm({ goal });
+  assertPresent(goal.potentialSubscribers, "potentialSubscribers must be present in goal");
+
+  const subscriptionsState = useSubscriptions(goal.potentialSubscribers, {
+    ignoreMe: true,
+    notifyPrioritySubscribers: true,
+  });
+  const form = useForm({ goal, subscriptionsState });
 
   return (
     <>
@@ -28,7 +34,7 @@ export function Form({ goal }: { goal: Goals.Goal }) {
         <TipTapEditor.StandardEditorForm editor={form.fields.editor.editor} />
       </div>
 
-      <Subscribers goal={goal} />
+      <Subscribers goal={goal} subscriptionsState={subscriptionsState} />
 
       <div className="flex items-center gap-4 mt-4">
         <PrimaryButton testId="post-discussion" onClick={form.submit} loading={form.submitting}>
@@ -41,14 +47,8 @@ export function Form({ goal }: { goal: Goals.Goal }) {
   );
 }
 
-function Subscribers({ goal }: { goal: Goals.Goal }) {
-  assertPresent(goal.potentialSubscribers, "potentialSubscribers must be present in goal");
+function Subscribers({ goal, subscriptionsState }: { goal: Goals.Goal; subscriptionsState: SubscriptionsState }) {
   assertPresent(goal.space, "space must be present in goal");
-
-  const subscriptionsState = useSubscriptions(goal.potentialSubscribers, {
-    ignoreMe: true,
-    notifyPrioritySubscribers: true,
-  });
 
   return (
     <div className="my-10">
