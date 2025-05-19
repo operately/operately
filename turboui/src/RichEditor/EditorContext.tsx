7b1af01e20769e6fb5a376d1f67f141c57f081d7
Igor Šarčević <igor@operately.com>
2025-05-19 11:12:47 +0200

    chore: Move rich content to turboui
    Signed-off-by: Igor Šarčević <igor@operately.com>

---
+++ turboui/src/RichEditor/EditorContext.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export interface Context {
+  editor: any;
+  linkEditActive: boolean;
+  setLinkEditActive: (active: boolean) => void;
+}
+
+export const EditorContext = React.createContext<Context>({} as Context);
