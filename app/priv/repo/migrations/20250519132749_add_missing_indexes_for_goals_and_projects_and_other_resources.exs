912382337ac9c27dba91e73c040249cd839aae03
Adriano Lazzaretti <lazzaretti136@gmail.com>
2025-05-19 10:49:01 -0300

    Add missing indexes
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

---
+++ app/priv/repo/migrations/20250519132749_add_missing_indexes_for_goals_and_projects_and_other_resources.exs
@@ -0,0 +1,17 @@
+defmodule Operately.Repo.Migrations.AddMissingIndexesForGoalsAndProjectsAndOtherResources do
+  use Ecto.Migration
+
+  def change do
+    create index(:goals, [:company_id])
+    create index(:goals, [:champion_id])
+    create index(:goals, [:reviewer_id])
+    create index(:goals, [:creator_id])
+
+    create index(:projects, [:creator_id])
+    create index(:project_contributors, [:project_id, :role], name: :project_contributors_project_id_role_idx)
+
+    create index(:groups, [:company_id])
+    create index(:blobs, [:company_id])
+    create index(:access_bindings, [:group_id, :access_level], name: :access_bindings_group_id_access_level_idx)
+  end
+end
