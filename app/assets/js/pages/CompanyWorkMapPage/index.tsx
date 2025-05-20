425a2a7444fdfd6c821c765569680a8d82c9ed02
Adriano Lazzaretti <lazzaretti136@gmail.com>
2025-05-20 16:48:28 -0300

    Cache works in Work Map pages
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- app/assets/js/pages/CompanyWorkMapPage/index.tsx
+++ app/assets/js/pages/CompanyWorkMapPage/index.tsx
@@ -3,9 +3,4 @@ import { PageModule } from "@/routes/types";
 import { loader } from "./loader";
 import { Page } from "./page";
 
-/**
- * Prevents the loader from rerunning when only the search parameters change.
- * This ensures that when users change tabs via URL parameters, we don't reload the data.
- */
-
 export default { name: "CompanyWorkMapPage", loader, Page } as PageModule;
