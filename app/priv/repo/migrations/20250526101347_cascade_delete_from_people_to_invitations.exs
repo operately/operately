98822b8961405e35142850126993f201d9b3fa35
Igor Šarčević <igor@operately.com>
2025-05-26 12:16:06 +0200

    Cascade deletion from people -> invitations
    Signed-off-by: Igor Šarčević <igor@operately.com>

---
+++ app/priv/repo/migrations/20250526101347_cascade_delete_from_people_to_invitations.exs
@@ -0,0 +1,19 @@
+defmodule Operately.Repo.Migrations.CascadeDeleteFromPeopleToInvitations do
+  use Ecto.Migration
+
+  def up do
+    drop constraint(:invitations, :invitations_admin_id_fkey)
+
+    alter table(:invitations) do
+      modify :admin_id, references(:people, on_delete: :delete_all, type: :binary_id)
+    end
+  end
+
+  def down do
+    drop constraint(:invitations, :invitations_admin_id_fkey)
+
+    alter table(:activities) do
+      modify :admin_id, references(:people, on_delete: :nothing, type: :binary_id)
+    end
+  end
+end
