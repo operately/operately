147e79751e4d8f1dd42566870895bdd7d25d9c5d
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2025-06-06 12:27:48 -0300

    feat: Add navigation to Space Work Map page (#2631)
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- app/assets/js/pages/SpaceWorkMapPage/page.tsx
+++ app/assets/js/pages/SpaceWorkMapPage/page.tsx
@@ -2,11 +2,19 @@ import * as React from "react";
 
 import { WorkMapPage } from "turboui";
 import { useLoadedData } from "./loader";
+import { Paths } from "@/routes/paths";
 
 export function Page() {
   const { data } = useLoadedData();
   const { workMap, space } = data;
   const title = `${space.name} Work Map`;
 
-  return <WorkMapPage title={title} items={workMap} columnOptions={{ hideSpace: true }} />;
+  return (
+    <WorkMapPage
+      title={title}
+      items={workMap}
+      columnOptions={{ hideSpace: true }}
+      navigation={[{ to: Paths.spacePath(space.id), label: space.name }]}
+    />
+  );
 }
