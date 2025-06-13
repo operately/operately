5465ad91f1bb97a2bf79cb5e27a74f5a69965c20
Adriano Lazzaretti <lazzaretti136@gmail.com>
2025-06-13 06:11:38 -0300

    fix project creation tests
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- app/assets/js/pages/ProfilePage/loader.tsx
+++ app/assets/js/pages/ProfilePage/loader.tsx
@@ -1,21 +1,45 @@
-import * as Pages from "@/components/Pages";
 import * as People from "@/models/people";
+import * as WorkMap from "@/models/workMap";
+import { convertToWorkMapItem } from "@/models/workMap";
+
+import { PageCache } from "@/routes/PageCache";
+import { fetchAll } from "@/utils/async";
 
 interface LoaderResult {
-  person: People.Person;
-}
-
-export async function loader({ params }): Promise<LoaderResult> {
-  return {
-    person: await People.getPerson({
-      id: params.id,
-      includeManager: true,
-      includeReports: true,
-      includePeers: true,
-    }).then((data) => data.person!),
+  data: {
+    person: People.PersonWithLink;
+    workMap: ReturnType<typeof convertToWorkMapItem>[];
+    reviewerWorkMap: ReturnType<typeof convertToWorkMapItem>[];
   };
+  cacheVersion: number;
 }
 
-export function useLoadedData(): LoaderResult {
-  return Pages.useLoadedData() as LoaderResult;
+export async function loader({ params, refreshCache = false }): Promise<LoaderResult> {
+  const personId = params.id;
+
+  return PageCache.fetch({
+    cacheKey: `v2-PersonalWorkMap-${personId}`,
+    refreshCache,
+    fetchFn: async () =>
+      fetchAll({
+        person: People.getPerson({
+          id: personId,
+          includeManager: true,
+          includeReports: true,
+          includePeers: true,
+          includePermissions: true,
+        }).then((data) => data.person!),
+        workMap: WorkMap.getFlatWorkMap({
+          championId: personId,
+          contributorId: personId,
+        }).then((d) => d.workMap?.map(convertToWorkMapItem) ?? []),
+        reviewerWorkMap: WorkMap.getFlatWorkMap({
+          reviewerId: personId,
+        }).then((d) => d.workMap?.map(convertToWorkMapItem) ?? []),
+      }),
+  });
+}
+
+export function useLoadedData() {
+  return PageCache.useData(loader).data;
 }
