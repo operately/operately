eab92f84e1bea3bc6442614d574707a52b4f879b
Igor Šarčević <igor@operately.com>
2025-06-17 17:47:18 +0200

    Fix generators
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- app/lib/mix/tasks/operation/gen_notification_handler.ex
+++ app/lib/mix/tasks/operation/gen_notification_handler.ex
@@ -2,7 +2,7 @@ defmodule Mix.Tasks.Operation.GenNotificationHandler do
   def gen(ctx) do
     Mix.Operately.generate_file(ctx.notification_handler_file_path, fn _ ->
       """
-      defmodule Operately.Activities.Notifications.#{ctx.activity_module_name} do
+      defmodule Operately.Activities.Notifications.#{ctx.activity_item_name} do
         def dispatch(_activity) do
           Operately.Notifications.bulk_create([])
         end
