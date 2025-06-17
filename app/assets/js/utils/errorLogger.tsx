a53935a43017440bbdef94c6ebf4e26313218fb1
Igor Šarčević <igor@operately.com>
2025-06-17 10:15:04 +0200

    chore: Add extra erorr logging for JS erorrs (#2717)
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- app/assets/js/utils/errorLogger.tsx
+++ app/assets/js/utils/errorLogger.tsx
@@ -1,21 +1,64 @@
 export function setupTestErrorLogger() {
   if (window.appConfig.environment === "test") {
     window.addEventListener("error", function (e) {
-      const stackLines = e.error.stack.split("\n");
+      log("");
+      log("");
+      log("Javascript error:");
+      log("  " + e.error.message);
 
-      console.log("                                ");
-      console.log("                                ");
-      console.log("     Javascript error:");
-      console.log("       " + e.error.message);
-      console.log("                                ");
-      console.log("     Stack trace:               ");
-      console.log("                                ");
+      logStackLines(e);
+    });
 
-      stackLines.forEach(function (line: string) {
-        console.log("       " + line);
-      });
+    window.addEventListener("unhandledrejection", function (e) {
+      log("");
+      log("");
+      log("Unhandled promise rejection:");
+      log("  " + e.reason);
 
-      return false;
+      logStackLines(e);
+      logAxiosErrorDetails(e);
     });
   }
+}
+
+function logStackLines(e: any) {
+  if (!e.error || !e.error.stack) {
+    console.log("No stack trace available.");
+    return;
+  }
+
+  log("");
+  log("Stack trace:");
+  log("");
+
+  e.error.stack.split("\n").forEach((line: string) => log(line));
+}
+
+function logAxiosErrorDetails(e: any) {
+  if (!e.reason && !e.reason.isAxiosError) {
+    return;
+  }
+
+  log("");
+  log("     AxiosError details:");
+
+  if (e.reason.config) {
+    log("URL: " + e.reason.config.url);
+    log("Method: " + e.reason.config.method);
+  }
+
+  if (e.reason.response) {
+    log("Status: " + e.reason.response.status);
+    log("Response data: " + JSON.stringify(e.reason.response.data));
+  }
+
+  if (e.reason.code) {
+    log("Code: " + e.reason.code);
+  }
+
+  log("");
+}
+
+function log(message: string) {
+  console.log("       " + message);
 }
