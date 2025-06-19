ad1fd8c88dd816f887320cd44fbc5a011bd1ad94
Igor Šarčević <igor@operately.com>
2025-06-19 12:26:24 +0200

    feat: Show champion update in the feed
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- app/lib/operately_web/api/serializers/activity_content/goal_champion_updating.ex
+++ app/lib/operately_web/api/serializers/activity_content/goal_champion_updating.ex
@@ -7,7 +7,12 @@ defimpl OperatelyWeb.Api.Serializable, for: Operately.Activities.Content.GoalCha
       space_id: Serializer.serialize(content["space_id"], level: :essential),
       goal_id: Serializer.serialize(content["goal_id"], level: :essential),
       old_champion_id: Serializer.serialize(content["old_champion_id"], level: :essential),
-      new_champion_id: Serializer.serialize(content["new_champion_id"], level: :essential)
+      new_champion_id: Serializer.serialize(content["new_champion_id"], level: :essential),
+      company: Serializer.serialize(content["company"], level: :essential),
+      space: Serializer.serialize(content["space"], level: :essential),
+      goal: Serializer.serialize(content["goal"], level: :essential),
+      old_champion: Serializer.serialize(content["old_champion"], level: :essential),
+      new_champion: Serializer.serialize(content["new_champion"], level: :essential)
     }
   end
 end
