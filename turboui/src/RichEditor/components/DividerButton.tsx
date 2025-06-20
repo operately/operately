dc356901866d15169e4399aa3f1e43ff3ca5cd79
Adriano Lazzaretti <lazzaretti136@gmail.com>
2025-06-20 14:01:48 -0300

    Optimize lucide icons
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- turboui/src/RichEditor/components/DividerButton.tsx
+++ turboui/src/RichEditor/components/DividerButton.tsx
@@ -1,12 +1,12 @@
 import * as React from "react";
-import { Minus } from "lucide-react";
 
+import { LucideMinus } from "../../icons";
 import { ToolbarButton } from "./ToolbarButton";
 
 export function DividerButton({ editor, iconSize }): JSX.Element {
   return (
     <ToolbarButton onClick={() => editor.chain().focus().setHorizontalRule().run()} title="Divider">
-      <Minus size={iconSize} />
+      <LucideMinus size={iconSize} />
     </ToolbarButton>
   );
 }
