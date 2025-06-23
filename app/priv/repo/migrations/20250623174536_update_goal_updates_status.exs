72fc88797b6a0cb98f6474e06b8a178e293acb70
Adriano Lazzaretti <lazzaretti136@gmail.com>
2025-06-23 13:57:31 -0300

    chore: Standardize goal updates status
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

---
+++ app/priv/repo/migrations/20250623174536_update_goal_updates_status.exs
@@ -0,0 +1,11 @@
+defmodule Operately.Repo.Migrations.UpdateGoalUpdatesStatus do
+  use Ecto.Migration
+
+  def up do
+    Operately.Data.Change059UpdateGoalUpdateStatus.run()
+  end
+
+  def down do
+
+  end
+end
