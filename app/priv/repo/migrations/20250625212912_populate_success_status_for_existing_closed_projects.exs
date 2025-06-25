5f5166b19e448d14bf53b7834af71f9e5c1d4c98
Adriano Lazzaretti <lazzaretti136@gmail.com>
2025-06-25 19:56:07 -0300

    feat: Add Yes/No achieved assessment to ProjectClosePage
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

---
+++ app/priv/repo/migrations/20250625212912_populate_success_status_for_existing_closed_projects.exs
@@ -0,0 +1,11 @@
+defmodule Operately.Repo.Migrations.PopulateSuccessStatusForExistingClosedProjects do
+  use Ecto.Migration
+
+  def up do
+    Operately.Data.Change063SetProjectSuccessStatus.run()
+  end
+
+  def down do
+
+  end
+end
