decc972c2289b20b9f95d8c72308c3c4910585be
Igor Šarčević <igor@operately.com>
2025-06-25 10:18:37 +0200

    Send notifications
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- app/lib/operately/activities/notifications/goal_champion_updating.ex
+++ app/lib/operately/activities/notifications/goal_champion_updating.ex
@@ -1,5 +1,22 @@
 defmodule Operately.Activities.Notifications.GoalChampionUpdating do
-  def dispatch(_activity) do
-    {:ok, []}
+  def dispatch(activity) do
+    new_champion = find_new_champion(activity)
+
+    if new_champion && new_champion.id != activity.author_id do
+      Operately.Notifications.bulk_create([
+        %{
+          person_id: new_champion.id,
+          activity_id: activity.id,
+          should_send_email: true
+        }
+      ])
+    end
+  end
+
+  defp find_new_champion(activity) do
+    case activity.content["new_champion_id"] do
+      nil -> nil
+      new_champion_id -> Operately.People.get_person!(new_champion_id)
+    end
   end
 end
