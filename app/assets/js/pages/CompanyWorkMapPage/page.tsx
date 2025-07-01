f966ba30bdfb6bbdbce5267394449f275bd8aad9
Igor Šarčević <igor@operately.com>
2025-07-01 14:27:41 +0200

    Show add button if the add is enabled
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- app/assets/js/pages/CompanyWorkMapPage/page.tsx
+++ app/assets/js/pages/CompanyWorkMapPage/page.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 
 import { WorkMapPage } from "turboui";
+import { hasFeature } from "../../models/companies";
 import { useLoadedData } from "./loader";
 
 export function Page() {
@@ -8,5 +9,5 @@ export function Page() {
   const { workMap, company } = data;
   const title = `${company.name} Work Map`;
 
-  return <WorkMapPage title={title} items={workMap} />;
+  return <WorkMapPage title={title} items={workMap} addingChildrenEnabled={hasFeature(company, "new-goal-add-page")} />;
 }
