29bf01d5a574a1b36ef827aa38e706cec9ac7aed
Marko Anastasov <marko@operately.com>
2025-07-02 12:34:23 +0200

    chore: Buttons use brand blue color
    Signed-off-by: Marko Anastasov <marko@operately.com>

--- turboui/src/Button/index.tsx
+++ turboui/src/Button/index.tsx
@@ -7,10 +7,10 @@ import { calcClassName } from "./calcClassNames";
 
 export function PrimaryButton(props: BaseButtonProps) {
   const className = calcClassName(props, {
-    always: "border border-accent-1",
-    normal: "text-white-1 bg-accent-1 hover:bg-accent-1-light",
-    loading: "text-content-subtle bg-accent-1-light border-accent-1-light",
-    disabled: "text-content-subtle bg-accent-1-light border-accent-1-light",
+    always: "border border-brand-1",
+    normal: "text-white-1 bg-brand-1 hover:bg-blue-600",
+    loading: "text-content-subtle bg-blue-400 border-blue-400",
+    disabled: "text-content-subtle bg-blue-400 border-blue-400",
   });
 
   return (
@@ -41,17 +41,17 @@ export function DangerButton(props: BaseButtonProps) {
 
 export function GhostButton(props: BaseButtonProps) {
   const className = calcClassName(props, {
-    always: "border border-accent-1",
-    normal: "text-accent-1 hover:text-white-1 hover:bg-accent-1",
-    loading: "text-content-subtle bg-accent-1-light border-accent-1-light",
-    disabled: "text-content-subtle border-accent-1-light",
+    always: "border border-brand-1",
+    normal: "text-brand-1 hover:text-white-1 hover:bg-brand-1",
+    loading: "text-content-subtle bg-brand-2 border-brand-2",
+    disabled: "text-content-subtle border-blue-300",
   });
 
   return (
     <UnstyledButton
       {...props}
       className={className}
-      spinner={<Spinner loading={props.loading} color="var(--color-accent-1)" size={props.size} />}
+      spinner={<Spinner loading={props.loading} color="#3185FF" size={props.size} />}
     />
   );
 }
