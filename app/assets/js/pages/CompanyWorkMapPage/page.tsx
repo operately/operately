baef6bc29497428f45043ac85e3a2bc386fe707c
Igor Šarčević <igor@operately.com>
2025-07-03 17:07:09 +0200

    feat: Connect project/goal creation with the API
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- app/assets/js/pages/CompanyWorkMapPage/page.tsx
+++ app/assets/js/pages/CompanyWorkMapPage/page.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 
 import { useSpaceSearch } from "@/models/spaces";
-import { WorkMapPage } from "turboui";
+import { WorkMap, WorkMapPage } from "turboui";
 import { Company, hasFeature } from "../../models/companies";
+import { accessLevelAsNumber, useCreateGoal } from "../../models/goals";
 import { useLoadedData } from "./loader";
 
 export function Page() {
@@ -17,16 +18,33 @@ export function Page() {
 
 function useAddItemConfig(company: Company) {
   const spaceSearch = useSpaceSearch();
+  const [saveGoal] = useCreateGoal();
 
-  const saveNewItem = async (props): Promise<{ id: string }> => {
-    console.log("Saving new item:", props);
-
-    // TODO: Placeholder for actual save logic
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve({ id: "new-item-id" });
-      }, 1000);
+  const saveNewGoal: WorkMap.SaveNewItemFn = async (props) => {
+    return saveGoal({
+      name: props.name,
+      spaceId: props.spaceId,
+      anonymousAccessLevel: 0,
+      companyAccessLevel: accessLevelAsNumber(props.accessLevels.company),
+      spaceAccessLevel: accessLevelAsNumber(props.accessLevels.space),
+      parentGoalId: props.parentId || null,
+    }).then((response) => {
+      return { id: response.goal!.id! };
     });
+  };
+
+  const saveNewProject: WorkMap.SaveNewItemFn = async () => {
+    throw new Error("Project creation is not implemented yet");
+  };
+
+  const saveNewItem: WorkMap.SaveNewItemFn = async (props) => {
+    if (props.type === "goal") {
+      return saveNewGoal(props);
+    } else if (props.type === "project") {
+      return saveNewProject(props);
+    } else {
+      throw new Error(`Unknown item type: ${props.type}`);
+    }
   };
 
   return {
