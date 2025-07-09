1b04217ff9ba8ae190e924eba8dd006582613a29
Igor Šarčević <igor@operately.com>
2025-07-09 19:09:04 +0200

    Move run prompt to the new namespace
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- app/lib/operately_web/api/ai.ex
+++ app/lib/operately_web/api/ai.ex
@@ -1,35 +1,23 @@
 defmodule OperatelyWeb.Api.Ai do
+  alias __MODULE__.Steps
+
   defmodule Prompt do
     use TurboConnect.Query
-    use OperatelyWeb.Api.Helpers
-
-    alias Operately.AI
 
     inputs do
-      field? :prompt, :string, null: true
+      field :prompt, :string
     end
 
     outputs do
-      field? :result, :string, null: true
+      field :result, :string
     end
 
     def call(conn, inputs) do
       conn
       |> Steps.start()
       |> Steps.check_if_ai_enabled()
-      |> run(:result, fn ctx -> {:ok, AI.run(ctx.me, inputs.prompt)} end)
-      |> run(:serialized, fn ctx -> {:ok, %{result: ctx.result}} end)
-      |> respond()
-    end
-
-    def respond(result) do
-      case result do
-        {:ok, ctx} -> {:ok, ctx.serialized}
-        {:error, :me, _} -> {:error, :unauthorized}
-        {:error, :company, _} -> {:error, :not_found}
-        {:error, :check_feature_enabled, error} -> {:error, %{message: error}}
-        _ -> {:error, :internal_server_error}
-      end
+      |> Steps.run_prompt(inputs.prompt)
+      |> Steps.respond(fn _ -> %{result: ctx.result} end)
     end
   end
 
@@ -40,6 +28,10 @@ defmodule OperatelyWeb.Api.Ai do
       %{conn: conn, me: find_me(conn)}
     end
 
+    def run_prompt(ctx, prompt) do
+      Map.put(ctx, :result, Operately.AI.run(ctx.me, prompt))
+    end
+
     def check_feature_enabled(ctx) do
       company = Operately.Companies.get_company!(ctx.me.company_id)
 
@@ -47,6 +39,40 @@ defmodule OperatelyWeb.Api.Ai do
         {:ok, true}
       else
         {:error, "AI Playground feature is not enabled for this company"}
+      end
+    end
+
+    def respond(result, ok_callback, error_callback \\ &handle_error/1) do
+      case result do
+        {:ok, changes} ->
+          {:ok, ok_callback.(changes)}
+
+        {:error, _, :idempotent, changes} ->
+          {:ok, ok_callback.(changes)}
+
+        e ->
+          error_callback.(e)
+      end
+    end
+
+    defp handle_error(reason) do
+      case reason do
+        {:error, _failed_operation, {:not_found, message}, _changes} ->
+          {:error, :not_found, message}
+
+        {:error, _failed_operation, :not_found, _changes} ->
+          {:error, :not_found}
+
+        {:error, _failed_operation, :forbidden, _changes} ->
+          {:error, :not_found}
+
+        {:error, _failed_operation, reason, _changes} ->
+          Logger.error("Transaction failed: #{inspect(reason)}")
+          {:error, :internal_server_error}
+
+        e ->
+          Logger.error("Unexpected error: #{inspect(e)}")
+          {:error, :internal_server_error}
       end
     end
   end
