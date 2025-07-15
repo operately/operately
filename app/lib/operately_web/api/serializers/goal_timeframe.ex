72c929e01ac5a5f833752559520a118f244a4dfa
Adriano Lazzaretti <lazzaretti136@gmail.com>
2025-07-15 14:59:07 -0300

    chore: Add contextual dates to Goal timeframe
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- app/lib/operately_web/api/serializers/goal_timeframe.ex
+++ app/lib/operately_web/api/serializers/goal_timeframe.ex
@@ -4,6 +4,8 @@ defimpl OperatelyWeb.Api.Serializable, for: Operately.Goals.Timeframe do
       type: timeframe.type,
       start_date: OperatelyWeb.Api.Serializer.serialize(timeframe.start_date),
       end_date: OperatelyWeb.Api.Serializer.serialize(timeframe.end_date),
+      contextual_start_date: OperatelyWeb.Api.Serializer.serialize(timeframe.contextual_start_date),
+      contextual_end_date: OperatelyWeb.Api.Serializer.serialize(timeframe.contextual_end_date)
     }
   end
 end
