56d2e74ee0ae6b4d1f8fc45d1f0ed562062c0084
Adriano Lazzaretti <lazzaretti136@gmail.com>
2025-07-16 06:10:10 -0300

    Fix tests
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- app/lib/operately_web/api/serializers/contextual_date.ex
+++ app/lib/operately_web/api/serializers/contextual_date.ex
@@ -1,7 +1,7 @@
 defimpl OperatelyWeb.Api.Serializable, for: Operately.ContextualDates.ContextualDate do
   def serialize(contextual_date, level: :essential) do
     %{
-      date_type: contextual_date.date_type,
+      date_type: Atom.to_string(contextual_date.date_type),
       value: contextual_date.value,
       date: OperatelyWeb.Api.Serializer.serialize(contextual_date.date)
     }
