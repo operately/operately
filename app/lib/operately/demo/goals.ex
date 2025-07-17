626d5d639f53985d85f1321be433d037c1014535
Adriano Lazzaretti <lazzaretti136@gmail.com>
2025-07-17 13:13:49 -0300

    chore: Adjust demo to create contextual dates for Goals
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- app/lib/operately/demo/goals.ex
+++ app/lib/operately/demo/goals.ex
@@ -83,31 +83,67 @@ defmodule Operately.Demo.Goals do
     start_date = Date.new!(year, 1, 1)
     end_date = Date.new!(year, 12, 31)
 
-    %{start_date: start_date, end_date: end_date, type: "year"}
+    contextual_start_date = %{
+      date_type: :year,
+      value: to_string(year),
+      date: start_date
+    }
+
+    contextual_end_date = %{
+      date_type: :year,
+      value: to_string(year),
+      date: end_date
+    }
+
+    %{
+      start_date: start_date,
+      end_date: end_date,
+      type: "year",
+      contextual_start_date: contextual_start_date,
+      contextual_end_date: contextual_end_date
+    }
   end
 
   defp create_timeframe(:current_quarter) do
     {year, month, _day} = Date.to_erl(Date.utc_today())
 
-    {start_month, end_month} =
+    {start_month, end_month, quarter} =
       case month do
-        1 -> {1, 3}
-        2 -> {1, 3}
-        3 -> {1, 3}
-        4 -> {4, 6}
-        5 -> {4, 6}
-        6 -> {4, 6}
-        7 -> {7, 9}
-        8 -> {7, 9}
-        9 -> {7, 9}
-        10 -> {10, 12}
-        11 -> {10, 12}
-        12 -> {10, 12}
+        1 -> {1, 3, "Q1"}
+        2 -> {1, 3, "Q1"}
+        3 -> {1, 3, "Q1"}
+        4 -> {4, 6, "Q2"}
+        5 -> {4, 6, "Q2"}
+        6 -> {4, 6, "Q2"}
+        7 -> {7, 9, "Q3"}
+        8 -> {7, 9, "Q3"}
+        9 -> {7, 9, "Q3"}
+        10 -> {10, 12, "Q4"}
+        11 -> {10, 12, "Q4"}
+        12 -> {10, 12, "Q4"}
       end
 
     start_date = Date.new!(year, start_month, 1)
     end_date = Date.new!(year, end_month, Date.days_in_month(Date.new!(year, end_month, 1)))
 
-    %{start_date: start_date, end_date: end_date, type: "quarter"}
+    contextual_start_date = %{
+      date_type: :quarter,
+      value: "#{quarter} #{year}",
+      date: start_date
+    }
+
+    contextual_end_date = %{
+      date_type: :quarter,
+      value: "#{quarter} #{year}",
+      date: end_date
+    }
+
+    %{
+      start_date: start_date,
+      end_date: end_date,
+      type: "quarter",
+      contextual_start_date: contextual_start_date,
+      contextual_end_date: contextual_end_date
+    }
   end
 end
