59ee6734774343dd6821163a9ecb28947d40afb6
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2025-07-18 08:02:04 -0300

    fix: Don't notify people who assign themselves as contributors of project (#2924)
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- app/lib/operately/activities/notifications/project_contributor_addition.ex
+++ app/lib/operately/activities/notifications/project_contributor_addition.ex
@@ -1,15 +1,16 @@
 defmodule Operately.Activities.Notifications.ProjectContributorAddition do
   def dispatch(activity) do
     person_id = activity.content["person_id"]
+    author_id = activity.author_id
 
-    notifications = [
-      %{
+    if person_id != author_id do
+      Operately.Notifications.bulk_create([%{
         person_id: person_id,
         activity_id: activity.id,
         should_send_email: true,
-      }
-    ]
-
-    Operately.Notifications.bulk_create(notifications)
+      }])
+    else
+      {:ok, []}
+    end
   end
 end
