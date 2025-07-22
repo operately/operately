adc2804be42a71e6aa09d51af4313e683c529798
Igor Šarčević <igor@operately.com>
2025-07-22 11:38:30 +0200

    feat: Add section for project discussions
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- app/assets/js/pages/ProjectPage/index.tsx
+++ app/assets/js/pages/ProjectPage/index.tsx
@@ -1,32 +1,40 @@
-import * as React from "react";
-import * as Paper from "@/components/PaperContainer";
+import * as Api from "@/api";
 import * as Pages from "@/components/Pages";
+import * as Paper from "@/components/PaperContainer";
+import * as Companies from "@/models/companies";
 import * as Projects from "@/models/projects";
+import * as React from "react";
 
+import { useClearNotificationsOnLoad } from "@/features/notifications";
+import { PageModule } from "@/routes/types";
+import { assertPresent } from "@/utils/assertions";
+import { fetchAll } from "../../utils/async";
 import { banner } from "./Banner";
+import { CheckInSection } from "./CheckInSection";
+import { ContributorsSection } from "./ContributorsSection";
 import { Header } from "./Header";
 import { Navigation } from "./Navigation";
-import { ProjectFeed } from "./ProjectFeed";
-import { CheckInSection } from "./CheckInSection";
-import { StatusOverview } from "./StatusOverview";
-import { ProjectOptions } from "./ProjectOptions";
-import { TimelineSection } from "./TimelineSection";
-import { ResourcesSection } from "./ResourcesSection";
-import { ContributorsSection } from "./ContributorsSection";
 import { ProjectDescriptionSection } from "./ProjectDescriptionSection";
-import { useClearNotificationsOnLoad } from "@/features/notifications";
-import { assertPresent } from "@/utils/assertions";
-import { PageModule } from "@/routes/types";
+import { ProjectDiscussionsSection } from "./ProjectDiscussionsSection";
+import { ProjectFeed } from "./ProjectFeed";
+import { ProjectOptions } from "./ProjectOptions";
+import { ResourcesSection } from "./ResourcesSection";
+import { StatusOverview } from "./StatusOverview";
+import { TimelineSection } from "./TimelineSection";
 
 export default { name: "ProjectPage", loader, Page } as PageModule;
 
 interface LoaderResult {
   project: Projects.Project;
+  company: Companies.Company;
 }
 
 async function loader({ params }): Promise<LoaderResult> {
-  return {
-    project: await Projects.getProject({
+  return fetchAll({
+    company: Companies.getCompany({
+      id: params.companyId,
+    }).then((d) => d.company!),
+    project: Projects.getProject({
       id: params.id,
       includeSpace: true,
       includeGoal: true,
@@ -41,11 +49,14 @@ async function loader({ params }): Promise<LoaderResult> {
       includeRetrospective: true,
       includeUnreadNotifications: true,
     }).then((data) => data.project!),
-  };
+    discussions: Api.projectDiscussions.list({
+      projectId: params.id,
+    }),
+  });
 }
 
 function Page() {
-  const { project } = Pages.useLoadedData() as LoaderResult;
+  const { project, company } = Pages.useLoadedData() as LoaderResult;
 
   assertPresent(project.notifications, "Project notifications must be defined");
   useClearNotificationsOnLoad(project.notifications);
@@ -65,6 +76,7 @@ function Page() {
             <ProjectDescriptionSection project={project} />
             <TimelineSection project={project} />
             <CheckInSection project={project} />
+            {Companies.hasFeature(company, "project_discussions") && <ProjectDiscussionsSection project={project} />}
             <ResourcesSection project={project} />
           </div>
 
