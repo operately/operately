fc8a59bee15ca17e72a364ed51fadeeca7194347
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2025-07-22 13:02:00 -0300

    chore: Update timeframes in GoalCheckInEdit activity (#2944)
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

---
+++ app/priv/repo/migrations/20250722155732_update_timeframes_in_goal_check_in_edit_activities.exs
@@ -0,0 +1,12 @@
+defmodule Operately.Repo.Migrations.UpdateTimeframesInGoalCheckInEditActivities do
+  use Ecto.Migration
+
+  def up do
+
+    {:ok, %{error_count: 0}} = Operately.Data.Change066UpdateTimeframeInGoalActivities.GoalCheckInEdit.run()
+  end
+
+  def down do
+
+  end
+end
