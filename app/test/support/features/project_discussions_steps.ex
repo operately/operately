83ab2a677dde810db889b9866ab66ed4d4bd9abc
Igor Šarčević <igor@operately.com>
2025-07-23 11:27:33 +0200

    List discussions test
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- app/test/support/features/project_discussions_steps.ex
+++ app/test/support/features/project_discussions_steps.ex
@@ -9,7 +9,27 @@ defmodule Operately.Support.Features.ProjectDiscussionSteps do
   step :setup, ctx do
     ctx
     |> Factory.setup()
+    |> Factory.enable_feature("project_discussions")
     |> Factory.add_space(:marketing)
-    |> Factory.add_project(:alpha, :marketing)
+    |> Factory.add_project(:project, :marketing)
+    |> Factory.log_in_person(:creator)
+  end
+
+  step :given_several_discussions_exist, ctx do
+    ctx
+    |> Factory.add_project_discussion(:discussion1, :project, title: "Discussion 1", content: "Content for discussion 1")
+    |> Factory.add_project_discussion(:discussion2, :project, title: "Discussion 2", content: "Content for discussion 2")
+    |> Factory.add_project_discussion(:discussion3, :project, title: "Discussion 3", content: "Content for discussion 3")
+  end
+
+  step :visit_project_page, ctx do
+    ctx |> UI.visit(OperatelyWeb.Paths.project_path(ctx.company, ctx.project))
+  end
+
+  step :assert_discussion_listed, ctx do
+    ctx
+    |> UI.assert_text("Discussion 1")
+    |> UI.assert_text("Discussion 2")
+    |> UI.assert_text("Discussion 3")
   end
 end
