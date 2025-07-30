50df442c81bbc19d9151b9f42cfb309552a149d1
Adriano Lazzaretti <lazzaretti136@gmail.com>
2025-07-30 11:26:11 -0300

    Fix EditTimelineOperation
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

---
+++ app/priv/repo/migrations/20250730142747_change_project_timeline_activity_fields_from_utcdatetime_to_date.exs
@@ -0,0 +1,11 @@
+defmodule Operately.Repo.Migrations.ChangeProjectTimelineActivityFieldsFromUtcdatetimeToDate do
+  use Ecto.Migration
+
+  def up do
+    Operately.Data.Change070UpdateProjectTimelineEditedActivity.run()
+  end
+
+  def down do
+
+  end
+end
