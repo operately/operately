c6380c72530c70f4a47372c956dd87fad814e975
Igor Šarčević <igor@operately.com>
2025-08-04 12:07:11 +0200

    feat: Implement Checks API
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- app/lib/operately_web/paths.ex
+++ app/lib/operately_web/paths.ex
@@ -237,6 +237,10 @@ defmodule OperatelyWeb.Paths do
     OperatelyWeb.Api.Helpers.id_with_comments(name, id)
   end
 
+  def goal_check_id(check_id) when is_binary(check_id) do
+    Operately.ShortUuid.encode!(check_id)
+  end
+
   def goal_discussion_id(%{id: id, title: title}) do
     id = Operately.ShortUuid.encode!(id)
     OperatelyWeb.Api.Helpers.id_with_comments(title, id)
