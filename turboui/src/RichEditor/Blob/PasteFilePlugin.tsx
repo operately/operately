eb35fdd6499bc533b13c151ece3ad82768fffcf6
Adriano Lazzaretti <lazzaretti136@gmail.com>
2025-08-06 13:08:59 -0300

    fix: Fix rich text drag-and-drop and copy-and-paste plugins
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- turboui/src/RichEditor/Blob/PasteFilePlugin.tsx
+++ turboui/src/RichEditor/Blob/PasteFilePlugin.tsx
@@ -1,31 +1,34 @@
 import { Plugin, PluginKey } from "prosemirror-state";
 import { AddBlobsEditorCommand } from "./AddBlobsEditorCommand";
+import { UploadFileFn } from "../useEditor";
 
-export const PasteFilePlugin = new Plugin({
-  key: new PluginKey("pasteFilePlugin"),
+export const createPasteFilePlugin = (uploadFile: UploadFileFn) =>
+  new Plugin({
+    key: new PluginKey("pasteFilePlugin"),
 
-  props: {
-    handlePaste(view, event, _slice) {
-      if (!view.editable) return false;
+    props: {
+      handlePaste(view, event, _slice) {
+        if (!view.editable) return false;
 
-      const items = Array.from(event.clipboardData?.items || []);
+        const items = Array.from(event.clipboardData?.items || []);
 
-      items.forEach((item) => {
-        const file = item.getAsFile();
-        if (!file) return;
+        items.forEach((item) => {
+          const file = item.getAsFile();
+          if (!file) return;
 
-        if (item.type.indexOf("image") === 0) {
-          event.preventDefault();
+          if (item.type.indexOf("image") === 0) {
+            event.preventDefault();
 
-          AddBlobsEditorCommand({
-            files: [file],
-            pos: view.state.selection.from,
-            view: view,
-          });
-        }
-      });
+            AddBlobsEditorCommand({
+              files: [file],
+              pos: view.state.selection.from,
+              view: view,
+              uploadFile: uploadFile,
+            });
+          }
+        });
 
-      return false;
+        return false;
+      },
     },
-  },
-});
+  });
