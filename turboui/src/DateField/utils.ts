dccf42cd0625835dbe94a5d4058d5f1fb6595d29
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2025-08-06 10:40:59 -0300

    fix: Fix useStartOfPeriod prop in DateField component (#3034)
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- turboui/src/DateField/utils.ts
+++ turboui/src/DateField/utils.ts
@@ -2,30 +2,30 @@ import { DateField } from ".";
 
 export const getCurrentYear = () => new Date().getFullYear();
 
-export const generateQuarters = (year = getCurrentYear(), useStartOfPeriod = false) => [
-  { value: useStartOfPeriod ? `${year}-01-01` : `${year}-03-31`, label: "Q1" },
-  { value: useStartOfPeriod ? `${year}-04-01` : `${year}-06-30`, label: "Q2" },
-  { value: useStartOfPeriod ? `${year}-07-01` : `${year}-09-30`, label: "Q3" },
-  { value: useStartOfPeriod ? `${year}-10-01` : `${year}-12-31`, label: "Q4" },
+export const generateQuarters = (year: number, useStartOfPeriod = false) => [
+  { value: useStartOfPeriod ? `${year}-01-01T00:00:00` : `${year}-03-31T23:59:59`, label: "Q1" },
+  { value: useStartOfPeriod ? `${year}-04-01T00:00:00` : `${year}-06-30T23:59:59`, label: "Q2" },
+  { value: useStartOfPeriod ? `${year}-07-01T00:00:00` : `${year}-09-30T23:59:59`, label: "Q3" },
+  { value: useStartOfPeriod ? `${year}-10-01T00:00:00` : `${year}-12-31T23:59:59`, label: "Q4" },
 ];
 
-export const generateMonths = (year = getCurrentYear(), useStartOfPeriod = false) => [
-  { value: useStartOfPeriod ? `${year}-01-01` : `${year}-01-31`, label: "Jan", name: "January" },
+export const generateMonths = (year: number, useStartOfPeriod = false) => [
+  { value: useStartOfPeriod ? `${year}-01-01T00:00:00` : `${year}-01-31T23:59:59`, label: "Jan", name: "January" },
   {
-    value: useStartOfPeriod ? `${year}-02-01` : `${year}-02-${year % 4 === 0 ? "29" : "28"}`,
+    value: useStartOfPeriod ? `${year}-02-01T00:00:00` : `${year}-02-${year % 4 === 0 ? "29" : "28"}T23:59:59`,
     label: "Feb",
     name: "February",
   }, // Leap year handling
-  { value: useStartOfPeriod ? `${year}-03-01` : `${year}-03-31`, label: "Mar", name: "March" },
-  { value: useStartOfPeriod ? `${year}-04-01` : `${year}-04-30`, label: "Apr", name: "April" },
-  { value: useStartOfPeriod ? `${year}-05-01` : `${year}-05-31`, label: "May", name: "May" },
-  { value: useStartOfPeriod ? `${year}-06-01` : `${year}-06-30`, label: "Jun", name: "June" },
-  { value: useStartOfPeriod ? `${year}-07-01` : `${year}-07-31`, label: "Jul", name: "July" },
-  { value: useStartOfPeriod ? `${year}-08-01` : `${year}-08-31`, label: "Aug", name: "August" },
-  { value: useStartOfPeriod ? `${year}-09-01` : `${year}-09-30`, label: "Sep", name: "September" },
-  { value: useStartOfPeriod ? `${year}-10-01` : `${year}-10-31`, label: "Oct", name: "October" },
-  { value: useStartOfPeriod ? `${year}-11-01` : `${year}-11-30`, label: "Nov", name: "November" },
-  { value: useStartOfPeriod ? `${year}-12-01` : `${year}-12-31`, label: "Dec", name: "December" },
+  { value: useStartOfPeriod ? `${year}-03-01T00:00:00` : `${year}-03-31T23:59:59`, label: "Mar", name: "March" },
+  { value: useStartOfPeriod ? `${year}-04-01T00:00:00` : `${year}-04-30T23:59:59`, label: "Apr", name: "April" },
+  { value: useStartOfPeriod ? `${year}-05-01T00:00:00` : `${year}-05-31T23:59:59`, label: "May", name: "May" },
+  { value: useStartOfPeriod ? `${year}-06-01T00:00:00` : `${year}-06-30T23:59:59`, label: "Jun", name: "June" },
+  { value: useStartOfPeriod ? `${year}-07-01T00:00:00` : `${year}-07-31T23:59:59`, label: "Jul", name: "July" },
+  { value: useStartOfPeriod ? `${year}-08-01T00:00:00` : `${year}-08-31T23:59:59`, label: "Aug", name: "August" },
+  { value: useStartOfPeriod ? `${year}-09-01T00:00:00` : `${year}-09-30T23:59:59`, label: "Sep", name: "September" },
+  { value: useStartOfPeriod ? `${year}-10-01T00:00:00` : `${year}-10-31T23:59:59`, label: "Oct", name: "October" },
+  { value: useStartOfPeriod ? `${year}-11-01T00:00:00` : `${year}-11-30T23:59:59`, label: "Nov", name: "November" },
+  { value: useStartOfPeriod ? `${year}-12-01T00:00:00` : `${year}-12-31T23:59:59`, label: "Dec", name: "December" },
 ];
 
 export const getYearDate = (year: number, useStartOfPeriod = false) => {
