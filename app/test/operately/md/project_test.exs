b3723dc08bb50d89cc08b55558d5a39c481b30c4
Igor Šarčević <igor@operately.com>
2025-08-06 17:24:52 +0200

    feat: Render project discussions
    Signed-off-by: Igor Šarčević <igor@operately.com>

---
+++ app/test/operately/md/project_test.exs
@@ -0,0 +1,20 @@
+defmodule Operately.MD.ProjectTest do
+  use Operately.DataCase
+
+  setup ctx do
+    ctx
+    |> Factory.setup()
+    |> Factory.add_space(:marketing)
+    |> Factory.add_project(:project, :marketing)
+  end
+
+  test "it renders discussions in the markdown", ctx do
+    ctx = Factory.add_project_discussion(ctx, :discussion, :project)
+
+    rendered = Operately.MD.Project.render(ctx.project)
+
+    assert rendered =~ "## Discussions"
+    assert rendered =~ ctx.discussion.title
+    assert rendered =~ ctx.creator.full_name
+  end
+end
