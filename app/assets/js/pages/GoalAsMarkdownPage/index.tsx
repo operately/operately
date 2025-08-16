700bb6c09a3a557225bc332b9ac7ae81be2bacbc
Marko Anastasov <marko@operately.com>
2025-08-16 13:22:36 +0200

    chore: Add 'copy to clipboard' button to goal and project markdown pages (#3134)
    Signed-off-by: Marko Anastasov <marko@operately.com>

--- app/assets/js/pages/GoalAsMarkdownPage/index.tsx
+++ app/assets/js/pages/GoalAsMarkdownPage/index.tsx
@@ -2,6 +2,7 @@ import * as Pages from "@/components/Pages";
 import * as Paper from "@/components/PaperContainer";
 import * as Goals from "@/models/goals";
 import * as React from "react";
+import { SecondaryButton } from "turboui";
 
 import { PageModule } from "@/routes/types";
 export default { name: "GoalAsMarkdownPage", loader, Page } as PageModule;
@@ -33,10 +34,24 @@ async function loader({ params }): Promise<LoaderResult> {
 function Page() {
   const { markdown } = Pages.useLoadedData<LoaderResult>();
 
+  const [copied, setCopied] = React.useState(false);
+
   return (
     <Pages.Page title={"Goal As Markdown"}>
       <Paper.Root>
         <Paper.Body>
+          <div className="flex justify-end mb-2">
+            <SecondaryButton
+              size="xs"
+              onClick={() => {
+                navigator.clipboard.writeText(markdown);
+                setCopied(true);
+                setTimeout(() => setCopied(false), 2000);
+              }}
+            >
+              {copied ? "Copied!" : "Copy to clipboard"}
+            </SecondaryButton>
+          </div>
           <pre className="whitespace-pre-wrap">
             <code>{markdown}</code>
           </pre>
