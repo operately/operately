0f112df8937c2483d88553c94ba5d05d0f0addea
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2025-08-18 12:59:42 -0300

    feat: Tasks can be deleted (#3149)
    * Separate task and project endpoints
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    * Add endpoint to delete tasks
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    * Update calls to backend
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    * Task can be deleted
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    * Add confirmation before deleting task
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    * Add activity item
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    * Adjustments
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    ---------
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

---
+++ app/assets/js/features/activities/TaskDeleting/index.tsx
@@ -0,0 +1,71 @@
+import type { ActivityContentTaskDeleting } from "@/api";
+import type { Activity } from "@/models/activities";
+import { Paths } from "@/routes/paths";
+import React from "react";
+import { feedTitle, projectLink } from "../feedItemLinks";
+import type { ActivityHandler } from "../interfaces";
+
+const TaskDeleting: ActivityHandler = {
+  pageHtmlTitle(_activity: Activity) {
+    throw new Error("Not implemented");
+  },
+
+  pagePath(_paths: Paths, _activity: Activity) {
+    throw new Error("Not implemented");
+  },
+
+  PageTitle(_props: { activity: any }) {
+    throw new Error("Not implemented");
+  },
+
+  PageContent(_props: { activity: Activity }) {
+    throw new Error("Not implemented");
+  },
+
+  PageOptions(_props: { activity: Activity }) {
+    return null;
+  },
+
+  FeedItemTitle(props: { activity: Activity; page: string }) {
+    const { taskName, project }  = content(props.activity);
+
+    if (props.page === "project") {
+      return feedTitle(props.activity, `deleted task "${taskName}"`);
+    } else {
+      return feedTitle(props.activity, `deleted task "${taskName}"`, "on", projectLink(project));
+    }
+  },
+
+  FeedItemContent(_props: { activity: Activity; page: any }) {
+    return null; // No additional content needed for deletion
+  },
+
+  feedItemAlignment(_activity: Activity): "items-start" | "items-center" {
+    return "items-center";
+  },
+
+  commentCount(_activity: Activity): number {
+    throw new Error("Not implemented");
+  },
+
+  hasComments(_activity: Activity): boolean {
+    throw new Error("Not implemented");
+  },
+
+  NotificationTitle(props: { activity: Activity }) {
+    const { taskName } = content(props.activity);
+    return <>Task "{taskName}" was deleted</>;
+  },
+
+  NotificationLocation(props: { activity: Activity }) {
+    const { project } = content(props.activity);
+
+    return <>Project: {project?.name}</>;
+  },
+};
+
+function content(activity: Activity): ActivityContentTaskDeleting {
+  return activity.content as ActivityContentTaskDeleting;
+}
+
+export default TaskDeleting;
