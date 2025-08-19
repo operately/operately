efcf3ff29a9951ccf4ed3b64fda32bc7f44f4c0a
Igor Šarčević <igor@operately.com>
2025-08-19 15:59:06 +0200

    feat: Reduce the size of the devbar to make place for Ask Alfred
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- app/assets/js/features/DevBar/index.tsx
+++ app/assets/js/features/DevBar/index.tsx
@@ -1,59 +1,35 @@
 import React from "react";
 
-import { useDevBarData } from "./useDevBarData";
-import { ToggleTestIds } from "./ToggleTestIds";
-import { ToggleCompanyOwner } from "./ToggleCompanyOwner";
 import classNames from "classnames";
+import { ToggleTestIds } from "./ToggleTestIds";
+import { useDevBarData } from "./useDevBarData";
 
 export function DevBar() {
   if (!window.appConfig.showDevBar) return;
 
   const className = classNames(
-    "bg-black text-white-1 p-1 text-sm font-mono z-50",
+    "absolute",
+    "bottom-0 left-0",
+    "bg-black text-white-1 p-1 text-xs font-mono z-50",
     "hidden lg:block", // hidden on mobile, start showing on large screens
   );
 
-  return (
-    <div className={className}>
-      <div className="flex justify-between">
-        <Left />
-        <Right />
-      </div>
-    </div>
-  );
-}
-
-function Left() {
-  return (
-    <div className="flex items-center gap-6">
-      <Name />
-      <ToggleTestIds />
-      <ToggleCompanyOwner />
-    </div>
-  );
-}
-
-function Name() {
-  return <div className="text-green-500">DEVBAR</div>;
-}
-
-function Right() {
   const data = useDevBarData();
   const pageLoadColor = data.loadTime < 500 ? "text-green-500" : "text-content-error";
 
   return (
-    <div className="flex items-center gap-8">
-      <div>
-        Page: <span>{data.pageName}</span>
+    <div className={className}>
+      <div className="flex justify-between mb-1">
+        <div className="w-20">Page Name:</div>
+        <div>{data.pageName}</div>
       </div>
 
-      <div>
-        Load Time: <span className={pageLoadColor}>{data.loadTime.toFixed(0)}ms</span>
+      <div className="flex justify-between mb-3">
+        <div className="w-20">Load Time:</div>
+        <div className={pageLoadColor}>{data.loadTime.toFixed(0)}ms</div>
       </div>
 
-      <div>
-        Network Requests: <span className="text-green-500">{data.networkRequests}</span>
-      </div>
+      <ToggleTestIds />
     </div>
   );
 }
