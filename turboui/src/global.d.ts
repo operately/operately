aac3979a0106d6a77143d16c0b41e8243207e16a
Marko Anastasov <marko@operately.com>
2025-08-20 15:05:36 +0200

    Typescript to understand importing images
    Signed-off-by: Marko Anastasov <marko@operately.com>

--- turboui/src/global.d.ts
+++ turboui/src/global.d.ts
@@ -3,3 +3,29 @@ interface Window {
 
   __tests?: any;
 }
+
+// Image module declarations for Vite/Webpack
+declare module "*.png" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.jpg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.jpeg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.gif" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.svg" {
+  const src: string;
+  export default src;
+}
