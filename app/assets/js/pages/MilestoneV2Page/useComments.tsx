ee97385ffb219670c2f02ec93d21e2024e805a0a
Adriano Lazzaretti <lazzaretti136@gmail.com>
2025-08-28 11:34:07 -0300

    feat: Comments can be posted on Milestone V2 page
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

---
+++ app/assets/js/pages/MilestoneV2Page/useComments.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+import Api from "@/api";
+import * as Milestones from "@/models/milestones";
+
+import { Paths } from "@/routes/paths";
+import { useMe } from "@/contexts/CurrentCompanyContext";
+import { PageCache } from "@/routes/PageCache";
+import { pageCacheKey } from ".";
+
+export function useComments(paths: Paths, milestone: Milestones.Milestone) {
+  const me = useMe()!;
+
+  const [comments, setComments] = React.useState(
+    Milestones.parseMilestoneCommentsForTurboUi(paths, milestone.comments),
+  );
+
+  const handleCreateComment = React.useCallback(async (content: any) => {
+    const tempId = `temp-${Date.now()}`;
+    const optimisticComment: Milestones.MilestoneComment = {
+      action: "none",
+      comment: {
+        id: tempId,
+        insertedAt: new Date().toISOString(),
+        content: JSON.stringify({ message: content }),
+        author: me,
+        reactions: [],
+      },
+    };
+
+    setComments((prev) => [...prev, Milestones.parseMilestoneCommentForTurboUi(paths, optimisticComment)]);
+
+    const res = await Api.postMilestoneComment({
+      milestoneId: milestone.id!,
+      action: "none",
+      content: JSON.stringify(content),
+    });
+
+    setComments((prev) => {
+      return prev.map((c) => {
+        if (c.id === tempId) {
+          const comment = { ...res.comment.comment, author: me };
+          return Milestones.parseMilestoneCommentForTurboUi(paths, { ...res.comment, comment });
+        } else {
+          return c;
+        }
+      });
+   });
+    PageCache.invalidate(pageCacheKey(milestone.id));
+  }, [paths, me, milestone.id]);
+
+  return { comments, handleCreateComment };
+}
