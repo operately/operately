3dbfad4c83296263475acd2305e484e37b14c0ad
Adriano Lazzaretti <lazzaretti136@gmail.com>
2025-08-29 08:47:01 -0300

    Add milestone_id to task activities
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- app/lib/operately/activities/content/task_description_change.ex
+++ app/lib/operately/activities/content/task_description_change.ex
@@ -5,6 +5,7 @@ defmodule Operately.Activities.Content.TaskDescriptionChange do
     belongs_to :company, Operately.Companies.Company
     belongs_to :space, Operately.Groups.Group
     belongs_to :project, Operately.Projects.Project
+    belongs_to :milestone, Operately.Projects.Milestone
     belongs_to :task, Operately.Tasks.Task
 
     field :task_name, :string
