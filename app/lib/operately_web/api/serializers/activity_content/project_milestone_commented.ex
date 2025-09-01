2357cb173de7aeb0abcff97fcfb6d928065ae91f
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2025-09-01 06:20:49 -0300

    fix: MilestoneCommented event doesn't break feed when Milestone is deleted (#3306)
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- app/lib/operately_web/api/serializers/activity_content/project_milestone_commented.ex
+++ app/lib/operately_web/api/serializers/activity_content/project_milestone_commented.ex
@@ -1,14 +1,10 @@
 defimpl OperatelyWeb.Api.Serializable, for: Operately.Activities.Content.ProjectMilestoneCommented do
   def serialize(content, level: :essential) do
     %{
-      comment: OperatelyWeb.Api.Serializer.serialize(content.comment),
       comment_action: content.comment_action,
-      milestone: %{
-        id: OperatelyWeb.Paths.milestone_id(content.milestone),
-        title: content.milestone.title,
-      },
+      comment: OperatelyWeb.Api.Serializer.serialize(content.comment),
+      milestone: OperatelyWeb.Api.Serializer.serialize(content.milestone),
       project: OperatelyWeb.Api.Serializer.serialize(content.project),
-      project_id: OperatelyWeb.Paths.project_id(content.project),
     }
   end
 end
