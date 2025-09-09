8278a396e36916f9c3137739dcc153c048fe7a20
Adriano Lazzaretti <lazzaretti136@gmail.com>
2025-09-09 10:48:50 -0300

    fix: TaskCreationModal opens only when user clicks + icon
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- turboui/src/TaskBoard/components/EmptyMilestoneDropZone.tsx
+++ turboui/src/TaskBoard/components/EmptyMilestoneDropZone.tsx
@@ -2,22 +2,14 @@ import React from "react";
 import { useDropZone } from "../../utils/DragAndDrop";
 
 export interface EmptyMilestoneDropZoneProps {
-  /**
-   * ID of the milestone this empty drop zone represents
-   */
   milestoneId: string;
-  
-  /**
-   * Optional callback that will be called when the user wants to create a new task in this milestone
-   */
-  onTaskCreation?: () => void;
 }
 
 /**
  * Empty milestone drop zone component that allows dropping tasks into empty milestones
  * This provides a visual cue and drop target when a milestone has no tasks
  */
-export function EmptyMilestoneDropZone({ milestoneId, onTaskCreation }: EmptyMilestoneDropZoneProps) {
+export function EmptyMilestoneDropZone({ milestoneId }: EmptyMilestoneDropZoneProps) {
   // Set up drop zone with the same ID pattern as TaskList
   const { ref } = useDropZone({ id: milestoneId, dependencies: [] });
 
@@ -25,7 +17,6 @@ export function EmptyMilestoneDropZone({ milestoneId, onTaskCreation }: EmptyMil
     <div
       ref={ref as React.RefObject<HTMLDivElement>}
       className="py-3 px-4 text-center text-content-subtle text-sm min-h-[40px] bg-surface-base"
-      onClick={onTaskCreation}
     >
       Click + to add a task or drag a task here.
     </div>
