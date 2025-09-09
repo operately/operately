9f6d7b2286c77c9eebe2d3f486089dd94e5518e6
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2025-09-09 11:36:14 -0300

    chore: Enable project description feature tests (#3377)
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- app/test/features/project_description_test.exs
+++ app/test/features/project_description_test.exs
@@ -10,48 +10,46 @@ defmodule Operately.Features.ProjectsDescriptionTest do
     {:ok, ctx}
   end
 
-  # TODO
-  # @tag login_as: :champion
-  # feature "writing a project description", ctx do
-  #   ctx
-  #   |> Steps.visit_project_page()
-  #   |> Steps.assert_project_description_absent()
-  #   |> Steps.submit_project_description(description: project_description())
-  #   |> Steps.expand_project_description()
-  #   |> Steps.assert_project_description_present(description: "TEXT END MARKER")
-  # end
+  @tag login_as: :champion
+  feature "writing a project description", ctx do
+    ctx
+    |> Steps.visit_project_page()
+    |> Steps.assert_project_description_absent()
+    |> Steps.submit_project_description(description: project_description())
+    |> Steps.expand_project_description()
+    |> Steps.assert_project_description_present(description: "TEXT END MARKER")
+  end
 
-  # TODO
-  # @tag login_as: :champion
-  # feature "editing a project description", ctx do
-  #   ctx
-  #   |> Steps.given_project_has_description(description: "Old description")
-  #   |> Steps.visit_project_page()
-  #   |> Steps.assert_project_description_present(description: "Old description")
-  #   |> Steps.edit_project_description(description: "New description")
-  #   |> Steps.assert_project_description_present(description: "New description")
-  # end
+  @tag login_as: :champion
+  feature "editing a project description", ctx do
+    ctx
+    |> Steps.given_project_has_description(description: "Old description")
+    |> Steps.visit_project_page()
+    |> Steps.assert_project_description_present(description: "Old description")
+    |> Steps.edit_project_description(description: "New description")
+    |> Steps.assert_project_description_present(description: "New description")
+  end
 
-  # defp project_description() do
-  #   """
-  #   SuperPace is an innovative project designed to track and quantify DevOps
-  #   TEXT START MARKER <- this is the start of the text
-  #   Research and Assessment (DORA) metrics for organizations across the globe. The
-  #   project's primary goal is to empower development and operations teams by
-  #   providing insightful, actionable data to drive performance and productivity
-  #   improvements.
+  defp project_description() do
+    """
+    SuperPace is an innovative project designed to track and quantify DevOps
+    TEXT START MARKER <- this is the start of the text
+    Research and Assessment (DORA) metrics for organizations across the globe. The
+    project's primary goal is to empower development and operations teams by
+    providing insightful, actionable data to drive performance and productivity
+    improvements.
 
-  #   DORA includes some fancy stuff that is mentioned in this line
+    DORA includes some fancy stuff that is mentioned in this line
 
-  #   SuperPace will do something called Y, instead of X
+    SuperPace will do something called Y, instead of X
 
-  #   SuperPace utilizes cutting-edge data collection and analytics technologies to
-  #   meticulously gather, measure, and interpret key DORA metrics, including
-  #   deployment frequency, lead time for changes, time to restore service, and
-  #   change failure rate. By translating these metrics into practical insights,
-  #   SuperPace fosters continuous learning, enhances collaboration, and accelerates
-  #   the pace of innovation in the complex, fast-paced world.
-  #   TEXT END MARKER <- this is the end of the text
-  #   """
-  # end
+    SuperPace utilizes cutting-edge data collection and analytics technologies to
+    meticulously gather, measure, and interpret key DORA metrics, including
+    deployment frequency, lead time for changes, time to restore service, and
+    change failure rate. By translating these metrics into practical insights,
+    SuperPace fosters continuous learning, enhances collaboration, and accelerates
+    the pace of innovation in the complex, fast-paced world.
+    TEXT END MARKER <- this is the end of the text
+    """
+  end
 end
