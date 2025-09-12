7209bde7c82d1f22d9427a0c1eb60383f082f974
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2025-09-12 09:45:55 -0300

    chore: Remove unused secondary Project pages (#3404)
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- app/assets/js/pages/ProjectEditProjectNamePage/index.tsx
+++
@@ -1,5 +0,0 @@
-import { PageModule } from "@/routes/types";
-import { Page } from "./page";
-import { loader } from "./loader";
-
-export default { name: "ProjectEditProjectNamePage", loader, Page } as PageModule;
