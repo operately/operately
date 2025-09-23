c06848f0d2cf77e54f7527fad69ebc4f8478defb
Igor Šarčević <igor@operately.com>
2025-09-23 20:11:05 +0200

    Apply loader changes everywhere
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- app/lib/operately_web/api/queries/get_assignments_count.ex
+++ app/lib/operately_web/api/queries/get_assignments_count.ex
@@ -9,20 +9,10 @@ defmodule OperatelyWeb.Api.Queries.GetAssignmentsCount do
   end
 
   def call(conn, _inputs) do
-    count = load_assignments_count(me(conn), company(conn))
+    me = me(conn)
+    company = company(conn)
 
-    {:ok, %{count: count}}
+    assignments = Loader.load(me, company)
+    {:ok, %{count: length(assignments)}}
   end
-
-  #
-  # Loading data
-  #
-
-  defp load_assignments_count(person, company) do
-    # Use the same logic as GetAssignments to ensure consistency
-    [mine: my_assignments, reports: _] = Loader.load(person, company)
-    length(my_assignments)
-  end
-
-
 end
