ca45beb9a214e2d37a7fc4a40f1abf5f3aa27521
Adriano Lazzaretti <lazzaretti136@gmail.com>
2025-09-26 08:57:49 -0300

    fix: Reopening Goal successfully updates its status
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- app/test/features/goal_closing_test.exs
+++ app/test/features/goal_closing_test.exs
@@ -28,4 +28,39 @@ defmodule Operately.Features.GoalClosingTest do
     |> Steps.visit_goal_closing_page()
     |> Steps.assert_no_active_projects_warning()
   end
+
+  feature "closing goal moves it to Completed tab in Work Map", ctx do
+    ctx
+    |> Steps.given_goal_with_no_projects_exists()
+    |> Steps.assert_open_goal_in_all_work_tab_in_work_map()
+    |> Steps.assert_open_goal_in_goals_tab_in_work_map()
+    |> Steps.refute_open_goal_in_completed_tab_in_work_map()
+    |> Steps.visit_goal_page()
+    |> Steps.close_goal()
+    |> Steps.assert_closed_goal_in_completed_tab_in_work_map()
+    |> Steps.refute_closed_goal_in_all_work_tab_in_work_map()
+    |> Steps.refute_closed_goal_in_goals_tab_in_work_map()
+  end
+
+  feature "reopening a goal moves it to All Work and Goals tabs in Work Map", ctx do
+    ctx
+    |> Steps.given_closed_goal_exists()
+    |> Steps.assert_closed_goal_in_completed_tab_in_work_map()
+    |> Steps.refute_closed_goal_in_all_work_tab_in_work_map()
+    |> Steps.refute_closed_goal_in_goals_tab_in_work_map()
+    |> Steps.visit_goal_page()
+    |> Steps.reopen_goal()
+    |> Steps.assert_open_goal_in_all_work_tab_in_work_map()
+    |> Steps.assert_open_goal_in_goals_tab_in_work_map()
+    |> Steps.refute_open_goal_in_completed_tab_in_work_map()
+  end
+
+  feature "goal is successfully reopened", ctx do
+    ctx
+    |> Steps.given_closed_goal_exists()
+    |> Steps.visit_goal_page()
+    |> Steps.assert_goal_is_closed()
+    |> Steps.reopen_goal()
+    |> Steps.assert_goal_is_reopened()
+  end
 end
