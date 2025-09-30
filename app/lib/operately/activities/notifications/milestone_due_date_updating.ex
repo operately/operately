21fa4784de9aa3839eb56f3274a6cd4757343fc9
Adriano Lazzaretti <lazzaretti136@gmail.com>
2025-09-30 10:45:07 -0300

    feat: Changing Milestone due date sends notifications
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- app/lib/operately/activities/notifications/milestone_due_date_updating.ex
+++ app/lib/operately/activities/notifications/milestone_due_date_updating.ex
@@ -1,6 +1,28 @@
 defmodule Operately.Activities.Notifications.MilestoneDueDateUpdating do
-  def dispatch(_activity) do
-    # Notification dispatcher for MilestoneDueDateUpdating not implemented yet
-    {:ok, []}
+  @moduledoc """
+  Notifies the following people:
+  - Project champion
+
+  The person who authored the comment is excluded from notifications.
+  """
+
+  alias Operately.Projects.Project
+
+  def dispatch(activity) do
+    project = Project.get!(:system, id: activity.content["project_id"], opts: [preload: :champion_contributor])
+
+    champion_id = if project.champion_contributor, do: project.champion_contributor.person_id, else: nil
+
+    [champion_id]
+    |> Enum.filter(fn id -> id != nil end)
+    |> Enum.filter(fn id -> id != activity.author_id end)
+    |> Enum.map(fn id ->
+      %{
+        person_id: id,
+        activity_id: activity.id,
+        should_send_email: true,
+      }
+    end)
+    |> Operately.Notifications.bulk_create()
   end
 end
--- app/lib/operately/activities/notifications/task_assignee_updating.ex
+++ app/lib/operately/activities/notifications/task_assignee_updating.ex
@@ -1,4 +1,12 @@
 defmodule Operately.Activities.Notifications.TaskAssigneeUpdating do
+  @moduledoc """
+  Notifies the following people:
+  - Previous assignee: Person who was assigned to work on the task
+  - New assignee: Person who is assigned to work on the task
+
+  The person who authored the comment is excluded from notifications.
+  """
+
   def dispatch(activity) do
     old_assignee_id = activity.content["old_assignee_id"]
     new_assignee_id = activity.content["new_assignee_id"]
