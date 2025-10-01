b514254c3bc7109ea5be4286d8b9e43da891d48f
Igor Šarčević <igor@operately.com>
2025-10-01 22:58:43 +0200

    Define all specs
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- app/test/support/features/invite_links_steps.ex
+++ app/test/support/features/invite_links_steps.ex
@@ -70,11 +70,15 @@ defmodule Operately.Support.Features.InviteLinksSteps do
   end
 
   step :follow_sign_up_and_join, ctx do
-    email = "hello@test.localhost"
-
     ctx
     |> UI.assert_text("Sign Up & Join")
     |> UI.click(testid: "sign-up-and-join")
+  end
+
+  step :sign_up_with_email, ctx do
+    email = "hello@test.localhost"
+
+    ctx
     |> UI.click(testid: "sign-up-with-email")
     |> UI.fill(testid: "email", with: email)
     |> UI.fill(testid: "name", with: "Michael")
@@ -93,6 +97,10 @@ defmodule Operately.Support.Features.InviteLinksSteps do
     |> UI.sleep(500)
   end
 
+  step :sign_up_with_google, ctx do
+    ctx |> UI.click(testid: "sign-up-with-google")
+  end
+
   step :assert_you_are_member_of_the_company, ctx do
     members = Operately.People.list_people(ctx.company.id)
     assert Enum.any?(members, fn member -> member.email == "hello@test.localhost" end)
@@ -106,92 +114,24 @@ defmodule Operately.Support.Features.InviteLinksSteps do
     |> UI.assert_has(testid: "company-home")
   end
 
-  # step :create_expired_invite_link, ctx do
-  #   {:ok, invite_link} =
-  #     Operately.InviteLinks.create_invite_link(%{
-  #       company_id: ctx.company.id,
-  #       author_id: ctx.creator.id,
-  #       expires_at: DateTime.add(DateTime.utc_now(), -1, :day)
-  #     })
+  step :follow_log_in_and_join, ctx do
+    ctx
+    |> UI.assert_text("Log In & Join")
+    |> UI.click(testid: "log-in-and-join")
+  end
 
-  #   Map.put(ctx, :invite_link, invite_link)
-  # end
+  step :log_in_with_google, _ctx do
+    raise "Not implemented yet"
+  end
 
-  # step :create_and_revoke_invite_link, ctx do
-  #   {:ok, invite_link} =
-  #     Operately.InviteLinks.create_invite_link(%{
-  #       company_id: ctx.company.id,
-  #       author_id: ctx.creator.id
-  #     })
-
-  #   {:ok, revoked_link} = Operately.InviteLinks.revoke_invite_link(invite_link)
-
-  #   Map.put(ctx, :invite_link, revoked_link)
-  # end
-
-  # step :visit_team_invite_page, ctx do
-  #   ctx
-  #   |> visit_page("/#{ctx.company.id}/invite-team")
-  # end
-
-  # step :visit_admin_manage_people_page, ctx do
-  #   ctx
-  #   |> visit_page("/#{ctx.company.id}/admin/manage-people")
-  # end
-
-  # step :visit_invite_join_page_with_token, ctx do
-  #   ctx
-  #   |> visit_page("/join/#{ctx.invite_link.token}")
-  # end
-
-  # step :visit_invite_join_page_with_invalid_token, ctx do
-  #   ctx
-  #   |> visit_page("/join/invalid-token-123")
-  # end
-
-  # step :generate_invite_link, ctx do
-  #   ctx
-  #   |> click(Query.button("Generate Invite Link"))
-  # end
-
-  # step :revoke_invite_link, ctx do
-  #   ctx
-  #   |> click(Query.button("Revoke"))
-  #   |> accept_browser_dialog()
-  # end
-
-  # step :assert_invite_team_page_loaded, ctx do
-  #   ctx
-  #   |> assert_has(Query.text("Invite Your Team"))
-  # end
-
-  # step :assert_invite_link_generated_successfully, ctx do
-  #   ctx
-  #   |> assert_has(Query.text("Invite link generated successfully!"))
-  #   |> assert_has(Query.text("Shareable Link"))
-  #   |> assert_has(Query.text("Message Template"))
-  # end
-
-  # step :assert_invite_links_section_visible, ctx do
-  #   ctx
-  #   |> assert_has(Query.text("Invite Links"))
-  #   |> assert_has(Query.text("Invite Link"))
-  #   |> assert_has(Query.text("Active"))
-  #   |> assert_has(Query.text("Created by #{ctx.creator.full_name}"))
-  # end
-
-  # step :assert_invite_link_revoked, ctx do
-  #   ctx
-  #   |> assert_has(Query.text("Revoked"))
-  #   |> refute_has(Query.button("Revoke"))
-  # end
-
-  # step :assert_welcome_page_with_invitation, ctx do
-  #   ctx
-  #   |> assert_has(Query.text("Welcome to Operately!"))
-  #   |> assert_has(Query.text("#{ctx.creator.full_name} invited you to join"))
-  #   |> assert_has(Query.text("Sign Up & Join"))
-  # end
+  step :log_in_with_email, ctx do
+    ctx
+    |> UI.click(testid: "log-in-with-email")
+    |> UI.fill(testid: "email", with: ctx.invited.email)
+    |> UI.fill(testid: "password", with: "password")
+    |> UI.click(testid: "submit")
+    |> UI.sleep(500)
+  end
 
   step :assert_expired_invite_link_message, ctx do
     ctx |> UI.assert_text("Expired Invitation")
