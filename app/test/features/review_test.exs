a596100e85b06393680012fc2b8676ac4b2777ee
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2025-10-03 16:47:20 -0300

    feat: Updating Milestone reloads review count (#3584)
    * feat: Updating Milestone reloads review count
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    * Add feature tests
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    * Improve subscription mantainability
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    * Fix tests
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    ---------
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- app/test/features/review_test.exs
+++ app/test/features/review_test.exs
@@ -50,6 +50,7 @@ defmodule Operately.Features.ReviewTest do
       |> Steps.given_there_are_due_goal_updates()
       |> Steps.given_there_are_submitted_project_check_ins()
       |> Steps.given_there_are_submitted_goal_updates()
+      |> Steps.visit_review_page()
       |> Steps.assert_the_review_item_count(is: 4)
       |> Steps.when_a_project_check_in_is_acknowledged()
       |> Steps.assert_the_review_item_count(is: 3)
@@ -146,9 +147,11 @@ defmodule Operately.Features.ReviewTest do
       |> Steps.given_there_are_due_goal_updates()
       |> Steps.given_there_are_submitted_project_check_ins()
       |> Steps.given_there_are_submitted_goal_updates()
+      |> Steps.visit_review_page()
       |> Steps.assert_the_review_item_count(is: 4)
       |> Steps.when_a_project_check_in_is_acknowledged()
       |> Steps.assert_the_review_item_count(is: 3)
+      |> Steps.visit_review_page()
       |> Steps.when_a_goal_update_is_acknowledged()
       |> Steps.assert_the_review_item_count(is: 2)
     end
@@ -156,6 +159,7 @@ defmodule Operately.Features.ReviewTest do
     feature "creating and deleting tasks updates counter", ctx do
       ctx
       |> Steps.given_there_are_due_project_check_ins()
+      |> Steps.visit_review_page()
       |> Steps.assert_the_review_item_count(is: 1)
       |> Steps.create_task()
       |> Steps.assert_the_review_item_count(is: 2)
@@ -167,6 +171,7 @@ defmodule Operately.Features.ReviewTest do
       ctx
       |> Steps.given_there_are_due_project_check_ins()
       |> Steps.given_there_are_tasks_without_assignee()
+      |> Steps.visit_review_page()
       |> Steps.assert_the_review_item_count(is: 1)
       |> Steps.change_task_assignee()
       |> Steps.assert_the_review_item_count(is: 2)
@@ -178,6 +183,7 @@ defmodule Operately.Features.ReviewTest do
       ctx
       |> Steps.given_there_are_due_project_check_ins()
       |> Steps.given_there_are_due_tasks()
+      |> Steps.visit_review_page()
       |> Steps.assert_the_review_item_count(is: 3)
       |> Steps.mark_task_as_completed()
       |> Steps.assert_the_review_item_count(is: 2)
@@ -185,6 +191,27 @@ defmodule Operately.Features.ReviewTest do
       |> Steps.assert_the_review_item_count(is: 3)
       |> Steps.mark_task_as_canceled()
       |> Steps.assert_the_review_item_count(is: 2)
+    end
+
+    feature "creating and deleting milestones updates counter", ctx do
+      ctx
+      |> Steps.given_there_are_due_project_check_ins()
+      |> Steps.visit_review_page()
+      |> Steps.assert_the_review_item_count(is: 1)
+      |> Steps.create_milestone()
+      |> Steps.assert_the_review_item_count(is: 2)
+      |> Steps.delete_milestone()
+      |> Steps.assert_the_review_item_count(is: 1)
+    end
+
+    feature "completing milestone updates counter", ctx do
+      ctx
+      |> Steps.given_there_are_due_project_check_ins()
+      |> Steps.given_there_are_due_milestones()
+      |> Steps.visit_review_page()
+      |> Steps.assert_the_review_item_count(is: 2)
+      |> Steps.complete_milestone()
+      |> Steps.assert_the_review_item_count(is: 1)
     end
   end
 end
