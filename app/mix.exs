9bea6337ca1880cf53a6768c6e071271453b2b8c
Igor Šarčević <igor@operately.com>
2025-10-14 13:29:52 +0200

    Update to elixir 1.18
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- app/mix.exs
+++ app/mix.exs
@@ -5,7 +5,7 @@ defmodule Operately.MixProject do
     [
       app: :operately,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
