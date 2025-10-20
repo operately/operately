6e191a235c410c1c26da637f6383d6094a657edd
Adriano Lazzaretti <lazzaretti136@gmail.com>
2025-10-20 13:42:10 -0300

    Update ProjectMilestoneCommented notification
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- app/lib/operately/activities/notifications/project_milestone_commented.ex
+++ app/lib/operately/activities/notifications/project_milestone_commented.ex
@@ -11,14 +11,15 @@ defmodule Operately.Activities.Notifications.ProjectMilestoneCommented do
 
   alias Operately.Repo
   alias Operately.Projects.Notifications
-  alias Operately.Projects.Milestone
+  alias Operately.Projects.{Milestone, Project}
 
   def dispatch(activity = %{ author_id: author_id }) do
-    with {:ok, milestone} <- fetch_milestone(activity.content["milestone_id"]) do
-      contribs = Operately.Projects.list_notification_subscribers(milestone.project_id, exclude: author_id)
-      subscriber_ids = Notifications.get_milestone_subscribers(milestone)
+    with {:ok, milestone} <- fetch_milestone(activity.content["milestone_id"]),
+         {:ok, project} <- fetch_project(activity.content["project_id"]) do
+      project_subscribers = Notifications.get_project_subscribers(project)
+      milestone_subscribers = Notifications.get_milestone_subscribers(milestone)
 
-      subscriber_ids ++ Enum.map(contribs, &(&1.id))
+      project_subscribers ++ milestone_subscribers
       |> Enum.uniq()
       |> Enum.filter(fn id -> id != author_id end)
       |> Enum.map(fn person_id ->
@@ -35,15 +36,25 @@ defmodule Operately.Activities.Notifications.ProjectMilestoneCommented do
   defp fetch_milestone(milestone_id) do
     case Milestone.get(:system, id: milestone_id, opts: [preload: [:project]]) do
       {:ok, milestone} ->
-        {:ok,
-         Repo.preload(milestone, [
-           :access_context,
-           subscription_list: :subscriptions
-         ])}
+        {:ok, milestone}
 
       {:error, reason} ->
         Logger.warning(
           "Unable to load milestone #{milestone_id} for comment notifications: #{inspect(reason)}"
+        )
+
+        {:error, reason}
+    end
+  end
+
+  defp fetch_project(project_id) do
+    case Project.get(:system, id: project_id) do
+      {:ok, project} ->
+        {:ok, project}
+
+      {:error, reason} ->
+        Logger.warning(
+          "Unable to load project #{project_id} for comment notifications: #{inspect(reason)}"
         )
 
         {:error, reason}
