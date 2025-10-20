6520f47f30ff1b5537ca181ace77d6ca6ea941e5
Adriano Lazzaretti <lazzaretti136@gmail.com>
2025-10-20 13:12:38 -0300

    Update ProjectKeyResourceAdded notification
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- app/lib/operately/activities/notifications/project_key_resource_added.ex
+++ app/lib/operately/activities/notifications/project_key_resource_added.ex
@@ -1,18 +1,21 @@
 defmodule Operately.Activities.Notifications.ProjectKeyResourceAdded do
-  alias Operately.{Notifications, Projects}
+  alias Operately.Projects.Notifications
 
   def dispatch(activity) do
-    author_id = activity.author_id
-    project_id = activity.content["project_id"]
+    {:ok, project} = Operately.Projects.Project.get(:system, id: activity.content["project_id"])
+    subscriber_ids = Notifications.get_project_subscribers(project, ignore: [activity.author_id])
 
-    Projects.list_notification_subscribers(project_id, exclude: author_id)
-    |> Enum.map(fn person ->
+    subscriber_ids
+    |> Enum.uniq_by(& &1)
+    |> Enum.filter(fn id -> id != nil end)
+    |> Enum.filter(fn id -> id != activity.author_id end)
+    |> Enum.map(fn id ->
       %{
-        person_id: person.id,
+        person_id: id,
         activity_id: activity.id,
         should_send_email: true,
       }
     end)
-    |> Notifications.bulk_create()
+    |> Operately.Notifications.bulk_create()
   end
 end
