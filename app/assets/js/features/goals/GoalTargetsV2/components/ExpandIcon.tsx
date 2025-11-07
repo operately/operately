d2bec65e41f618b269d4a982d5c71bf58f9a2d25
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2025-11-07 14:52:13 -0300

    fix: Improve Goal target toggle (#3796)
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- app/assets/js/features/goals/GoalTargetsV2/components/ExpandIcon.tsx
+++ app/assets/js/features/goals/GoalTargetsV2/components/ExpandIcon.tsx
@@ -5,12 +5,10 @@ import { IconChevronDown } from "turboui";
 interface Props {
   expanded: boolean;
   className?: string;
-
-  onClick: () => void;
 }
 
-export function ExpandIcon({ expanded, onClick, className }: Props) {
+export function ExpandIcon({ expanded, className }: Props) {
   className = classNames(className, "mt-2 transition-all", { "rotate-180": expanded });
 
-  return <IconChevronDown size={14} className={className} onClick={onClick} />;
+  return <IconChevronDown size={14} className={className} />;
 }
