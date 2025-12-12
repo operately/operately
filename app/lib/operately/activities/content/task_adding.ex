ec861534135916a6446124c6f0eb7f0e163cd7e6
Adriano Lazzaretti <lazzaretti136@gmail.com>
2025-12-12 09:22:35 -0300

    chore: API supports space tasks creation
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- app/lib/operately/activities/content/task_adding.ex
+++ app/lib/operately/activities/content/task_adding.ex
@@ -14,7 +14,7 @@ defmodule Operately.Activities.Content.TaskAdding do
   def changeset(attrs) do
     %__MODULE__{}
     |> cast(attrs, __schema__(:fields))
-    |> validate_required([:company_id, :space_id, :project_id, :task_id, :name])
+    |> validate_required([:company_id, :space_id, :task_id, :name])
   end
 
   def build(params) do
