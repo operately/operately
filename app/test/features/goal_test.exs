fadd84b1b70defa59fcf36e323902a5321847266
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2026-01-26 09:00:35 -0300

    chore: Add contributor tests for goal (#4080)
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- app/test/features/goal_test.exs
+++ app/test/features/goal_test.exs
@@ -201,4 +201,51 @@ defmodule Operately.Features.GoalTest do
       |> Steps.assert_goal_cannot_be_deleted()
     end
   end
+
+  describe "access management" do
+    feature "outside collaborator cannot see goal with comment access until admin grants access", ctx do
+      ctx
+      |> Steps.setup_goal_with_comment_access_and_outside_collaborator()
+      |> Steps.login_as_outside_collaborator()
+      |> Steps.visit_goal_page_as_collaborator()
+      |> Steps.assert_goal_page_not_found()
+      |> Steps.login_as_admin()
+      |> Steps.visit_goal_access_management_page()
+      |> Steps.give_collaborator_view_access()
+      |> Steps.login_as_outside_collaborator()
+      |> Steps.visit_goal_page_as_collaborator()
+      |> Steps.assert_goal_page_loaded()
+    end
+
+    feature "space member cannot create discussion with comment access until admin grants edit access", ctx do
+      ctx
+      |> Steps.setup_goal_with_comment_access_and_space_member()
+      |> Steps.login_as_space_member()
+      |> Steps.visit_goal_discussions_page_as_member()
+      |> Steps.assert_start_discussion_button_not_visible()
+      |> Steps.login_as_admin()
+      |> Steps.visit_goal_access_management_page()
+      |> Steps.give_member_edit_access()
+      |> Steps.login_as_space_member()
+      |> Steps.visit_goal_discussions_page_as_member()
+      |> Steps.assert_start_discussion_button_visible()
+      |> Steps.click_start_discussion_button()
+      |> Steps.assert_add_discussion_page_loaded()
+    end
+
+    feature "outside collaborator loses access when admin removes it", ctx do
+      ctx
+      |> Steps.setup_goal_with_comment_access_and_outside_collaborator()
+      |> Steps.login_as_admin()
+      |> Steps.visit_goal_access_management_page()
+      |> Steps.give_collaborator_view_access()
+      |> Steps.login_as_outside_collaborator()
+      |> Steps.visit_goal_page_as_collaborator()
+      |> Steps.assert_goal_page_loaded()
+      |> Steps.login_as_admin()
+      |> Steps.visit_goal_access_management_page()
+      |> Steps.remove_collaborator_access()
+      |> Steps.assert_collaborator_has_no_access()
+    end
+  end
 end
