718a008d6bba631919a64f79c8652afa464ae796
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2026-01-29 09:03:11 -0300

    fix: Adjust rotate_settings_key script (#4096)
    * Fix rotate-key script
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    * fix: Adjust rotate_settings_key script
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    ---------
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- app/lib/operately/system_settings/key_rotation.ex
+++ app/lib/operately/system_settings/key_rotation.ex
@@ -4,11 +4,16 @@ defmodule Operately.SystemSettings.KeyRotation do
   alias Operately.Repo
   alias Operately.SystemSettings
   alias Operately.SystemSettings.Settings
+  alias Ecto.Changeset
 
   def rotate do
     case SystemSettings.get() do
       nil -> {:error, :not_configured}
-      settings -> settings |> Settings.changeset(%{email_secrets: settings.email_secrets}) |> Repo.update()
+      settings ->
+        settings
+        |> Settings.changeset(%{})
+        |> Changeset.force_change(:email_secrets, settings.email_secrets)
+        |> Repo.update()
     end
   end
 end
