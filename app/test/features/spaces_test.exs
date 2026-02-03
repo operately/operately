bc748e40d7a3df24972d6e155f8b122324a2e149
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2026-02-03 14:42:06 -0300

    fix: Adjust permissions for Space page (#4114)
    * Add goal/project buttons are shown only if user has permissions
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    * Add files button only appears if users have permissions
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    * Add tests
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    ---------
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- app/test/features/spaces_test.exs
+++ app/test/features/spaces_test.exs
@@ -245,4 +245,42 @@ defmodule Operately.Features.SpacesTest do
     |> Steps.confirm_space_deletion()
     |> Steps.assert_space_deleted()
   end
+
+  describe "permissions" do
+    feature "user with full access can see all action", ctx do
+      ctx
+      |> Steps.given_user_has_full_access()
+      |> Steps.assert_user_has_full_access()
+      |> Steps.visit_page()
+      |> Steps.assert_edit_space_visible()
+      |> Steps.assert_configure_tools_and_delete_options_visible()
+      |> Steps.assert_manage_access_visible()
+    end
+
+    feature "user with edit access can see actions they can perform only", ctx do
+      ctx
+      |> Steps.given_user_has_edit_access()
+      |> Steps.assert_user_has_edit_access()
+      |> Steps.visit_page()
+      |> Steps.refute_edit_space_visible()
+      |> Steps.refute_configure_tools_and_delete_options_visible()
+      |> Steps.refute_manage_access_visible()
+      |> Steps.assert_user_can_add_projects_and_goals_zero_state()
+      |> Steps.assert_user_can_add_projects_and_goals_non_zero_state()
+      |> Steps.assert_user_can_post_discussion()
+      |> Steps.assert_user_can_add_resources()
+      |> Steps.assert_user_can_add_tasks()
+    end
+
+    feature "user with comment access can't see actions they can't perform", ctx do
+      ctx
+      |> Steps.given_user_has_comment_access()
+      |> Steps.assert_user_has_comment_access()
+      |> Steps.visit_page()
+      |> Steps.refute_user_can_add_projects_and_goals_zero_state()
+      |> Steps.refute_user_can_add_projects_and_goals_non_zero_state()
+      |> Steps.refute_user_can_post_discussion()
+      |> Steps.refute_user_can_add_resources()
+    end
+  end
 end
