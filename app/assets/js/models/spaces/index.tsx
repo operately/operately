1e34f28f611d240839009cf2334d45a4dc8920d2
Adriano Lazzaretti <lazzaretti136@gmail.com>
2026-02-09 15:55:50 -0300

    fix: Company Work Map allows adding only if user has permissions
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- app/assets/js/models/spaces/index.tsx
+++ app/assets/js/models/spaces/index.tsx
@@ -26,11 +26,11 @@ export async function getSpaces(params: api.GetSpacesInput): Promise<api.Space[]
   return await api.getSpaces(params).then((res) => res.spaces!);
 }
 
-export function useSpaceSearch(): SpaceField.SearchSpaceFn {
+export function useSpaceSearch(attrs?: { accessLevel?: api.AccessOptions }): SpaceField.SearchSpaceFn {
   const paths = usePaths();
 
   return async ({ query }: { query: string }): Promise<SpaceField.Space[]> => {
-    const data = await Api.spaces.search({ query: query });
+    const data = await Api.spaces.search({ query: query, accessLevel: attrs?.accessLevel });
 
     return data.spaces.map((space) => ({
       id: space.id,
