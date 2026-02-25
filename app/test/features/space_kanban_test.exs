3a0172ff3e1f2f3d08c3e794f52f4a20a3ccaf5d
Adriano Lazzaretti <lazzaretti136@gmail.com>
2026-02-25 13:44:00 -0300

    Add feature tests
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- app/test/features/space_kanban_test.exs
+++ app/test/features/space_kanban_test.exs
@@ -97,34 +97,6 @@ defmodule Operately.Features.SpaceKanbanTest do
     |> Steps.assert_task_removed(task_key: :second_task, status_value: new_status)
   end
 
-  feature "move a space task to a project from the slide-in sidebar", ctx do
-    source_status = hd(ctx.status_values)
-
-    ctx
-    |> Steps.visit_kanban_page()
-    |> Steps.assert_task_in_status(task_key: :task, status_value: source_status)
-    |> Steps.open_task_slide_in(:task)
-    |> Steps.move_task_to_project(project_name: ctx.destination_project.name)
-    |> Steps.assert_task_removed(task_key: :task, status_value: source_status)
-    |> Steps.visit_destination_project()
-    |> Steps.assert_task_present(task_key: :task)
-    |> Steps.assert_task_belongs_to_destination_project(task_key: :task, destination_project_key: :destination_project)
-  end
-
-  feature "move a space task to another space from the slide-in sidebar", ctx do
-    source_status = hd(ctx.status_values)
-
-    ctx
-    |> Steps.visit_kanban_page()
-    |> Steps.assert_task_in_status(task_key: :task, status_value: source_status)
-    |> Steps.open_task_slide_in(:task)
-    |> Steps.move_task_to_space(space_name: ctx.destination_space.name)
-    |> Steps.assert_task_removed(task_key: :task, status_value: source_status)
-    |> Steps.visit_destination_space_kanban_page()
-    |> Steps.assert_task_in_status(task_key: :task, status_value: source_status)
-    |> Steps.assert_task_belongs_to_destination_space(task_key: :task, destination_space_key: :destination_space)
-  end
-
   feature "cannot delete the last remaining status", ctx do
     statuses = Steps.single_status("Solo")
     status_params = Enum.map(statuses, &Map.from_struct/1)
@@ -153,6 +125,47 @@ defmodule Operately.Features.SpaceKanbanTest do
     |> Steps.assert_status_absent(value: primary_status)
     |> Steps.visit_kanban_page()
     |> Steps.assert_task_in_status(task_key: :task, status_value: replacement_status)
+  end
+
+  describe "move task" do
+    feature "move to a project from the slide-in sidebar", ctx do
+      source_status = hd(ctx.status_values)
+
+      ctx
+      |> Steps.visit_kanban_page()
+      |> Steps.assert_task_in_status(task_key: :task, status_value: source_status)
+      |> Steps.open_task_slide_in(:task)
+      |> Steps.move_task_to_project(project_name: ctx.destination_project.name)
+      |> Steps.assert_task_removed(task_key: :task, status_value: source_status)
+      |> Steps.visit_destination_project()
+      |> Steps.assert_task_present(task_key: :task)
+      |> Steps.assert_task_belongs_to_destination_project(task_key: :task, destination_project_key: :destination_project)
+    end
+
+    feature "move to another space from the slide-in sidebar", ctx do
+      source_status = hd(ctx.status_values)
+
+      ctx
+      |> Steps.visit_kanban_page()
+      |> Steps.assert_task_in_status(task_key: :task, status_value: source_status)
+      |> Steps.open_task_slide_in(:task)
+      |> Steps.move_task_to_space(space_name: ctx.destination_space.name)
+      |> Steps.assert_task_removed(task_key: :task, status_value: source_status)
+      |> Steps.visit_destination_space_kanban_page()
+      |> Steps.assert_task_in_status(task_key: :task, status_value: source_status)
+      |> Steps.assert_task_belongs_to_destination_space(task_key: :task, destination_space_key: :destination_space)
+    end
+
+    feature "current space not shown as option", ctx do
+      source_status = hd(ctx.status_values)
+
+      ctx
+      |> Steps.visit_kanban_page()
+      |> Steps.assert_task_in_status(task_key: :task, status_value: source_status)
+      |> Steps.open_task_slide_in(:task)
+      |> Steps.open_move_task_modal()
+      |> Steps.assert_only_destination_space_shown()
+    end
   end
 
   describe "activities" do
