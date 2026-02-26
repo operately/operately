a08e16eed7ebff6a2f5f0fe3489e6e5942d3d43d
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2026-02-26 13:35:19 -0300

    chore: Add route and token authentication for external API (#4202)
    * chore: Add route and token authentication for external API
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    * Handle Bearer tokens too
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    ---------
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

---
+++ app/lib/operately_web/api/external.ex
@@ -0,0 +1,12 @@
+defmodule OperatelyWeb.Api.External do
+  use TurboConnect.Api
+
+  plug(OperatelyWeb.Api.Plugs.RequireApiToken)
+  plug(OperatelyWeb.Api.Plugs.EnforceTokenAccessMode)
+
+  use_types(OperatelyWeb.Api.Types)
+
+  import OperatelyWeb.Api
+
+  define_all()
+end
