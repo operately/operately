b2a6e7a58dc3e53a13fc77aa20b8de2f74124727
Adriano Lazzaretti <80377510+Rockyy174@users.noreply.github.com>
2026-03-02 07:25:47 -0300

    chore: Improve email notifications for ProjectDescriptionChanged (#4212)
    * chore: Improve email notifications for ProjectDescriptionChanged
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    * Add unit tests
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    * Add feature tests
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>
    
    ---------
    
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- app/lib/operately/operations/project_description_updating.ex
+++ app/lib/operately/operations/project_description_updating.ex
@@ -3,10 +3,15 @@ defmodule Operately.Operations.ProjectDescriptionUpdating do
   alias Operately.Repo
   alias Operately.Activities
   alias Operately.Projects.Project
+  alias Operately.Operations.Notifications.Subscription
 
   def run(author, project, description) do
     Multi.new()
     |> Multi.update(:project, Project.changeset(project, %{description: description}))
+    |> Multi.run(:subscription_list, fn _repo, _changes ->
+      Operately.Notifications.SubscriptionList.get(:system, id: project.subscription_list_id, opts: [preload: :subscriptions])
+    end)
+    |> Subscription.update_mentioned_people(description)
     |> Activities.insert_sync(author.id, :project_description_changed, fn _ ->
       %{
         company_id: project.company_id,
