93e9ee7184adbdbf2d6eec9383ed67c39c66dc11
Adriano Lazzaretti <lazzaretti136@gmail.com>
2026-03-19 10:09:03 -0300

    ID decoder skips non-encoded IDs
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- app/lib/operately_web/api/types/ids.ex
+++ app/lib/operately_web/api/types/ids.ex
@@ -4,13 +4,22 @@ defmodule OperatelyWeb.Api.Types.Id do
   end
 
   def decode(id) when is_binary(id) do
-    remove_comments(id)
-    |> Operately.ShortUuid.decode()
-    |> handle_error()
+    if is_canonical_uuid?(id) do
+      {:ok, id}
+    else
+      id
+      |> remove_comments()
+      |> Operately.ShortUuid.decode()
+      |> handle_error()
+    end
   end
 
   defp remove_comments(id) do
     id |> String.split("-") |> List.last()
+  end
+
+  defp is_canonical_uuid?(id) do
+    String.match?(id, ~r/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i)
   end
 
   defp handle_error({:ok, id}), do: {:ok, id}
