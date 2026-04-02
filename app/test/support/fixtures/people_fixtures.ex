e2041acdf91451b77c8447f6fa7ab3fe64136626
Adriano Lazzaretti <lazzaretti136@gmail.com>
2026-04-02 13:24:37 -0300

    Person is created with correct defaults
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- app/test/support/fixtures/people_fixtures.ex
+++ app/test/support/fixtures/people_fixtures.ex
@@ -11,13 +11,6 @@ defmodule Operately.PeopleFixtures do
         full_name: "some full_name",
         title: "some title",
         email: "some-email@localhost",
-        preferences: %{
-          notifications: %{
-            send_daily_summary: true,
-            notify_on_mention: true,
-            notify_about_assignments: true
-          }
-        }
       })
       |> Operately.People.create_person()
 
