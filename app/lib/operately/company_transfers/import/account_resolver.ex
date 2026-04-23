f0377e98854bbf6e0f267dd8bd1ac5b10d2ad91b
Adriano Lazzaretti <lazzaretti136@gmail.com>
2026-04-22 22:47:47 -0300

    Improvements
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- app/lib/operately/company_transfers/import/account_resolver.ex
+++ app/lib/operately/company_transfers/import/account_resolver.ex
@@ -14,10 +14,10 @@ defmodule Operately.CompanyTransfers.Import.AccountResolver do
     end)
   end
 
-  def status_by_destination_account(%{resolved_accounts: resolved_accounts}) when is_map(resolved_accounts) do
-    Map.new(resolved_accounts, fn {_source_id, resolution} ->
-      {resolution.destination_account_id, resolution.status}
-    end)
+  def destination_account_ids(%{mapping: mapping}) when is_map(mapping) do
+    mapping
+    |> Map.values()
+    |> Enum.uniq()
   end
 
   defp resolve_account(%{"id" => source_id, "email" => email} = account_row, resolution) when is_binary(source_id) and is_binary(email) do
@@ -29,7 +29,6 @@ defmodule Operately.CompanyTransfers.Import.AccountResolver do
          %{
            resolution
            | mapping: Map.put(resolution.mapping, source_id, account.id),
-             resolved_accounts: put_resolution(resolution.resolved_accounts, source_id, account.id, normalized_email, :reused),
              reused_count: resolution.reused_count + 1
          }}
 
@@ -46,7 +45,6 @@ defmodule Operately.CompanyTransfers.Import.AccountResolver do
              %{
                resolution
                | mapping: Map.put(resolution.mapping, source_id, account.id),
-                 resolved_accounts: put_resolution(resolution.resolved_accounts, source_id, account.id, normalized_email, :created),
                  created_count: resolution.created_count + 1
              }}
 
@@ -63,18 +61,9 @@ defmodule Operately.CompanyTransfers.Import.AccountResolver do
   defp empty_resolution do
     %{
       mapping: %{},
-      resolved_accounts: %{},
       reused_count: 0,
       created_count: 0
     }
-  end
-
-  defp put_resolution(resolved_accounts, source_id, destination_account_id, email, status) do
-    Map.put(resolved_accounts, source_id, %{
-      destination_account_id: destination_account_id,
-      email: email,
-      status: status
-    })
   end
 
   defp random_password do
--- app/lib/operately/company_transfers/import/post_import_notifier.ex
+++ app/lib/operately/company_transfers/import/post_import_notifier.ex
@@ -12,15 +12,16 @@ defmodule Operately.CompanyTransfers.Import.PostImportNotifier do
   alias Operately.Companies.Company
   alias Operately.CompanyTransfers.Import.AccountResolver
   alias Operately.InviteLinks
+  alias Operately.People
   alias Operately.People.Person
   alias Operately.Repo
   alias OperatelyEmail.Emails.{CompanyMemberAddedEmail, GuestInvitedEmail}
 
   def notify(company_id, requested_by_id, account_resolution) when is_binary(company_id) and is_binary(requested_by_id) do
     company = Companies.get_company!(company_id)
-    account_statuses = AccountResolver.status_by_destination_account(account_resolution)
+    destination_account_ids = AccountResolver.destination_account_ids(account_resolution)
 
-    people = people_to_notify(company_id, requested_by_id, Map.keys(account_statuses))
+    people = people_to_notify(company_id, requested_by_id, destination_account_ids)
 
     if people == [] do
       :ok
@@ -28,7 +29,7 @@ defmodule Operately.CompanyTransfers.Import.PostImportNotifier do
       authors = author_candidates(company, requested_by_id)
 
       Enum.each(people, fn person ->
-        notify_person(person, company, authors, account_statuses)
+        notify_person(person, company, authors)
       end)
 
       :ok
@@ -68,11 +69,10 @@ defmodule Operately.CompanyTransfers.Import.PostImportNotifier do
     |> Enum.uniq_by(& &1.id)
   end
 
-  defp notify_person(%Person{} = person, %Company{} = company, authors, account_statuses) do
-    status = Map.get(account_statuses, person.account_id)
+  defp notify_person(%Person{} = person, %Company{} = company, authors) do
     author = Enum.find(authors, &(&1.id != person.id)) || person
 
-    case invite_link_for(person, company, author, status) do
+    case invite_link_for(person, company, author) do
       {:ok, invite_link} ->
         activity = email_activity(person, company, author, invite_link)
         deliver_email(email_module(person), person, activity)
@@ -82,24 +82,22 @@ defmodule Operately.CompanyTransfers.Import.PostImportNotifier do
     end
   end
 
-  defp invite_link_for(_person, _company, _author, :reused), do: {:ok, nil}
+  defp invite_link_for(person, company, author) do
+    if People.account_used?(person.email) do
+      {:ok, nil}
+    else
+      case InviteLinks.get_personal_invite_link_for_person(person.id) do
+        {:ok, invite_link} ->
+          InviteLinks.refresh_personal_invite_link(invite_link)
 
-  defp invite_link_for(person, company, author, :created) do
-    case InviteLinks.get_personal_invite_link_for_person(person.id) do
-      {:ok, invite_link} ->
-        InviteLinks.refresh_personal_invite_link(invite_link)
-
-      {:error, :not_found} ->
-        InviteLinks.create_personal_invite_link(%{
-          company_id: company.id,
-          author_id: author.id,
-          person_id: person.id
-        })
+        {:error, :not_found} ->
+          InviteLinks.create_personal_invite_link(%{
+            company_id: company.id,
+            author_id: author.id,
+            person_id: person.id
+          })
+      end
     end
-  end
-
-  defp invite_link_for(person, _company, _author, status) do
-    {:error, {:unsupported_account_resolution_status, person.id, status}}
   end
 
   defp email_activity(%Person{} = person, %Company{} = company, %Person{} = author, invite_link) do
