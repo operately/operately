d05d340643a95f9120397669008f4f1c495f0d53
Adriano Lazzaretti <lazzaretti136@gmail.com>
2026-05-06 12:11:40 -0300

    Add e2e tests
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

---
+++ app/test/cli_e2e/goals/create_test.exs
@@ -0,0 +1,35 @@
+defmodule Operately.CliE2E.Goals.CreateTest do
+  use Operately.CliE2ECase
+
+  alias Operately.Support.CliE2E.Goals.CreateSteps, as: Steps
+
+  setup ctx do
+    {:ok, Steps.setup(ctx)}
+  end
+
+  test "creates a goal with just a name", ctx do
+    ctx
+    |> Steps.create_goal_with_name("Q2 Revenue Growth")
+    |> Steps.assert_goal_created_successfully()
+  end
+
+  test "creates a goal with name and description", ctx do
+    ctx
+    |> Steps.create_goal_with_description(%{
+      name: "Improve Customer Retention",
+      description: "Focus on reducing churn and improving customer satisfaction"
+    })
+    |> Steps.assert_goal_created_successfully()
+    |> Steps.assert_goal_has_description()
+  end
+
+  test "creates a goal with markdown description", ctx do
+    ctx
+    |> Steps.create_goal_with_description(%{
+      name: "Product Roadmap Q2",
+      description: "Key initiatives:\n- Launch new dashboard\n- Improve API performance\n- Add analytics"
+    })
+    |> Steps.assert_goal_created_successfully()
+    |> Steps.assert_goal_has_description()
+  end
+end
