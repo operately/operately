6094efed38a8c10117761e4ded9641d43debf59e
Adriano Lazzaretti <lazzaretti136@gmail.com>
2026-05-08 16:57:18 -0300

    Fix old migration
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- app/priv/repo/migrations/20241201132900_confirm_all_previous_accounts.exs
+++ app/priv/repo/migrations/20241201132900_confirm_all_previous_accounts.exs
@@ -2,6 +2,6 @@ defmodule Operately.Repo.Migrations.ConfirmAllPreviousAccounts do
   use Ecto.Migration
 
   def change do
-    Operately.Repo.update_all(Operately.People.Account, set: [confirmed_at: DateTime.utc_now()])
+    execute "UPDATE accounts SET confirmed_at = NOW()"
   end
 end
