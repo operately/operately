ddc1a1ee25fc59310835e3f1f6091b08850bc71b
Adriano Lazzaretti <lazzaretti136@gmail.com>
2026-05-18 07:18:18 -0300

    feat: Remove export-company ff
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- app/assets/js/pages/CompanyExportPage/loader.tsx
+++ app/assets/js/pages/CompanyExportPage/loader.tsx
@@ -1,19 +1,11 @@
 import Api, { CompanyExportRun } from "@/api";
 import * as Pages from "@/components/Pages";
-import * as CompanyExports from "@/models/companyExports";
-import { Paths } from "@/routes/paths";
-import { redirectIfFeatureNotEnabled } from "@/routes/redirectUtils";
 
 interface LoaderResult {
   exportRuns: CompanyExportRun[];
 }
 
-export async function loader({ params }): Promise<LoaderResult> {
-  await redirectIfFeatureNotEnabled(params, {
-    feature: CompanyExports.FEATURE_NAME,
-    path: new Paths({ companyId: params.companyId! }).homePath(),
-  });
-
+export async function loader(): Promise<LoaderResult> {
   return {
     exportRuns: await Api.company_transfers.listExportRuns({}).then((res) => res.exportRuns),
   };
