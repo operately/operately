6a27f36f5bc1ac62e1d7b138ca7cbda38748bce2
Adriano Lazzaretti <lazzaretti136@gmail.com>
2026-05-28 14:49:28 -0300

    Improvements
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- app/lib/operately_web/plugs/https_redirect.ex
+++ app/lib/operately_web/plugs/https_redirect.ex
@@ -11,7 +11,7 @@ defmodule OperatelyWeb.Plugs.HttpsRedirect do
 
   def call(conn, _opts) do
     cond do
-      conn.scheme != :http -> conn
+      request_scheme(conn) != :http -> conn
       not https_enabled?() -> conn
       acme_challenge?(conn.request_path) -> conn
       localhost_health_check?(conn) -> conn
@@ -40,6 +40,22 @@ defmodule OperatelyWeb.Plugs.HttpsRedirect do
 
   defp redirect_status(method) when method in @safe_methods, do: 301
   defp redirect_status(_method), do: 307
+
+  defp request_scheme(conn) do
+    case forwarded_proto(conn) do
+      "https" -> :https
+      "http" -> :http
+      _ -> conn.scheme
+    end
+  end
+
+  defp forwarded_proto(conn) do
+    conn
+    |> get_req_header("x-forwarded-proto")
+    |> List.first()
+    |> first_forwarded_value()
+    |> normalize_forwarded_proto()
+  end
 
   defp redirect_url(conn) do
     endpoint_url = endpoint_url()
@@ -72,6 +88,12 @@ defmodule OperatelyWeb.Plugs.HttpsRedirect do
   defp join_paths(nil, request_path), do: request_path
   defp join_paths("", request_path), do: request_path
   defp join_paths(prefix, request_path), do: String.trim_trailing(prefix, "/") <> request_path
+
+  defp first_forwarded_value(nil), do: nil
+  defp first_forwarded_value(value), do: value |> String.split(",", parts: 2) |> List.first()
+
+  defp normalize_forwarded_proto(nil), do: nil
+  defp normalize_forwarded_proto(value), do: value |> String.trim() |> String.downcase()
 
   defp blank_to_nil(""), do: nil
   defp blank_to_nil(query), do: query
