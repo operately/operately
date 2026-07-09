b6c83b986b10acb63771e4a44e7d27f622b3faf1
Adriano Lazzaretti <lazzaretti136@gmail.com>
2026-07-09 20:51:11 -0300

    Adjustment
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- app/test/support/features/ui/rich_text.ex
+++ app/test/support/features/ui/rich_text.ex
@@ -1,7 +1,7 @@
 defmodule Operately.Support.Features.UI.RichText do
   alias Operately.People.Person
   alias Operately.Support.Features.UI
-  alias Wallaby.{Browser, Query}
+  alias Wallaby.{Browser, Element, Query, Session}
 
   def mention_person_in_rich_text(state, person_or_name) do
     search_text = mention_search_text(person_or_name)
@@ -16,7 +16,7 @@ defmodule Operately.Support.Features.UI.RichText do
 
       session = select_mention_option(session, person_or_name, editor_text)
 
-      wait_until_editor_contains_mention(session, editor_query, editor_text)
+      wait_until_editor_contains_selected_mention(session, editor_query, search_text, editor_text)
     end)
   end
 
@@ -27,41 +27,89 @@ defmodule Operately.Support.Features.UI.RichText do
   defp mention_editor_text(name) when is_binary(name), do: name
 
   defp select_mention_option(session, %Person{} = person, _editor_text) do
-    option_query = UI.query(testid: UI.testid(["mention-person", person.full_name]))
-
-    case Wallaby.Browser.retry(fn ->
-           case Browser.execute_query(session, option_query) do
-             {:ok, _} -> {:ok, session}
-             _ -> {:error, :not_yet}
-           end
-         end) do
-      {:ok, session} ->
-        Browser.click(session, option_query)
-
-      {:error, _} ->
-        raise "Timed out waiting for mention option '#{person.full_name}'"
-    end
-  end
-
-  defp select_mention_option(session, _name, editor_text) do
+    option_parent = option_query_parent(session)
+    option_query = Query.css("button, [role=\"option\"], [data-test-id^=\"mention-person-\"]", text: person.full_name)
+    testid_query = UI.query(testid: UI.testid(["mention-person", person.full_name]))
     editor_query = Query.css(".ProseMirror[contenteditable=true]")
 
-    case Wallaby.Browser.retry(fn ->
-           if Browser.has_text?(session, editor_text), do: {:ok, session}, else: {:error, :not_yet}
-         end) do
-      {:ok, session} ->
+    case wait_for_query(option_parent, option_query, attempts: mention_option_attempts()) do
+      :ok ->
         Browser.find(session, editor_query, fn element ->
           Browser.send_keys(element, [:enter])
         end)
 
-      {:error, _} ->
-        raise "Timed out waiting for mention option '#{editor_text}'"
+      :error ->
+        case wait_for_query(option_parent, testid_query, attempts: mention_option_attempts()) do
+          :ok ->
+            Browser.find(session, editor_query, fn element ->
+              Browser.send_keys(element, [:enter])
+            end)
+
+          :error ->
+            raise "Timed out waiting for mention option '#{person.full_name}'"
+        end
     end
   end
 
-  defp wait_until_editor_contains_mention(session, editor_query, editor_text) do
+  defp select_mention_option(session, _name, editor_text) do
+    select_mention_option_by_text(session, editor_text)
+  end
+
+  defp select_mention_option_by_text(session, text) do
+    editor_query = Query.css(".ProseMirror[contenteditable=true]")
+    option_parent = option_query_parent(session)
+
+    case wait_for_text(option_parent, text, attempts: mention_option_attempts()) do
+      :ok ->
+        Browser.find(session, editor_query, fn element ->
+          Browser.send_keys(element, [:enter])
+        end)
+
+      :error ->
+        raise "Timed out waiting for mention option '#{text}'"
+    end
+  end
+
+  defp mention_option_attempts, do: [25, 50, 100, 150, 250, 400, 800, 1600, 2400]
+
+  defp wait_for_query(session, query, attempts: []) do
+    if Browser.has?(session, query), do: :ok, else: :error
+  end
+
+  defp wait_for_query(session, query, attempts: [delay | remaining_attempts]) do
+    if Browser.has?(session, query) do
+      :ok
+    else
+      Process.sleep(delay)
+      wait_for_query(session, query, attempts: remaining_attempts)
+    end
+  end
+
+  defp wait_for_text(session, text, attempts: []) do
+    if Browser.has_text?(session, text), do: :ok, else: :error
+  end
+
+  defp wait_for_text(session, text, attempts: [delay | remaining_attempts]) do
+    if Browser.has_text?(session, text) do
+      :ok
+    else
+      Process.sleep(delay)
+      wait_for_text(session, text, attempts: remaining_attempts)
+    end
+  end
+
+  defp option_query_parent(%Element{session_url: session_url, driver: driver}) do
+    %Session{url: session_url, session_url: session_url, driver: driver}
+  end
+
+  defp option_query_parent(session), do: session
+
+  defp wait_until_editor_contains_selected_mention(session, editor_query, search_text, editor_text) do
     case Wallaby.Browser.retry(fn ->
-           if Browser.has_text?(session, editor_query, editor_text), do: {:ok, session}, else: {:error, :not_yet}
+           has_mention_text = Browser.has_text?(session, editor_query, editor_text)
+           still_has_search_text = Browser.has_text?(session, editor_query, "@#{search_text}")
+
+           if has_mention_text and not still_has_search_text, do: {:ok, session}, else: {:error, :not_yet}
          end) do
       {:ok, session} -> session
       {:error, _} -> raise "Timed out waiting for mention '#{editor_text}' in rich text editor"
