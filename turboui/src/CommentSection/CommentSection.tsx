8fd642e959be78717bb4e6ee112db1713a4f127d
Adriano Lazzaretti <lazzaretti136@gmail.com>
2026-08-12 17:15:31 -0300

    chore: Migrate CommentSection to TurboUI
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

---
+++ turboui/src/CommentSection/CommentSection.tsx
@@ -0,0 +1,143 @@
+import React, { useMemo } from "react";
+import { CommentInput } from "./CommentInput";
+import { CommentItem } from "./CommentItem";
+import { AcknowledgmentFeedRow, MilestoneCompletedFeedRow, MilestoneReopenedFeedRow } from "./FeedRows";
+import type { CommentFormState, CommentSectionItem, CommentSectionProps } from "./types";
+
+export function CommentSection({
+  items,
+  currentUser,
+  canComment,
+  submitting = false,
+  onAddComment,
+  onEditComment,
+  onDeleteComment,
+  onAddReaction,
+  onRemoveReaction,
+  richTextHandlers,
+  formattedTimePreferences,
+  commentParentType = "comment",
+  commentDraftKey,
+  editCommentDraftKey,
+  commentNotificationInfo,
+  ackLabel = "Check-In",
+  onCommentVisible,
+}: CommentSectionProps) {
+  const form = useMemo<CommentFormState>(
+    () => ({
+      items: [],
+      submitting,
+      postComment: onAddComment,
+      editComment: onEditComment,
+      deleteComment: onDeleteComment,
+      commentDraftKey,
+      editCommentDraftKey:
+        editCommentDraftKey ||
+        ((commentId: string) => (commentDraftKey ? `${commentDraftKey}:edit-comment:${commentId}` : undefined)),
+    }),
+    [submitting, onAddComment, onEditComment, onDeleteComment, commentDraftKey, editCommentDraftKey],
+  );
+
+  return (
+    <div className="flex flex-col">
+      {items.map((item, index) => (
+        <CommentSectionRow
+          key={commentSectionItemKey(item, index)}
+          item={item}
+          form={form}
+          currentUserId={currentUser.id}
+          canComment={canComment}
+          commentParentType={commentParentType}
+          richTextHandlers={richTextHandlers}
+          formattedTimePreferences={formattedTimePreferences}
+          onAddReaction={onAddReaction}
+          onRemoveReaction={onRemoveReaction}
+          onCommentVisible={onCommentVisible}
+          ackLabel={ackLabel}
+        />
+      ))}
+
+      {canComment && (
+        <CommentInput
+          form={form}
+          currentUser={currentUser}
+          richTextHandlers={richTextHandlers}
+          notificationInfo={commentNotificationInfo}
+        />
+      )}
+    </div>
+  );
+}
+
+function CommentSectionRow({
+  item,
+  form,
+  currentUserId,
+  canComment,
+  commentParentType,
+  richTextHandlers,
+  formattedTimePreferences,
+  onAddReaction,
+  onRemoveReaction,
+  onCommentVisible,
+  ackLabel,
+}: {
+  item: CommentSectionItem;
+  form: CommentFormState;
+  currentUserId: string;
+  canComment: boolean;
+  commentParentType: string;
+  richTextHandlers: CommentSectionProps["richTextHandlers"];
+  formattedTimePreferences: CommentSectionProps["formattedTimePreferences"];
+  onAddReaction?: CommentSectionProps["onAddReaction"];
+  onRemoveReaction?: CommentSectionProps["onRemoveReaction"];
+  onCommentVisible?: CommentSectionProps["onCommentVisible"];
+  ackLabel: string;
+}) {
+  switch (item.type) {
+    case "comment":
+      return (
+        <CommentItem
+          comment={item.value}
+          form={form}
+          commentParentType={commentParentType}
+          canComment={canComment}
+          currentUserId={currentUserId}
+          richTextHandlers={richTextHandlers}
+          onAddReaction={onAddReaction}
+          onRemoveReaction={onRemoveReaction}
+          formattedTimePreferences={formattedTimePreferences}
+          appearance="flat"
+          onVisible={onCommentVisible}
+        />
+      );
+
+    case "milestone-completed":
+      return <MilestoneCompletedFeedRow activity={item.value} formattedTimePreferences={formattedTimePreferences} />;
+
+    case "milestone-reopened":
+      return <MilestoneReopenedFeedRow activity={item.value} formattedTimePreferences={formattedTimePreferences} />;
+
+    case "acknowledgment":
+      return (
+        <AcknowledgmentFeedRow
+          person={item.value}
+          ackAt={item.insertedAt}
+          label={ackLabel}
+          formattedTimePreferences={formattedTimePreferences}
+        />
+      );
+  }
+}
+
+function commentSectionItemKey(item: CommentSectionItem, index: number): string {
+  if (item.type === "acknowledgment") {
+    return `acknowledgment-${item.insertedAt}`;
+  }
+
+  if (item.value?.id) {
+    return `${item.type}-${item.value.id}`;
+  }
+
+  return `${item.type}-${index}`;
+}
--- turboui/src/CommentSection/index.tsx
+++ turboui/src/CommentSection/index.tsx
@@ -1,11 +1,23 @@
+export { CommentSection } from "./CommentSection";
 export { CommentItem } from "./CommentItem";
 export { CommentInput } from "./CommentInput";
+export { useDraftActivatedInput } from "./useDraftActivatedInput";
+export { useCommentVisibility } from "./useCommentVisibility";
+export {
+  AcknowledgmentFeedRow,
+  MilestoneCompletedFeedRow,
+  MilestoneReopenedFeedRow,
+} from "./FeedRows";
 
 export type {
   Person,
   Comment,
   CommentItem as CommentItemType,
+  CommentSectionItem,
+  CommentSectionProps,
   CommentFormState,
   CommentItemProps,
   CommentInputProps,
+  CommentAppearance,
+  CommentNotificationInfo,
 } from "./types";
