b67d54e53e2a954b089352c0d661ad1179556b7c
Adriano Lazzaretti <lazzaretti136@gmail.com>
2026-08-21 19:43:51 -0300

    Add homepage+navgation story
    Signed-off-by: Adriano Lazzaretti <lazzaretti136@gmail.com>

--- turboui/src/CompanyNavigation/index.stories.tsx
+++ turboui/src/CompanyNavigation/index.stories.tsx
@@ -1,5 +1,8 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
+import React from "react";
 
+import { HomePage } from "../HomePage";
+import { defaultProps as homePageProps } from "../HomePage/mockData";
 import { CompanyNavigation } from "./index";
 import { defaultProps } from "./mockData";
 
@@ -33,4 +36,16 @@ export const RestrictedPermissions: Story = {
     canAddSpace: false,
     canInvitePeople: false,
   },
+};
+
+export const WithHomePage: Story = {
+  args: defaultProps,
+  render: () => (
+    <div className="flex flex-col h-screen">
+      <CompanyNavigation {...defaultProps} />
+      <div className="relative flex-1 min-h-0 overflow-y-auto">
+        <HomePage {...homePageProps} />
+      </div>
+    </div>
+  ),
 };
